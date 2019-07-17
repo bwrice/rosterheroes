494bc4cc38cce7c7806328248947c69cce997cab
Brian Rice <bwrice83@gmail.com>
2019-07-17 14:33:12 -0400

    get uuid from aggregate passing 2nd argument

--- app/Projectors/MeasurableProjector.php
+++ app/Projectors/MeasurableProjector.php
@@ -4,7 +4,7 @@ namespace App\Projectors;
 
 use App\Events\MeasurableCreationRequested;
 use App\Domain\Models\Measurable;
-use App\StorableEvents\HeroMeasurableCreated;
+use App\StorableEvents\MeasurableCreated;
 use Spatie\EventProjector\Projectors\Projector;
 use Spatie\EventProjector\Projectors\ProjectsEvents;
 
@@ -12,9 +12,11 @@ class MeasurableProjector implements Projector
 {
     use ProjectsEvents;
 
-    public function onMeasurableCreated(HeroMeasurableCreated $event)
+    public function onMeasurableCreated(MeasurableCreated $event, string $aggregateUuid)
     {
         Measurable::query()->create([
+            'uuid' => $aggregateUuid,
+            'measurable_type_id' => $event->measurableTypeID,
             'has_measurables_type' => $event->hasMeasurablesType,
             'has_measurables_id' => $event->hasMeasurablesID,
             'amount_raised' => $event->amountRaised
