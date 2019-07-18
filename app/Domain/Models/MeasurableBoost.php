48a3bc974ee7455113693cac9e167908fd08217d
Brian Rice <bwrice83@gmail.com>
2019-07-18 12:12:14 -0400

    add relation and custom collection override

--- app/Domain/Models/MeasurableBoost.php
+++ app/Domain/Models/MeasurableBoost.php
@@ -2,9 +2,28 @@
 
 namespace App\Domain\Models;
 
+use App\Domain\Collections\MeasurableBoostCollection;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class MeasurableBoost
+ * @package App\Domain\Models
+ *
+ * @property int $boost_level
+ *
+ * @property MeasurableType $measurableType
+ */
 class MeasurableBoost extends Model
 {
     protected $guarded = [];
+
+    public function newCollection(array $models = [])
+    {
+        return new MeasurableBoostCollection($models);
+    }
+
+    public function measurableType()
+    {
+        return $this->belongsTo(MeasurableType::class);
+    }
 }
