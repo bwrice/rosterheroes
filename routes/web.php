4d4c07f427624e5a3b4af420a6b0112c6b9f478f
Brian Rice <bwrice83@gmail.com>
2019-07-19 14:37:02 -0400

    add password routes

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,27 @@
 |
 */
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CommandCenterController;
 use App\Http\Controllers\SquadController;
 
+/*
+ * Login
+ */
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('login/google', [LoginController::class, 'redirectToProvider']);
 Route::get('login/google/callback', [LoginController::class, 'handleProviderCallback']);
+
+/*
+ * Password Reset
+ */
+Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ForgotPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ForgotPasswordController::class, 'reset'])->name('password.update');
 
 Route::get('/', function () {
     echo phpinfo();
