b42a1fb32715e1292c33ca4a5000be3b8ee1ef38
Brian Rice <bwrice83@gmail.com>
2019-07-20 19:52:51 -0400

    create PlayerSpiritSelection component

---
+++ resources/js/components/commandCenter/roster/PlayerSpiritSelection.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card>
+        <h3>{{hero.name}}</h3>
+        <v-btn v-on:click="unFocus">Cancel</v-btn>
+    </v-card>
+</template>
+
+<script>
+    import { mapActions } from 'vuex'
+
+    export default {
+        name: "PlayerSpiritSelection",
+        props: ['hero'],
+
+        methods: {
+            ...mapActions([
+                'setRosterFocusedHero'
+            ]),
+            unFocus: function() {
+                this.setRosterFocusedHero(null);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
