10649e200b056bc93423cc06e58ae8d25fe51fe1
Brian Rice <bwrice83@gmail.com>
2019-07-21 18:18:50 -0400

    test current week controller

---
+++ tests/Feature/CurrentWeekControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Domain\Models\Week;
+use Tests\TestCase;
+
+class CurrentWeekControllerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_will_return_the_current_week()
+    {
+        $this->withExceptionHandling();
+
+        /** @var Week $week */
+        $week = factory(Week::class)->create();
+        $week->wasRecentlyCreated = false; // set to false otherwise response returns 201
+        Week::setTestCurrent($week);
+
+        $response = $this->get('api/v1/week/current');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+            'data' => [
+                'uuid' => $week->uuid
+            ]
+        ]);
+    }
+}
