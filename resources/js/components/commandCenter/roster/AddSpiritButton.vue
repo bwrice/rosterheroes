be9dbd3a5a3c8b8316011a6d23674098417f39c0
Brian Rice <bwrice83@gmail.com>
2019-07-23 10:45:15 -0400

    create button for adding spirits to a hero

---
+++ resources/js/components/commandCenter/roster/AddSpiritButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-btn v-on:click="addSpirit">
+        Add
+    </v-btn>
+</template>
+
+<script>
+    export default {
+        name: "AddSpiritButton",
+        props: ['playerSpirit', 'hero'],
+        methods: {
+            addSpirit: function() {
+                alert(this.playerSpirit.player.first_name);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
