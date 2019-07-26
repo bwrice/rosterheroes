92dd160467f2d06e71145e8581e41f29f2be24ce
Brian Rice <bwrice83@gmail.com>
2019-07-25 23:23:14 -0400

    add tests to confirm for availableEssence() getter

---
+++ tests/Unit/HeroUnitTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Domain\Models\Hero;
+use App\Domain\Models\HeroClass;
+use App\Domain\Models\HeroPost;
+use App\Domain\Models\PlayerSpirit;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class HeroUnitTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @test
+     */
+    public function it_will_return_the_correct_essence_available_when_other_heroes_have_spirits()
+    {
+        /** @var Hero $hero */
+        $hero = factory(Hero::class)->create();
+        /** @var HeroPost $heroPost */
+        $heroPost = factory(HeroPost::class)->create([
+            'hero_id' => $hero->id
+        ]);
+
+        $squad = $heroPost->squad;
+        $squad->spirit_essence = 30000;
+        $squad->save();
+
+        $otherHeroSpiritEssenceUsed = 6000;
+        $playerSpirit = factory(PlayerSpirit::class)->create([
+            'essence_cost' => $otherHeroSpiritEssenceUsed
+        ]);
+
+        /** @var Hero $otherHero */
+        $otherHero = factory(Hero::class)->create([
+            'player_spirit_id' => $playerSpirit->id
+        ]);
+        /** @var HeroPost $otherHeroPost */
+        $otherHeroPost = factory(HeroPost::class)->create([
+            'hero_id' => $otherHero->id,
+            'squad_id' => $squad->id
+        ]);
+
+        $this->assertEquals($squad->spirit_essence - $otherHeroSpiritEssenceUsed, $hero->availableEssence());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_return_the_correct_essence_available_when_it_has_a_spirit_already()
+    {
+        $otherHeroSpiritEssenceUsed = 6000;
+        $playerSpirit = factory(PlayerSpirit::class)->create([
+            'essence_cost' => $otherHeroSpiritEssenceUsed
+        ]);
+
+        /** @var Hero $hero */
+        $hero = factory(Hero::class)->create([
+            'player_spirit_id' => $playerSpirit->id
+        ]);
+
+        /** @var HeroPost $heroPost */
+        $heroPost = factory(HeroPost::class)->create([
+            'hero_id' => $hero->id
+        ]);
+
+        $squad = $heroPost->squad;
+        $squad->spirit_essence = 30000;
+        $squad->save();
+
+        $this->assertEquals($squad->spirit_essence, $hero->availableEssence());
+    }
+}
