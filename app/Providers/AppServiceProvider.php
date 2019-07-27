c86340f835087d3dea51a8427ba185bedc505484
Brian Rice <bwrice83@gmail.com>
2019-07-27 17:14:54 -0400

    disable setting the current week

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->setTheCurrentWeek();
+//        $this->setTheCurrentWeek();
 
         Date::use(CarbonImmutable::class);
     }
