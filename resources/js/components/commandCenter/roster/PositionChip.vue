6f3bfe1298e4155e2d3678830aff063b7ea4cc9e
Brian Rice <bwrice83@gmail.com>
2019-07-26 20:27:57 -0400

    create PositionChip component

---
+++ resources/js/components/commandCenter/roster/PositionChip.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-chip :color="color" text-color="#FFFFFF">{{position.abbreviation}}</v-chip>
+</template>
+
+<script>
+    export default {
+        name: "PositionChip",
+        props: ['position'],
+        computed: {
+            color: function() {
+                switch(this.position.sport_id) {
+                    case 1:
+                        return 'success';
+                    case 2:
+                        return 'warning';
+                    case 3:
+                        return 'primary';
+                    case 4:
+                    default:
+                        return 'error';
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
