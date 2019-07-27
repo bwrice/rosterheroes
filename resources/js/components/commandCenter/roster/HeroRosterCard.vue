2b21730cad9da4e5974373ad4c7b0f496f02b517
Brian Rice <bwrice83@gmail.com>
2019-07-26 21:08:52 -0400

    render position chips inside HeroRoserCard component

--- resources/js/components/commandCenter/roster/HeroRosterCard.vue
+++ resources/js/components/commandCenter/roster/HeroRosterCard.vue
@@ -2,6 +2,7 @@
     <v-card>
         <v-card-title>
             <h3>{{hero.name}}</h3>
+            <PositionChip v-for="position in heroPositions" v-bind:key="position.id" :position="position"></PositionChip>
         </v-card-title>
         <slot name="body">
             <!-- Body Content-->
@@ -10,9 +11,18 @@
 </template>
 
 <script>
+    import PositionChip from "./PositionChip";
+
     export default {
         name: "HeroRosterCard",
+        components: {PositionChip},
         props: ['hero'],
+
+        computed: {
+            heroPositions: function() {
+                return this.hero.heroRace.positions;
+            }
+        }
     }
 </script>
 
