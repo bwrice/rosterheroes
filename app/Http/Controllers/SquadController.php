3012c44b962ddc3ceb601c62f2d969a55916d97d
Brian Rice <bwrice83@gmail.com>
2019-07-27 18:54:24 -0400

    return squad resouce with no relations

--- app/Http/Controllers/SquadController.php
+++ app/Http/Controllers/SquadController.php
@@ -59,6 +59,7 @@ class SquadController extends Controller
     public function show($squadSlug)
     {
         $squad = Squad::slugOrFail($squadSlug);
+        return new SquadResource($squad);
         return new SquadResource($squad->loadMissing([
             'heroPosts.hero.heroRace.positions',
             'heroPosts.hero.heroClass',
