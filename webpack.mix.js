e3d476d48239bb5a6912f1d2eeee99f38b39cc61
Brian Rice <bwrice83@gmail.com>
2019-07-30 21:50:25 -0400

    remove no longer used vuetify app.styl

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/createSquad.js', 'public/js').version();
 mix.js('resources/js/commandCenter.js', 'public/js').version();
 
-mix.stylus('resources/stylus/app.styl', 'public/css').version();
+// mix.stylus('resources/stylus/app.styl', 'public/css').version();
