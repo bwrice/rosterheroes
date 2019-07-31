56c948b10163e236aa69c2f7614a7481d57b7925
Brian Rice <bwrice83@gmail.com>
2019-07-31 18:09:44 -0400

    create BaseView component

---
+++ resources/js/components/commandCenter/views/BaseView.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container class="pa-1">
+        <v-layout>
+            <router-view></router-view>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "BaseView"
+    }
+</script>
+
+<style scoped>
+
+</style>
