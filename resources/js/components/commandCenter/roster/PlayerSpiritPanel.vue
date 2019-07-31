de941e11314f56429907629962929bd7a551c7b0
Brian Rice <bwrice83@gmail.com>
2019-07-31 18:47:51 -0400

    convert PlayerSpiritPanel component to grid

--- resources/js/components/commandCenter/roster/PlayerSpiritPanel.vue
+++ resources/js/components/commandCenter/roster/PlayerSpiritPanel.vue
@@ -1,33 +1,27 @@
 <template>
-    <v-card color="blue-grey darken-1">
+    <v-card>
         <v-card-title class="primary-title">
-            <div>
-                <h4 class="headline mb-0">{{ playerName }}</h4>
-                <v-layout class="row wrap">
-                    <v-flex class="xs8">
-                        <v-layout class="row wrap">
-                            <v-flex class="xs12">
-                                {{ gameDescription }}
-                            </v-flex>
-                            <v-flex class="xs12">
-                                Essence Cost: {{ essenceCost }}
-                            </v-flex>
-                            <v-flex class="xs12">
-                                Energy: {{ energy }}
-                            </v-flex>
-                            <v-flex class="xs12">
-                                <PositionChipList :positions="playerSpiritPositions"></PositionChipList>
-                            </v-flex>
-                        </v-layout>
-                    </v-flex>
-                    <v-flex class="xs4">
-                        <slot name="spirit-actions">
-                            <!-- Slotted Spirit Actions -->
-                        </slot>
-                    </v-flex>
-                </v-layout>
-            </div>
+            {{ playerName }}
         </v-card-title>
+        <v-card-text>
+            <v-layout>
+                <v-flex class="xs8">
+                    <p class="ma-0">{{ gameDescription }}</p>
+                    <p class="ma-0">Essence Cost: {{ essenceCost }}</p>
+                    <p class="ma-0">Energy: {{ energy }}</p>
+                </v-flex>
+                <v-flex class="xs4">
+                    <slot name="spirit-actions">
+                        <!-- Slotted Spirit Actions -->
+                    </slot>
+                </v-flex>
+            </v-layout>
+            <v-layout>
+                <v-flex class="xs12">
+                    <PositionChipList :positions="playerSpiritPositions"></PositionChipList>
+                </v-flex>
+            </v-layout>
+        </v-card-text>
     </v-card>
 </template>
 
