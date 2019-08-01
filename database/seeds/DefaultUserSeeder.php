0256c2d43d602d46793d5bde2510714fb810e310
Brian Rice <bwrice83@gmail.com>
2019-08-01 16:12:55 -0400

    create seeder to bootstrap a default user

---
+++ database/seeds/DefaultUserSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Domain\Actions\AddNewHeroToSquadAction;
+use App\Domain\Actions\CreateSquadAction;
+use App\Domain\Actions\CreateUserAction;
+use App\Domain\Models\HeroClass;
+use App\Domain\Models\HeroRace;
+use Illuminate\Database\Seeder;
+
+class DefaultUserSeeder extends Seeder
+{
+    /**
+     * @param CreateUserAction $createUserAction
+     * @param CreateSquadAction $createSquadAction
+     * @param AddNewHeroToSquadAction $addNewHeroToSquadAction
+     * @throws \App\Exceptions\HeroPostNotFoundException
+     * @throws \App\Exceptions\InvalidHeroClassException
+     */
+    public function run(CreateUserAction $createUserAction, CreateSquadAction $createSquadAction, AddNewHeroToSquadAction $addNewHeroToSquadAction)
+    {
+        $user = $createUserAction->execute('bwrice83@gmail.com', 'Brian Rice', 'password');
+        $squad = $createSquadAction->execute($user->id, 'My Squad');
+
+        $heroes = [
+            [
+                'name' => 'My Human',
+                'hero_race' => HeroRace::human(),
+                'hero_class' => HeroClass::ranger()
+            ],
+            [
+                'name' => 'My Elf',
+                'hero_race' => HeroRace::elf(),
+                'hero_class' => HeroClass::sorcerer()
+            ],
+            [
+                'name' => 'My Dwarf',
+                'hero_race' => HeroRace::dwarf(),
+                'hero_class' => HeroClass::warrior()
+            ],
+            [
+                'name' => 'My Orc',
+                'hero_race' => HeroRace::orc(),
+                'hero_class' => HeroClass::warrior()
+            ]
+        ];
+
+        foreach ($heroes as $hero) {
+            $addNewHeroToSquadAction->execute($squad, $hero['name'], $hero['hero_class'], $hero['hero_race']);
+        }
+    }
+}
