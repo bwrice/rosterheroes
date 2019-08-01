c965a554fd95967601da7b26ac09e2462c193e56
Brian Rice <bwrice83@gmail.com>
2019-07-31 23:49:49 -0400

    create Map base view component

---
+++ resources/js/components/commandCenter/views/map/Map.vue
@@ -0,0 +1,18 @@
+<template>
+    <BaseView></BaseView>
+</template>
+
+<script>
+    import BaseView from "../BaseView";
+    export default {
+        name: "Map",
+        components: {BaseView},
+        mounted: function() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
