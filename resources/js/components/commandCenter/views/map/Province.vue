4d03b6e88bc967269f68c40e6c2a6b240f5b9d1e
Brian Rice <bwrice83@gmail.com>
2019-08-01 11:18:45 -0400

    create Province vue component

---
+++ resources/js/components/commandCenter/views/map/Province.vue
@@ -0,0 +1,22 @@
+<template>
+    <g v-html="province.vector_paths">
+    </g>
+</template>
+
+<script>
+    export default {
+        name: "Province",
+        props: {
+            province: {
+                required: true
+            }
+        },
+        computed: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
