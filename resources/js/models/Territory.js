8a669cf75591a72c362124723f26bd57e57d86e2
Brian Rice <bwrice83@gmail.com>
2019-08-01 23:57:06 -0400

    fix Territory js model resource

--- resources/js/models/Territory.js
+++ resources/js/models/Territory.js
@@ -2,6 +2,10 @@ import Model from './Model'
 
 export default class Territory extends Model {
 
+    resource() {
+        return 'territories';
+    }
+
     primaryKey() {
         return 'name';
     }
