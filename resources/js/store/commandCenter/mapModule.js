f47819558ea01c3e4ab342992fea26ec327c833b
Brian Rice <bwrice83@gmail.com>
2019-08-01 21:25:50 -0400

    update map vuex store to handle territories and continents

--- resources/js/store/commandCenter/mapModule.js
+++ resources/js/store/commandCenter/mapModule.js
@@ -1,26 +1,46 @@
 import Province from "../../models/Province";
+import Territory from "../../models/Territory";
+import Continent from "../../models/Continent";
 
 export default {
 
     state: {
-        provinces: []
+        provinces: [],
+        territories: [],
+        continents: []
     },
 
     getters: {
         _provinces(state) {
             return state.provinces;
+        },
+        _territories(state) {
+            return state.territories;
+        },
+        _continents(state) {
+            return state.continents;
         }
     },
     mutations: {
         UPDATE_PROVINCES(state, payload) {
             state.provinces = payload;
+        },
+        UPDATE_TERRITORIES(state, payload) {
+            state.territories = payload;
+        },
+        UPDATE_CONTINENTS(state, payload) {
+            state.continents = payload;
         }
     },
 
     actions: {
         async setMap({commit}) {
             let provinces = await Province.$get();
-            commit('UPDATE_PROVINCES', provinces)
+            commit('UPDATE_PROVINCES', provinces);
+            let territories = await Territory.$get();
+            commit('UPDATE_TERRITORIES', territories);
+            let continents = await Continent.$get();
+            commit('UPDATE_CONTINENTS', continents);
         },
     }
 };
