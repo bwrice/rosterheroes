1ed8967d91219bc559a4e1f5eb14252a63f92c78
Brian Rice <bwrice83@gmail.com>
2019-08-03 19:08:52 -0400

    move map-mode butotns to footer-content slot

--- resources/js/components/commandCenter/views/map/MapMain.vue
+++ resources/js/components/commandCenter/views/map/MapMain.vue
@@ -2,34 +2,38 @@
     <v-flex class="xs12 lg8 offset-lg2">
         <v-layout>
             <v-flex class="xs12">
-                <Realm v-if="inContinentMode">
-                    <ContinentVector v-for="(continent, id) in this._continents" :key="id" :continent="continent"></ContinentVector>
+                <Realm>
+                    <template v-if="inContinentMode">
+                        <ContinentVector v-for="(continent, id) in this._continents" :key="id" :continent="continent"></ContinentVector>
+                    </template>
+                    <template v-else>
+                        <TerritoryVector v-for="(territory, id) in this._territories" :key="id" :territory="territory"></TerritoryVector>
+                    </template>
+                    <template slot="footer-content">
+                        <v-layout>
+                            <v-flex class="xs12">
+                                <v-chip class="ma-1"
+                                        :input-value="inContinentMode"
+                                        @click="setRealmMapMode('continent')"
+                                        filter
+                                        filter-icon="mdi-eye"
+                                        label
+                                >
+                                    Continents
+                                </v-chip>
+                                <v-chip class="ma-1"
+                                        :input-value="inTerritoryMode"
+                                        @click="setRealmMapMode('territory')"
+                                        filter
+                                        filter-icon="mdi-eye"
+                                        label
+                                >
+                                    Territories
+                                </v-chip>
+                            </v-flex>
+                        </v-layout>
+                    </template>
                 </Realm>
-                <Realm v-else>
-                    <TerritoryVector v-for="(territory, id) in this._territories" :key="id" :territory="territory"></TerritoryVector>
-                </Realm>
-            </v-flex>
-        </v-layout>
-        <v-layout>
-            <v-flex class="xs12 pa-2">
-                <v-chip
-                        :input-value="inContinentMode"
-                        @click="setRealmMapMode('continent')"
-                        filter
-                        filter-icon="mdi-eye"
-                        label
-                >
-                    Continents
-                </v-chip>
-                <v-chip
-                        :input-value="inTerritoryMode"
-                        @click="setRealmMapMode('territory')"
-                        filter
-                        filter-icon="mdi-eye"
-                        label
-                >
-                    Territories
-                </v-chip>
             </v-flex>
         </v-layout>
     </v-flex>
