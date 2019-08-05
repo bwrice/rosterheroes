994f5e9f5c78418b1ac4dc67f069c6bc6c51f567
Brian Rice <bwrice83@gmail.com>
2019-08-05 12:23:49 -0400

    create border cost calculator class

---
+++ app/Domain/Services/Travel/BorderTravelCostCalculator.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Domain\Services\Travel;
+
+
+use App\Domain\Interfaces\TravelsBorders;
+use App\Domain\Models\Province;
+
+class BorderTravelCostCalculator
+{
+    public function goldCost(TravelsBorders $travelsBorders, Province $border)
+    {
+        if ($travelsBorders->borderTravelIsFree($border)) {
+            return 0;
+        }
+
+        // TODO expense calculation
+        return 0;
+    }
+}
