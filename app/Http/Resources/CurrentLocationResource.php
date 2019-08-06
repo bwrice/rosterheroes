3fda25042f64ddaed7efd0493c84f8b449ed89ef
Brian Rice <bwrice83@gmail.com>
2019-08-06 11:08:38 -0400

    create new CurrentLocationResource that extends ProvinceResource

---
+++ app/Http/Resources/CurrentLocationResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+class CurrentLocationResource extends ProvinceResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $parent = parent::toArray($request);
+
+        $data = [
+            'borders' => $this->whenLoaded('borders'),
+            'squads_count' => $this->squads_count
+        ];
+
+        return array_merge($parent, $data);
+    }
+}
