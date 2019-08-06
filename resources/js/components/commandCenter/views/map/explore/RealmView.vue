712b7d04172fd4b6b64c24fa54bab819350d24d6
Brian Rice <bwrice83@gmail.com>
2019-08-06 14:16:34 -0400

    add realm view for default explore route

---
+++ resources/js/components/commandCenter/views/map/explore/RealmView.vue
@@ -0,0 +1,84 @@
+<template>
+    <MapCard>
+        <template v-if="inContinentMode">
+            <ContinentVector v-for="(continent, id) in this._continents" :key="id" :continent="continent"></ContinentVector>
+        </template>
+        <template v-else>
+            <TerritoryVector v-for="(territory, id) in this._territories" :key="id" :territory="territory"></TerritoryVector>
+        </template>
+        <template slot="footer-content">
+            <v-layout>
+                <v-flex class="xs12">
+                    <v-chip class="ma-1"
+                            :input-value="inContinentMode"
+                            @click="setRealmMapMode('continent')"
+                            filter
+                            filter-icon="mdi-eye"
+                            label
+                    >
+                        Continents
+                    </v-chip>
+                    <v-chip class="ma-1"
+                            :input-value="inTerritoryMode"
+                            @click="setRealmMapMode('territory')"
+                            filter
+                            filter-icon="mdi-eye"
+                            label
+                    >
+                        Territories
+                    </v-chip>
+                </v-flex>
+            </v-layout>
+        </template>
+    </MapCard>
+</template>
+
+<script>
+
+    import {mapGetters} from 'vuex';
+    import {mapActions} from 'vuex';
+
+    import MapCard from "../../../map/MapCard";
+    import ContinentVector from "../../../map/ContinentVector";
+    import TerritoryVector from "../../../map/TerritoryVector";
+
+    export default {
+        name: "RealmView",
+        components: {
+            TerritoryVector,
+            ContinentVector,
+            MapCard
+        },
+
+        data: function() {
+            return {
+                mode: 'continent'
+            }
+        },
+
+        methods: {
+            ...mapActions([
+                'setRealmMapMode',
+            ]),
+        },
+
+        computed: {
+            ...mapGetters([
+                '_provinces',
+                '_territories',
+                '_continents',
+                '_realmMapMode'
+            ]),
+            inContinentMode: function() {
+                return this._realmMapMode === 'continent';
+            },
+            inTerritoryMode: function() {
+                return this._realmMapMode === 'territory';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/routes/mapRoutes.js
+++ resources/js/routes/mapRoutes.js
@@ -1,10 +1,11 @@
 import MapMain from "../components/commandCenter/views/map/MapMain";
 import MapBase from "../components/commandCenter/views/map/MapBase";
 import MapNavigationDrawer from "../components/commandCenter/views/map/MapNavigationDrawer";
-import ContinentView from "../components/commandCenter/views/map/ContinentView";
-import TerritoryView from "../components/commandCenter/views/map/TerritoryView";
-import ProvinceView from "../components/commandCenter/views/map/ProvinceView";
-import ExploreView from "../components/commandCenter/views/map/ExploreView";
+import ContinentView from "../components/commandCenter/views/map/explore/ContinentView";
+import TerritoryView from "../components/commandCenter/views/map/explore/TerritoryView";
+import ProvinceView from "../components/commandCenter/views/map/explore/ProvinceView";
+import ExploreView from "../components/commandCenter/views/map/explore/ExploreView";
+import RealmView from "../components/commandCenter/views/map/explore/RealmView";
 
 export const mapRoutes = {
     path: '/command-center/:squadSlug/map',
@@ -13,7 +14,10 @@ export const mapRoutes = {
     },
     components: {
         default: MapBase,
-        drawer: MapNavigationDrawer
+        drawer: MapNavigationDrawer,
+        meta: {
+            footerButton: 'map'
+        }
     },
     children: [
         {
@@ -27,34 +31,44 @@ export const mapRoutes = {
         {
             path: 'explore',
             component: ExploreView,
-            name: 'map-explore',
+            name: 'explore',
             meta: {
                 footerButton: 'map'
-            }
+            },
+            children: [
+                {
+                    path: '',
+                    component: RealmView,
+                    name: 'explore-realm',
+                    meta: {
+                        footerButton: 'map'
+                    }
+                },
+                {
+                    path: 'continents/:continentSlug',
+                    component: ContinentView,
+                    name: 'explore-continent',
+                    meta: {
+                        footerButton: 'map'
+                    }
+                },
+                {
+                    path: 'territories/:territorySlug',
+                    component: TerritoryView,
+                    name: 'explore-territory',
+                    meta: {
+                        footerButton: 'map'
+                    }
+                },
+                {
+                    path: 'provinces/:provinceSlug',
+                    component: ProvinceView,
+                    name: 'explore-province',
+                    meta: {
+                        footerButton: 'map'
+                    }
+                }
+            ]
         },
-        {
-            path: 'continents/:continentSlug',
-            component: ContinentView,
-            name: 'map-continent',
-            meta: {
-                footerButton: 'map'
-            }
-        },
-        {
-            path: 'territories/:territorySlug',
-            component: TerritoryView,
-            name: 'map-territory',
-            meta: {
-                footerButton: 'map'
-            }
-        },
-        {
-            path: 'provinces/:provinceSlug',
-            component: ProvinceView,
-            name: 'map-province',
-            meta: {
-                footerButton: 'map'
-            }
-        }
     ]
 };
