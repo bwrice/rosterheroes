db1fe3b3b048d88af1e1efc84f1ce8e72f10b7fd
Brian Rice <bwrice83@gmail.com>
2019-08-05 22:41:36 -0400

    refactor domain action to reuse same exception

--- app/Domain/Actions/BorderTravelAction.php
+++ app/Domain/Actions/BorderTravelAction.php
@@ -6,10 +6,8 @@ namespace App\Domain\Actions;
 
 use App\Domain\Interfaces\TravelsBorders;
 use App\Domain\Models\Province;
-use App\Domain\Models\Squad;
 use App\Domain\Services\Travel\BorderTravelCostCalculator;
-use App\Exceptions\NotBorderedByException;
-use App\Exceptions\NotEnoughGoldException;
+use App\Exceptions\BorderTravelException;
 
 class BorderTravelAction
 {
@@ -23,21 +21,50 @@ class BorderTravelAction
         $this->costCalculator = $costCalculator;
     }
 
+    /**
+     * @param TravelsBorders $travelsBorders
+     * @param Province $border
+     * @throws BorderTravelException
+     */
     public function execute(TravelsBorders $travelsBorders, Province $border)
     {
-        $currentLocation = $travelsBorders->getCurrentLocation();
-        if(! $currentLocation->isBorderedBy($border)) {
-            throw (new NotBorderedByException())->setProvinces($currentLocation, $border);
-        }
+        $this->validateBorder($travelsBorders, $border);
 
         $availableGold = $travelsBorders->getAvailableGold();
         $costToTravel = $this->costCalculator->goldCost($travelsBorders, $border);
 
-        if($availableGold < $costToTravel) {
-            throw new NotEnoughGoldException($availableGold, $costToTravel);
-        }
+        $this->validateTravelCost($travelsBorders, $border, $availableGold, $costToTravel);
 
         $travelsBorders->decreaseGold($costToTravel);
         $travelsBorders->updateLocation($border);
+    }
+
+    /**
+     * @param TravelsBorders $travelsBorders
+     * @param Province $border
+     * @throws BorderTravelException
+     */
+    protected function validateBorder(TravelsBorders $travelsBorders, Province $border): void
+    {
+        $currentLocation = $travelsBorders->getCurrentLocation();
+        if (!$currentLocation->isBorderedBy($border)) {
+            $message = $currentLocation->name . ' is not bordered by ' . $border->name;
+            throw new BorderTravelException($travelsBorders, $border, $message, BorderTravelException::NOT_BORDERED_BY);
+        }
+    }
+
+    /**
+     * @param TravelsBorders $travelsBorders
+     * @param Province $border
+     * @param int $availableGold
+     * @param int $costToTravel
+     * @throws BorderTravelException
+     */
+    protected function validateTravelCost(TravelsBorders $travelsBorders, Province $border, int $availableGold, int $costToTravel): void
+    {
+        if ($availableGold < $costToTravel) {
+            $message = $costToTravel . " gold is needed, but only " . $availableGold . " gold available";
+            throw new BorderTravelException($travelsBorders, $border, $message, BorderTravelException::NOT_ENOUGH_GOLD);
+        }
     }
 }
--- app/Domain/Services/Travel/SquadBorderTravelCostExemption.php
+++ app/Domain/Services/Travel/SquadBorderTravelCostExemption.php
@@ -11,7 +11,7 @@ class SquadBorderTravelCostExemption
 {
     public function isExempt(Squad $squad, Province $border): bool
     {
-        // TODO: any logic such as free until a certain level, or ally provinces
+        // TODO: any logic such as exempt until a certain level, or ally provinces
         return false;
     }
 }
