49fac4f338fa9f692945f873cb6a1be1c798a009
Brian Rice <bwrice83@gmail.com>
2019-08-07 00:35:33 -0400

    render route mini-map

--- app/Http/Resources/ProvinceResource.php
+++ app/Http/Resources/ProvinceResource.php
@@ -22,6 +22,7 @@ class ProvinceResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'uuid' => $this->uuid,
             'name' => $this->name,
             'slug' => $this->slug,
             'color' => $this->color,
--- resources/js/components/commandCenter/views/map/TravelView.vue
+++ resources/js/components/commandCenter/views/map/TravelView.vue
@@ -1,12 +1,60 @@
 <template>
-    <h3>
-        TODO: Travel Page
-    </h3>
+    <v-flex class="xs12">
+        <v-card>
+            <v-layout>
+                <v-flex class="xs5">
+                    <MapViewPort :ocean-color="oceanColor">
+
+                        <!-- Borders -->
+                        <ProvinceVector
+                            v-for="(province, uuid) in this._provinces"
+                            :key="uuid"
+                            :province="province"
+                            :fill-color="provinceColor(province)"
+                        >
+                        </ProvinceVector>
+                    </MapViewPort>
+                </v-flex>
+            </v-layout>
+        </v-card>
+    </v-flex>
 </template>
 
 <script>
+
+    import { mapActions } from 'vuex'
+    import { mapGetters } from 'vuex';
+
+    import MapViewPort from "../../map/MapViewPort";
+    import ProvinceVector from "../../map/ProvinceVector";
+
     export default {
-        name: "TravelView"
+        name: "TravelView",
+        components: {
+            ProvinceVector,
+            MapViewPort
+        },
+        methods: {
+            ...mapActions([
+                'setCurrentLocation'
+            ]),
+            provinceColor: function(province) {
+                if (province.uuid === this._currentLocation.uuid) {
+                    return '#035afc';
+                }
+                return '#FFFFFF';
+            }
+        },
+
+        computed: {
+            ...mapGetters([
+                '_currentLocation',
+                '_provinces'
+            ]),
+            oceanColor() {
+                return '#000000';
+            }
+        }
     }
 </script>
 
