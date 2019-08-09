08f46cec061082941197b27a42c9ac08b1847cfd
Brian Rice <bwrice83@gmail.com>
2019-08-08 22:41:52 -0400

    extract out ExploreMapCard to replace old MapCard component

---
+++ resources/js/components/commandCenter/map/ExploreMapCard.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-card>
+        <v-layout>
+            <v-flex class="xs12">
+                <MapViewPort :view-box="currentViewBox">
+                    <slot>
+                        <!-- Default slot for province vectors -->
+                    </slot>
+                </MapViewPort>
+            </v-flex>
+        </v-layout>
+        <v-layout>
+            <v-flex class="xs7 md9">
+                <v-layout>
+                    <v-flex class="xs12">
+                        <slot name="footer-content">
+                            <!-- left footer content slot -->
+                        </slot>
+                    </v-flex>
+                </v-layout>
+            </v-flex>
+            <v-flex class="xs5 md3 pa-1">
+                <MapControls
+                    @panUp="panUp"
+                    @panDown="panDown"
+                    @panLeft="panLeft"
+                    @panRight="panRight"
+                    @zoomIn="zoomIn"
+                    @zoomOut="zoomOut"
+                    @reset="restViewBox"
+                ></MapControls>
+            </v-flex>
+        </v-layout>
+    </v-card>
+</template>
+
+<script>
+    import {viewBoxControlsMixin} from "../../../mixins/viewBoxControlsMixin";
+    import MapControls from "./MapControls";
+    import MapViewPort from "./MapViewPort";
+
+    export default {
+        name: "ExploreMapCard",
+        props: {
+            viewBox: {
+                type: Object,
+                default: {
+                    pan_x: 0,
+                    pan_y: 0,
+                    zoom_x: 315,
+                    zoom_y: 240,
+                }
+            }
+        },
+        watch: {
+            viewBox: function(newValue) {
+                this.setViewBox(newValue)
+            }
+        },
+        components: {
+            MapViewPort,
+            MapControls
+        },
+        mixins: [
+            viewBoxControlsMixin
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/commandCenter/views/map/explore/RealmView.vue
+++ resources/js/components/commandCenter/views/map/explore/RealmView.vue
@@ -1,55 +1,36 @@
 <template>
-    <v-card>
-        <v-layout>
-            <v-flex class="xs12">
-                <MapViewPort :view-box="currentViewBox">
-                    <template v-if="inContinentMode">
-                        <ContinentVector v-for="(continent, id) in this._continents" :key="id" :continent="continent"></ContinentVector>
-                    </template>
-                    <template v-else>
-                        <TerritoryVector v-for="(territory, id) in this._territories" :key="id" :territory="territory"></TerritoryVector>
-                    </template>
-                </MapViewPort>
-            </v-flex>
-        </v-layout>
-        <v-layout>
-            <v-flex class="xs7 md9">
-                <v-layout>
-                    <v-flex class="xs12">
-                        <v-chip class="ma-1"
-                                :input-value="inContinentMode"
-                                @click="setRealmMapMode('continent')"
-                                filter
-                                filter-icon="mdi-eye"
-                                label
-                        >
-                            Continents
-                        </v-chip>
-                        <v-chip class="ma-1"
-                                :input-value="inTerritoryMode"
-                                @click="setRealmMapMode('territory')"
-                                filter
-                                filter-icon="mdi-eye"
-                                label
-                        >
-                            Territories
-                        </v-chip>
-                    </v-flex>
-                </v-layout>
-            </v-flex>
-            <v-flex class="xs5 md3 pa-1">
-                <MapControls
-                    @panUp="panUp"
-                    @panDown="panDown"
-                    @panLeft="panLeft"
-                    @panRight="panRight"
-                    @zoomIn="zoomIn"
-                    @zoomOut="zoomOut"
-                    @reset="restViewBox"
-                ></MapControls>
-            </v-flex>
-        </v-layout>
-    </v-card>
+    <ExploreMapCard>
+        <template v-if="inContinentMode">
+            <ContinentVector v-for="(continent, id) in this._continents" :key="id" :continent="continent"></ContinentVector>
+        </template>
+        <template v-else>
+            <TerritoryVector v-for="(territory, id) in this._territories" :key="id" :territory="territory"></TerritoryVector>
+        </template>
+        <template slot="footer-content">
+            <v-layout>
+                <v-flex class="xs12">
+                    <v-chip class="ma-1"
+                            :input-value="inContinentMode"
+                            @click="setRealmMapMode('continent')"
+                            filter
+                            filter-icon="mdi-eye"
+                            label
+                    >
+                        Continents
+                    </v-chip>
+                    <v-chip class="ma-1"
+                            :input-value="inTerritoryMode"
+                            @click="setRealmMapMode('territory')"
+                            filter
+                            filter-icon="mdi-eye"
+                            label
+                    >
+                        Territories
+                    </v-chip>
+                </v-flex>
+            </v-layout>
+        </template>
+    </ExploreMapCard>
 </template>
 
 <script>
@@ -64,10 +45,12 @@
     import TerritoryVector from "../../../map/TerritoryVector";
     import MapViewPort from "../../../map/MapViewPort";
     import MapControls from "../../../map/MapControls";
+    import ExploreMapCard from "../../../map/ExploreMapCard";
 
     export default {
         name: "RealmView",
         components: {
+            ExploreMapCard,
             MapControls,
             MapViewPort,
             TerritoryVector,
