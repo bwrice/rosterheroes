9b880109196480fecac13d070072cf5bd3829792
Brian Rice <bwrice83@gmail.com>
2019-08-09 14:26:30 -0400

    really simplify travel module

--- resources/js/store/commandCenter/travelModule.js
+++ resources/js/store/commandCenter/travelModule.js
@@ -3,33 +3,24 @@ import Province from "../../models/Province";
 export default {
 
     state: {
-        routePosition: null,
         travelRoute: []
     },
 
     getters: {
         _routePosition(state, getters, rootState) {
-            if (state.routePosition) {
-                return state.routePosition;
+            let length = state.travelRoute.length;
+            if (length) {
+                // Return last element of route
+                return state.travelRoute[length - 1];
             } else {
                 return rootState.currentLocationModule.currentLocation;
             }
         },
         _travelRoute(state) {
             return state.travelRoute;
-        }
+        },
     },
     mutations: {
-        SET_CURRENT_LOCATION(state, payload) {
-            state.routePosition = payload;
-            state.routePositionBorders = payload.borders;
-        },
-        SET_ROUTE_POSITION(state, payload) {
-            state.routePosition = payload;
-        },
-        SET_ROUTE_BORDERS(state, payload) {
-            state.routePositionBorders = payload;
-        },
         ADD_TO_TRAVEL_ROUTE(state, payload) {
             state.travelRoute.push(payload);
         },
@@ -44,9 +35,7 @@ export default {
                 let squad = rootState.squadModule.squad;
                 let response = await axios.get('/api/v1/squads/' + squad.slug + '/border/' + payload.slug);
                 let routeProvince = response.data;
-                commit('SET_ROUTE_POSITION', routeProvince);
                 commit('ADD_TO_TRAVEL_ROUTE', routeProvince);
-                commit('SET_ROUTE_BORDERS', borders);
             } catch (error) {
                 console.log("ERROR");
                 console.log(error);
