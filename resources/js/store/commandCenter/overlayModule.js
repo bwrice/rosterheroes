fb9abdd64f9dbedd4fa6b3a1fd451137b6723741
Brian Rice <bwrice83@gmail.com>
2019-08-10 10:55:06 -0400

    add stop overlay action

--- resources/js/store/commandCenter/overlayModule.js
+++ resources/js/store/commandCenter/overlayModule.js
@@ -16,12 +16,21 @@ export default {
     mutations: {
         SET_OVERLAY(state, payload) {
             state.overlay = payload;
+        },
+        STOP_OVERLAY(state) {
+            state.overlay = {
+                show: false,
+                message: null
+            }
         }
     },
 
     actions: {
         setOverlay({commit}, payload) {
-            commit('SET_OVERLAY', payload)
+            commit('SET_OVERLAY', payload);
         },
+        stopOverlay({commit}) {
+            commit('STOP_OVERLAY');
+        }
     }
 };
