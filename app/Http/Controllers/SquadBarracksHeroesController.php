091d168a80cc9b63bc9a9849862352df133d0d7f
Brian Rice <bwrice83@gmail.com>
2019-08-10 23:18:44 -0400

    add barracks heroes controller

---
+++ app/Http/Controllers/SquadBarracksHeroesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domain\Models\Hero;
+use App\Domain\Models\Squad;
+use App\Http\Resources\HeroResource;
+use Illuminate\Http\Request;
+
+class SquadBarracksHeroesController extends Controller
+{
+    public function __invoke($squadSlug)
+    {
+        $squad = Squad::slugOrFail($squadSlug);
+        $this->authorize(Squad::MANAGE_AUTHORIZATION, $squad);
+        $heroes = Hero::query()->amongSquad($squad)->get();
+        return response()->json(HeroResource::collection($heroes), 200);
+    }
+}
