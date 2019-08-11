0ac4310c92bc30c254fe7b4b9f6e7a304650831f
Brian Rice <bwrice83@gmail.com>
2019-08-10 23:52:54 -0400

    update barracks heroes in barracks main

--- resources/js/components/commandCenter/views/barracks/BarracksMain.vue
+++ resources/js/components/commandCenter/views/barracks/BarracksMain.vue
@@ -5,10 +5,21 @@
 </template>
 
 <script>
+
+    import {mapActions} from 'vuex';
+
     export default {
         name: "BarracksMain",
-        created: () => {
+        created() {
             console.log("Barracks Created!");
+        },
+        mounted() {
+            this.updateBarracksHeroes(this.$route.params.squadSlug);
+        },
+        methods: {
+            ...mapActions([
+                'updateBarracksHeroes'
+            ])
         }
     }
 </script>
