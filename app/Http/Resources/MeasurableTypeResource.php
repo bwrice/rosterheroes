01ff14adadf93e775276d3d2df4dc53f556b650f
Brian Rice <bwrice83@gmail.com>
2019-08-13 15:15:11 -0400

    create measurable type api resource

---
+++ app/Http/Resources/MeasurableTypeResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\MeasurableType;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class MeasurableTypeResource
+ * @package App\Http\Resources
+ *
+ * @mixin MeasurableType
+ */
+class MeasurableTypeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name
+        ];
+    }
+}
