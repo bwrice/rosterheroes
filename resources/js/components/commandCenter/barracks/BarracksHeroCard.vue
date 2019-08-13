117872a8acf9d74fa207a3829f0722ea00983f74
Brian Rice <bwrice83@gmail.com>
2019-08-12 21:27:18 -0400

    create summary card component for barracks heroes

---
+++ resources/js/components/commandCenter/barracks/BarracksHeroCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-card>
+        <v-card-title>{{hero.name}}</v-card-title>
+        <v-card-actions>
+            <v-btn>Edit</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "BarracksHeroCard",
+        props: {
+            hero: {
+                type: Object,
+                require: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
