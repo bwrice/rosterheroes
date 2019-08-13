17d8ae7c4c4d330c2c019f9bf993f6c07f9db428
Brian Rice <bwrice83@gmail.com>
2019-08-12 23:42:40 -0400

    router to barracks hero view

--- resources/js/components/commandCenter/barracks/BarracksHeroCard.vue
+++ resources/js/components/commandCenter/barracks/BarracksHeroCard.vue
@@ -2,7 +2,7 @@
     <v-card>
         <v-card-title>{{hero.name}}</v-card-title>
         <v-card-actions>
-            <v-btn>Edit</v-btn>
+            <v-btn :to="barracksHeroRoute">Edit</v-btn>
         </v-card-actions>
     </v-card>
 </template>
@@ -14,6 +14,18 @@
             hero: {
                 type: Object,
                 require: true
+            }
+        },
+        computed: {
+            barracksHeroRoute() {
+                let squadSlugParam = this.$route.params.squadSlug;
+                return {
+                    name: 'barracks-hero',
+                    params: {
+                        squadSlug: squadSlugParam,
+                        heroSlug: this.hero.slug
+                    }
+                }
             }
         }
     }
