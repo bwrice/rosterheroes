85e9ba126267bd430f62580aa3ecaadea7403ec5
Brian Rice <bwrice83@gmail.com>
2019-08-14 19:06:22 -0400

    get matching barracks hero from barraks hero view

--- resources/js/components/commandCenter/views/barracks/HeroBarracksView.vue
+++ resources/js/components/commandCenter/views/barracks/HeroBarracksView.vue
@@ -1,14 +1,36 @@
 <template>
     <v-flex class="xs12">
         <v-card>
-            <v-card-title>Hero Barracks View</v-card-title>
+            <v-card-title>{{ hero.name }}</v-card-title>
         </v-card>
     </v-flex>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
+
     export default {
-        name: "HeroView"
+        name: "HeroView",
+        computed: {
+            ...mapGetters([
+                '_barracksHeroes'
+            ]),
+            hero() {
+
+                let self = this;
+                let hero = this._barracksHeroes.find(function (hero) {
+                    return hero.slug === self.$route.params.heroSlug;
+                });
+
+                if (hero) {
+                    return hero;
+                }
+                return {
+                    'name': '',
+                    'measurables': []
+                }
+            }
+        }
     }
 </script>
 
