45cc1a47ff99dbb7da77cdf190057aad9088fc54
Brian Rice <bwrice83@gmail.com>
2019-08-13 20:16:30 -0400

    create measurable-type behavior class

---
+++ app/Domain/Behaviors/MeasurableTypes/MeasurableTypeBehavior.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Domain\Behaviors\MeasurableTypes;
+
+
+class MeasurableTypeBehavior
+{
+    /**
+     * @var string
+     */
+    private $measurableGroup;
+    /**
+     * @var int
+     */
+    private $baseAmount;
+
+    public function __construct(string $measurableGroup, int $baseAmount)
+    {
+        $this->measurableGroup = $measurableGroup;
+        $this->baseAmount = $baseAmount;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMeasurableGroup(): string
+    {
+        return $this->measurableGroup;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBaseAmount(): int
+    {
+        return $this->baseAmount;
+    }
+}
