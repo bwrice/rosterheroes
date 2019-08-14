d6d3af21d1feccdf8f55d4846cfc4109fd22bbb4
Brian Rice <bwrice83@gmail.com>
2019-08-13 20:16:42 -0400

    add factory method for getting behavior

--- app/Domain/Models/MeasurableType.php
+++ app/Domain/Models/MeasurableType.php
@@ -2,6 +2,8 @@
 
 namespace App\Domain\Models;
 
+use App\Domain\Behaviors\MeasurableTypes\MeasurableTypeBehavior;
+use App\Exceptions\UnknownBehaviorException;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
@@ -58,5 +60,43 @@ class MeasurableType extends Model
             self::PRESTIGE,
             self::WRATH
         ]);
+    }
+
+    public function getBehavior(): MeasurableTypeBehavior
+    {
+        switch ($this->name) {
+            case self::STRENGTH:
+                return new MeasurableTypeBehavior('attribute', 20);
+            case self::VALOR:
+                return new MeasurableTypeBehavior('attribute', 20);
+            case self::AGILITY:
+                return new MeasurableTypeBehavior('attribute', 20);
+            case self::FOCUS:
+                return new MeasurableTypeBehavior('attribute', 20);
+            case self::APTITUDE:
+                return new MeasurableTypeBehavior('attribute', 20);
+            case self::INTELLIGENCE:
+                return new MeasurableTypeBehavior('attribute', 20);
+
+            case self::HEALTH:
+                return new MeasurableTypeBehavior('resource', 250);
+            case self::STAMINA:
+                return new MeasurableTypeBehavior('resource', 250);
+            case self::MANA:
+                return new MeasurableTypeBehavior('resource', 250);
+
+            case self::PASSION:
+                return new MeasurableTypeBehavior('quality', 100);
+            case self::BALANCE:
+                return new MeasurableTypeBehavior('quality', 100);
+            case self::HONOR:
+                return new MeasurableTypeBehavior('quality', 100);
+            case self::PRESTIGE:
+                return new MeasurableTypeBehavior('quality', 100);
+            case self::WRATH:
+                return new MeasurableTypeBehavior('quality', 100);
+        }
+
+        throw new UnknownBehaviorException($this->name, MeasurableTypeBehavior::class);
     }
 }
