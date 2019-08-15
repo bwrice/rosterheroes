1370853cadc479390dd5526efc1950c9f7ee26a8
Brian Rice <bwrice83@gmail.com>
2019-08-15 10:01:56 -0400

    add group to resource

--- app/Http/Resources/MeasurableTypeResource.php
+++ app/Http/Resources/MeasurableTypeResource.php
@@ -22,7 +22,8 @@ class MeasurableTypeResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name' => $this->name
+            'name' => $this->name,
+            'group' => $this->getMeasurableGroup()
         ];
     }
 }
