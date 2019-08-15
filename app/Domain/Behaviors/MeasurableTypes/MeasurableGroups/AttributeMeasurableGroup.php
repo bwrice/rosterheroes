5c03bcb6f004cc4fb7197603a29f2c3f910d606d
Brian Rice <bwrice83@gmail.com>
2019-08-14 22:41:05 -0400

    create measurable groups

---
+++ app/Domain/Behaviors/MeasurableTypes/MeasurableGroups/AttributeMeasurableGroup.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Domain\Behaviors\MeasurableTypes\MeasurableGroups;
+
+
+class AttributeMeasurableGroup extends MeasurableGroup
+{
+
+}
---
+++ app/Domain/Behaviors/MeasurableTypes/MeasurableGroups/ResourceMeasurableGroup.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Domain\Behaviors\MeasurableTypes\MeasurableGroups;
+
+
+class ResourceMeasurableGroup extends MeasurableGroup
+{
+
+}
