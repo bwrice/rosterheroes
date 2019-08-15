13b370baf6b3c1c89ab023d0439fd59ffd3f9125
Brian Rice <bwrice83@gmail.com>
2019-08-15 10:02:14 -0400

    create measurable component

---
+++ resources/js/components/commandCenter/barracks/MeasurablePanel.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-sheet>
+        {{name}}
+    </v-sheet>
+</template>
+
+<script>
+    export default {
+        name: "MeasurablePanel",
+        props: {
+            measurable: Object,
+            required: true
+        },
+        computed: {
+            name() {
+                return this.measurable.measurable_type.name;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
