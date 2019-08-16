cd2f76706a9c5cf4e974675313e904ac16fac8fc
Brian Rice <bwrice83@gmail.com>
2019-08-15 20:44:30 -0400

    add api method for roster-heroes

--- resources/js/api/squadApi.js
+++ resources/js/api/squadApi.js
@@ -1,10 +1,16 @@
 
+
+export async function getSquad(squadSlug) {
+    let response = await axios.get('/api/v1/squads/' + squadSlug);
+    return response.data.data;
+}
+
 export async function getBarracksHeroes(squadSlug) {
     let response = await axios.get('/api/v1/squads/' + squadSlug + '/barracks/heroes');
     return response.data.data;
 }
 
-export async function getSquad(squadSlug) {
-    let response = await axios.get('/api/v1/squads/' + squadSlug);
+export async function getRosterHeroes(squadSlug) {
+    let response = await axios.get('/api/v1/squads/' + squadSlug + '/roster/heroes');
     return response.data.data;
 }
