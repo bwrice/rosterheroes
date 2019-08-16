5195b88b841d1388e6465ab809f6877743f484b8
Brian Rice <bwrice83@gmail.com>
2019-08-15 21:37:46 -0400

    clean-up squad vuex module

--- resources/js/store/commandCenter/squadModule.js
+++ resources/js/store/commandCenter/squadModule.js
@@ -9,35 +9,18 @@ export default {
     getters: {
         _squad(state) {
             return state.squad;
-        },
-        _availableSpiritEssence(state) {
-            return state.squad.availableSpiritEssence;
         }
     },
     mutations: {
         SET_SQUAD(state, payload) {
             state.squad = payload;
-        },
-        UPDATE_HERO(state, payload) {
-            // state.squad = new Squad();
-            let current = state.squad;
-            current.heroPosts.forEach(function (heroPost) {
-                if (heroPost.hero && heroPost.hero.uuid === payload.uuid) {
-                    heroPost.hero = payload;
-                }
-            });
-            state.squad = current;
-        },
-
+        }
     },
 
     actions: {
         async updateSquad({commit}, route) {
             let squad = await squadApi.getSquad(route.params.squadSlug);
             commit('SET_SQUAD', squad)
-        },
-        updateHero({commit}, payload) {
-            commit('UPDATE_HERO', payload)
-        },
+        }
     }
 };
