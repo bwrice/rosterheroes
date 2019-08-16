7e7a3d10f4c03cef2c306ba4b02fb11df50a935a
Brian Rice <bwrice83@gmail.com>
2019-08-16 15:52:27 -0400

    test removing wrong spirit throws exception

--- tests/Unit/RemoveSpiritFromHeroActionTest.php
+++ tests/Unit/RemoveSpiritFromHeroActionTest.php
@@ -62,4 +62,47 @@ class RemoveSpiritFromHeroActionTest extends TestCase
 
         $this->fail("Exception not thrown");
     }
+
+    /**
+     * @test
+     */
+    public function removing_a_spirit_not_embodying_a_hero_will_throw_an_exception()
+    {
+        /** @var PlayerSpirit $playerSpirit */
+        $playerSpirit = factory(PlayerSpirit::class)->create();
+        // set current week to spirit's week
+        Week::setTestCurrent($playerSpirit->week);
+
+        /** @var Hero $hero */
+        $hero = factory(Hero::class)->create([
+            'player_spirit_id' => $playerSpirit->id
+        ]);
+
+        /** @var HeroPost $heroPost */
+        factory(HeroPost::class)->create([
+            'hero_id' => $hero->id
+        ]);
+
+        /** @var RemoveSpiritFromHeroAction $removeAction */
+        $removeAction = app(RemoveSpiritFromHeroAction::class);
+
+        /** @var PlayerSpirit $playerSpirit */
+        $wrongPlayerSpirit = factory(PlayerSpirit::class)->create();
+
+        try {
+            $playerSpirit = $playerSpirit->fresh();
+            $removeAction->execute($hero, $wrongPlayerSpirit);
+
+        } catch (HeroPlayerSpiritException $exception) {
+
+            $this->assertEquals(HeroPlayerSpiritException::NOT_EMBODIED_BY, $exception->getCode());
+            $this->assertNotNull($hero->fresh()->playerSpirit);
+
+            return;
+        }
+
+        $this->fail("Exception not thrown");
+    }
+
+
 }
