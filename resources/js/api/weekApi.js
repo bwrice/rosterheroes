9d317aff5acb766993e37f329d0d4fcc86892bbd
Brian Rice <bwrice83@gmail.com>
2019-08-15 20:44:19 -0400

    add api method for player spirits

--- resources/js/api/weekApi.js
+++ resources/js/api/weekApi.js
@@ -3,3 +3,8 @@ export async function getCurrentWeek() {
     let response = await axios.get('/api/v1/weeks/current');
     return response.data.data;
 }
+
+export async function getCurrentPlayerSpirits() {
+    let response = await axios.get('/api/v1/weeks/current/player-spirits');
+    return response.data.data;
+}
