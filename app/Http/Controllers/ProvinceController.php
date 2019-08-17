5317cc75c8c42d033e545f7b47246ccc00e06188
Brian Rice <bwrice83@gmail.com>
2019-08-16 23:02:34 -0400

    cache province index response

--- app/Http/Controllers/ProvinceController.php
+++ app/Http/Controllers/ProvinceController.php
@@ -5,16 +5,20 @@ namespace App\Http\Controllers;
 use App\Domain\Models\Province;
 use App\Http\Resources\ProvinceResource;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Cache;
 
 class ProvinceController extends Controller
 {
     public function index()
     {
-        $provinces = Province::query()->with([
-            'borders' => function(BelongsToMany $builder) {
-                return $builder->select('uuid');
-            }
-        ])->get();
+        $provinces = Cache::remember('all_provinces', 60 * 60 * 24, function() {
+            return Province::query()->with([
+                'borders' => function(BelongsToMany $builder) {
+                    return $builder->select('uuid');
+                }
+            ])->get();
+        });
+
         return ProvinceResource::collection($provinces);
     }
 }
