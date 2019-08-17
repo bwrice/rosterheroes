76ebc96b669a57c49735a181fc002e6b230c59b6
Brian Rice <bwrice83@gmail.com>
2019-08-16 20:54:56 -0400

    add remove spirt api function

--- resources/js/api/heroApi.js
+++ resources/js/api/heroApi.js
@@ -3,3 +3,8 @@ export async function addSpirit(heroSlug, spiritUuid) {
     let response = await axios.post('/api/v1/heroes/' + heroSlug + '/player-spirit/' + spiritUuid);
     return response.data.data;
 }
+
+export async function removeSpirit(heroSlug, spiritUuid) {
+    let response = await axios.delete('/api/v1/heroes/' + heroSlug + '/player-spirit/' + spiritUuid);
+    return response.data.data;
+}
