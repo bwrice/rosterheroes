5a1ce54eb7f41b4fcdd789b844e7175f2ae5c974
Brian Rice <bwrice83@gmail.com>
2019-08-17 22:51:29 -0400

    add api getter for current location

--- resources/js/api/squadApi.js
+++ resources/js/api/squadApi.js
@@ -14,3 +14,8 @@ export async function getRosterHeroes(squadSlug) {
     let response = await axios.get('/api/v1/squads/' + squadSlug + '/roster/heroes');
     return response.data.data;
 }
+
+export async function getCurrentLocation(squadSlug) {
+    let response = await axios.get('/api/v1/squads/' + squadSlug + '/current-location');
+    return response.data.data;
+}
