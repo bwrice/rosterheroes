2f5aedaef86abfb3b33a40ccd49f7720782f4ead
Brian Rice <bwrice83@gmail.com>
2019-08-17 22:53:20 -0400

    clean up map base

--- resources/js/components/commandCenter/views/map/MapBase.vue
+++ resources/js/components/commandCenter/views/map/MapBase.vue
@@ -3,29 +3,11 @@
 </template>
 
 <script>
-    import {mapActions} from 'vuex';
-    import Squad from "../../../../models/Squad";
-    import Province from "../../../../models/Province";
     import BaseView from "../BaseView";
 
     export default {
         name: "MapBase",
         components: {BaseView},
-
-        async mounted() {
-
-            let squadSlug = this.$route.params.squadSlug;
-            let squad = new Squad({slug: squadSlug});
-            let currentLocation = await Province.custom(squad, 'current-location').$first();
-            this.setCurrentLocation(currentLocation);
-        },
-
-        methods: {
-            ...mapActions([
-                'setCurrentLocation'
-            ])
-        },
-
     }
 </script>
 
