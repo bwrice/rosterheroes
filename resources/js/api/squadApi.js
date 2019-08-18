ab28e482017bbc29ffbec99f945c0d678a6cac3f
Brian Rice <bwrice83@gmail.com>
2019-08-18 00:10:30 -0400

    add fast travel api method

--- resources/js/api/squadApi.js
+++ resources/js/api/squadApi.js
@@ -19,3 +19,10 @@ export async function getCurrentLocation(squadSlug) {
     let response = await axios.get('/api/v1/squads/' + squadSlug + '/current-location');
     return response.data.data;
 }
+
+export async function fastTravel(squadSlug, provinces) {
+    let response = await axios.post('/api/v1/squads/' + squadSlug + '/fast-travel', {
+        travelRoute: provinces
+    });
+    return response.data.data;
+}
