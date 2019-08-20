24a90de6b52941f01c28620bc0d3479e08578970
Brian Rice <bwrice83@gmail.com>
2019-08-20 17:44:13 -0400

    fix response data returned

--- resources/js/api/measurableApi.js
+++ resources/js/api/measurableApi.js
@@ -1,5 +1,5 @@
 
 export async function getCostToRaise(measurableUuid, raiseAmount) {
     let response = await axios.get('/api/v1/measurables/' + measurableUuid + '/raise?amount=' + raiseAmount);
-    return parseInt(response.data.data);
+    return response.data;
 }
