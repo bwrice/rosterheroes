b3749e35ae6ea15a60fb5cf178970c48ee93e357
Brian Rice <bwrice83@gmail.com>
2019-08-20 10:28:04 -0400

    fix raise measurable dialog layout

--- resources/js/components/commandCenter/barracks/HeroMeasurablesCard.vue
+++ resources/js/components/commandCenter/barracks/HeroMeasurablesCard.vue
@@ -27,96 +27,28 @@
                 @measurableClicked="setFocusedMeasurable"
             ></MeasurablePanel>
         </v-card>
-        <v-card>
-            <v-container>
-                <v-row :justify="'center'">
-                    <v-col>
-                        <v-row :align="'center'">
-                            <v-card>
-                                <v-btn
-                                    fab
-                                    small
-                                    @click="increaseRaiseAmount"
-                                >
-                                    <v-icon dark>add</v-icon>
-                                </v-btn>
-                            </v-card>
-                            <!--                                    <v-text-field-->
-                            <!--                                        outlined-->
-                            <!--                                        solo-->
-                            <!--                                        type="number"-->
-                            <!--                                        v-model="measurableRaiseAmount"-->
-                            <!--                                    ></v-text-field>-->
-                        </v-row>
-                    </v-col>
-                    <v-col>
-                        <v-row no gutters>
-                            <v-col class="py-1">
-                                <v-btn
-                                    fab
-                                    small
-                                    @click="increaseRaiseAmount"
-                                >
-                                    <v-icon dark>add</v-icon>
-                                </v-btn>
-                            </v-col>
-                        </v-row>
-                        <v-row no gutters>
-                            <v-col class="py-1">
-                                <v-btn
-                                    fab
-                                    small
-                                    @click="lowerRaiseAmount"
-                                >
-                                    <v-icon dark>remove</v-icon>
-                                </v-btn>
-                            </v-col>
-                        </v-row>
-                    </v-col>
-                </v-row>
-                <v-row no gutters>
-                    <v-btn
-                        color="primary"
-                        block
-                    >
-                        Raise {{measurableName}}
-                    </v-btn>
-                </v-row>
-            </v-container>
-        </v-card>
         <v-dialog
             v-model="measurableFocused"
             max-width="600"
         >
-            <v-card>
-                <v-card-title class="headline">{{measurableCardTitle}}</v-card-title>
-                <v-card-actions>
-                    <v-container fluid>
-                        <v-row :justify="'center'">
-                            <v-col>
-                                <v-container>
-                                    <v-row :align="'center'">
-                                        <v-card>
-                                            <v-btn
-                                                fab
-                                                small
-                                                @click="increaseRaiseAmount"
-                                            >
-                                                <v-icon dark>add</v-icon>
-                                            </v-btn>
-                                        </v-card>
-                                        <!--                                    <v-text-field-->
-                                        <!--                                        outlined-->
-                                        <!--                                        solo-->
-                                        <!--                                        type="number"-->
-                                        <!--                                        v-model="measurableRaiseAmount"-->
-                                        <!--                                    ></v-text-field>-->
-                                    </v-row>
-                                </v-container>
-                            </v-col>
-                            <v-col>
-                                <v-row no gutters>
-                                    <v-col class="py-1">
+                <v-card>
+                    <v-card-title class="headline">{{measurableCardTitle}}</v-card-title>
+                    <v-card-actions>
+
+                        <v-container>
+                        <v-row>
+                            <v-col cols="10" offset="1">
+                                <v-row>
+                                    <v-col cols="5">
+                                        <v-text-field
+                                            outlined
+                                            solo
+                                            type="number"
+                                            v-model="measurableRaiseAmount"
+                                        >
+                                        </v-text-field>
+                                    </v-col>
+                                    <v-col cols="7">
                                         <v-btn
                                             fab
                                             small
@@ -124,10 +56,6 @@
                                         >
                                             <v-icon dark>add</v-icon>
                                         </v-btn>
-                                    </v-col>
-                                </v-row>
-                                <v-row no gutters>
-                                    <v-col class="py-1">
                                         <v-btn
                                             fab
                                             small
@@ -147,9 +75,9 @@
                                 Raise {{measurableName}}
                             </v-btn>
                         </v-row>
-                    </v-container>
-                </v-card-actions>
-            </v-card>
+                        </v-container>
+                    </v-card-actions>
+                </v-card>
         </v-dialog>
     </v-card>
 </template>
