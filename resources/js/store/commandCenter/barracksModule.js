fc7fafdd72884316076ab71497d18e5ad733dd42
Brian Rice <bwrice83@gmail.com>
2019-08-19 21:27:03 -0400

    remove “focused hero” form barracks module

--- resources/js/store/commandCenter/barracksModule.js
+++ resources/js/store/commandCenter/barracksModule.js
@@ -3,28 +3,17 @@ import * as squadApi from '../../api/squadApi';
 export default {
 
     state: {
-        barracksHeroes: [],
-        barracksFocusedHero: {
-            name: '',
-            slug: '',
-            measurables: []
-        }
+        barracksHeroes: []
     },
 
     getters: {
         _barracksHeroes(state) {
             return state.barracksHeroes;
-        },
-        _barracksFocusedHero(state) {
-            return state.barracksFocusedHero;
         }
     },
     mutations: {
         SET_BARRACKS_HEROES(state, payload) {
             state.barracksHeroes = payload;
-        },
-        SET_BARRACKS_FOCUSED_HERO(state, payload) {
-            state.barracksFocusedHero = payload;
         }
     },
 
@@ -32,20 +21,6 @@ export default {
         async updateBarracks({commit, dispatch}, route) {
             let heroes = await squadApi.getBarracksHeroes(route.params.squadSlug);
             commit('SET_BARRACKS_HEROES', heroes);
-            if ('barracks-hero' === route.name) {
-                dispatch('setBarracksFocusedHeroBySlug', route.params.heroSlug);
-            }
-        },
-        setBarracksFocusedHeroBySlug({state, commit}, heroSlug) {
-            if (heroSlug !== state.barracksFocusedHero.slug) {
-                let focusedHero = state.barracksHeroes.find(function (hero) {
-                    return hero.slug === heroSlug;
-                });
-
-                if (focusedHero) {
-                    commit('SET_BARRACKS_FOCUSED_HERO', focusedHero);
-                }
-            }
         }
     }
 };
