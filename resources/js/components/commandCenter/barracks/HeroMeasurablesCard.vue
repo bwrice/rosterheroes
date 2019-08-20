3c48ee89abcb395717321c86a674c4c108ec239f
Brian Rice <bwrice83@gmail.com>
2019-08-20 12:33:37 -0400

    move raise dialog to inside measurable panel

--- resources/js/components/commandCenter/barracks/HeroMeasurablesCard.vue
+++ resources/js/components/commandCenter/barracks/HeroMeasurablesCard.vue
@@ -6,7 +6,6 @@
                 v-for="(attribute, uuid) in attributes"
                 :key="uuid"
                 :measurable="attribute"
-                @measurableClicked="setFocusedMeasurable"
             ></MeasurablePanel>
         </v-card>
         <v-card>
@@ -15,7 +14,6 @@
                 v-for="(resource, uuid) in resources"
                 :key="uuid"
                 :measurable="resource"
-                @measurableClicked="setFocusedMeasurable"
             ></MeasurablePanel>
         </v-card>
         <v-card>
@@ -24,83 +22,8 @@
                 v-for="(quality, uuid) in qualities"
                 :key="uuid"
                 :measurable="quality"
-                @measurableClicked="setFocusedMeasurable"
             ></MeasurablePanel>
         </v-card>
-        <v-dialog
-            v-model="measurableFocused"
-            max-width="600"
-        >
-                <v-card>
-                    <v-card-title class="headline">{{measurableName}}: {{potentialAmount}}(+{{measurableRaiseAmount}})</v-card-title>
-                    <v-card-actions>
-                        <v-container>
-                            <v-row no-gutters>
-                                <v-col cols="6">
-                                    <v-text-field
-                                        outlined
-                                        solo
-                                        type="number"
-                                        v-model="measurableRaiseAmount"
-                                    >
-                                    </v-text-field>
-                                </v-col>
-                                <v-col cols="6">
-                                    <v-row :justify="'center'">
-                                        <v-btn
-                                            fab
-                                            small
-                                            :disabled="increaseDisabled"
-                                            @click="increaseRaiseAmount"
-                                        >
-                                            <v-icon dark>add</v-icon>
-                                        </v-btn>
-                                        <v-btn
-                                            fab
-                                            small
-                                            :disabled="decreaseDisabled"
-                                            @click="decreaseRaiseAmount"
-                                        >
-                                            <v-icon dark>remove</v-icon>
-                                        </v-btn>
-                                    </v-row>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col cols="6">
-                                    <v-row :justify="'center'">
-                                        Available: 48304
-                                    </v-row>
-                                </v-col>
-                                <v-col cols="6">
-                                    <v-row :justify="'center'">
-                                        Cost: 9834
-                                    </v-row>
-                                </v-col>
-                            </v-row>
-                            <v-row no gutters>
-                                <v-col cols="4">
-                                    <v-btn
-                                        color="error"
-                                        block
-                                        @click="measurableFocused = false"
-                                    >
-                                        Cancel
-                                    </v-btn>
-                                </v-col>
-                                <v-col cols="8">
-                                    <v-btn
-                                        color="primary"
-                                        block
-                                    >
-                                        Raise {{measurableName}}
-                                    </v-btn>
-                                </v-col>
-                            </v-row>
-                        </v-container>
-                    </v-card-actions>
-                </v-card>
-        </v-dialog>
     </v-card>
 </template>
 
@@ -118,31 +41,6 @@
             barracksHeroMixin
         ],
 
-        data() {
-            return {
-                focusedMeasurable: {
-                    measurable_type: {
-                        name: ''
-                    }
-                },
-                measurableFocused: false,
-                measurableRaiseAmount: 1
-            }
-        },
-
-        methods: {
-            setFocusedMeasurable(measurable) {
-                this.focusedMeasurable = measurable;
-                this.measurableFocused = true;
-            },
-            decreaseRaiseAmount() {
-                this.measurableRaiseAmount--;
-            },
-            increaseRaiseAmount() {
-                this.measurableRaiseAmount++;
-            }
-        },
-
         computed: {
             attributes() {
                 return this.barracksHero.measurables.filter(function (measurable) {
@@ -158,25 +56,6 @@
                 return this.barracksHero.measurables.filter(function (measurable) {
                     return measurable.measurable_type.group === 'quality';
                 })
-            },
-            measurableName() {
-                return this.focusedMeasurable.measurable_type.name.toUpperCase();
-            },
-            potentialAmount() {
-                let potential = this.focusedMeasurable.current_amount;
-                if (this.raiseAmount) {
-                    potential += this.raiseAmount;
-                }
-                return potential;
-            },
-            raiseAmount() {
-                return parseInt(this.measurableRaiseAmount);
-            },
-            increaseDisabled() {
-                return this.measurableRaiseAmount >= 100;
-            },
-            decreaseDisabled() {
-                return this.measurableRaiseAmount <= 1;
             }
         }
     }
--- resources/js/components/commandCenter/barracks/MeasurablePanel.vue
+++ resources/js/components/commandCenter/barracks/MeasurablePanel.vue
@@ -1,9 +1,83 @@
 <template>
     <v-sheet flat color="#706856" class="pa-1 ma-1">
         <v-row no-gutters justify="space-between">
-            <p>{{name}}</p>
-            <v-btn @click="clicked">+</v-btn>
+            <p>{{measurableName}}</p>
+            <v-btn @click="measurableFocused = true">+</v-btn>
         </v-row>
+        <v-dialog
+            v-model="measurableFocused"
+            max-width="600"
+        >
+            <v-card>
+                <v-card-title class="headline">{{measurableName}}: {{potentialAmount}}(+{{measurableRaiseAmount}})</v-card-title>
+                <v-card-actions>
+                    <v-container>
+                        <v-row no-gutters>
+                            <v-col cols="6">
+                                <v-text-field
+                                    outlined
+                                    solo
+                                    type="number"
+                                    v-model="measurableRaiseAmount"
+                                >
+                                </v-text-field>
+                            </v-col>
+                            <v-col cols="6">
+                                <v-row :justify="'center'">
+                                    <v-btn
+                                        fab
+                                        small
+                                        :disabled="increaseDisabled"
+                                        @click="increaseRaiseAmount"
+                                    >
+                                        <v-icon dark>add</v-icon>
+                                    </v-btn>
+                                    <v-btn
+                                        fab
+                                        small
+                                        :disabled="decreaseDisabled"
+                                        @click="decreaseRaiseAmount"
+                                    >
+                                        <v-icon dark>remove</v-icon>
+                                    </v-btn>
+                                </v-row>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="6">
+                                <v-row :justify="'center'">
+                                    Available: 48304
+                                </v-row>
+                            </v-col>
+                            <v-col cols="6">
+                                <v-row :justify="'center'">
+                                    Cost: 9834
+                                </v-row>
+                            </v-col>
+                        </v-row>
+                        <v-row no gutters>
+                            <v-col cols="4">
+                                <v-btn
+                                    color="error"
+                                    block
+                                    @click="measurableFocused = false"
+                                >
+                                    Cancel
+                                </v-btn>
+                            </v-col>
+                            <v-col cols="8">
+                                <v-btn
+                                    color="primary"
+                                    block
+                                >
+                                    Raise {{measurableName}}
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-sheet>
 </template>
 
@@ -14,14 +88,40 @@
             measurable: Object,
             required: true
         },
+
+        data() {
+            return {
+                measurableFocused: false,
+                measurableRaiseAmount: 1
+            }
+        },
         methods: {
-            clicked(event) {
-                this.$emit('measurableClicked', this.measurable, event);
+            decreaseRaiseAmount() {
+                this.measurableRaiseAmount--;
+            },
+            increaseRaiseAmount() {
+                this.measurableRaiseAmount++;
             }
         },
         computed: {
-            name() {
-                return this.measurable.measurable_type.name;
+            measurableName() {
+                return this.measurable.measurable_type.name.toUpperCase();
+            },
+            potentialAmount() {
+                let potential = this.measurable.current_amount;
+                if (this.raiseAmount) {
+                    potential += this.raiseAmount;
+                }
+                return potential;
+            },
+            raiseAmount() {
+                return parseInt(this.measurableRaiseAmount);
+            },
+            increaseDisabled() {
+                return this.measurableRaiseAmount >= 100;
+            },
+            decreaseDisabled() {
+                return this.measurableRaiseAmount <= 1;
             }
         }
     }
