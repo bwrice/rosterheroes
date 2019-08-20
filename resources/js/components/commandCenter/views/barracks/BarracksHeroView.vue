052728a6419b59fdab16d727b495e1ae1be18c15
Brian Rice <bwrice83@gmail.com>
2019-08-19 21:32:38 -0400

    use mixin inside barracks hero view

--- resources/js/components/commandCenter/views/barracks/BarracksHeroView.vue
+++ resources/js/components/commandCenter/views/barracks/BarracksHeroView.vue
@@ -3,7 +3,7 @@
         <v-row no-gutters>
             <v-col cols="12">
                 <v-card class="my-2">
-                    <v-card-title>{{ _barracksFocusedHero.name }}</v-card-title>
+                    <v-card-title>{{ barracksHero.name }}</v-card-title>
                 </v-card>
             </v-col>
         </v-row>
@@ -15,38 +15,15 @@
 </template>
 
 <script>
-    import {mapGetters} from 'vuex';
-    import {mapActions} from 'vuex';
+    import {barracksHeroMixin} from "../../../../mixins/barracksHeroMixin";
     import MeasurablePanel from "../../barracks/MeasurablePanel";
 
     export default {
         name: "BarracksHeroView",
         components: {MeasurablePanel},
-
-        mounted() {
-            this.setBarracksFocusedHeroBySlug(this.$route.params.heroSlug);
-        },
-
-        watch: {
-            $route (to) {
-                if (to.params.heroSlug && to.params.heroSlug !== this._barracksFocusedHero.slug) {
-                    this.setBarracksFocusedHeroBySlug(to.params.heroSlug);
-                }
-            }
-        },
-
-        methods: {
-            ...mapActions([
-                'setBarracksFocusedHeroBySlug'
-            ]),
-        },
-
-        computed: {
-            ...mapGetters([
-                '_barracksHeroes',
-                '_barracksFocusedHero'
-            ]),
-        }
+        mixins: [
+            barracksHeroMixin
+        ]
     }
 </script>
 
