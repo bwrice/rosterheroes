c5d746acd8fa7705a6c5e5e112ee08e148c71c9b
Brian Rice <bwrice83@gmail.com>
2019-08-19 21:04:28 -0400

    extract out component

---
+++ resources/js/components/commandCenter/barracks/HeroMeasurablesCard.vue
@@ -0,0 +1,116 @@
+<template>
+    <v-card>
+        <v-card>
+            <v-card-title>Attributes</v-card-title>
+            <MeasurablePanel
+                v-for="(attribute, uuid) in attributes"
+                :key="uuid"
+                :measurable="attribute"
+                @measurableClicked="setFocusedMeasurable"
+            ></MeasurablePanel>
+        </v-card>
+        <v-card>
+            <v-card-title>Resources</v-card-title>
+        </v-card>
+        <v-card>
+            <v-card-title>Qualities</v-card-title>
+        </v-card>
+        <v-dialog
+            v-model="measurableFocused"
+            max-width="600"
+        >
+            <v-card>
+                <v-card-title class="headline">{{measurableCardTitle}}</v-card-title>
+                <v-card-actions>
+                    <v-row>
+                        <v-col cols="7">
+                            <v-btn
+                                icon
+                                @click="lowerRaiseAmount"
+                            >
+                                <v-icon dark>remove</v-icon>
+                            </v-btn>
+                            <v-btn
+                                icon
+                                @click="increaseRaiseAmount"
+                            >
+                                <v-icon dark>add</v-icon>
+                            </v-btn>
+
+                            <v-text-field
+                                type="number"
+                                v-model="measurableRaiseAmount"
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="5">
+                            <v-btn
+                                color="primary"
+                                block
+                            >
+                                Raise {{focusedMeasurable.measurable_type.name.toUpperCase()}}
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-card>
+</template>
+
+<script>
+    import MeasurablePanel from "./MeasurablePanel";
+
+    export default {
+        name: "HeroMeasurablesCard",
+        components: {MeasurablePanel},
+        props: {
+            measurables: {
+                type: Array,
+                required: true
+            }
+        },
+
+        data() {
+            return {
+                focusedMeasurable: {
+                    measurable_type: {
+                        name: ''
+                    }
+                },
+                measurableFocused: false,
+                measurableRaiseAmount: 1
+            }
+        },
+
+        methods: {
+            setFocusedMeasurable(measurable) {
+                this.focusedMeasurable = measurable;
+                this.measurableFocused = true;
+            },
+            lowerRaiseAmount() {
+
+            },
+            increaseRaiseAmount() {
+
+            }
+        },
+
+        computed: {
+            attributes() {
+                return this.measurables.filter(function (measurable) {
+                    return measurable.measurable_type.group === 'attribute';
+                })
+            },
+            measurableName() {
+                return this.focusedMeasurable.measurable_type.name.toUpperCase();
+            },
+            measurableCardTitle() {
+                return this.measurableName() + ': ' + this.focusedMeasurable.amount_raised;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/commandCenter/views/barracks/BarracksHeroView.vue
+++ resources/js/components/commandCenter/views/barracks/BarracksHeroView.vue
@@ -9,64 +9,8 @@
         </v-row>
         <v-row no-gutters>
             <v-col cols="12">
-                <v-card>
-                    <v-card>
-                        <v-card-title>Attributes</v-card-title>
-                        <MeasurablePanel
-                            v-for="(attribute, uuid) in attributes"
-                            :key="uuid"
-                            :measurable="attribute"
-                            @measurableClicked="setFocusedMeasurable"
-                        ></MeasurablePanel>
-                    </v-card>
-                    <v-card>
-                        <v-card-title>Resources</v-card-title>
-                    </v-card>
-                    <v-card>
-                        <v-card-title>Qualities</v-card-title>
-                    </v-card>
-                </v-card>
             </v-col>
         </v-row>
-        <v-dialog
-            v-model="measurableFocused"
-            max-width="600"
-        >
-            <v-card>
-                <v-card-title class="headline">{{}}</v-card-title>
-                <v-card-actions>
-                        <v-row>
-                            <v-col cols="7">
-                                <v-btn
-                                    icon
-                                    @click="lowerRaiseAmount"
-                                >
-                                    <v-icon dark>remove</v-icon>
-                                </v-btn>
-                                <v-btn
-                                    icon
-                                    @click="increaseRaiseAmount"
-                                >
-                                    <v-icon dark>add</v-icon>
-                                </v-btn>
-
-                                <v-text-field
-                                    type="number"
-                                    v-model="measurableRaiseAmount"
-                                ></v-text-field>
-                            </v-col>
-                            <v-col cols="5">
-                                <v-btn
-                                    color="primary"
-                                    block
-                                >
-                                    Raise {{focusedMeasurable.measurable_type.name.toUpperCase()}}
-                                </v-btn>
-                            </v-col>
-                        </v-row>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
     </v-col>
 </template>
 
@@ -83,18 +27,6 @@
             this.setBarracksFocusedHeroBySlug(this.$route.params.heroSlug);
         },
 
-        data() {
-            return {
-                focusedMeasurable: {
-                    measurable_type: {
-                        name: ''
-                    }
-                },
-                measurableFocused: false,
-                measurableRaiseAmount: 1
-            }
-        },
-
         watch: {
             $route (to) {
                 if (to.params.heroSlug && to.params.heroSlug !== this._barracksFocusedHero.slug) {
@@ -107,16 +39,6 @@
             ...mapActions([
                 'setBarracksFocusedHeroBySlug'
             ]),
-            setFocusedMeasurable(measurable) {
-                this.focusedMeasurable = measurable;
-                this.measurableFocused = true;
-            },
-            lowerRaiseAmount() {
-
-            },
-            increaseRaiseAmount() {
-
-            }
         },
 
         computed: {
@@ -124,17 +46,6 @@
                 '_barracksHeroes',
                 '_barracksFocusedHero'
             ]),
-            attributes() {
-                return this._barracksFocusedHero.measurables.filter(function (measurable) {
-                    return measurable.measurable_type.group === 'attribute';
-                })
-            },
-            measurableName() {
-                return this.focusedMeasurable.measurable_type.name.toUpperCase();
-            },
-            measurableCardTitle() {
-                return this.measurableName() + ': ' + this.focusedMeasurable.amount_raised;
-            }
         }
     }
 </script>
