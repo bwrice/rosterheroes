07f8fad5de6e2a846ccebbd56abf52cc432e6af7
Brian Rice <bwrice83@gmail.com>
2019-08-21 14:03:58 -0400

    add aggregate method to record measurable raised

--- app/Aggregates/MeasurableAggregate.php
+++ app/Aggregates/MeasurableAggregate.php
@@ -3,6 +3,7 @@
 namespace App\Aggregates;
 
 use App\StorableEvents\MeasurableCreated;
+use App\StorableEvents\MeasurableRaised;
 use Spatie\EventProjector\AggregateRoot;
 
 final class MeasurableAggregate extends AggregateRoot
@@ -10,6 +11,13 @@ final class MeasurableAggregate extends AggregateRoot
     public function createMeasurable(int $measurableTypID, string $hasMeasurablesType, int $hasMeasurablesID, $amountIncreased = 0)
     {
         $this->recordThat(new MeasurableCreated($measurableTypID, $hasMeasurablesType, $hasMeasurablesID, $amountIncreased));
+
+        return $this;
+    }
+
+    public function raiseMeasurable(int $amount)
+    {
+        $this->recordThat(new MeasurableRaised($amount));
 
         return $this;
     }
