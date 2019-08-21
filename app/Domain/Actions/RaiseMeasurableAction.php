3495e47aae99efb90f0a4166dfb39cbc80c888bb
Brian Rice <bwrice83@gmail.com>
2019-08-21 14:06:09 -0400

    create domain action for raising a measurable

---
+++ app/Domain/Actions/RaiseMeasurableAction.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Aggregates\MeasurableAggregate;
+use App\Domain\Models\Measurable;
+use App\Exceptions\RaiseMeasurableException;
+
+class RaiseMeasurableAction
+{
+
+    /**
+     * @param Measurable $measurable
+     * @param int $amount
+     * @return Measurable
+     * @throws RaiseMeasurableException
+     */
+    public function execute(Measurable $measurable, int $amount): Measurable
+    {
+        if ($amount < 1) {
+            $message =  "Raise amount must be positive. " . $amount . " given.";
+            $code = RaiseMeasurableException::CODE_NON_POSITIVE_NUMBER;
+            throw new RaiseMeasurableException($measurable, $amount, $message, $code);
+        }
+
+        $costToRaise = $measurable->getCostToRaise();
+        $availableExp = $measurable->hasMeasurables->availableExperience();
+        if ($costToRaise > $availableExp) {
+            $message = $costToRaise .  " experience required, but only " . $availableExp . " available";
+            $code = RaiseMeasurableException::INSUFFICIENT_EXPERIENCE;
+            throw new RaiseMeasurableException($measurable, $amount, $message, $code);
+        }
+
+        /** @var MeasurableAggregate $aggregate */
+        $aggregate = MeasurableAggregate::retrieve($measurable->uuid);
+        $aggregate->raiseMeasurable($amount)->persist();
+        return $measurable->fresh();
+    }
+
+}
