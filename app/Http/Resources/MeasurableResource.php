4fe3ed41f193ad7aab197110f76f9f7ed273861e
Brian Rice <bwrice83@gmail.com>
2019-08-21 22:28:48 -0400

    always return measurable_type with measurable resource

--- app/Http/Resources/MeasurableResource.php
+++ app/Http/Resources/MeasurableResource.php
@@ -24,7 +24,7 @@ class MeasurableResource extends JsonResource
         return [
             'uuid' => $this->uuid,
             'amount_raised' => $this->amount_raised,
-            'measurable_type' => new MeasurableTypeResource($this->whenLoaded('measurableType')),
+            'measurable_type' => new MeasurableTypeResource($this->measurableType),
             'cost_to_raise' => $this->getCostToRaise(),
             'current_amount' => $this->getCurrentAmount(),
             'spent_on_raising' => $this->spentOnRaising()
