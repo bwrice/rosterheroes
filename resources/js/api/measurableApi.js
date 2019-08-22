12f2b63506bc4da27e7ae248742bbdd6642e793c
Brian Rice <bwrice83@gmail.com>
2019-08-21 22:28:23 -0400

    add api post method to raise measurable

--- resources/js/api/measurableApi.js
+++ resources/js/api/measurableApi.js
@@ -3,3 +3,10 @@ export async function getCostToRaise(measurableUuid, raiseAmount) {
     let response = await axios.get('/api/v1/measurables/' + measurableUuid + '/raise?amount=' + raiseAmount);
     return response.data;
 }
+
+export async function raise(measurableUuid, raiseAmount) {
+    let response = await axios.post('/api/v1/measurables/' + measurableUuid + '/raise', {
+        amount: raiseAmount
+    });
+    return response.data.data;
+}
