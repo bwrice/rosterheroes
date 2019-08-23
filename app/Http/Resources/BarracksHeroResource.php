638e58b5a3946a4028227b516e6a687f5a45baa5
Brian Rice <bwrice83@gmail.com>
2019-08-22 23:14:45 -0400

    create specific resource for barracks heroes

---
+++ app/Http/Resources/BarracksHeroResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class BarracksHeroResource
+ * @package App\Http\Resources
+ *
+ * @mixin \App\Domain\Models\Hero
+ */
+class BarracksHeroResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'uuid' => $this->uuid,
+            'slug' => $this->slug,
+            'playerSpirit' => new PlayerSpiritResource($this->playerSpirit),
+            'heroRace' => new HeroRaceResource($this->heroRace),
+            'heroClass' => new HeroClassResource($this->heroClass),
+            'measurables' => MeasurableResource::collection($this->measurables)
+        ];
+    }
+}
