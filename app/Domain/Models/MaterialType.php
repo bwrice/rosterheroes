1ed1459a0e27daab7a350e57eef8a067b3138797
Brian Rice <bwrice83@gmail.com>
2019-08-23 12:54:28 -0400

    add materialGroup relation

--- app/Domain/Models/MaterialType.php
+++ app/Domain/Models/MaterialType.php
@@ -10,8 +10,16 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @property int $id
  * @property string $name
+ * @property int $grade
+ *
+ * @property MaterialGroup $materialGroup
  */
 class MaterialType extends Model
 {
     protected $guarded = [];
+
+    public function materialGroup()
+    {
+        return $this->belongsTo(MaterialGroup::class);
+    }
 }
