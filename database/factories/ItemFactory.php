dfad06aeb482e4ea5e36fd86f50a0a9f37fdf7d4
Brian Rice <bwrice83@gmail.com>
2019-08-23 19:27:55 -0400

    fix item model factory

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -4,14 +4,16 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Domain\Models\Item::class, function (Faker $faker) {
 
-    /** @var \App\Domain\Models\ItemBase $itemBase */
-    $itemBase = \App\Domain\Models\ItemBase::where('name','=', \App\Domain\Models\ItemBase::HELMET)->first();
     /** @var \App\Domain\Models\ItemType $itemType */
-    $itemType = $itemBase->itemTypes()->inRandomOrder()->first();
+    $itemType = \App\Domain\Models\ItemType::query()->inRandomOrder()->first();
 
     return [
-        'item_class_id' => \App\Domain\Models\ItemClass::where('name', '=', \App\Domain\Models\ItemClass::GENERIC)->first()->id,
+        'uuid' => \Illuminate\Support\Str::uuid(),
+        'item_class_id' => \App\Domain\Models\ItemClass::query()->where('name', '=', \App\Domain\Models\ItemClass::GENERIC)->first()->id,
         'item_type_id' => $itemType->id,
         'material_type_id' => $itemType->materialTypes()->inRandomOrder()->first()->id,
+        'item_blueprint_id' => function() {
+            return factory(\App\Domain\Models\ItemBlueprint::class)->create()->id;
+        }
     ];
 });
