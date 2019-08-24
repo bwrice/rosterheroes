82b188c68ce9e418cd7a564e7cda5c1151a4aaab
Brian Rice <bwrice83@gmail.com>
2019-08-23 21:03:14 -0400

    add starting items on new hero creation for squad

--- app/Domain/Actions/AddNewHeroToSquadAction.php
+++ app/Domain/Actions/AddNewHeroToSquadAction.php
@@ -9,17 +9,14 @@
 namespace App\Domain\Actions;
 
 
+use App\Domain\Models\ItemBlueprint;
 use App\Exceptions\HeroPostNotFoundException;
 use App\Exceptions\InvalidHeroClassException;
 use App\Domain\Models\Hero;
 use App\Domain\Models\HeroClass;
-use App\Domain\Models\HeroPost;
-use App\Domain\Collections\HeroPostCollection;
 use App\Domain\Models\HeroRace;
 use App\Domain\Models\HeroRank;
 use App\Domain\Models\Squad;
-use App\Squads\HeroClassAvailability;
-use App\Squads\HeroPostAvailability;
 
 class AddNewHeroToSquadAction
 {
@@ -27,10 +24,23 @@ class AddNewHeroToSquadAction
      * @var CreateHeroAction
      */
     private $createHeroAction;
+    /**
+     * @var GenerateItemFromBlueprintAction
+     */
+    private $generateItemFromBlueprintAction;
+    /**
+     * @var FillSlotAction
+     */
+    private $fillSlotAction;
 
-    public function __construct(CreateHeroAction $createHeroAction)
+    public function __construct(
+        CreateHeroAction $createHeroAction,
+        GenerateItemFromBlueprintAction $generateItemFromBlueprintAction,
+        FillSlotAction $fillSlotAction)
     {
         $this->createHeroAction = $createHeroAction;
+        $this->generateItemFromBlueprintAction = $generateItemFromBlueprintAction;
+        $this->fillSlotAction = $fillSlotAction;
     }
 
     /**
@@ -38,11 +48,11 @@ class AddNewHeroToSquadAction
      * @param string $heroName
      * @param HeroClass $heroClass
      * @param HeroRace $heroRace
-     * @return Hero|null
+     * @return Hero
      * @throws HeroPostNotFoundException
      * @throws InvalidHeroClassException
      */
-    public function execute(Squad $squad, string $heroName, HeroClass $heroClass, HeroRace $heroRace)
+    public function execute(Squad $squad, string $heroName, HeroClass $heroClass, HeroRace $heroRace): Hero
     {
         $heroPost= $squad->getHeroPostAvailability()->heroRace($heroRace)->first();
         if (! $heroPost) {
@@ -53,11 +63,20 @@ class AddNewHeroToSquadAction
             throw new InvalidHeroClassException($heroClass);
         }
 
-        // invoke the CreateNewHeroAction
+        // Create hero
         $hero = $this->createHeroAction->execute($heroName, $heroClass, $heroRace, HeroRank::getStarting());
 
+        // Attach hero to hero post
         $heroPost->hero_id = $hero->id;
         $heroPost->save();
+
+        // Create new hero items and slot them
+        $heroClass->getBehavior()->getStartItemBlueprints()->each(function (ItemBlueprint $itemBlueprint) use ($hero) {
+            $hero = $hero->fresh();
+            $item = $this->generateItemFromBlueprintAction->execute($itemBlueprint);
+            $this->fillSlotAction->execute($hero, $item);
+        });
+
         return $hero->fresh();
     }
 }
