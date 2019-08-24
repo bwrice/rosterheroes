562111dcb9ee4a827793e5ffcc0ceb0bb5fe4a04
Brian Rice <bwrice83@gmail.com>
2019-08-24 16:39:15 -0400

    refactor domain to work with items only

--- app/Domain/Actions/FillSlotsWithItemAction.php
+++ app/Domain/Actions/FillSlotsWithItemAction.php
@@ -11,6 +11,7 @@ namespace App\Domain\Actions;
 
 use App\Domain\Interfaces\HasSlots;
 use App\Domain\Interfaces\Slottable;
+use App\Domain\Models\Item;
 use App\Exceptions\FillSlotException;
 
 class FillSlotAction
@@ -18,14 +19,14 @@ class FillSlotAction
 
     /**
      * @param HasSlots $hasSlots
-     * @param Slottable $slottable
+     * @param Item $item
      * @param bool $secondAttempt
      * @throws FillSlotException
      */
-    public function execute(HasSlots $hasSlots, Slottable $slottable, $secondAttempt = false)
+    public function execute(HasSlots $hasSlots, Item $item, $secondAttempt = false)
     {
-        $slotTypeIDs = $slottable->getSlotTypeIDs();
-        $slotsNeededCount = $slottable->getSlotsCount();
+        $slotTypeIDs = $item->getSlotTypeIDs();
+        $slotsNeededCount = $item->getSlotsCount();
         $emptySlots = $hasSlots->getEmptySlots($slotsNeededCount, $slotTypeIDs);
 
         $slotsToEmptyCount = $slotsNeededCount - $emptySlots->count();
@@ -34,23 +35,23 @@ class FillSlotAction
 
             if ($secondAttempt) {
                 $message = "Failed to equip";
-                throw new FillSlotException($hasSlots, $slottable, $message, FillSlotException::CODE_SECOND_ATTEMPT);
+                throw new FillSlotException($hasSlots, $item, $message, FillSlotException::CODE_SECOND_ATTEMPT);
             }
 
             $backup = $hasSlots->getBackupHasSlots();
 
             if (! $backup) {
                 $message = "Failed to find back-up storage";
-                throw new FillSlotException($hasSlots, $slottable, $message, FillSlotException::NO_BACKUP);
+                throw new FillSlotException($hasSlots, $item, $message, FillSlotException::NO_BACKUP);
             }
 
             // Get slots we need to remove from their slots to make space for what we intend to slot
-            $slottables = $hasSlots->getSlots()->filled()->take($slotsToEmptyCount)->getSlottables();
+            $items = $hasSlots->getSlots()->filled()->take($slotsToEmptyCount)->getItems();
             // Now get the slots those slottables are currently slotted into and empty them
-            $slottables->getSlots()->emptySlottables();
+            $items->getSlots()->emptySlottables();
 
             // Re-slot the slottables that were removed
-            $slottables->each(function (Slottable $slottable) use ($backup) {
+            $items->each(function (Slottable $slottable) use ($backup) {
                 $this->execute($backup, $slottable, false);
             });
 
