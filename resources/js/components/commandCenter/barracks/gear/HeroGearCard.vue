bf7e39adad758c2d292b5362bd69926626d0a4c7
Brian Rice <bwrice83@gmail.com>
2019-08-25 17:54:56 -0400

    add gradient to hero gear svg

--- resources/js/components/commandCenter/barracks/gear/HeroGearCard.vue
+++ resources/js/components/commandCenter/barracks/gear/HeroGearCard.vue
@@ -1,23 +1,24 @@
 <template>
-    <v-card class="py-3">
-        <HeroGearSVG></HeroGearSVG>
+    <v-card class="py-3" style="background-image: linear-gradient(to bottom right, #524c59, #7c7287 , #524c59 )">
+        <HeroGearSVG v-if="barracksHeroFromRoute" :hero="barracksHeroFromRoute"></HeroGearSVG>
     </v-card>
 </template>
 
 <script>
     import {mapGetters} from 'vuex';
+    import {barracksHeroMixin} from "../../../../mixins/barracksHeroMixin";
 
     import HeroGearSVG from "./HeroGearSVG";
     export default {
         name: "HeroGearCard",
         components: {HeroGearSVG},
+        mixins: [
+            barracksHeroMixin
+        ],
         computed: {
             ...mapGetters([
                 '_barracksHeroes'
             ]),
-            hero() {
-                let heroSlug = this.$route.params.heroSlug;
-            }
         }
     }
 </script>
