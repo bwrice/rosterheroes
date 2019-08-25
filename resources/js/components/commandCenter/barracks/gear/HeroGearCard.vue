c191b56267408c1fee7dc95fedf312e4b3bd3244
Brian Rice <bwrice83@gmail.com>
2019-08-24 23:29:43 -0400

    build out some of HeroGearCard

---
+++ resources/js/components/commandCenter/barracks/gear/HeroGearCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-card class="py-3">
+        <HeroGearSVG></HeroGearSVG>
+    </v-card>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+
+    import HeroGearSVG from "./HeroGearSVG";
+    export default {
+        name: "HeroGearCard",
+        components: {HeroGearSVG},
+        computed: {
+            ...mapGetters([
+                '_barracksHeroes'
+            ]),
+            hero() {
+                let heroSlug = this.$route.params.heroSlug;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
