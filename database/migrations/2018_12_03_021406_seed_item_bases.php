8d54d7293cc3109c2bfc8fc651906d9c7814d667
Brian Rice <bwrice83@gmail.com>
2019-08-27 14:58:10 -0400

    remove pivots from item bases

--- database/migrations/2018_12_03_021406_seed_item_bases.php
+++ database/migrations/2018_12_03_021406_seed_item_bases.php
@@ -1,424 +1,57 @@
 <?php
 
 use App\Domain\Models\ItemBase;
-use App\Domain\Models\ItemGroup;
-use App\Domain\Models\MeasurableType;
-use App\Domain\Models\SlotType;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+use App\Helpers\ModelNameSeeding\ModelNameSeederMigration;
 
-class SeedItemBases extends Migration
+class SeedItemBases extends ModelNameSeederMigration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
+     * @return string
      */
-    public function up()
+    protected function getModelClass(): string
     {
-        $itemGroups = ItemGroup::all();
-        $measurableTypes = MeasurableType::all();
-        $slotTypes = SlotType::all();
-
-        $itemBases = [
-            [
-                'name' => ItemBase::DAGGER,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::AGILITY,
-                    MeasurableType::FOCUS
-                ]
-            ],
-            [
-                'name' => ItemBase::SWORD,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::VALOR,
-                    MeasurableType::AGILITY
-                ]
-            ],
-            [
-                'name' => ItemBase::AXE,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::STRENGTH,
-                    MeasurableType::VALOR
-                ]
-            ],
-            [
-                'name' => ItemBase::MACE,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::STRENGTH,
-                    MeasurableType::AGILITY
-                ]
-            ],
-            [
-                'name' => ItemBase::BOW,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::AGILITY,
-                    MeasurableType::FOCUS
-                ]
-            ],
-            [
-                'name' => ItemBase::CROSSBOW,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::STRENGTH,
-                    MeasurableType::AGILITY,
-                    MeasurableType::FOCUS
-                ]
-            ],
-            [
-                'name' => ItemBase::THROWING_WEAPON,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::STRENGTH,
-                    MeasurableType::FOCUS
-                ]
-            ],
-            [
-                'name' => ItemBase::POLE_ARM,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::STRENGTH,
-                    MeasurableType::AGILITY,
-                    MeasurableType::FOCUS
-                ]
-            ],
-            [
-                'name' => ItemBase::TWO_HAND_SWORD,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::STRENGTH,
-                    MeasurableType::VALOR,
-                    MeasurableType::AGILITY
-                ]
-            ],
-            [
-                'name' => ItemBase::TWO_HAND_AXE,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::STRENGTH,
-                    MeasurableType::VALOR
-                ]
-            ],
-            [
-                'name' => ItemBase::WAND,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::FOCUS,
-                    MeasurableType::INTELLIGENCE
-                ]
-            ],
-            [
-                'name' => ItemBase::ORB,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::FOCUS,
-                    MeasurableType::APTITUDE
-                ]
-            ],
-            [
-                'name' => ItemBase::STAFF,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::FOCUS,
-                    MeasurableType::APTITUDE,
-                    MeasurableType::INTELLIGENCE
-                ]
-            ],
-            [
-                'name' => ItemBase::PSIONIC_ONE_HAND,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::AGILITY,
-                    MeasurableType::APTITUDE,
-                    MeasurableType::INTELLIGENCE
-                ]
-            ],
-            [
-                'name' => ItemBase::PSIONIC_TWO_HAND,
-                'group' => ItemGroup::WEAPON,
-                'slot_types' => [
-                    SlotType::RIGHT_ARM,
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-                    MeasurableType::STRENGTH,
-                    MeasurableType::FOCUS,
-                    MeasurableType::APTITUDE
-                ]
-            ],
-            [
-                'name' => ItemBase::SHIELD,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::PSIONIC_SHIELD,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::LEFT_ARM
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::HELMET,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::HEAD
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::CAP,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::HEAD
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::EYE_WEAR,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::HEAD
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::HEAVY_ARMOR,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::TORSO
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::LIGHT_ARMOR,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::TORSO
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::ROBES,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::TORSO
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::GLOVES,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::HANDS
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::GAUNTLETS,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::HANDS
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::SHOES,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::FEET
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::BOOTS,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::FEET
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::BELT,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::WAIST
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::SASH,
-                'group' => ItemGroup::ARMOR,
-                'slot_types' => [
-                    SlotType::WAIST
-                ],
-                'measurable types' => [
-
-                ]
-            ],
-            [
-                'name' => ItemBase::NECKLACE,
-                'group' => ItemGroup::JEWELRY,
-                'slot_types' => [
-                    SlotType::NECK
-                ],
-                'measurable types' => [
-                    MeasurableType::HEALTH
-                ]
-            ],
-            [
-                'name' => ItemBase::BRACELET,
-                'group' => ItemGroup::JEWELRY,
-                'slot_types' => [
-                    SlotType::LEFT_WRIST,
-                    SlotType::RIGHT_WRIST
-                ],
-                'measurable types' => [
-                    MeasurableType::STAMINA
-                ]
-            ],
-            [
-                'name' => ItemBase::RING,
-                'group' => ItemGroup::JEWELRY,
-                'slot_types' => [
-                    SlotType::LEFT_RING,
-                    SlotType::RIGHT_RING
-                ],
-                'measurable types' => [
-                    MeasurableType::MANA
-                ]
-            ],
-            [
-                'name' => ItemBase::CROWN,
-                'group' => ItemGroup::JEWELRY,
-                'slot_types' => [
-                    SlotType::HEAD
-                ],
-                'measurable types' => [
-                    MeasurableType::HEALTH
-                ]
-            ],
-        ];
-
-        foreach ( $itemBases as $itemBase ) {
-
-            $itemGroup = $itemGroups->where( 'name', $itemBase['group'] )->first();
-            $itemBaseCreated = $itemGroup->itemBases()->create( [
-                'name' => $itemBase['name'],
-            ] );
-
-            $measurableTypesToSave = $measurableTypes->whereIn( 'name', $itemBase['measurable types'] );
-            /** @var ItemBase $itemBaseCreated */
-            $itemBaseCreated->measurableTypes()->saveMany( $measurableTypesToSave );
-
-            //Add universal and wagon slot type to all current item bases
-            $itemBase['slot_types'][] = SlotType::UNIVERSAL;
-            $slotTypesToSave = $slotTypes->whereIn( 'name', $itemBase['slot_types'] );
-            $itemBaseCreated->slotTypes()->saveMany( $slotTypesToSave );
-        }
+        return ItemBase::class;
     }
 
     /**
-     * Reverse the migrations.
-     *
-     * @return void
+     * @return array
      */
-    public function down()
+    protected function getSeedNames(): array
     {
-        \Illuminate\Support\Facades\DB::table('item_base_slot_type')->truncate();
-        \Illuminate\Support\Facades\DB::table('item_base_measurable_type')->truncate();
-        ItemBase::query()->delete();
+        return [
+            ItemBase::DAGGER,
+            ItemBase::SWORD,
+            ItemBase::AXE,
+            ItemBase::MACE,
+            ItemBase::BOW,
+            ItemBase::CROSSBOW,
+            ItemBase::THROWING_WEAPON,
+            ItemBase::POLE_ARM,
+            ItemBase::TWO_HAND_SWORD,
+            ItemBase::TWO_HAND_AXE,
+            ItemBase::WAND,
+            ItemBase::ORB,
+            ItemBase::STAFF,
+            ItemBase::PSIONIC_ONE_HAND,
+            ItemBase::PSIONIC_TWO_HAND,
+            ItemBase::SHIELD,
+            ItemBase::PSIONIC_SHIELD,
+            ItemBase::HELMET,
+            ItemBase::CAP,
+            ItemBase::EYE_WEAR,
+            ItemBase::HEAVY_ARMOR,
+            ItemBase::LIGHT_ARMOR,
+            ItemBase::ROBES,
+            ItemBase::GLOVES,
+            ItemBase::GAUNTLETS,
+            ItemBase::SHOES,
+            ItemBase::BOOTS,
+            ItemBase::BELT,
+            ItemBase::SASH,
+            ItemBase::NECKLACE,
+            ItemBase::BRACELET,
+            ItemBase::RING,
+            ItemBase::CROWN
+        ];
     }
 }
