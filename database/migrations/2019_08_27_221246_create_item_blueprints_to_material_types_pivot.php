41cb34ce8e2af3fb9e10af14e779635d8386d6cf
Brian Rice <bwrice83@gmail.com>
2019-08-27 18:15:17 -0400

    create blueprint to material type pivot table

---
+++ database/migrations/2019_08_27_221246_create_item_blueprints_to_material_types_pivot.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemBlueprintsToMaterialTypesPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_blueprint_material_type', function (Blueprint $table) {
+            $table->integer('blueprint_id')->unsigned();
+            $table->integer('m_type_id')->unsigned();
+            $table->primary(['blueprint_id', 'm_type_id']);
+            $table->timestamps();
+        });
+
+        Schema::table('item_blueprint_material_type', function (Blueprint $table) {
+            $table->foreign('blueprint_id')->references('id')->on('item_blueprints');
+            $table->foreign('m_type_id')->references('id')->on('material_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_blueprints_to_material_types_pivot');
+    }
+}
