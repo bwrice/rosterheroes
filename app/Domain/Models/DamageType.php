b4b7e315a0a2ab5a225b0ec54e0455de111bb699
Brian Rice <bwrice83@gmail.com>
2019-08-28 23:47:06 -0400

    seed damage types

--- app/Domain/Models/DamageType.php
+++ app/Domain/Models/DamageType.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class DamageType extends Model
 {
-    //
+    public const SINGLE_TARGET = 'single-target';
+    public const MULTI_TARGET = 'multi-target';
+    public const AREA_OF_EFFECT = 'area-of-effect';
+    public const DISPERSED = 'dispersed';
 }
---
+++ database/migrations/2019_08_29_033735_seed_damage_types.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Domain\Models\DamageType;
+use App\Helpers\ModelNameSeeding\ModelNameSeederMigration;
+
+class SeedDamageTypes extends ModelNameSeederMigration
+{
+    protected function getModelClass(): string
+    {
+        return DamageType::class;
+    }
+
+    public function getSeedNames(): array
+    {
+        return [
+            DamageType::SINGLE_TARGET,
+            DamageType::MULTI_TARGET,
+            DamageType::AREA_OF_EFFECT,
+            DamageType::DISPERSED
+        ];
+    }
+}
