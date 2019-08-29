f77eb8d1ceff2cc8cda5eaac664eb752f0ee35da
Brian Rice <bwrice83@gmail.com>
2019-08-28 23:50:17 -0400

    seed target priorities

--- app/Domain/Models/TargetPriority.php
+++ app/Domain/Models/TargetPriority.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class TargetPriority extends Model
 {
-    //
+    public const ANY = 'any';
 }
---
+++ database/migrations/2019_08_29_034851_seed_target_priorities.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Domain\Models\TargetPriority;
+use App\Helpers\ModelNameSeeding\ModelNameSeederMigration;
+
+class SeedTargetPriorities extends ModelNameSeederMigration
+{
+    protected function getModelClass(): string
+    {
+        return TargetPriority::class;
+    }
+
+    public function getSeedNames(): array
+    {
+        return [
+            TargetPriority::ANY
+        ];
+    }
+}
