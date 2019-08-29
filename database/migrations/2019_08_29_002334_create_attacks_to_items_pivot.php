64c18c0f7aaaa4622f173906306a37802e546c84
Brian Rice <bwrice83@gmail.com>
2019-08-28 20:33:01 -0400

    add attacks to items pivot table

---
+++ database/migrations/2019_08_29_002334_create_attacks_to_items_pivot.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttacksToItemsPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('attack_item', function (Blueprint $table) {
+            $table->integer('attack_id')->unsigned();
+            $table->bigInteger('item_id')->unsigned();
+            $table->primary(['attack_id', 'item_id']);
+            $table->timestamps();
+        });
+
+        Schema::table('player_position', function (Blueprint $table) {
+            $table->foreign('attack_id')->references('id')->on('attacks');
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
