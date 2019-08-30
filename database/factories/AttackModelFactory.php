c3d3f8e30b22ac5f61f841aa4f67558d49cd9e4a
Brian Rice <bwrice83@gmail.com>
2019-08-30 19:09:38 -0400

    create Attack model factory

---
+++ database/factories/AttackModelFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Domain\Models\Attack::class, function (Faker $faker) {
+    return [
+        'name' => 'Factory Attack',
+        'damage_type_id' => function() {
+            return \App\Domain\Models\DamageType::query()->inRandomOrder()->first()->id;
+        },
+        'target_range_id' => function() {
+            return \App\Domain\Models\TargetRange::query()->inRandomOrder()->first()->id;
+        },
+        'target_priority_id' => function() {
+            return \App\Domain\Models\TargetPriority::query()->inRandomOrder()->first()->id;
+        },
+        'grade' => 50,
+        'speed_rating' => 50,
+        'damage_rating' => 50,
+        'resource_costs' => json_encode([]),
+        'requirements' => json_encode([])
+    ];
+});
