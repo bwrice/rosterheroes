1347269b221116199c94751721c2cc1cf2c1162f
Brian Rice <bwrice83@gmail.com>
2019-09-11 16:11:53 -0400

    implement getBaseDamageMeasurablesModifier() for wand

--- app/Domain/Behaviors/ItemBases/Weapons/WandBehavior.php
+++ app/Domain/Behaviors/ItemBases/Weapons/WandBehavior.php
@@ -13,9 +13,12 @@ use App\Domain\Behaviors\ItemBases\Weapons\ArmBehaviors\SingleArmBehavior;
 use App\Domain\Behaviors\ItemBases\Weapons\WeaponBehavior;
 use App\Domain\Behaviors\ItemGroup\WeaponGroup;
 use App\Domain\Interfaces\UsesItems;
+use App\Domain\Models\MeasurableType;
 
 class WandBehavior extends WeaponBehavior
 {
+    public const SPEED_RATING = 70;
+    public const BASE_DAMAGE_RAGING = 30;
 
     public function __construct(WeaponGroup $weaponGroup, SingleArmBehavior $armBehavior)
     {
@@ -51,16 +54,18 @@ class WandBehavior extends WeaponBehavior
 
     protected function getBaseDamageMeasurablesModifier(UsesItems $usesItems): float
     {
-        return 0;
+        $aptitudeBonus =  .02 * $usesItems->getMeasurableAmount(MeasurableType::APTITUDE);
+        $intelligenceBonus =  .02 * $usesItems->getMeasurableAmount(MeasurableType::INTELLIGENCE);
+        return 1 + ($aptitudeBonus + $intelligenceBonus);
     }
 
     protected function getStartingSpeedRating(): int
     {
-        return 1;
+        return self::SPEED_RATING;
     }
 
     protected function getStartingBaseDamageRating(): int
     {
-        return 1;
+        return self::BASE_DAMAGE_RAGING;
     }
 }
