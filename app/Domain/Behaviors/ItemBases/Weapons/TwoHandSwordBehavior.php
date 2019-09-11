d6079356bdbe54430b3143280f39f756ab4be0c7
Brian Rice <bwrice83@gmail.com>
2019-09-11 12:10:42 -0400

    implement getBaseDamageModifier for two hand sword

--- app/Domain/Behaviors/ItemBases/Weapons/TwoHandSwordBehavior.php
+++ app/Domain/Behaviors/ItemBases/Weapons/TwoHandSwordBehavior.php
@@ -13,9 +13,13 @@ use App\Domain\Behaviors\ItemBases\Weapons\ArmBehaviors\TwoArmBehavior;
 use App\Domain\Behaviors\ItemBases\Weapons\WeaponBehavior;
 use App\Domain\Behaviors\ItemGroup\WeaponGroup;
 use App\Domain\Interfaces\UsesItems;
+use App\Domain\Models\MeasurableType;
 
 class TwoHandSwordBehavior extends WeaponBehavior
 {
+    public const SPEED_RATING = 32;
+    public const BASE_DAMAGE_RAGING = 9;
+
     public function __construct(WeaponGroup $weaponGroup, TwoArmBehavior $armBehavior)
     {
         parent::__construct($weaponGroup, $armBehavior);
@@ -48,8 +52,13 @@ class TwoHandSwordBehavior extends WeaponBehavior
         return .95;
     }
 
+
     public function getBaseDamageModifier(UsesItems $usesItems = null): float
     {
-        return $this->itemBaseDamageModifier() / $this->getCombatSpeedModifier();
+        $strengthModifier =  1 + $usesItems->getMeasurableAmount(MeasurableType::STRENGTH)/60;
+        $valorModifier =  1 + $usesItems->getMeasurableAmount(MeasurableType::VALOR)/30;
+        $baseDamageModifier = self::BASE_DAMAGE_RAGING/self::SPEED_RATING;
+        $twoHandBonus = 1.6;
+        return $twoHandBonus * $strengthModifier * $valorModifier * $baseDamageModifier;
     }
 }
