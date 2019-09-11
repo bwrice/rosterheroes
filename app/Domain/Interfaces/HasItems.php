eb9b44cb875b3ac7062f8902344605316396e268
Brian Rice <bwrice83@gmail.com>
2019-09-11 09:23:26 -0400

    simplify interface

--- app/Domain/Interfaces/HasItems.php
+++ app/Domain/Interfaces/HasItems.php
@@ -6,15 +6,5 @@ namespace App\Domain\Interfaces;
 
 interface HasItems
 {
-    public function getStrengthAmount(): int;
-
-    public function getValorAmount(): int;
-
-    public function getAgilityAmount(): int;
-
-    public function getFocusAmount(): int;
-
-    public function getAptitudeAmount(): int;
-
-    public function getIntelligenceAmount(): int;
+    public function getMeasurableAmount(string $measurableTypeName): int;
 }
