53fda178177688e041aee4fc3b1ab106e44226ae
Brian Rice <bwrice83@gmail.com>
2019-09-11 12:10:54 -0400

    implement getBaseDamageModifier for throwing weapon

--- app/Domain/Behaviors/ItemBases/Weapons/ThrowingWeaponBehavior.php
+++ app/Domain/Behaviors/ItemBases/Weapons/ThrowingWeaponBehavior.php
@@ -14,9 +14,13 @@ use App\Domain\Behaviors\ItemBases\Weapons\ArmBehaviors\TwoArmBehavior;
 use App\Domain\Behaviors\ItemBases\Weapons\WeaponBehavior;
 use App\Domain\Behaviors\ItemGroup\WeaponGroup;
 use App\Domain\Interfaces\UsesItems;
+use App\Domain\Models\MeasurableType;
 
 class ThrowingWeaponBehavior extends WeaponBehavior
 {
+    public const SPEED_RATING = 65;
+    public const BASE_DAMAGE_RAGING = 35;
+
     public function __construct(WeaponGroup $weaponGroup, SingleArmBehavior $armBehavior)
     {
         parent::__construct($weaponGroup, $armBehavior);
@@ -51,6 +55,10 @@ class ThrowingWeaponBehavior extends WeaponBehavior
 
     public function getBaseDamageModifier(UsesItems $usesItems = null): float
     {
-        return $this->itemBaseDamageModifier() / $this->getCombatSpeedModifier();
+        $strengthModifier =  1 + $usesItems->getMeasurableAmount(MeasurableType::STRENGTH)/80;
+        $agilityModifier =  1 + $usesItems->getMeasurableAmount(MeasurableType::AGILITY)/80;
+        $focusModifier =  1 + $usesItems->getMeasurableAmount(MeasurableType::AGILITY)/35;
+        $baseDamageModifier = self::BASE_DAMAGE_RAGING/self::SPEED_RATING;
+        return $strengthModifier * $agilityModifier * $agilityModifier * $focusModifier * $baseDamageModifier;
     }
 }
