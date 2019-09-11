36b2a847a27afbc3759c7f02a993cc5349baed7b
Brian Rice <bwrice83@gmail.com>
2019-09-11 12:11:22 -0400

    implement getBaseDamageModifier for bow

--- app/Domain/Behaviors/ItemBases/Weapons/BowBehavior.php
+++ app/Domain/Behaviors/ItemBases/Weapons/BowBehavior.php
@@ -12,9 +12,13 @@ namespace App\Domain\Behaviors\ItemBases\Weapons;
 use App\Domain\Behaviors\ItemBases\Weapons\ArmBehaviors\TwoArmBehavior;
 use App\Domain\Behaviors\ItemGroup\WeaponGroup;
 use App\Domain\Interfaces\UsesItems;
+use App\Domain\Models\MeasurableType;
 
 class BowBehavior extends WeaponBehavior
 {
+    public const SPEED_RATING = 65;
+    public const BASE_DAMAGE_RAGING = 35;
+
     public function __construct(WeaponGroup $weaponGroup, TwoArmBehavior $armBehavior)
     {
         parent::__construct($weaponGroup, $armBehavior);
@@ -49,7 +53,12 @@ class BowBehavior extends WeaponBehavior
 
     public function getBaseDamageModifier(UsesItems $usesItems = null): float
     {
-        return $this->itemBaseDamageModifier() / $this->getCombatSpeedModifier();
+        $strengthModifier =  1 + $usesItems->getMeasurableAmount(MeasurableType::STRENGTH)/80;
+        $agilityModifier =  1 + $usesItems->getMeasurableAmount(MeasurableType::AGILITY)/80;
+        $focusModifier =  1 + $usesItems->getMeasurableAmount(MeasurableType::FOCUS)/35;
+        $baseDamageModifier = self::BASE_DAMAGE_RAGING/self::SPEED_RATING;
+        $twoHandBonus = 1.6;
+        return $twoHandBonus * $strengthModifier * $agilityModifier * $agilityModifier * $focusModifier * $baseDamageModifier;
     }
 
 }
