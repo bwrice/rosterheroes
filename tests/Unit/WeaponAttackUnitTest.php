390664f1a463ced73a5e3a98c94b248d94095d3e
Brian Rice <bwrice83@gmail.com>
2019-09-13 14:19:01 -0400

    test combat speed using heroes and items

--- tests/Unit/WeaponAttackUnitTest.php
+++ tests/Unit/WeaponAttackUnitTest.php
@@ -384,75 +384,84 @@ class WeaponAttackUnitTest extends TestCase
 
     /**
      * @test
-     * @param $weaponBehaviorClass
-     * @dataProvider provides_higher_agility_increases_the_speed_of_weapons
+     * @dataProvider provides_weapon_speed_is_increased_by_agility
+     * @param $itemBaseName
      */
-    public function higher_agility_increases_the_speed_of_weapons($weaponBehaviorClass)
+    public function weapon_speed_is_increased_by_agility($itemBaseName)
     {
+        /** @var ItemType $itemType */
+        $itemType = ItemType::query()->whereHas('itemBase', function (Builder $builder) use ($itemBaseName) {
+            return $builder->where('name', '=', $itemBaseName);
+        })->inRandomOrder()->first();
 
-        /** @var WeaponBehavior $weaponBehavior */
-        $weaponBehavior = app($weaponBehaviorClass);
+        $this->item->item_type_id = $itemType->id;
+        $this->item->save();
 
-        $this->usesItems->setMeasurable(MeasurableType::AGILITY, 10);
-        $lowAgilityModifier = $weaponBehavior->getCombatSpeedBonus($this->usesItems);
+        $measurable = $this->hero->getMeasurable(MeasurableType::AGILITY);
+        $measurable->amount_raised = 0;
+        $measurable->save();
 
+        $lowMeasurableBaseDamage = $this->attack->getCombatSpeed($this->item->fresh());
 
-        $this->usesItems->setMeasurable(MeasurableType::AGILITY, 99);
-        $higherAgilityModifier = $weaponBehavior->getCombatSpeedBonus($this->usesItems);
+        $measurable->amount_raised = 99;
+        $measurable->save();
 
-        $diff = $higherAgilityModifier - $lowAgilityModifier;
+        $higherMeasurableBaseDamage = $this->attack->getCombatSpeed($this->item->fresh());
+
+        $diff = $higherMeasurableBaseDamage - $lowMeasurableBaseDamage;
         // Make sure the diff is greater than PHP float error, AKA, a number very close to zero
         $this->assertGreaterThan(PHP_FLOAT_EPSILON, $diff);
     }
 
-    public function provides_higher_agility_increases_the_speed_of_weapons()
+    public function provides_weapon_speed_is_increased_by_agility()
     {
+
         return [
-            ItemBase::WAND => [
-                'weaponBehaviorClass' => WandBehavior::class
-            ],
-            ItemBase::ORB => [
-                'weaponBehaviorClass' => OrbBehavior::class
-            ],
-            ItemBase::STAFF => [
-                'weaponBehaviorClass' => StaffBehavior::class
-            ],
-            ItemBase::PSIONIC_ONE_HAND => [
-                'weaponBehaviorClass' => PsionicOneHandBehavior::class
-            ],
-            ItemBase::PSIONIC_TWO_HAND => [
-                'weaponBehaviorClass' => PsionicTwoHandBehavior::class
-            ],
-            ItemBase::CROSSBOW => [
-                'weaponBehaviorClass' => CrossbowBehavior::class
-            ],
-            ItemBase::AXE => [
-                'weaponBehaviorClass' => AxeBehavior::class
-            ],
-            ItemBase::MACE => [
-                'weaponBehaviorClass' => MaceBehavior::class
+            ItemBase::DAGGER => [
+                'itemBaseName' => ItemBase::DAGGER,
             ],
             ItemBase::SWORD => [
-                'weaponBehaviorClass' => SwordBehavior::class
+                'itemBaseName' => ItemBase::SWORD,
             ],
-            ItemBase::TWO_HAND_AXE => [
-                'weaponBehaviorClass' => TwoHandAxeBehavior::class
+            ItemBase::AXE => [
+                'itemBaseName' => ItemBase::AXE,
             ],
-            ItemBase::TWO_HAND_SWORD => [
-                'weaponBehaviorClass' => TwoHandSwordBehavior::class
-            ],
-            ItemBase::POLE_ARM => [
-                'weaponBehaviorClass' => PoleArmBehavior::class
-            ],
-            ItemBase::DAGGER => [
-                'weaponBehaviorClass' => DaggerBehavior::class
+            ItemBase::MACE => [
+                'itemBaseName' => ItemBase::MACE,
             ],
             ItemBase::BOW => [
-                'weaponBehaviorClass' => BowBehavior::class
+                'itemBaseName' => ItemBase::BOW,
+            ],
+            ItemBase::CROSSBOW => [
+                'itemBaseName' => ItemBase::CROSSBOW,
             ],
             ItemBase::THROWING_WEAPON => [
-                'weaponBehaviorClass' => ThrowingWeaponBehavior::class
+                'itemBaseName' => ItemBase::THROWING_WEAPON,
             ],
+            ItemBase::POLE_ARM => [
+                'itemBaseName' => ItemBase::POLE_ARM,
+            ],
+            ItemBase::TWO_HAND_SWORD => [
+                'itemBaseName' => ItemBase::TWO_HAND_SWORD,
+            ],
+            ItemBase::TWO_HAND_AXE => [
+                'itemBaseName' => ItemBase::TWO_HAND_AXE,
+            ],
+            ItemBase::WAND => [
+                'itemBaseName' => ItemBase::WAND,
+            ],
+            ItemBase::ORB => [
+                'itemBaseName' => ItemBase::ORB,
+            ],
+            ItemBase::STAFF => [
+                'itemBaseName' => ItemBase::STAFF,
+            ],
+            ItemBase::PSIONIC_ONE_HAND => [
+                'itemBaseName' => ItemBase::PSIONIC_ONE_HAND,
+            ],
+            ItemBase::PSIONIC_TWO_HAND => [
+                'itemBaseName' => ItemBase::PSIONIC_TWO_HAND,
+            ]
         ];
     }
 }
