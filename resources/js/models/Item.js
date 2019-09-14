85491d4ec28886bd177823708b47709e15575a94
Brian Rice <bwrice83@gmail.com>
2019-09-14 17:53:48 -0400

    map item attacks into JS model

--- resources/js/models/Item.js
+++ resources/js/models/Item.js
@@ -1,7 +1,11 @@
+import Attack from "./Attack";
 
 export default class Item {
 
-    constructor({name = ''}) {
+    constructor({name = '', attacks = []}) {
         this.name = name;
+        this.attacks = attacks.map(function (attack) {
+            return new Attack(attack);
+        })
     }
 }
