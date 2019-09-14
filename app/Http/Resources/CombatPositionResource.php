e807f55c0b44988607e57a1a58480fe8a5edf13d
Brian Rice <bwrice83@gmail.com>
2019-09-14 15:58:59 -0400

    add icons to resource

--- app/Http/Resources/CombatPositionResource.php
+++ app/Http/Resources/CombatPositionResource.php
@@ -22,7 +22,9 @@ class CombatPositionResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name' => $this->name
+            'name' => $this->name,
+            'attacker_icon' => $this->attackerIcon(),
+            'target_icon' => $this->targetIcon()
         ];
     }
 }
