1505bb60ea6703ccfdd0deb441b2dd62d3da545b
Brian Rice <bwrice83@gmail.com>
2019-09-16 11:07:07 -0400

    test item type increases item weight

---
+++ tests/Unit/ItemUnitTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Domain\Models\Item;
+use App\Domain\Models\ItemBase;
+use App\Domain\Models\ItemType;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ItemUnitTest extends TestCase
+{
+    /** @var Item */
+    protected $item;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->item = factory(Item::class)->create();
+    }
+
+
+    /**
+     * @test
+     */
+    public function a_higher_grade_item_type_of_the_same_base_and_material_weighs_more()
+    {
+        $itemBaseID = ItemBase::query()->inRandomOrder()->first()->id;
+        $itemTypes = ItemType::query()->where('item_base_id', '=', $itemBaseID)->orderBy('grade')->get();
+        $this->assertGreaterThan(1, $itemTypes->count());
+
+        /** @var ItemType $type1 */
+        $type1 = $itemTypes->shift();
+        /** @var ItemType $type2 */
+        $type2 = $itemTypes->shift();
+
+        $this->assertGreaterThan($type1->grade, $type2->grade);
+
+        $this->item->item_type_id = $type1->id;
+        $this->item->save();
+        $weight1 = $this->item->fresh()->getWeight();
+
+        $this->item->item_type_id = $type2->id;
+        $this->item->save();
+        $weight2 = $this->item->fresh()->getWeight();
+
+        $this->assertGreaterThan($weight1, $weight2);
+    }
+
+    /**
+     * @test
+     */
+    public function a_higher_grade_material_of_the_same_item_type_weighs_more()
+    {
+        //TODO
+        $this->assertTrue(true);
+    }
+
+
+    /**
+     * @test
+     */
+    public function a_two_handed_weapon_weighs_more_than_single_hand_weapon()
+    {
+        //TODO
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function shields_are_very_heavy()
+    {
+        //TODO
+        $this->assertTrue(true);
+    }
+}
