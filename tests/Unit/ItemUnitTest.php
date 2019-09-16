6095cfd587317e2d203391f85450ce4f171e0125
Brian Rice <bwrice83@gmail.com>
2019-09-16 14:06:12 -0400

    test item weight increased by material grade

--- tests/Unit/ItemUnitTest.php
+++ tests/Unit/ItemUnitTest.php
@@ -5,6 +5,8 @@ namespace Tests\Unit;
 use App\Domain\Models\Item;
 use App\Domain\Models\ItemBase;
 use App\Domain\Models\ItemType;
+use App\Domain\Models\Material;
+use App\Domain\Models\MaterialType;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -19,7 +21,6 @@ class ItemUnitTest extends TestCase
         parent::setUp();
         $this->item = factory(Item::class)->create();
     }
-
 
     /**
      * @test
@@ -51,10 +52,28 @@ class ItemUnitTest extends TestCase
     /**
      * @test
      */
-    public function a_higher_grade_material_of_the_same_item_type_weighs_more()
+    public function an_item_with_a_higher_grade_material_of_the_same_material_type_for_the_item_type_weighs_more()
     {
-        //TODO
-        $this->assertTrue(true);
+        $materialTypeID = MaterialType::query()->inRandomOrder()->first()->id;
+        $materials = Material::query()->where('material_type_id', '=', $materialTypeID)->orderBy('grade')->get();
+        $this->assertGreaterThan(1, $materials->count());
+
+        /** @var Material $material1 */
+        $material1 = $materials->shift();
+        /** @var Material $material2 */
+        $material2 = $materials->shift();
+
+        $this->assertGreaterThan($material1->grade, $material2->grade);
+
+        $this->item->material_id = $material1->id;
+        $this->item->save();
+        $weight1 = $this->item->fresh()->getWeight();
+
+        $this->item->material_id = $material2->id;
+        $this->item->save();
+        $weight2 = $this->item->fresh()->getWeight();
+
+        $this->assertGreaterThan($weight1, $weight2);
     }
 
 
