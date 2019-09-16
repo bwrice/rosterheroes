e1de7642bb27c0bbb86f9b0d8cae0a12b7cd3705
Brian Rice <bwrice83@gmail.com>
2019-09-16 15:04:33 -0400

    add factory method for getting material type behavior

--- app/Domain/Models/MaterialType.php
+++ app/Domain/Models/MaterialType.php
@@ -2,9 +2,21 @@
 
 namespace App\Domain\Models;
 
+use App\Domain\Behaviors\MaterialTypes\BoneBehavior;
+use App\Domain\Behaviors\MaterialTypes\ClothBehavior;
+use App\Domain\Behaviors\MaterialTypes\GemstoneBehavior;
+use App\Domain\Behaviors\MaterialTypes\HideBehavior;
+use App\Domain\Behaviors\MaterialTypes\MaterialTypeBehaviorInterface;
+use App\Domain\Behaviors\MaterialTypes\MetalBehavior;
+use App\Domain\Behaviors\MaterialTypes\PreciousMetalBehavior;
+use App\Domain\Behaviors\MaterialTypes\PsionicBehavior;
+use App\Domain\Behaviors\MaterialTypes\WoodBehavior;
+use App\Domain\Models\Traits\HasUniqueNames;
+use App\Exceptions\UnknownBehaviorException;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * @property int $id
  * @property string $name
  */
 class MaterialType extends Model
@@ -18,10 +30,40 @@ class MaterialType extends Model
     const PRECIOUS_METAL = 'precious-metal';
     const PSIONIC = 'psionic';
 
+    use HasUniqueNames;
+
     protected $guarded = [];
 
     public function materials()
     {
         return $this->hasMany(Material::class);
+    }
+
+    public function getBehavior(): MaterialTypeBehaviorInterface
+    {
+        switch ($this->name) {
+            case self::HIDE:
+                return app(HideBehavior::class);
+            case self::METAL:
+                return app(MetalBehavior::class);
+            case self::CLOTH:
+                return app(ClothBehavior::class);
+            case self::WOOD:
+                return app(WoodBehavior::class);
+            case self::GEMSTONE:
+                return app(GemstoneBehavior::class);
+            case self::BONE:
+                return app(BoneBehavior::class);
+            case self::PRECIOUS_METAL:
+                return app(PreciousMetalBehavior::class);
+            case self::PSIONIC:
+                return app(PsionicBehavior::class);
+        }
+        throw new UnknownBehaviorException($this->name, MaterialTypeBehaviorInterface::class);
+    }
+
+    public function getWeightModifier()
+    {
+
     }
 }
