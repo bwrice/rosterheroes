5ed26dcd48414195750e2562cb6a71b8910aea0c
Brian Rice <bwrice83@gmail.com>
2019-09-17 21:26:05 -0400

    create enchantment resource

---
+++ app/Http/Resources/EnchantmentResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\Enchantment;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class EnchantmentResource
+ * @package App\Http\Resources
+ *
+ * @mixin Enchantment
+ */
+class EnchantmentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'measurableBoosts' => MeasurableBoostResource::collection($this->measurableBoosts)
+        ];
+    }
+}
