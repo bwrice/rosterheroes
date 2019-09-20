2121c11b93bd8ffc828bf822056ab02220c01580
Brian Rice <bwrice83@gmail.com>
2019-09-19 22:09:15 -0400

    add item-type prop to Item JS model

--- resources/js/models/Item.js
+++ resources/js/models/Item.js
@@ -1,11 +1,13 @@
 import Attack from "./Attack";
+import ItemType from "./ItemType";
 
 export default class Item {
 
-    constructor({name = '', attacks = []}) {
+    constructor({name = '', attacks = [], itemType}) {
         this.name = name;
         this.attacks = attacks.map(function (attack) {
             return new Attack(attack);
-        })
+        });
+        this.itemType = itemType ? new ItemType(itemType) : new ItemType({});
     }
 }
