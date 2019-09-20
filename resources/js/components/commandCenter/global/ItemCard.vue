f262deaa9ac426fae57ecf39d06d10e116552a3c
Brian Rice <bwrice83@gmail.com>
2019-09-20 11:21:03 -0400

    render Item properties in card

--- resources/js/components/commandCenter/global/ItemCard.vue
+++ resources/js/components/commandCenter/global/ItemCard.vue
@@ -1,19 +1,38 @@
 <template>
-    <v-row>
-        <v-col cols="3"></v-col>
-        <v-col cols="6">
-            <p>{{item.name}}</p>
-        </v-col>
-        <v-col cols="4"></v-col>
-    </v-row>
+    <v-card>
+        <v-card-text>
+            <v-row>
+                <h4>{{item.name}}</h4>
+            </v-row>
+            <v-row>
+                <v-col cols="6">
+                    <p class="caption">
+                        Type: {{item.itemType.name}} <br>
+                        Class: {{item.itemClass.name}} <br>
+                        Material: {{item.material.name}} <br>
+                    </p>
+                </v-col>
+                <v-col cols="6">
+                    <p class="caption">
+                        Protection: {{item.protection}} <br>
+                        Block %: {{item.blockChance}} <br>
+                        Burden: {{item.burden}} <br>
+                        Value: {{item.value}} <br>
+                    </p>
+                </v-col>
+            </v-row>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
+    import Item from "../../../models/Item";
+
     export default {
         name: "ItemCard",
         props: {
             item: {
-                type: Object,
+                type: Item,
                 required: true
             }
         }
