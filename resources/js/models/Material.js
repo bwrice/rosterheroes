4af4aed270786736fe547f3a98ae23e7c44f9202
Brian Rice <bwrice83@gmail.com>
2019-09-20 11:17:37 -0400

    add materialType prop to Material JS model

--- resources/js/models/Material.js
+++ resources/js/models/Material.js
@@ -1,7 +1,9 @@
+import MaterialType from "./MaterialType";
 
 export default class Material {
 
     constructor({name = '', materialType}) {
         this.name = name;
+        this.materialType = materialType ? new MaterialType(materialType) : new MaterialType({});
     }
 }
