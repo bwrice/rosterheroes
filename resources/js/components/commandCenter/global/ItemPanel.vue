ec96189bf2e17c978746abcef3db64cf07651d09
Brian Rice <bwrice83@gmail.com>
2019-09-19 20:18:55 -0400

    create item panel component

---
+++ resources/js/components/commandCenter/global/ItemPanel.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-row>
+        <v-col cols="3"></v-col>
+        <v-col cols="5">
+            <p>{{item.name}}</p>
+        </v-col>
+        <v-col cols="4"></v-col>
+    </v-row>
+</template>
+
+<script>
+    export default {
+        name: "ItemPanel",
+        props: {
+            item: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
