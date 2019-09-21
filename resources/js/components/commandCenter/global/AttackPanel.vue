2859516409c205c58a58d36d5e7241570f085f89
Brian Rice <bwrice83@gmail.com>
2019-09-20 20:46:35 -0400

    start building out AttackPanel component

---
+++ resources/js/components/commandCenter/global/AttackPanel.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-sheet>
+        <v-row>
+            <v-col cols="2"></v-col>
+            <v-col cols="6">{{attack.name}}</v-col>
+            <v-col cols="4">
+                <v-row>
+                    <v-col cols="6" class="pa-0">
+                        <v-img
+                            :src="attack.attackerPosition.icon.src"
+                            :alt="attack.attackerPosition.icon.alt"
+                        >
+                        </v-img>
+                    </v-col>
+                    <v-col cols="6" class="pa-0">
+                        <v-img
+                            :src="attack.targetPosition.icon.src"
+                            :alt="attack.targetPosition.icon.alt"
+                        >
+                        </v-img>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="6"></v-col>
+                    <v-col cols="6"></v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-sheet>
+</template>
+
+<script>
+    import Attack from "../../../models/Attack";
+
+    export default {
+        name: "AttackPanel",
+        props: {
+            attack: {
+                type: Attack,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
