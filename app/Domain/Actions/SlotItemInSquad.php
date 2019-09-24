bcf0fe41deb673c1528dd3c5b2c19e796ece7875
Brian Rice <bwrice83@gmail.com>
2019-09-24 09:23:29 -0400

    stash item if squad is full

--- app/Domain/Actions/SlotItemInSquad.php
+++ app/Domain/Actions/SlotItemInSquad.php
@@ -12,7 +12,14 @@ class SlotItemInSquad
     public function execute(Squad $squad, Item $item)
     {
         $slotsNeeded = $item->getSlotsCount();
-        $slotsToFill = $squad->slots()->take($slotsNeeded)->get();
-        $item->slots()->saveMany($slotsToFill);
+        $squadSlots = $squad->slots->slotEmpty();
+        if ($squadSlots->count() < $slotsNeeded) {
+            $stash = $squad->getLocalStash();
+            $slotsToFill = $stash->getEmptySlots($slotsNeeded);
+            $item->slots()->saveMany($slotsToFill);
+        } else {
+            $slotsToFill = $squadSlots->take($slotsNeeded);
+            $item->slots()->saveMany($slotsToFill);
+        }
     }
 }
