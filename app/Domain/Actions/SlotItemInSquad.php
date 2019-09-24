82e4b5decf2bace106a8b718bb54b219a6156942
Brian Rice <bwrice83@gmail.com>
2019-09-24 12:00:08 -0400

    slot in storehouse when available

--- app/Domain/Actions/SlotItemInSquad.php
+++ app/Domain/Actions/SlotItemInSquad.php
@@ -12,14 +12,39 @@ class SlotItemInSquad
     public function execute(Squad $squad, Item $item)
     {
         $slotsNeeded = $item->getSlotsCount();
-        $squadSlots = $squad->slots->slotEmpty();
-        if ($squadSlots->count() < $slotsNeeded) {
+        $emptySquadSlots = $squad->slots->slotEmpty();
+        if ($emptySquadSlots->count() >= $slotsNeeded) {
+            // Squad
+            $slotsToFill = $emptySquadSlots->take($slotsNeeded);
+            $item->slots()->saveMany($slotsToFill);
+        } elseif ($this->storeHouseHasEnoughSlots($squad, $slotsNeeded)) {
+            // StoreHouse
+            $localStoreHouse = $squad->getLocalStoreHouse();
+            $emptyStoreHouseSlots = $localStoreHouse->slots->slotEmpty()->take($slotsNeeded);
+            $item->slots()->saveMany($emptyStoreHouseSlots);
+        } else {
+            // Stash
             $stash = $squad->getLocalStash();
             $slotsToFill = $stash->getEmptySlots($slotsNeeded);
             $item->slots()->saveMany($slotsToFill);
-        } else {
-            $slotsToFill = $squadSlots->take($slotsNeeded);
-            $item->slots()->saveMany($slotsToFill);
         }
+    }
+
+    /**
+     * @param Squad $squad
+     * @param int $slotsNeeded
+     * @return bool
+     */
+    protected function storeHouseHasEnoughSlots(Squad $squad, int $slotsNeeded)
+    {
+        $localStoreHouse = $squad->getLocalStoreHouse();
+        if (! $localStoreHouse) {
+            return false;
+        }
+        $emptyStoreHouseSlots = $localStoreHouse->slots->slotEmpty();
+        if ($emptyStoreHouseSlots->count() < $slotsNeeded) {
+            return false;
+        }
+        return true;
     }
 }
