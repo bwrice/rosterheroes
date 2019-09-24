dc247a9a5bd7a2537d9a11ea3eff1b00355796cb
Brian Rice <bwrice83@gmail.com>
2019-09-24 15:39:29 -0400

    create HeroPolicy

---
+++ app/Policies/HeroPolicy.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Policies;
+
+use App\Domain\Models\Hero;
+use App\Domain\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class HeroPolicy
+{
+    use HandlesAuthorization;
+
+    public const MANAGE = 'manage';
+
+    /**
+     * @var SquadPolicy
+     */
+    private $squadPolicy;
+
+    public function __construct(SquadPolicy $squadPolicy)
+    {
+        $this->squadPolicy = $squadPolicy;
+    }
+
+    public function manage(User $user, Hero $hero)
+    {
+        $squad = $hero->getSquad();
+        if (! $squad) {
+            return false;
+        }
+        return $this->squadPolicy->manage($user, $squad);
+    }
+}
