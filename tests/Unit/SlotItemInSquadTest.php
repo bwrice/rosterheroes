52187ba9066ffbc5e06607b50d9436be917797d0
Brian Rice <bwrice83@gmail.com>
2019-09-24 12:51:21 -0400

    test full store-house items get stashed

--- tests/Unit/SlotItemInSquadTest.php
+++ tests/Unit/SlotItemInSquadTest.php
@@ -26,6 +26,9 @@ class SlotItemInSquadTest extends TestCase
     /** @var Item */
     protected $itemTwo;
 
+    /** @var Item */
+    protected $itemThree;
+
     /** @var Squad */
     protected $squad;
 
@@ -41,6 +44,7 @@ class SlotItemInSquadTest extends TestCase
 
         $this->item = factory(Item::class)->create();
         $this->itemTwo = factory(Item::class)->create();
+        $this->itemThree = factory(Item::class)->create();
         $this->squad = factory(Squad::class)->states('with-slots')->create();
         $this->storeHouse = factory(StoreHouse::class)->states('with-slots')->create([
             'squad_id' => $this->squad->id,
@@ -103,7 +107,6 @@ class SlotItemInSquadTest extends TestCase
             $this->assertEquals($slot->has_slots_id, $hasSlots->id);
         });
 
-//        dd($this->squad->toArray(), $this->storeHouse->toArray());
         $localStoreHouse = $this->squad->fresh()->getLocalStoreHouse();
 
         $this->itemTwo = $this->itemTwo->fresh();
@@ -150,7 +153,7 @@ class SlotItemInSquadTest extends TestCase
         $filledSlots->each(function (Slot $slot) {
             $hasSlots = $slot->hasSlots;
             $this->assertInstanceOf(Squad::class, $hasSlots);
-            $this->assertEquals($slot->has_slots_id, $hasSlots->id);
+            $this->assertEquals($slot->has_slots_id, $this->squad->id);
         });
 
         $localStash = $this->squad->getLocalStash();
@@ -158,6 +161,63 @@ class SlotItemInSquadTest extends TestCase
         $this->itemTwo = $this->itemTwo->fresh();
         $filledSlots = $this->itemTwo->slots;
         $this->assertEquals($this->itemTwo->getSlotsCount(), $filledSlots->count());
+        $filledSlots->each(function (Slot $slot) use ($localStash) {
+            $hasSlots = $slot->hasSlots;
+            $this->assertInstanceOf(Stash::class, $hasSlots);
+            $this->assertEquals($slot->has_slots_id, $localStash->id);
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_stash_an_item_if_both_the_squad_and_local_storehouse_is_full()
+    {
+        $itemType = ItemType::query()->inRandomOrder()->first();
+
+        $this->item->item_type_id = $itemType->id;
+        $this->item->save();
+        $this->item = $this->item->fresh();
+
+        $this->itemTwo->item_type_id = $itemType->id;
+        $this->itemTwo->save();
+        $this->itemTwo = $this->itemTwo->fresh();
+
+        $squadSlotsCount = $this->squad->slots()->count();
+        $this->squad->slots()->take($squadSlotsCount - $this->item->getSlotsCount())->delete();
+
+        $storeHouseSlots = $this->storeHouse->slots()->count();
+        $this->storeHouse->slots()->take($storeHouseSlots - $this->item->getSlotsCount())->delete();
+
+        $this->domainAction->execute($this->squad, $this->item);
+        $this->squad = $this->squad->fresh();
+        $this->domainAction->execute($this->squad, $this->itemTwo);
+        $this->squad = $this->squad->fresh();
+        $this->domainAction->execute($this->squad, $this->itemThree);
+
+        $this->item = $this->item->fresh();
+        $filledSlots = $this->item->slots;
+        $this->assertEquals($this->item->getSlotsCount(), $filledSlots->count());
+        $filledSlots->each(function (Slot $slot) {
+            $hasSlots = $slot->hasSlots;
+            $this->assertInstanceOf(Squad::class, $hasSlots);
+            $this->assertEquals($slot->has_slots_id, $this->squad->id);
+        });
+
+        $this->itemTwo = $this->itemTwo->fresh();
+        $filledSlots = $this->itemTwo->slots;
+        $this->assertEquals($this->itemTwo->getSlotsCount(), $filledSlots->count());
+        $filledSlots->each(function (Slot $slot){
+            $hasSlots = $slot->hasSlots;
+            $this->assertInstanceOf(StoreHouse::class, $hasSlots);
+            $this->assertEquals($slot->has_slots_id, $this->storeHouse->id);
+        });
+
+        $localStash = $this->squad->getLocalStash();
+
+        $this->itemThree = $this->itemThree->fresh();
+        $filledSlots = $this->itemThree->slots;
+        $this->assertEquals($this->itemThree->getSlotsCount(), $filledSlots->count());
         $filledSlots->each(function (Slot $slot) use ($localStash) {
             $hasSlots = $slot->hasSlots;
             $this->assertInstanceOf(Stash::class, $hasSlots);
