94a3b3aedd4bb06efa5644b542ede5e255e51e13
Brian Rice <bwrice83@gmail.com>
2019-09-24 09:09:30 -0400

    create custom exception

---
+++ app/Exceptions/HeroSlottingException.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Exceptions;
+
+
+use App\Domain\Interfaces\HasSlots;
+use App\Domain\Models\Hero;
+use App\Domain\Models\Slot;
+use Throwable;
+
+class HeroSlottingException extends \RuntimeException
+{
+
+    public const CODE_DOESNT_BELONG = 1;
+    public const CODE_ALREADY_EMPTY = 2;
+
+    /**
+     * @var Slot
+     */
+    private $slot;
+
+    /**
+     * @var HasSlots|null
+     */
+    private $hasSlots;
+
+    public function __construct(Slot $slot, HasSlots $hasSlots = null, $message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->slot = $slot;
+        $this->hasSlots = $hasSlots;
+    }
+
+    /**
+     * @return Slot
+     */
+    public function getSlot(): Slot
+    {
+        return $this->slot;
+    }
+
+    /**
+     * @return HasSlots|null
+     */
+    public function getHasSlots(): ?HasSlots
+    {
+        return $this->hasSlots;
+    }
+}
