073d0662e8164c44a9b8365fb31860dd7536cd5c
Brian Rice <bwrice83@gmail.com>
2019-09-25 19:40:07 -0400

    create BarracksHero JS model

---
+++ resources/js/models/BarracksHero.js
@@ -0,0 +1,17 @@
+import Slot from "./Slot";
+import Measurable from "./Measurable";
+
+export default class BarracksHero {
+
+    constructor({name = '', uuid, slug, measurables = [], slots = []}) {
+        this.name = name;
+        this.uuid = uuid;
+        this.slug = slug;
+        this.slots = slots.map(function (slot) {
+            return new Slot(slot);
+        });
+        this.measurables = measurables.map(function (measurable) {
+            return new Measurable(measurable);
+        })
+    }
+}
