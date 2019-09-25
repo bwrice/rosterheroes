7245b18840eda28c71a7f2d46a9aae20bbebe398
Brian Rice <bwrice83@gmail.com>
2019-09-25 19:57:11 -0400

    create empty slot button

---
+++ resources/js/components/commandCenter/barracks/gear/EmptyHeroSlotButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-btn @click="emptySlot">Un-Equip Item</v-btn>
+</template>
+
+<script>
+    import Slot from "../../../../models/Slot";
+    import BarracksHero from "../../../../models/BarracksHero";
+
+    export default {
+        name: "EmptyHeroSlotButton",
+        props: {
+            heroSlot: {
+                type: Slot,
+                required: true
+            },
+            hero: {
+                type: BarracksHero,
+                required: true
+            }
+        },
+
+        data() {
+            return {
+                pending: false
+            }
+        },
+
+        methods: {
+            async emptySlot() {
+                this.pending = true;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
