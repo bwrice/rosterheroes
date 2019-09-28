c0827f0f4b62c0f67d5d3876067b4a78e6335c46
Brian Rice <bwrice83@gmail.com>
2019-09-28 17:48:24 -0400

    create fill slot from wagon button

---
+++ resources/js/components/commandCenter/barracks/gear/FillSlotFromWagonButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-btn color="success">
+        <v-icon>unarchive</v-icon>
+    </v-btn>
+</template>
+
+<script>
+    import BarracksHero from "../../../../models/BarracksHero";
+
+    export default {
+        name: "FillSlotFromWagonButton",
+        props: {
+            hero: {
+                type: BarracksHero,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
