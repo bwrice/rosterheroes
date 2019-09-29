3b8fd259dfda42975686b570f47a1349c99181e5
Brian Rice <bwrice83@gmail.com>
2019-09-28 22:48:27 -0400

    create EquipHeroSlotFromWagonAction domain action

---
+++ app/Domain/Actions/EquipHeroSlotFromWagonAction.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Models\Hero;
+use App\Domain\Models\Item;
+use App\Domain\Models\Slot;
+use App\Exceptions\SlottingException;
+
+class EquipHeroSlotFromWagonAction
+{
+    public function execute(Hero $hero, Slot $slot, Item $item)
+    {
+        $heroSlots = $hero->slots;
+        $matchingSlot = $heroSlots->first(function (Slot $heroSlot) use ($slot) {
+            return (string) $heroSlot->uuid === (string) $slot->uuid;
+        });
+        if (! $matchingSlot) {
+            throw new SlottingException($slot, $hero, null, "Slot does not belong to hero", SlottingException::CODE_INVALID_SLOT_OWNERSHIP);
+        }
+        $squad = $hero->getSquad();
+        if (! $squad) {
+            throw new SlottingException($slot, $hero, $item, "No matching squad for hero", SlottingException::CODE_INVALID_ITEM_OWNERSHIP);
+        }
+    }
+}
