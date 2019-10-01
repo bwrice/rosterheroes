f69cdb4d41d77ef0c8925947a99488c6f78338bd
Brian Rice <bwrice83@gmail.com>
2019-10-01 15:24:04 -0400

    create syncSlots() method

--- resources/js/models/SlotTransaction.js
+++ resources/js/models/SlotTransaction.js
@@ -11,4 +11,44 @@ export default class SlotTransaction {
         });
         this.hasSlots = hasSlots ? new HasSlots(hasSlots) : new HasSlots({});
     }
+
+    syncSlots({state, commit, dispatch}) {
+        switch (this.hasSlots.type) {
+            case 'hero':
+                this.updateHeroes(state.barracksHeroes, commit);
+                return;
+            case 'squad':
+                this.updateMobileStorage(state.mobileStorage, commit);
+                return;
+            default:
+                return;
+        }
+    }
+
+    updateHeroes(heroes, commit) {
+        let heroUuid = this.hasSlots.uniqueIdentifer;
+        let newHeroes = _.cloneDeep(heroes);
+        let matchingHero = newHeroes.find(function (hero) {
+            return heroUuid === hero.uuid;
+        });
+        // let itemToFill = (this.type === 'fill') ? this.item : null;
+        this.slots.forEach(function (newSlot) {
+            let slotIndex = matchingHero.slots.findIndex(function (oldSlot) {
+                return oldSlot.uuid === newSlot.uuid;
+            });
+            matchingHero.slots.splice(slotIndex, 1, new Slot(newSlot));
+        });
+        commit('SET_BARRACKS_HEROES', newHeroes);
+    }
+
+    updateMobileStorage(mobileStorage, commit) {
+        let newMobileStorage = _.cloneDeep(mobileStorage);
+        this.slots.forEach(function (newSlot) {
+            let slotIndex = newMobileStorage.slots.findIndex(function (oldSlot) {
+                return oldSlot.uuid === newSlot.uuid;
+            });
+            newMobileStorage.slots.splice(slotIndex, 1, new Slot(newSlot));
+        });
+        commit('SET_MOBILE_STORAGE', newMobileStorage);
+    }
 }
