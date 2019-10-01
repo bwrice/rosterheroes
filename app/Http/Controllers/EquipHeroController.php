a4dd3e5f285879fc36adac8852af65cdd63debb9
Brian Rice <bwrice83@gmail.com>
2019-09-30 22:52:13 -0400

    create EquipHero controller

---
+++ app/Http/Controllers/EquipHeroController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domain\Actions\EquipHeroSlotFromWagonAction;
+use App\Domain\Models\Hero;
+use App\Domain\Models\Item;
+use App\Domain\Models\Slot;
+use App\Domain\Support\SlotTransaction;
+use App\Exceptions\SlottingException;
+use App\Http\Resources\SlotTransactionResource;
+use App\Policies\HeroPolicy;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
+
+class EquipHeroController extends Controller
+{
+    public function __invoke($heroSlug, Request $request, EquipHeroSlotFromWagonAction $domainAction)
+    {
+        $hero = Hero::findSlugOrFail($heroSlug);
+        $this->authorize(HeroPolicy::MANAGE, $hero);
+
+        $slot = Slot::findUuidOrFail($request->slot);
+        $item = Item::findUuidOrFail($request->item);
+
+        try {
+            $slotTransactions = DB::transaction(function () use ($hero, $slot, $item, $domainAction) {
+                return $domainAction->execute($hero, $slot, $item);
+            });
+        } catch (SlottingException $exception) {
+
+            throw ValidationException::withMessages([
+                'equip' => $exception->getMessage()
+            ]);
+        }
+
+        return SlotTransactionResource::collection($slotTransactions);
+    }
+}
