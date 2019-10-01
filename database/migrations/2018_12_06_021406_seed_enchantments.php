5962628061b8a79c56fe4e67fa2a4385838583da
Brian Rice <bwrice83@gmail.com>
2019-10-01 16:47:23 -0400

    create Beginner's Blessing enchantment

--- database/migrations/2018_12_06_021406_seed_enchantments.php
+++ database/migrations/2018_12_06_021406_seed_enchantments.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Domain\Models\Enchantment;
+use App\Domain\Models\MeasurableType;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -13,40 +16,65 @@ class SeedEnchantments extends Migration
      */
     public function up()
     {
-        $measurableTypes = \App\Domain\Models\MeasurableType::all();
+        $measurableTypes = MeasurableType::all();
 
         $measurableTypesArray = [
-            \App\Domain\Models\MeasurableType::STRENGTH,
-            \App\Domain\Models\MeasurableType::VALOR,
-            \App\Domain\Models\MeasurableType::AGILITY,
-            \App\Domain\Models\MeasurableType::FOCUS,
-            \App\Domain\Models\MeasurableType::APTITUDE,
-            \App\Domain\Models\MeasurableType::INTELLIGENCE,
-            \App\Domain\Models\MeasurableType::HEALTH,
-            \App\Domain\Models\MeasurableType::STAMINA,
-            \App\Domain\Models\MeasurableType::MANA,
-            \App\Domain\Models\MeasurableType::PASSION,
-            \App\Domain\Models\MeasurableType::BALANCE,
-            \App\Domain\Models\MeasurableType::HONOR,
-            \App\Domain\Models\MeasurableType::PRESTIGE,
-            \App\Domain\Models\MeasurableType::WRATH
+            MeasurableType::STRENGTH,
+            MeasurableType::VALOR,
+            MeasurableType::AGILITY,
+            MeasurableType::FOCUS,
+            MeasurableType::APTITUDE,
+            MeasurableType::INTELLIGENCE,
+            MeasurableType::HEALTH,
+            MeasurableType::STAMINA,
+            MeasurableType::MANA,
+            MeasurableType::PASSION,
+            MeasurableType::BALANCE,
+            MeasurableType::HONOR,
+            MeasurableType::PRESTIGE,
+            MeasurableType::WRATH
         ];
 
-        foreach ( $measurableTypesArray as $type ) {
-            foreach( range( 1, 50 ) as $boostLevel ) {
+        foreach ($measurableTypesArray as $type) {
+            foreach(range( 1, 50) as $boostLevel) {
 
-                $name = 'Level ' . $boostLevel . ' ' . ucfirst( $type );
+                $name = 'Level ' . $boostLevel . ' ' . ucfirst($type);
 
-                /** @var \App\Domain\Models\Enchantment $enchantment */
-                $enchantment = \App\Domain\Models\Enchantment::create( [
+                /** @var Enchantment $enchantment */
+                $enchantment = Enchantment::create([
                     'name' => $name
-                ] );
+                ]);
 
                 $enchantment->measurableBoosts()->create([
-                    'measurable_type_id' => $measurableTypes->where('name', $type )->first()->id,
+                    'measurable_type_id' => $measurableTypes->where('name', $type)->first()->id,
                     'boost_level' => $boostLevel
                 ]);
             }
+        }
+
+        $this->createBeginnersBlessing($measurableTypes);
+    }
+
+    protected function createBeginnersBlessing(Collection $measurableTypes)
+    {
+        /** @var Enchantment $enchantment */
+        $enchantment = Enchantment::create([
+            'name' => "Beginner's Blessing"
+        ]);
+
+        $qualities = [
+            MeasurableType::PASSION,
+            MeasurableType::BALANCE,
+            MeasurableType::HONOR,
+            MeasurableType::PRESTIGE,
+            MeasurableType::WRATH
+        ];
+
+        foreach ($qualities as $quality) {
+            $enchantment->measurableBoosts()->create([
+                'measurable_type_id' => $measurableTypes->where('name', $quality)->first()->id,
+                'boost_level' => 1
+            ]);
         }
     }
 
