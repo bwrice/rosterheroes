35975309fc950a1dbbab782b277edef0383357f8
Brian Rice <bwrice83@gmail.com>
2019-10-02 14:23:35 -0400

    remove dialog from hero gear card

--- resources/js/components/commandCenter/barracks/gear/HeroGearCard.vue
+++ resources/js/components/commandCenter/barracks/gear/HeroGearCard.vue
@@ -1,5 +1,56 @@
 <template>
-    <v-card>
+    <v-card v-if="focusedSlotUuid">
+        <v-card-title class="pb-0">
+            <v-row align="center" justify="center">
+                <span>{{focusedHeroSlot.slotType.displayName}}</span>
+                <div class="flex-grow-1"></div>
+                <v-icon @click="focusedSlotUuid = null">close</v-icon>
+            </v-row>
+        </v-card-title>
+        <v-card-text class="px-2">
+            <v-row align="center" justify="center">
+                <template v-if="focusedHeroSlot.item">
+                    <v-col cols="12">
+                        <v-row align="center" justify="center" class="py-2">
+                            <EmptyHeroSlotButton :heroSlot="focusedHeroSlot"
+                                                 :hero="barracksHeroFromRoute"></EmptyHeroSlotButton>
+                        </v-row>
+                        <ItemCard :item="focusedHeroSlot.item"></ItemCard>
+                    </v-col>
+                </template>
+                <template v-else>
+                    <span class="subtitle-1 font-weight-light">(empty)</span>
+                </template>
+            </v-row>
+            <v-row justify="center" no-gutters>
+                <v-col cols="12">
+                    <!-- key on FilledSlotIterator prevents pagination persisting -->
+                    <FilledSlotIterator
+                        :filled-slots="mobileStorageSlots"
+                        :items-per-page="6"
+                        :search-label="'Search Wagon'"
+                        :key="this.focusedHeroSlot.uuid"
+                        :item-name-truncate-extra="4"
+                    >
+                        <template v-slot:before-expand="props">
+                            <div class="px-2">
+                                <FillSlotFromWagonButton
+                                    :hero="barracksHeroFromRoute"
+                                    :hero-slot="focusedHeroSlot"
+                                    :item="props.item"
+                                >
+                                </FillSlotFromWagonButton>
+                            </div>
+                        </template>
+                    </FilledSlotIterator>
+                </v-col>
+            </v-row>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn @click="focusedSlotUuid = null" block>Close</v-btn>
+        </v-card-actions>
+    </v-card>
+    <v-card v-else>
         <v-sheet class="py-5" style="background-image: linear-gradient(to bottom right, #524c59, #7c7287 , #524c59)">
             <HeroGearSVG
                 v-if="barracksHeroFromRoute"
@@ -7,61 +58,6 @@
                 @heroSlotClicked="handleHeroSlotClicked"
             ></HeroGearSVG>
         </v-sheet>
-        <v-dialog
-            v-model="heroGearDialog"
-            max-width="600">
-            <v-card>
-                <v-card-title class="pb-0">
-                    <v-row align="center" justify="center">
-                        <span>{{focusedHeroSlot.slotType.displayName}}</span>
-                        <div class="flex-grow-1"></div>
-                        <v-icon @click="heroGearDialog = false">close</v-icon>
-                    </v-row>
-                </v-card-title>
-                <v-card-text class="px-2">
-                    <v-row align="center" justify="center">
-                        <template v-if="focusedHeroSlot.item">
-                            <v-col cols="12">
-                                <v-row align="center" justify="center" class="py-2">
-                                    <EmptyHeroSlotButton :heroSlot="focusedHeroSlot"
-                                                         :hero="barracksHeroFromRoute"></EmptyHeroSlotButton>
-                                </v-row>
-                                <ItemCard :item="focusedHeroSlot.item"></ItemCard>
-                            </v-col>
-                        </template>
-                        <template v-else>
-                            <span class="subtitle-1 font-weight-light">(empty)</span>
-                        </template>
-                    </v-row>
-                    <v-row justify="center" no-gutters>
-                        <v-col cols="12">
-                            <!-- key on FilledSlotIterator prevents pagination persisting -->
-                            <FilledSlotIterator
-                                :filled-slots="mobileStorageSlots"
-                                :items-per-page="6"
-                                :search-label="'Search Wagon'"
-                                :key="this.focusedHeroSlot.uuid"
-                                :item-name-truncate-extra="4"
-                            >
-                                <template v-slot:before-expand="props">
-                                    <div class="px-2">
-                                        <FillSlotFromWagonButton
-                                            :hero="barracksHeroFromRoute"
-                                            :hero-slot="focusedHeroSlot"
-                                            :item="props.item"
-                                        >
-                                        </FillSlotFromWagonButton>
-                                    </div>
-                                </template>
-                            </FilledSlotIterator>
-                        </v-col>
-                    </v-row>
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn @click="heroGearDialog = null" block>Close</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
     </v-card>
 </template>
 
@@ -91,14 +87,12 @@
         ],
         data() {
             return {
-                heroGearDialog: false,
                 focusedSlotUuid: null
             }
         },
         methods: {
             handleHeroSlotClicked(slotUuid) {
                 this.focusedSlotUuid = slotUuid;
-                this.heroGearDialog = true;
             }
         },
         computed: {
