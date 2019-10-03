630b7e8e5f2907b0cb6bffdbe91e3f55a2fd7c64
Brian Rice <bwrice83@gmail.com>
2019-10-02 20:12:59 -0400

    create first row of summary panel

--- resources/js/components/commandCenter/barracks/HeroSummaryPanel.vue
+++ resources/js/components/commandCenter/barracks/HeroSummaryPanel.vue
@@ -1,15 +1,48 @@
 <template>
-    <v-card class="my-2">
-        <v-card-title>{{hero.name}}</v-card-title>
-        <v-card-actions>
-            <v-btn :to="barracksHeroRoute">Edit</v-btn>
-        </v-card-actions>
-    </v-card>
+    <v-sheet
+        color="rgba(255, 255, 255, 0.15)"
+        class="my-1"
+        elevation="5"
+    >
+        <v-row no-gutters align="center">
+            <v-col cols="4">
+                <v-row no-gutters>
+                    <v-col cols="6">
+                        <SvgIconSheet
+                            :color="'#C8DDE0'"
+                            :svg-icon="hero.heroRace.icon"
+                            :classes-object="{'pa-1': true}"
+                        >
+                        </SvgIconSheet>
+                    </v-col>
+                    <v-col cols="6">
+                        <SvgIconSheet
+                            :color="'#C8DDE0'"
+                            :svg-icon="hero.heroClass.icon"
+                            :classes-object="{'pa-1': true}"
+                        >
+                        </SvgIconSheet>
+                    </v-col>
+                </v-row>
+            </v-col>
+            <v-col cols="6">
+                <v-row align="center" justify="center">
+                    <span class="subtitle-1">{{hero.name}}</span>
+                </v-row>
+            </v-col>
+            <v-col cols="2">
+                <SvgIconSheet :svg-icon="hero.combatPosition.icon">
+                </SvgIconSheet>
+            </v-col>
+        </v-row>
+    </v-sheet>
 </template>
 
 <script>
+    import SvgIconSheet from "../global/SvgIconSheet";
     export default {
         name: "HeroSummaryPanel",
+        components: {SvgIconSheet},
         props: {
             hero: {
                 type: Object,
