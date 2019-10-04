c2d297bfc16c0f425aceece43fc280a6286e2679
Brian Rice <bwrice83@gmail.com>
2019-10-04 10:25:33 -0400

    rename interface

--- app/Domain/Interfaces/HasMeasurables.php
+++ app/Domain/Interfaces/HasMeasurables.php
@@ -6,13 +6,15 @@ namespace App\Domain\Interfaces;
 
 use App\Domain\Models\Measurable;
 
-interface RaisesMeasurables
+interface HasMeasurables
 {
     public function costToRaiseMeasurable(Measurable $measurable, int $amount = 1): int;
 
     public function spentOnRaisingMeasurable(Measurable $measurable): int;
 
     public function getCurrentMeasurableAmount(Measurable $measurable): int;
+
+    public function getBuffedMeasurableAmount(Measurable $measurable): int;
 
     public function availableExperience(): int;
 }
