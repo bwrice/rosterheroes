f77844f1aa5fee4e11080bb6224ac92a95f78ad8
Brian Rice <bwrice83@gmail.com>
2019-10-05 18:56:17 -0400

    add empty player spirit logic in player spirit panel

--- resources/js/components/commandCenter/global/PlayerSpiritSummaryPanel.vue
+++ resources/js/components/commandCenter/global/PlayerSpiritSummaryPanel.vue
@@ -1,15 +1,22 @@
 <template>
-    <v-sheet color="rgba(255, 255, 255, .5)" class="ma-1 px-2" style="color: #322638">
-        <v-row no-gutters>
-            <v-col cols="12">
-                <span class="subtitle-1 font-weight-regular">{{playerSpirit.player.fullName}}</span>
-            </v-col>
-        </v-row>
-        <v-row no-gutters justify="center">
-            <span class="caption font-weight-light">{{playerSpirit.game.simpleDescription}}</span>
-            <div class="flex-grow-1"></div>
-            <span class="title font-weight-bold">{{playerSpirit.essenceCost.toLocaleString()}}</span>
-        </v-row>
+    <v-sheet :color="sheetColor" class="ma-1 px-2" style="color: rgba(255, 255, 255, .9)">
+        <template v-if="playerSpirit">
+            <v-row no-gutters>
+                <v-col cols="12">
+                    <span class="subtitle-1 font-weight-regular">{{playerSpirit.player.fullName}}</span>
+                </v-col>
+            </v-row>
+            <v-row no-gutters justify="center">
+                <span class="caption font-weight-light">{{playerSpirit.game.simpleDescription}}</span>
+                <div class="flex-grow-1"></div>
+                <span class="title font-weight-bold">{{playerSpirit.essenceCost.toLocaleString()}}</span>
+            </v-row>
+        </template>
+        <template v-else>
+            <v-row justify="center" align="center">
+                <span class="subtitle-2 my-4">(no spirit)</span>
+            </v-row>
+        </template>
     </v-sheet>
 </template>
 
@@ -20,8 +27,15 @@
         name: "PlayerSpiritSummaryPanel",
         props: {
             playerSpirit: {
-                type: PlayerSpirit,
-                required: true
+                type: PlayerSpirit
+            }
+        },
+        computed: {
+            sheetColor() {
+                if (this.playerSpirit) {
+                    return 'rgba(0, 181, 147, .8)';
+                }
+                return 'rgba(0, 0, 0, .25)';
             }
         }
     }
