f9d1598a2510f30889b0b822c717605a2d604cf5
Brian Rice <bwrice83@gmail.com>
2019-10-05 23:13:40 -0400

    remove nested cards

--- resources/js/components/commandCenter/barracks/HeroMeasurablesCard.vue
+++ resources/js/components/commandCenter/barracks/HeroMeasurablesCard.vue
@@ -1,30 +1,49 @@
 <template>
-    <v-card>
-        <v-card>
-            <v-card-title>Attributes</v-card-title>
-            <MeasurablePanel
-                v-for="(attribute, uuid) in attributes"
-                :key="uuid"
-                :measurable="attribute"
-            ></MeasurablePanel>
-        </v-card>
-        <v-card>
-            <v-card-title>Resources</v-card-title>
-            <MeasurablePanel
-                v-for="(resource, uuid) in resources"
-                :key="uuid"
-                :measurable="resource"
-            ></MeasurablePanel>
-        </v-card>
-        <v-card>
-            <v-card-title>Qualities</v-card-title>
-            <MeasurablePanel
-                v-for="(quality, uuid) in qualities"
-                :key="uuid"
-                :measurable="quality"
-            ></MeasurablePanel>
-        </v-card>
+    <v-card class="py-1">
+        <v-row no-gutters align="center">
+            <span class="title font-weight-light px-3">Attributes</span>
+        </v-row>
+        <MeasurablePanel
+            v-for="(attribute, uuid) in attributes"
+            :measurable="attribute"
+            :key="attribute.uuid"
+        ></MeasurablePanel>
+        <v-row no-gutters align="center">
+            <span class="title font-weight-light px-3">Resources</span>
+        </v-row>
+        <MeasurablePanel
+            v-for="(resource, uuid) in resources"
+            :measurable="resource"
+            :key="resource.uuid"
+        ></MeasurablePanel>
+        <v-row no-gutters align="center">
+            <span class="title font-weight-light px-3">Qualities</span>
+        </v-row>
+        <MeasurablePanel
+            v-for="(quality, uuid) in qualities"
+            :measurable="quality"
+            :key="quality.uuid"
+        ></MeasurablePanel>
     </v-card>
+<!--    <MeasurablesPanelGroup :title="'Attributes'" :measurables="attributes"></MeasurablesPanelGroup>-->
+<!--    <v-card>-->
+<!--        <v-card>-->
+<!--            <v-card-title>Resources</v-card-title>-->
+<!--            <MeasurablePanel-->
+<!--                v-for="(resource, uuid) in resources"-->
+<!--                :key="uuid"-->
+<!--                :measurable="resource"-->
+<!--            ></MeasurablePanel>-->
+<!--        </v-card>-->
+<!--        <v-card>-->
+<!--            <v-card-title>Qualities</v-card-title>-->
+<!--            <MeasurablePanel-->
+<!--                v-for="(quality, uuid) in qualities"-->
+<!--                :key="uuid"-->
+<!--                :measurable="quality"-->
+<!--            ></MeasurablePanel>-->
+<!--        </v-card>-->
+<!--    </v-card>-->
 </template>
 
 <script>
@@ -32,10 +51,11 @@
     import {barracksHeroMixin} from "../../../mixins/barracksHeroMixin";
 
     import MeasurablePanel from "./MeasurablePanel";
+    import MeasurablesPanelGroup from "./MeasurablesPanelGroup";
 
     export default {
         name: "HeroMeasurablesCard",
-        components: {MeasurablePanel},
+        components: {MeasurablesPanelGroup, MeasurablePanel},
 
         mixins: [
             barracksHeroMixin
