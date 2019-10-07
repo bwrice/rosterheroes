0dcb4565f36264d74367924830a93d228823a756
Brian Rice <bwrice83@gmail.com>
2019-10-07 16:44:23 -0400

    extract out loading-overlay component

---
+++ resources/js/components/commandCenter/global/LoadingOverlay.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-overlay :value="showOverlay">
+        <v-progress-circular
+            size="64"
+            indeterminate
+            color="primary"
+        ></v-progress-circular>
+    </v-overlay>
+</template>
+
+<script>
+    export default {
+        name: "LoadingOverlay",
+        props: {
+            showOverlay: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
