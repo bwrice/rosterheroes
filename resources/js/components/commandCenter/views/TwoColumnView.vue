8c798fdd76d706564856f8c5ff5b5777eea8e07d
Brian Rice <bwrice83@gmail.com>
2019-10-07 12:05:24 -0400

    create two-column view

---
+++ resources/js/components/commandCenter/views/TwoColumnView.vue
@@ -0,0 +1,22 @@
+<template>
+    <BaseView>
+        <v-col cols="12" offset-sm="2" sm="8" offset-md="0" md="6" offset-lg="2" lg="4" offset-xl="3" xl="3">
+            <slot name="first"></slot>
+        </v-col>
+        <v-col cols="12" offset-sm="2" sm="8" offset-md="0" md="6" lg="4" xl="3">
+            <slot name="second"></slot>
+        </v-col>
+    </BaseView>
+</template>
+
+<script>
+    import BaseView from "./BaseView";
+    export default {
+        name: "TwoColumnView",
+        components: {BaseView}
+    }
+</script>
+
+<style scoped>
+
+</style>
