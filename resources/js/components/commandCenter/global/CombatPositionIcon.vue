60ccd987dc30399c1809aa3a7de570a13f4352d7
Brian Rice <bwrice83@gmail.com>
2019-10-08 11:34:16 -0400

    create CombatPositionIcon vue component

---
+++ resources/js/components/commandCenter/global/CombatPositionIcon.vue
@@ -0,0 +1,43 @@
+<template>
+    <SvgIconSheet :svg="svg">
+    </SvgIconSheet>
+</template>
+
+<script>
+    import SvgIconSheet from "./SvgIconSheet";
+    import {mapGetters} from 'vuex';
+
+    export default {
+        name: "CombatPositionIcon",
+        components: {SvgIconSheet},
+        props: {
+            combatPositionId: {
+                type: Number,
+                required: true
+            },
+            attackerMode: {
+                type: Boolean,
+                default: true
+            }
+        },
+        computed: {
+            ...mapGetters([
+                '_combatPositionByID'
+            ]),
+            combatPosition() {
+                return this._combatPositionByID(this.combatPositionId);
+            },
+            svg() {
+                if (this.attackerMode) {
+                    return this.combatPosition.attackerSVG;
+                }
+                return this.combatPosition.defenderSVG;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
