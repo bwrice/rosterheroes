17d2475b47536d91040430803fa2e9a1c916d9b8
Brian Rice <bwrice83@gmail.com>
2019-10-07 21:32:31 -0400

    create HeroClassIcon component

---
+++ resources/js/components/commandCenter/global/HeroClassIcon.vue
@@ -0,0 +1,37 @@
+<template>
+    <SvgIconSheet
+        :color="'#C8DDE0'"
+        :svg="heroClass.svg"
+        :classes-object="{'pa-1': true}"
+    >
+    </SvgIconSheet>
+</template>
+
+<script>
+    import SvgIconSheet from "./SvgIconSheet";
+
+    import {mapGetters} from 'vuex';
+
+    export default {
+        name: "HeroClassIcon",
+        components: {SvgIconSheet},
+        props: {
+            heroClassId: {
+                type: Number,
+                required: true
+            }
+        },
+        computed: {
+            ...mapGetters([
+                '_heroClassByID'
+            ]),
+            heroClass() {
+                return this._heroClassByID(this.heroClassId);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
