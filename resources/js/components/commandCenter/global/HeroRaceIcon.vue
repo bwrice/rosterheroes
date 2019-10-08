8b09a62a85559cd6dbd7244f793ff66100bef188
Brian Rice <bwrice83@gmail.com>
2019-10-07 23:42:33 -0400

    create HeroRaceIcon component

---
+++ resources/js/components/commandCenter/global/HeroRaceIcon.vue
@@ -0,0 +1,36 @@
+<template>
+    <SvgIconSheet
+        :color="'#C8DDE0'"
+        :svg="heroRace.svg"
+        :classes-object="{'pa-1': true}"
+    >
+    </SvgIconSheet>
+</template>
+
+<script>
+    import SvgIconSheet from "./SvgIconSheet";
+    import {mapGetters} from 'vuex';
+
+    export default {
+        name: "HeroRaceIcon",
+        components: {SvgIconSheet},
+        props: {
+            heroRaceId: {
+                type: Number,
+                required: true
+            }
+        },
+        computed: {
+            ...mapGetters([
+                '_heroRaceByID'
+            ]),
+            heroRace() {
+                return this._heroRaceByID(this.heroRaceId);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
