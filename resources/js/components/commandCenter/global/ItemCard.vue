295273345855af6ff40e1bf491ccea2c703189c6
Brian Rice <bwrice83@gmail.com>
2019-10-08 21:26:34 -0400

    move sheet oustide of ItemCard component

--- resources/js/components/commandCenter/global/ItemCard.vue
+++ resources/js/components/commandCenter/global/ItemCard.vue
@@ -1,41 +1,41 @@
 <template>
-    <v-sheet :color="sheetColor">
-        <v-row>
-            <v-col cols="12" class="text-center px-3">
-                <span class="headline">{{item.name}}</span>
-            </v-col>
-        </v-row>
-        <v-row no-gutters>
-            <v-col cols="6" class="px-2">
-                <p class="caption ma-0">
-                    Type: {{item.itemType.name}}<br>
-                    Base: {{item.itemType.itemBase.name}}<br>
-                    Class: {{item.itemClass.name}}<br>
-                    Material: {{item.material.name}}<br>
-                </p>
-            </v-col>
-            <v-col cols="6" class="px-2">
-                <p class="caption ma-0">
-                    Protection: {{item.protection}}<br>
-                    Block %: {{item.blockChance}}<br>
-                    Burden: {{item.burden}}<br>
-                    Value: {{item.value}}<br>
-                </p>
-            </v-col>
-        </v-row>
-        <v-row class="no-gutters">
-            <v-col cols="12" class="px-1">
-                <h4 v-if="item.attacks.length">Attacks:</h4>
-                <AttackPanel v-for="attack in item.attacks" v-bind:key="attack.name" :attack="attack"></AttackPanel>
-            </v-col>
-        </v-row>
-        <v-row class="no-gutters">
-            <v-col cols="12" class="px-1">
-                <h4 v-if="item.enchantments.length">Enchantments:</h4>
-                <EnchantmentSheet v-for="enchantment in item.enchantments" v-bind:key="enchantment.name" :enchantment="enchantment"></EnchantmentSheet>
-            </v-col>
-        </v-row>
-    </v-sheet>
+    <v-row>
+        <v-col cols="12">
+            <v-row no-gutters justify="center" align="center">
+                <span class="headline text-center mx-3 my-2">{{item.name}}</span>
+            </v-row>
+            <v-row no-gutters class="pb-2">
+                <v-col cols="6" class="px-2">
+                    <p class="caption ma-0">
+                        Type: {{item.itemType.name}}<br>
+                        Base: {{item.itemType.itemBase.name}}<br>
+                        Class: {{item.itemClass.name}}<br>
+                        Material: {{item.material.name}}<br>
+                    </p>
+                </v-col>
+                <v-col cols="6" class="px-2">
+                    <p class="caption ma-0">
+                        Protection: {{item.protection}}<br>
+                        Block %: {{item.blockChance}}<br>
+                        Burden: {{item.burden}}<br>
+                        Value: {{item.value}}<br>
+                    </p>
+                </v-col>
+            </v-row>
+            <v-row class="no-gutters">
+                <v-col cols="12" class="px-1">
+                    <h4 v-if="item.attacks.length">Attacks:</h4>
+                    <AttackPanel v-for="attack in item.attacks" v-bind:key="attack.name" :attack="attack"></AttackPanel>
+                </v-col>
+            </v-row>
+            <v-row class="no-gutters">
+                <v-col cols="12" class="px-1">
+                    <h4 v-if="item.enchantments.length">Enchantments:</h4>
+                    <EnchantmentSheet v-for="enchantment in item.enchantments" v-bind:key="enchantment.name" :enchantment="enchantment"></EnchantmentSheet>
+                </v-col>
+            </v-row>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
