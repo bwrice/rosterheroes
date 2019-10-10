ed47e6bc0d4d9520580c651be838da40ea95b37e
Brian Rice <bwrice83@gmail.com>
2019-10-09 23:20:18 -0400

    add positionIDs to heroRace JS model

--- resources/js/models/HeroRace.js
+++ resources/js/models/HeroRace.js
@@ -1,9 +1,10 @@
 
 export default class HeroRace {
 
-    constructor({id, name = '', svg = ''}) {
+    constructor({id, name = '', svg = '', positionIDs = []}) {
         this.id = id;
         this.name = name;
         this.svg = svg;
+        this.positionIDs = positionIDs;
     }
 }
