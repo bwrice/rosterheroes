dca3806292fe349775679610a451e28dc6779833
Brian Rice <bwrice83@gmail.com>
2019-10-09 23:19:51 -0400

    move player-spirits to week module

--- resources/js/api/weekApi.js
+++ resources/js/api/weekApi.js
@@ -1,10 +1,10 @@
 
 export async function getCurrentWeek() {
     let response = await axios.get('/api/v1/weeks/current');
-    return response.data.data;
+    return response.data;
 }
 
 export async function getCurrentPlayerSpirits() {
     let response = await axios.get('/api/v1/weeks/current/player-spirits');
-    return response.data.data;
+    return response.data;
 }
--- resources/js/store/commandCenter/weekModule.js
+++ resources/js/store/commandCenter/weekModule.js
@@ -3,24 +3,43 @@ import * as weekApi from '../../api/weekApi';
 export default {
 
     state: {
-        week: null
+        week: null,
+        playerSpirits: [],
     },
 
     getters: {
         _currentWeek(state) {
             return state.week;
+        },
+        _playerSpirits(state) {
+            return state.playerSpirits;
         }
     },
     mutations: {
         SET_CURRENT_WEEK(state, payload) {
             state.week = payload;
+        },
+        SET_PLAYER_SPIRITS(state, payload) {
+            state.playerSpirits = payload;
         }
     },
 
     actions: {
         async updateCurrentWeek({commit}) {
-            let week = await weekApi.getCurrentWeek();
-            commit('SET_CURRENT_WEEK', week)
+            try {
+                let weekResponse = await weekApi.getCurrentWeek();
+                commit('SET_CURRENT_WEEK', weekResponse.data);
+            } catch (e) {
+                console.warn("Failed to update current week");
+            }
         },
+        async updatePlayerSpirits({commit}) {
+            try {
+                let playerSpiritsResponse = await weekApi.getCurrentPlayerSpirits();
+                commit('SET_PLAYER_SPIRITS', playerSpiritsResponse.data);
+            } catch (e) {
+                console.warn("Failed to update player spirits");
+            }
+        }
     }
 };
