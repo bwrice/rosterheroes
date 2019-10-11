7104d538857bafffc78373852ecac0aa64a0ba4c
Brian Rice <bwrice83@gmail.com>
2019-10-11 12:06:30 -0400

    update RosterHeroView

--- resources/js/components/commandCenter/views/roster/RosterHeroView.vue
+++ resources/js/components/commandCenter/views/roster/RosterHeroView.vue
@@ -1,51 +1,61 @@
 <template>
     <SingleColumnLayout>
-        <template v-slot:column-one><v-card>
-            <v-btn :to="rosterPage">
-                <v-icon>arrow_back</v-icon>Back
-            </v-btn>
-            <HeroRosterCard :hero="hero">
-                <template slot="body">
-                    <template v-if="hero.playerSpirit">
-                        <PlayerSpiritPanel :player-spirit="hero.playerSpirit">
-                            <template v-slot:spirit-actions>
-                                <RemoveSpiritButton :hero="hero" :player-spirit="hero.playerSpirit"></RemoveSpiritButton>
+        <template v-slot:column-one>
+            <v-card>
+                <v-card-text class="px-1 py-2">
+                    <HeroRosterCard :hero="hero">
+                        <template slot="body">
+                            <template v-if="hero.playerSpirit">
+                                <PlayerSpiritPanel :player-spirit="hero.playerSpirit">
+                                    <template v-slot:spirit-actions>
+                                        <RemoveSpiritButton :hero="hero"
+                                                            :player-spirit="hero.playerSpirit"></RemoveSpiritButton>
+                                    </template>
+                                </PlayerSpiritPanel>
                             </template>
-                        </PlayerSpiritPanel>
-                    </template>
-                </template>
-            </HeroRosterCard>
-            <v-data-iterator
-                :items="filteredSpirits"
-                :loading="_rosterLoading"
-                row
-                no-gutters
-            >
-                <template v-slot:header>
-                    <v-text-field
-                        v-model="spiritSearch"
-                        clearable
-                        flat
-                        solo-inverted
-                        hide-details
-                        prepend-inner-icon="search"
-                        label="Search Player Spirits"
-                    ></v-text-field>
-                </template>
-                <template v-slot:item="props">
-                    <PlayerSpiritPanel :player-spirit="props.item">
-                        <template v-slot:spirit-actions>
-                            <AddSpiritButton :hero="hero" :player-spirit="props.item"></AddSpiritButton>
                         </template>
-                    </PlayerSpiritPanel>
-                </template>
-                <template v-slot:loading>
-                    <v-row :justify="'center'" class="py-5">
-                        <v-progress-circular indeterminate size="36"></v-progress-circular>
-                    </v-row>
-                </template>
-            </v-data-iterator>
-        </v-card>
+                    </HeroRosterCard>
+                    <v-data-iterator
+                        :items="filteredSpirits"
+                        :items-per-page="itemsPerPage"
+                        :page="page"
+                        :loading="_rosterLoading"
+                        hide-default-footer
+                        row
+                        no-gutters
+                        no-data-text="No spirits match the criteria"
+                        no-results-text="No spirits match the criteria"
+                    >
+                        <template v-slot:header>
+                            <v-text-field
+                                v-model="spiritSearch"
+                                clearable
+                                flat
+                                solo-inverted
+                                hide-details
+                                prepend-inner-icon="search"
+                                label="Search Player Spirits"
+                            ></v-text-field>
+                        </template>
+                        <template v-slot:item="props">
+                            <PlayerSpiritPanel :player-spirit="props.item">
+                                <template v-slot:spirit-actions>
+                                    <AddSpiritButton :hero="hero" :player-spirit="props.item"></AddSpiritButton>
+                                </template>
+                            </PlayerSpiritPanel>
+                        </template>
+                        <template v-slot:loading>
+                            <v-row :justify="'center'" class="py-5">
+                                <v-progress-circular indeterminate size="36"></v-progress-circular>
+                            </v-row>
+                        </template>
+                        <template v-slot:footer>
+                            <IteratorFooter :page="page" :number-of-pages="numberOfPages" @formerPage="decreasePage" @nextPage="increasePage">
+                            </IteratorFooter>
+                        </template>
+                    </v-data-iterator>
+                </v-card-text>
+            </v-card>
         </template>
     </SingleColumnLayout>
 </template>
@@ -60,11 +70,13 @@
     import RemoveSpiritButton from "../../roster/RemoveSpiritButton";
     import HeroRosterCard from "../../roster/HeroRosterCard";
     import SingleColumnLayout from "../../layouts/SingleColumnLayout";
+    import IteratorFooter from "../../global/IteratorFooter";
 
     export default {
         name: "RosterHeroView",
 
         components: {
+            IteratorFooter,
             SingleColumnLayout,
             HeroRosterCard,
             RemoveSpiritButton,
@@ -72,10 +84,25 @@
             PlayerSpiritPanel
         },
 
+        mounted() {
+            console.log("Height");
+            console.log(window.innerHeight);
+        },
+
         data() {
             return {
-                spiritSearch: ''
+                spiritSearch: '',
+                page: 1
             }
+        },
+
+        methods: {
+            increasePage () {
+                if (this.page + 1 <= this.numberOfPages) this.page += 1
+            },
+            decreasePage () {
+                if (this.page - 1 >= 1) this.page -= 1
+            },
         },
 
         computed: {
@@ -93,23 +120,28 @@
             hero() {
                 return this._focusedHero(this.$route);
             },
+            itemsPerPage() {
+                let items = Math.ceil(window.innerHeight/90) - 4;
+                return Math.max(items, 3);
+            },
+            numberOfPages() {
+                let spiritsCount = this.filteredSpirits.length;
+                if (! spiritsCount) return 1;
+                return Math.ceil(spiritsCount / this.itemsPerPage);
+            },
             playerSpiritsForHero() {
                 let heroPositionIDs = this._heroRaceByID(this.hero.heroRaceID).positionIDs;
-                return this._playerSpirits.filter(function (playerSpirit) {
+                let filtered = this._playerSpirits.filter(function (playerSpirit) {
                     let matchingIDs = playerSpirit.player.positionIDs.filter(playerPositionID => heroPositionIDs.includes(playerPositionID));
                     return matchingIDs.length > 0;
                 });
-                //
-                // let heroPositionIDs = this.hero.heroRace.positions.map(function (position) {
-                //     return position.id;
-                // });
-                // return this._playerSpiritsPool.filter(function (playerSpirit) {
-                //     let spiritPositionIDs = playerSpirit.player.positions.map(function (position) {
-                //         return position.id;
-                //     });
-                //     let filtered = spiritPositionIDs.filter(spiritPosID => heroPositionIDs.includes(spiritPosID));
-                //     return filtered.length > 0;
-                // });
+                if (this.hero.playerSpirit) {
+                    let spiritUuid = this.hero.playerSpirit.uuid;
+                    filtered = filtered.filter(function(playerSpirit) {
+                        return playerSpirit.uuid !== spiritUuid;
+                    })
+                }
+                return filtered;
             },
             filteredSpirits() {
                 if (this.spiritSearch && this.spiritSearch.length) {
