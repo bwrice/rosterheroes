a0b9b69a2b80327744885998eda37bf0ee466a9e
Brian Rice <bwrice83@gmail.com>
2019-10-12 14:17:48 -0400

    create SpiritEnergyBar component

---
+++ resources/js/components/commandCenter/global/SpiritEnergyBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-progress-linear
+        height="16"
+        :value="value"
+        background-color="#ccc"
+        background-opacity=".2"
+        class="my-1 energy-bar"
+    >
+        {{energy}}
+    </v-progress-linear>
+</template>
+
+<script>
+    export default {
+        name: "SpiritEnergyBar",
+        props: {
+            energy: {
+                type: Number,
+                required: true
+            }
+        },
+        computed: {
+            value() {
+                return Math.ceil( Math.sqrt(this.energy) * 5);
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+    .v-application .primary[style] {
+        background-color: red;
+    }
+</style>
