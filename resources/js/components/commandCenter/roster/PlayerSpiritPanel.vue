9da7ce4e246a467e491678eb47e0b39a1e989a67
Brian Rice <bwrice83@gmail.com>
2019-10-12 15:21:08 -0400

    remove fade transition

--- resources/js/components/commandCenter/roster/PlayerSpiritPanel.vue
+++ resources/js/components/commandCenter/roster/PlayerSpiritPanel.vue
@@ -1,36 +1,34 @@
 <template>
-    <transition name="fade">
-        <v-sheet color="#29272b" class="mx-1 px-1" style="margin-bottom: 1px; color: rgba(255, 255, 255, 0.9)">
-            <v-row no-gutters justify="space-between" align="center">
-                <span class="title">{{ playerSpirit.fullName }}</span>
-                <PositionChipList :positions="positions"></PositionChipList>
-            </v-row>
-            <v-row no-gutters>
-                <v-col cols="4">
-                    <v-row no-gutters class="flex-column">
-                        <span class="caption font-weight-light">{{ gameDescription }}</span>
-                        <span class="caption font-weight-light">{{ gameDescription }}</span>
-                    </v-row>
-                </v-col>
-                <v-col cols="8">
-                    <v-row no-gutters align="center">
-                        <v-col cols="5">
-                            <v-row no-gutters justify="end">
-                                <span class="headline font-weight-bold">{{ playerSpirit.essenceCost.toLocaleString() }}</span>
-                            </v-row>
-                        </v-col>
-                        <v-col cols="7">
-                            <v-row no-gutters justify="end">
-                                <slot name="spirit-actions">
-                                    <!-- Slotted Spirit Actions -->
-                                </slot>
-                            </v-row>
-                        </v-col>
-                    </v-row>
-                </v-col>
-            </v-row>
-        </v-sheet>
-    </transition>
+    <v-sheet color="#29272b" class="mx-1 px-1" style="margin-bottom: 1px; color: rgba(255, 255, 255, 0.9)">
+        <v-row no-gutters justify="space-between" align="center">
+            <span class="title">{{ playerSpirit.fullName }}</span>
+            <PositionChipList :positions="positions"></PositionChipList>
+        </v-row>
+        <v-row no-gutters>
+            <v-col cols="4">
+                <v-row no-gutters class="flex-column">
+                    <span class="caption font-weight-light">{{ gameDescription }}</span>
+                    <SpiritEnergyBar :energy="playerSpirit.energy"></SpiritEnergyBar>
+                </v-row>
+            </v-col>
+            <v-col cols="8">
+                <v-row no-gutters align="center">
+                    <v-col cols="5">
+                        <v-row no-gutters justify="end">
+                            <span class="headline font-weight-bold">{{ playerSpirit.essenceCost.toLocaleString() }}</span>
+                        </v-row>
+                    </v-col>
+                    <v-col cols="7">
+                        <v-row no-gutters justify="end">
+                            <slot name="spirit-actions">
+                                <!-- Slotted Spirit Actions -->
+                            </slot>
+                        </v-row>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-sheet>
 </template>
 
 <script>
@@ -38,10 +36,11 @@
     import PlayerSpirit from "../../../models/PlayerSpirit";
 
     import {mapGetters} from 'vuex';
+    import SpiritEnergyBar from "../global/SpiritEnergyBar";
 
     export default {
         name: "PlayerSpiritPanel",
-        components: {PositionChipList},
+        components: {SpiritEnergyBar, PositionChipList},
         props: {
             playerSpirit: {
                 type: PlayerSpirit,
@@ -64,7 +63,5 @@
 </script>
 
 <style scoped>
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity .5s;
-    }
+
 </style>
