d36e1c7ee8accc8526d2257eaa9cd99a7238d4dd
Brian Rice <bwrice83@gmail.com>
2019-10-12 17:46:45 -0400

    reduce ratio from 5 to 4

--- app/Domain/Models/PlayerSpirit.php
+++ app/Domain/Models/PlayerSpirit.php
@@ -36,7 +36,7 @@ class PlayerSpirit extends EventSourcedModel
     public const ESSENCE_COST_PER_POINT = 400;
     public const STARTING_ENERGY = 100;
     public const MAX_USAGE_BEFORE_ENERGY_ADJUSTMENT = 10;
-    public const MIN_MAX_ENERGY_RATIO = 5;
+    public const MIN_MAX_ENERGY_RATIO = 4;
 
     public function newCollection(array $models = [])
     {
