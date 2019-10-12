f923b9ba05235bdb9f66719638376993eb52dca8
Brian Rice <bwrice83@gmail.com>
2019-10-12 00:05:58 -0400

    create AddSpiritRouterButton component

---
+++ resources/js/components/commandCenter/global/AddSpiritRouterButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-btn
+        block
+        large
+        :to="heroRoute"
+        color="primary"
+    >
+        Add Spirit
+    </v-btn>
+</template>
+
+<script>
+    export default {
+        name: "AddSpiritRouterButton",
+        props: {
+            heroSlug: {
+                type: String,
+                required: true
+            }
+        },
+        computed: {
+            heroRoute() {
+                return {
+                    name: 'roster-hero',
+                    params: {
+                        heroSlug: this.heroSlug
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
