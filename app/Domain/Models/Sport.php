a5ac6be05e8aade172666e48df08a0f459508d49
Brian Rice <bwrice83@gmail.com>
2019-10-11 21:56:32 -0400

    add factory method to get behaviors

--- app/Domain/Models/Sport.php
+++ app/Domain/Models/Sport.php
@@ -2,9 +2,15 @@
 
 namespace App\Domain\Models;
 
+use App\Domain\Behaviors\Sports\BaseballBehavior;
+use App\Domain\Behaviors\Sports\BasketballBehavior;
+use App\Domain\Behaviors\Sports\FootballBehavior;
+use App\Domain\Behaviors\Sports\HockeyBehavior;
+use App\Domain\Behaviors\Sports\SportBehavior;
 use App\Domain\Collections\PositionCollection;
 use App\Domain\Models\League;
 use App\Domain\Models\Position;
+use App\Exceptions\UnknownBehaviorException;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,6 +18,7 @@ use Illuminate\Database\Eloquent\Model;
  * Class Sport
  * @package App
  *
+ * @property int $id
  * @property string $name
  *
  * @property PositionCollection $positions
@@ -40,5 +47,20 @@ class Sport extends Model
     public function statTypes()
     {
         return $this->hasMany(StatType::class);
+    }
+
+    public function getBehavior(): SportBehavior
+    {
+        switch ($this->name) {
+            case self::FOOTBALL:
+                return app(FootballBehavior::class);
+            case self::BASEBALL:
+                return app(BaseballBehavior::class);
+            case self::BASKETBALL:
+                return app(BasketballBehavior::class);
+            case self::HOCKEY:
+                return app(HockeyBehavior::class);
+        }
+        throw new UnknownBehaviorException($this->name, SportBehavior::class);
     }
 }
