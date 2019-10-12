0587960ba5288239775588ad03dadcf8ec03c973
Brian Rice <bwrice83@gmail.com>
2019-10-12 17:46:32 -0400

    create gradient-bar component

---
+++ resources/js/components/commandCenter/global/GradientBar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="v-progress-linear rh-gradient-progress" :style="progressStyles">
+        <div class="v-progress-linear__determinate rh-progress-missing" :style="progressMissingStyles">
+        </div>
+        <div class="v-progress-linear__content">
+            <slot>
+                <!-- default slot -->
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GradientBar",
+        props: {
+            height: {
+                type: Number,
+                required: true
+            },
+            percent: {
+                type: Number,
+                required: true
+            }
+        },
+        computed: {
+            progressStyles() {
+                return {
+                    height: this.height + 'px'
+                }
+            },
+            progressMissing() {
+                return 100 - this.percent;
+            },
+            progressMissingStyles() {
+                return {
+                    left: this.percent + '%',
+                    width: this.progressMissing + '%'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .rh-gradient-progress {
+        /*background-image: linear-gradient(to right, #333491, #6024ad, #9c13cf, #f500f5);*/
+        background-image: linear-gradient(to right, #cfc07e, #e0a06c, #ed4040, #fc0065);
+    }
+    .rh-progress-missing {
+        position: absolute;
+        background-color: #424242
+    }
+</style>
