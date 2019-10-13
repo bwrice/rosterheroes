9911bcf9e7a35cfade98122e084a06d944f5b36e
Brian Rice <bwrice83@gmail.com>
2019-10-12 22:21:39 -0400

    add api methods for realm/map

---
+++ resources/js/api/realmApi.js
@@ -0,0 +1,15 @@
+
+export async function getProvinces() {
+    let response = await axios.get('/api/v1/provinces');
+    return response.data;
+}
+
+export async function getTerritories() {
+    let response = await axios.get('/api/v1/territories');
+    return response.data;
+}
+
+export async function getContinents() {
+    let response = await axios.get('/api/v1/continents');
+    return response.data;
+}
