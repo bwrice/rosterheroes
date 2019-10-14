a8066c27f572fce7a44b09b43af2d78baabc3bac
Brian Rice <bwrice83@gmail.com>
2019-10-14 17:49:37 -0400

    build CombatPositionDialog component

---
+++ resources/js/components/commandCenter/barracks/CombatPositionDialog.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-card class="pa-3">
+        <v-card-title>
+            <v-row justify="center">
+                <span class="title" style="color: rgba(255, 255, 255, .85)">Change Combat Position</span>
+            </v-row>
+        </v-card-title>
+        <v-row no-gutters>
+            <v-col cols="8" offset="2" class="pa-2">
+                <CombatPositionIcon :combat-position-id="selectedCombatPositionID"></CombatPositionIcon>
+            </v-col>
+            <v-col cols="8" offset="2">
+                <v-row justify="center" align="center">
+                    <v-select
+                        v-model="selectedCombatPositionID"
+                        :items="_combatPositions"
+                        item-text="name" item-value="id"
+                    ></v-select>
+                </v-row>
+            </v-col>
+            <v-col cols="12">
+                <v-row justify="center" align="center">
+                    <v-btn color="error" class="mx-2" @click="emitClose">Cancel</v-btn>
+                    <v-btn :disabled="disabled" color="primary" class="mx-2">Change Position</v-btn>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
+
+<script>
+    import Hero from "../../../models/Hero";
+    import CombatPositionIcon from "../global/CombatPositionIcon";
+    import {mapGetters} from 'vuex';
+
+    export default {
+        name: "CombatPositionDialog",
+        components: {CombatPositionIcon},
+        created() {
+            this.selectedCombatPositionID = this.hero.combatPositionID;
+        },
+        props: {
+            hero: {
+                type: Hero,
+                required: true
+            }
+        },
+        data() {
+            return {
+                selectedCombatPositionID: 0
+            }
+        },
+        methods: {
+            emitClose() {
+                this.$emit('close')
+            }
+        },
+        computed: {
+            ...mapGetters([
+                '_combatPositions',
+                '_combatPositionByID'
+            ]),
+            disabled() {
+                return this.hero.combatPositionID === this.selectedCombatPositionID;
+            },
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
