962f8de3ba74bed445dd5f4928bcc19bd886af40
Brian Rice <bwrice83@gmail.com>
2019-10-17 00:16:47 -0400

    update nation routes for new architecture

--- resources/js/routes/nationRoutes.js
+++ resources/js/routes/nationRoutes.js
@@ -1,14 +1,26 @@
 import NationMain from "../components/commandCenter/views/nation/NationMain";
 import NationNavigationDrawer from "../components/commandCenter/views/nation/NationNavigationDrawer";
+import CommandCenter from "../views/CommandCenter";
+import SquadAppBarContent from "../components/commandCenter/appBarContent/SquadAppBarContent";
 
 export const nationRoutes = {
     path: '/command-center/:squadSlug/nation',
-    name: 'nation',
     meta: {
         footerButton: 'nation'
     },
-    components: {
-        default: NationMain,
-        drawer: NationNavigationDrawer
-    }
+    component: CommandCenter,
+    children: [
+        {
+            path: '',
+            components: {
+                default: NationMain,
+                drawer: NationNavigationDrawer,
+                appBarContent: SquadAppBarContent
+            },
+            name: 'nation-main',
+            meta: {
+                footerButton: 'nation'
+            }
+        }
+    ]
 };
