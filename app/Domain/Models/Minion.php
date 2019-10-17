38ed575557cefd68ee0b86f6c830ac981166a240
Brian Rice <bwrice83@gmail.com>
2019-10-17 17:13:19 -0400

    seed minions

--- app/Domain/Models/Minion.php
+++ app/Domain/Models/Minion.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Minion extends Model
 {
+    protected $guarded = [];
+
     public function attacks()
     {
         return $this->belongsToMany(Attack::class);
---
+++ database/migrations/2019_10_17_163145_seed_minions.php
@@ -0,0 +1,116 @@
+<?php
+
+use App\Domain\Models\Attack;
+use App\Domain\Models\CombatPosition;
+use App\Domain\Models\EnemyType;
+use App\Domain\Models\Minion;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
+
+class SeedMinions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $minions = collect([
+            [
+                'name' => 'Skeleton Scout',
+                'level' => 8,
+                'damage_rating' => 35,
+                'health_rating' => 35,
+                'protection_rating' => 30,
+                'enemy_type' => EnemyType::SKELETON,
+                'combat_position' => CombatPosition::BACK_LINE,
+                'attacks' => [
+                    'Slash',
+                    'Arrow'
+                ]
+            ],
+            [
+                'name' => 'Skeleton Soldier',
+                'level' => 14,
+                'damage_rating' => 15,
+                'health_rating' => 35,
+                'protection_rating' => 50,
+                'enemy_type' => EnemyType::SKELETON,
+                'combat_position' => CombatPosition::FRONT_LINE,
+                'attacks' => [
+                    'Slash',
+                    'Double Slash',
+                    'Slice'
+                ]
+            ],
+            [
+                'name' => 'Skeleton Archer',
+                'level' => 8,
+                'damage_rating' => 60,
+                'health_rating' => 15,
+                'protection_rating' => 25,
+                'enemy_type' => EnemyType::SKELETON,
+                'combat_position' => CombatPosition::BACK_LINE,
+                'attacks' => [
+                    'Arrow',
+                    'Double Arrow'
+                ]
+            ],
+        ]);
+
+        $attacks = Attack::all();
+
+        $minions->each(function ($minionData) {
+            $ratingSum = $minionData['damage_rating'] + $minionData['health_rating'] + $minionData['protection_rating'];
+            if ($ratingSum != 100 ) {
+                throw new RuntimeException("Rating sum of: " . $ratingSum . " does not equal 100");
+            }
+        });
+
+        $minions->each(function ($minionData) use ($attacks) {
+            $count = count($minionData['attacks']);
+            $attacksToAttach = $attacks->filter(function (Attack $attack) use ($minionData) {
+                return in_array($attack->name, $minionData['attacks']);
+            });
+            if ($count != $attacksToAttach->count() ) {
+                throw new RuntimeException("Not all of the attacks for " . $minionData['name'] . " were found");
+            }
+        });
+
+        $enemyTypes = EnemyType::all();
+        $combatPositions = CombatPosition::all();
+
+        $minions->each(function ($minionData) use ($enemyTypes, $combatPositions, $attacks) {
+
+            /** @var Minion $minion */
+            $minion = Minion::query()->create([
+                'uuid' => Str::uuid(),
+                'name' => $minionData['name'],
+                'level' => $minionData['level'],
+                'damage_rating' => $minionData['damage_rating'],
+                'health_rating' => $minionData['health_rating'],
+                'protection_rating' => $minionData['protection_rating'],
+                'enemy_type_id' => $enemyTypes->where('name', '=', $minionData['enemy_type'])->first()->id,
+                'combat_position_id' => $combatPositions->where('name', '=', $minionData['combat_position'])->first()->id
+            ]);
+            $attacksToAttach = $attacks->filter(function (Attack $attack) use ($minionData) {
+                return in_array($attack->name, $minionData['attacks']);
+            });
+
+            $minion->attacks()->saveMany($attacksToAttach);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
