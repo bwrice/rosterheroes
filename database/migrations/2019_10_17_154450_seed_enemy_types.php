b162bd9ceb44f197bbd5ddf5e62f618da28b71d1
Brian Rice <bwrice83@gmail.com>
2019-10-17 16:32:57 -0400

    create seeder for enemy types

---
+++ database/migrations/2019_10_17_154450_seed_enemy_types.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Domain\Models\EnemyType;
+use App\Helpers\ModelNameSeeding\ModelNameSeederMigration;
+
+class SeedEnemyTypes extends ModelNameSeederMigration
+{
+    protected function getModelClass(): string
+    {
+        return EnemyType::class;
+    }
+
+    public function getSeedNames(): array
+    {
+        return [
+            EnemyType::SKELETON
+        ];
+    }
+}
