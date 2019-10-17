caa6d0d59d64e5b525f511b0287f8cbbd7eed990
Brian Rice <bwrice83@gmail.com>
2019-10-17 12:55:44 -0400

    create EnemyType model and migration

---
+++ app/Domain/Models/EnemyType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EnemyType extends Model
+{
+    protected $guarded = [];
+
+    public const SKELETON = 'Skeleton';
+
+}
