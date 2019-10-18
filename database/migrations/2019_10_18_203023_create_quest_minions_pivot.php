34598d09a54e4fefd693a1648a44da7b3bb034b5
Brian Rice <bwrice83@gmail.com>
2019-10-18 17:20:42 -0400

    add minion_quest pivot schema

---
+++ database/migrations/2019_10_18_203023_create_quest_minions_pivot.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestMinionsPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('minion_quest', function (Blueprint $table) {
+            $table->integer('minion_id')->unsigned();
+            $table->integer('quest_id')->unsigned();
+            $table->primary(['minion_id', 'quest_id']);
+            $table->timestamps();
+        });
+
+        Schema::table('minion_quest', function (Blueprint $table) {
+            $table->foreign('minion_id')->references('id')->on('minions');
+            $table->foreign('quest_id')->references('id')->on('quests');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('minion_quest');
+    }
+}
