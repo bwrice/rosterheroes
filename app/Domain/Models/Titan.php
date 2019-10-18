11d32c8aff48d633becd5291690b284088b29d41
Brian Rice <bwrice83@gmail.com>
2019-10-18 16:22:17 -0400

    set guarded attribute

--- app/Domain/Models/Titan.php
+++ app/Domain/Models/Titan.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Titan extends Model
 {
+    protected $guarded = [];
+
     public function attacks()
     {
         return $this->belongsToMany(Attack::class);
