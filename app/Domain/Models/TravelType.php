62c71811dd8cf5b5c56b482aa6fca42baac0939f
Brian Rice <bwrice83@gmail.com>
2019-10-18 17:51:12 -0400

    create TravelType model and migration

---
+++ app/Domain/Models/TravelType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TravelType extends Model
+{
+    protected $guarded;
+
+    public const STATIONARY = 'Stationary';
+    public const BORDER = 'Border';
+    public const TERRITORY = 'Territory';
+    public const CONTINENT = 'Continent';
+    public const REALM = 'Realm';
+}
