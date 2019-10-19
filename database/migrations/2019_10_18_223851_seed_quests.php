17acbf0637c4af31bf3bc3a7e8d1cd7c2ae2f45c
Brian Rice <bwrice83@gmail.com>
2019-10-19 11:41:12 -0400

    Seed our first Quest

---
+++ database/migrations/2019_10_18_223851_seed_quests.php
@@ -0,0 +1,158 @@
+<?php
+
+use App\Domain\Models\Minion;
+use App\Domain\Models\Province;
+use App\Domain\Models\Quest;
+use App\Domain\Models\Skirmish;
+use App\Domain\Models\Titan;
+use App\Domain\Models\TravelType;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Str;
+
+class SeedQuests extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $quests = collect([
+            [
+                'name' => 'North Skeleton Fortress',
+                'travel_type' => TravelType::STATIONARY,
+                'level' => 125,
+                'starting_province' => 'Everium',
+                'titans' => [
+                    [
+                        'name' => 'Skeleton Overlord',
+                        'count' => 1
+                    ],
+                    [
+                        'name' => 'Skeleton General',
+                        'count' => 4
+                    ]
+                ],
+                'minions' => [
+                    [
+                        'name' => 'Skeleton Guard',
+                        'weight' => 35
+                    ],
+                    [
+                        'name' => 'Skeleton Archer',
+                        'weight' => 20
+                    ],
+                    [
+                        'name' => 'Skeleton Mage',
+                        'weight' => 20
+                    ],
+                    [
+                        'name' => 'Skeleton Soldier',
+                        'weight' => 15
+                    ],
+                    [
+                        'name' => 'Skeleton Marksman',
+                        'weight' => 10
+                    ],
+                ],
+                'skirmishes' => [
+                    'Small Skeleton Pack',
+                    'Medium Skeleton Pack',
+                    'Large Skeleton Pack'
+                ]
+
+            ]
+        ]);
+
+        $minions = Minion::all();
+        $titans = Titan::all();
+        $skirmishes = Skirmish::all();
+        $provinces = Province::all();
+        $travelTypes = TravelType::all();
+
+        $quests->each(function ($questData) {
+            $weightSum = collect($questData['minions'])->sum(function ($minionData) {
+                return $minionData['weight'];
+            });
+            if ($weightSum != 100) {
+                throw new RuntimeException("Minion weight of " . $weightSum . " does not equal 100 for quest: " . $questData['name']);
+            }
+        });
+
+        $quests->each(function ($questData) use ($minions) {
+            $minionNames = array_map(function ($minionData) {
+                return $minionData['name'];
+            }, $questData['minions']);
+
+            $minionsToAttach = $minions->whereIn('name', $minionNames);
+            if ($minionsToAttach->count() != count($minionNames)) {
+                throw new RuntimeException("Couldn't find all the minions for quest: " . $questData['name']);
+            }
+        });
+
+        $quests->each(function ($questData) use ($titans) {
+            $titanNames = array_map(function ($minionData) {
+                return $minionData['name'];
+            }, $questData['titans']);
+
+            $titansToAttach = $titans->whereIn('name', $titanNames);
+            if ($titansToAttach->count() != count($titanNames)) {
+                throw new RuntimeException("Couldn't find all the titans for quest: " . $questData['name']);
+            }
+        });
+
+        $quests->each(function ($questData) use ($skirmishes) {
+
+            $skirmishesToAttach = $skirmishes->whereIn('name', $questData['skirmishes']);
+            if ($skirmishesToAttach->count() != count($questData['skirmishes'])) {
+                throw new RuntimeException("Couldn't find all the skirmishes for quest: " . $questData['name']);
+            }
+        });
+
+        $quests->each(function ($questData) use ($provinces) {
+            $province = $provinces->firstWhere('name', $questData['starting_province']);
+            if (! $province) {
+                throw new RuntimeException("Couldn't find province for quest: " . $questData['name']);
+            }
+        });
+
+        $quests->each(function ($questData) use ($skirmishes, $minions, $titans, $provinces, $travelTypes) {
+
+            /** @var Quest $quest */
+            $quest = Quest::query()->create([
+                'uuid' => Str::uuid(),
+                'name' => $questData['name'],
+                'level' => $questData['level'],
+                'percent' => 100,
+                'province_id' => $provinces->firstWhere('name', $questData['starting_province'])->id,
+                'travel_type_id' => $travelTypes->firstWhere('name', $questData['travel_type'])->id,
+            ]);
+
+            collect($questData['minions'])->each(function ($minionData) use ($quest, $minions) {
+                $minion = $minions->firstWhere('name', $minionData['name']);
+                $quest->minions()->save($minion, ['weight' => $minionData['weight']]);
+            });
+
+            collect($questData['titans'])->each(function ($titanData) use ($quest, $titans) {
+                $titan = $titans->firstWhere('name', $titanData['name']);
+                $quest->titans()->save($titan, ['count' => $titanData['count']]);
+            });
+
+            $skirmishesToAttach = $skirmishes->whereIn('name', $questData['skirmishes']);
+            $quest->skirmishes()->saveMany($skirmishesToAttach);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
