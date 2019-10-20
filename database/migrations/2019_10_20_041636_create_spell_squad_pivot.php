b4c7bd8f8bcbbd6f060b7f1534e207d55e28c83c
Brian Rice <bwrice83@gmail.com>
2019-10-20 00:47:58 -0400

    create spell_squad pivot

---
+++ database/migrations/2019_10_20_041636_create_spell_squad_pivot.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpellSquadPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spell_squad', function (Blueprint $table) {
+            $table->integer('spell_id')->unsigned();
+            $table->integer('squad_id')->unsigned();
+            $table->primary(['spell_id', 'squad_id']);
+            $table->timestamps();
+        });
+
+        Schema::table('campaign_quest', function (Blueprint $table) {
+            $table->foreign('spell_id')->references('id')->on('spells');
+            $table->foreign('squad_id')->references('id')->on('squads');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spell_squad');
+    }
+}
