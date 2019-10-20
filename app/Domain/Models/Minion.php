baefedba9daacaabfaa4c91cc88120fe73b4deec
Brian Rice <bwrice83@gmail.com>
2019-10-19 21:07:29 -0400

    add properties and enemyType relation for Minion

--- app/Domain/Models/Minion.php
+++ app/Domain/Models/Minion.php
@@ -9,7 +9,17 @@ use Illuminate\Database\Eloquent\Model;
  * Class Minion
  * @package App\Domain\Models
  *
+ * @property string $uuid
  * @property string $name
+ * @property string $slug
+ * @property int $level
+ * @property int $damage_rating
+ * @property int $health_rating
+ * @property int $protection_rating
+ * @property int $enemy_type_id
+ * @property int $combat_position_id
+ *
+ * @property EnemyType $enemyType
  */
 class Minion extends Model
 {
@@ -21,4 +31,10 @@ class Minion extends Model
     {
         return $this->belongsToMany(Attack::class)->withTimestamps();
     }
+
+    public function enemyType()
+    {
+        return $this->belongsTo(EnemyType::class);
+    }
+
 }
