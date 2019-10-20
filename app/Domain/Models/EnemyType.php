3bcee82662929cecc5618a895849781dab41d287
Brian Rice <bwrice83@gmail.com>
2019-10-19 20:27:16 -0400

    add getBehavior() method to EnemyType

--- app/Domain/Models/EnemyType.php
+++ app/Domain/Models/EnemyType.php
@@ -2,12 +2,26 @@
 
 namespace App\Domain\Models;
 
+use App\Domain\Behaviors\EnemyTypes\EnemyTypeBehavior;
+use App\Domain\Behaviors\EnemyTypes\EnemyTypeBehaviorFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class EnemyType
+ * @package App\Domain\Models
+ *
+ * @property string $name
+ */
 class EnemyType extends Model
 {
     protected $guarded = [];
 
     public const SKELETON = 'Skeleton';
 
+    public function getBehavior(): EnemyTypeBehavior
+    {
+        /** @var EnemyTypeBehaviorFactory $factory */
+        $factory = app(EnemyTypeBehaviorFactory::class);
+        return $factory->getBehavior($this->name);
+    }
 }
