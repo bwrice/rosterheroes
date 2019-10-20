2964ada55d80d7cd56a3fc8ea97fed90ef0f92cf
Brian Rice <bwrice83@gmail.com>
2019-10-20 01:15:58 -0400

    implement BoostsMeasurables in Spell

--- app/Domain/Models/Spell.php
+++ app/Domain/Models/Spell.php
@@ -2,10 +2,22 @@
 
 namespace App\Domain\Models;
 
+use App\Domain\Behaviors\MeasurableTypes\Attributes\AttributeBehavior;
+use App\Domain\Behaviors\MeasurableTypes\MeasurableTypeBehavior;
+use App\Domain\Behaviors\MeasurableTypes\Qualities\QualityBehavior;
+use App\Domain\Behaviors\MeasurableTypes\Resources\ResourceBehavior;
+use App\Domain\Collections\MeasurableBoostCollection;
+use App\Domain\Interfaces\BoostsMeasurables;
 use App\Domain\Models\MeasurableBoost;
 use Illuminate\Database\Eloquent\Model;
 
-class Spell extends Model
+/**
+ * Class Spell
+ * @package App\Domain\Models
+ *
+ * @property MeasurableBoostCollection $measurableBoosts
+ */
+class Spell extends Model implements BoostsMeasurables
 {
     const RELATION_MORPH_MAP_KEY = 'spells';
 
@@ -14,5 +26,18 @@ class Spell extends Model
     public function measurableBoosts()
     {
         return $this->morphMany(MeasurableBoost::class, 'booster' );
+    }
+
+    public function getMeasurableBoostMultiplier(MeasurableTypeBehavior $measurableTypeBehavior): int
+    {
+        switch($measurableTypeBehavior->getGroupName()) {
+            case ResourceBehavior::GROUP_NAME:
+                return 8;
+            case QualityBehavior::GROUP_NAME:
+                return 4;
+            case AttributeBehavior::GROUP_NAME:
+                return 2;
+        }
+        throw new \InvalidArgumentException("Unknown group name: " . $measurableTypeBehavior->getGroupName());
     }
 }
