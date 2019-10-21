a75010d0c46558e2ada472bcf915f3b6195e69ca
Brian Rice <bwrice83@gmail.com>
2019-10-20 20:00:42 -0400

    create domain action AddSpellToLibraryAction

---
+++ app/Domain/Actions/AddSpellToLibraryAction.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Models\Spell;
+use App\Domain\Models\Squad;
+use App\Exceptions\AddSpellException;
+
+class AddSpellToLibraryAction
+{
+    public function execute(Squad $squad, Spell $spell)
+    {
+        $existingSpell = $squad->spells()->where('id' , '=', $spell->id)->first();
+        if ($existingSpell) {
+            throw new AddSpellException($squad, $spell, $squad->name ." already owns " . $spell->name, AddSpellException::CODE_ALREADY_OWNS);
+        }
+    }
+}
