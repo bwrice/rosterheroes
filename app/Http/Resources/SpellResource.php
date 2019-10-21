c55478d19189bc89330f3f18b80c988f870d143f
Brian Rice <bwrice83@gmail.com>
2019-10-21 12:50:34 -0400

    create spell resource

---
+++ app/Http/Resources/SpellResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\Spell;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class SpellResource
+ * @package App\Http\Resources
+ *
+ * @mixin Spell
+ */
+class SpellResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'manaCost' => $this->manaCost(),
+            'measurableBoosts' => MeasurableBoostResource::collection($this->measurableBoosts)
+        ];
+    }
+}
