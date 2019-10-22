9ac8757aebddaaa1ade810f0db9ddc78bec36e64
Brian Rice <bwrice83@gmail.com>
2019-10-21 20:15:57 -0400

    create spell panel component

---
+++ resources/js/components/commandCenter/barracks/SpellPanel.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-sheet color="#564c59">
+        <span class="subtitle-2">{{spell.name}}</span>
+    </v-sheet>
+</template>
+
+<script>
+    import Spell from "../../../models/Spell";
+
+    export default {
+        name: "SpellPanel",
+        props: {
+            spell: {
+                type: Spell,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
