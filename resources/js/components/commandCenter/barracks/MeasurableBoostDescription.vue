dfee542207b5bcbc54d8bede6cef2f3c0e67808d
Brian Rice <bwrice83@gmail.com>
2019-10-21 23:53:23 -0400

    create MeasurableBoostDescription component

---
+++ resources/js/components/commandCenter/barracks/MeasurableBoostDescription.vue
@@ -0,0 +1,28 @@
+<template>
+    <span class="subtitle-2 font-weight-regular" style="color: rgba(0, 0, 0, .85)">
+        {{description}}
+    </span>
+</template>
+
+<script>
+    import MeasurableBoost from "../../../models/MeasurableBoost";
+
+    export default {
+        name: "MeasurableBoostDescription",
+        props: {
+            measurableBoost: {
+                type: MeasurableBoost,
+                required: true
+            }
+        },
+        computed: {
+            description() {
+                return '+' + this.measurableBoost.boostAmount;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
