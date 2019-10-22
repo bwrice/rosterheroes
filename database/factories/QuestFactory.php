c5476efd524a2c86a63980cd63c9fcc5f3a115ab
Brian Rice <bwrice83@gmail.com>
2019-10-22 11:25:07 -0400

    add columns to QuestModelFactory

--- database/factories/QuestFactory.php
+++ database/factories/QuestFactory.php
@@ -4,8 +4,13 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Domain\Models\Quest::class, function (Faker $faker) {
     return [
+        'level' => random_int(100,999),
+        'percent' => 100,
         'uuid' => (string) \Ramsey\Uuid\Uuid::uuid4(),
         'name' => $faker->company . ' ' . $faker->city,
-        'province_id' => \App\Domain\Models\Province::query()->inRandomOrder()->first()->id
+        'province_id' => \App\Domain\Models\Province::query()->inRandomOrder()->first()->id,
+        'travel_type_id' => function() {
+            return \App\Domain\Models\TravelType::query()->inRandomOrder()->first()->id;
+        }
     ];
 });
