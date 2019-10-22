b2cb300b19185812392e3147aa9eb11453c1494d
Brian Rice <bwrice83@gmail.com>
2019-10-22 00:00:06 -0400

    map boost values into correct MeasurableBoost object

--- resources/js/models/Spell.js
+++ resources/js/models/Spell.js
@@ -1,9 +1,11 @@
+import MeasurableBoost from "./MeasurableBoost";
+
 export default class Spell {
 
     constructor({id, name = '', manaCost, measurableBoosts = []}) {
         this.id = id;
         this.name = name;
         this.manaCost = manaCost;
-        this.measurableBoosts = measurableBoosts;
+        this.measurableBoosts = measurableBoosts.map(boost => new MeasurableBoost(boost));
     }
 }
