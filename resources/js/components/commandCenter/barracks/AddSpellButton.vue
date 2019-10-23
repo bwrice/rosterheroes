c437ff10c571834cf08e9473abec8c404033c1f8
Brian Rice <bwrice83@gmail.com>
2019-10-23 00:53:31 -0400

    create AddSpellButton component

---
+++ resources/js/components/commandCenter/barracks/AddSpellButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-btn class="my-2" block color="primary">Cast Spell</v-btn>
+</template>
+
+<script>
+    import Spell from "../../../models/Spell";
+    import Hero from "../../../models/Hero";
+
+    export default {
+        name: "AddSpellButton",
+        props: {
+            hero: {
+                type: Hero,
+                required: true
+            },
+            spell: {
+                type: Spell,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
