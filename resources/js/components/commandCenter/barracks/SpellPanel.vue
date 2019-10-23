e200afd051342749fdae86426e244e3128c1ed24
Brian Rice <bwrice83@gmail.com>
2019-10-23 00:53:47 -0400

    tweak margin and padding

--- resources/js/components/commandCenter/barracks/SpellPanel.vue
+++ resources/js/components/commandCenter/barracks/SpellPanel.vue
@@ -18,7 +18,7 @@
         </v-row>
         <v-row v-if="expanded" no-gutters>
             <v-col cols="12">
-                <v-sheet color="rgba(0, 0, 0, 0.4)" class="mx-2 my-1 px-3 pt-1">
+                <v-sheet color="rgba(0, 0, 0, 0.4)" class="mx-2 my-1 px-3 pb-1 pt-1">
                     <v-row no-gutters class="flex-column" align="start">
                         <span class="subtitle-1 font-weight-bold" style="color: rgba(255, 255, 255, 0.85)">
                             Mana Cost: {{this.spell.manaCost}}
