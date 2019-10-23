78519d1bac650e8155c24812155e7c8d50823363
Brian Rice <bwrice83@gmail.com>
2019-10-23 19:36:15 -0400

    add manaCost() method to SpellCollection

--- app/Domain/Collections/SpellCollection.php
+++ app/Domain/Collections/SpellCollection.php
@@ -4,9 +4,15 @@
 namespace App\Domain\Collections;
 
 
+use App\Domain\Models\Spell;
 use Illuminate\Database\Eloquent\Collection;
 
 class SpellCollection extends Collection
 {
-
+    public function manaCost(): int
+    {
+        return $this->sum(function (Spell $spell) {
+            return $spell->manaCost();
+        });
+    }
 }
