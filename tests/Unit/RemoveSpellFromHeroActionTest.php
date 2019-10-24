2f4486aaa4ab740873867f22ba926d5a99f35f5a
Brian Rice <bwrice83@gmail.com>
2019-10-23 22:48:42 -0400

    add week locked validation on RemoveSpellFromHeroAction

---
+++ tests/Unit/RemoveSpellFromHeroActionTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Domain\Actions\RemoveSpellFromHeroAction;
+use App\Domain\Models\Hero;
+use App\Domain\Models\HeroPost;
+use App\Domain\Models\Spell;
+use App\Domain\Models\Squad;
+use App\Domain\Models\Week;
+use App\Exceptions\SpellCasterException;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Date;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class RemoveSpellFromHeroActionTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @var Hero */
+    protected $hero;
+
+    /** @var Squad */
+    protected $squad;
+
+    /** @var Spell */
+    protected $spell;
+
+    /** @var RemoveSpellFromHeroAction */
+    protected $domainAction;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->squad = factory(Squad::class)->create();
+        $this->hero = factory(Hero::class)->create();
+        factory(HeroPost::class)->create([
+            'squad_id' => $this->squad->id,
+            'hero_id' => $this->hero->id
+        ]);
+        $this->spell = Spell::query()->where('name', '=', 'Resolve')->inRandomOrder()->first();
+        $this->squad->spells()->save($this->spell);
+        $this->hero->spells()->save($this->spell);
+        /** @var Week $week */
+        $week = factory(Week::class)->create();
+        $week->everything_locks_at = Date::now()->addHour();
+        $week->save();
+        Week::setTestCurrent($week);
+        $this->domainAction = app(RemoveSpellFromHeroAction::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_throw_an_exception_if_the_current_week_is_locked()
+    {
+
+        /** @var Week $week */
+        $week = factory(Week::class)->create();
+        $week->everything_locks_at = Date::now()->subHour();
+        $week->save();
+
+        Week::setTestCurrent($week);
+
+        try {
+            $this->domainAction->execute($this->hero, $this->spell);
+        } catch (SpellCasterException $exception) {
+            $this->assertEquals(SpellCasterException::CODE_WEEK_LOCKED, $exception->getCode());
+            return;
+        }
+
+        $this->fail("Exception not thrown");
+    }
+}
