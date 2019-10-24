d5cd157f48d62ade2f5a39a98d6e6d812f8ad1c9
Brian Rice <bwrice83@gmail.com>
2019-10-23 21:07:07 -0400

    execute domain action and return fresh HeroResource

--- app/Http/Controllers/HeroSpellController.php
+++ app/Http/Controllers/HeroSpellController.php
@@ -2,15 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Domain\Actions\CastSpellOnHeroAction;
 use App\Domain\Models\Hero;
+use App\Domain\Models\Spell;
+use App\Exceptions\SpellCasterException;
+use App\Http\Resources\HeroResource;
 use App\Policies\HeroPolicy;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class HeroSpellController extends Controller
 {
-    public function store($heroSlug, Request $request)
+    /**
+     * @param $heroSlug
+     * @param Request $request
+     * @param CastSpellOnHeroAction $castSpellOnHeroAction
+     * @return HeroResource
+     * @throws AuthorizationException
+     * @throws ValidationException
+     */
+    public function store($heroSlug, Request $request, CastSpellOnHeroAction $castSpellOnHeroAction)
     {
         $hero = Hero::findSlugOrFail($heroSlug);
         $this->authorize(HeroPolicy::MANAGE, $hero);
+        /** @var Spell $spell */
+        $spell = Spell::query()->find($request->spell);
+        try {
+            $castSpellOnHeroAction->execute($hero, $spell);
+        } catch (SpellCasterException $exception) {
+            throw ValidationException::withMessages([
+                'spell-caster' => $exception->getMessage()
+            ]);
+        }
+        return new HeroResource($hero->fresh(Hero::heroResourceRelations()));
     }
 }
