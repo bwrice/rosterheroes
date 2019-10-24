6d51499bcef94bb826ab636c4e9dc21500945071
Brian Rice <bwrice83@gmail.com>
2019-10-24 00:03:33 -0400

    build out logic for remove spell button

--- resources/js/components/commandCenter/barracks/RemoveSpellButton.vue
+++ resources/js/components/commandCenter/barracks/RemoveSpellButton.vue
@@ -1,10 +1,18 @@
 <template>
-    <v-btn class="my-2" block color="error">Remove Spell</v-btn>
+    <v-btn
+        class="my-2"
+        block color="error"
+        :disabled="pending"
+        @click="removeSpell"
+    >
+        Remove Spell
+    </v-btn>
 </template>
 
 <script>
     import Spell from "../../../models/Spell";
     import Hero from "../../../models/Hero";
+    import {mapActions} from 'vuex';
 
     export default {
         name: "RemoveSpellButton",
@@ -16,6 +24,24 @@
             spell: {
                 type: Spell,
                 required: true
+            }
+        },
+        data() {
+            return {
+                pending: false
+            }
+        },
+        methods: {
+            ...mapActions([
+                'removeSpellOnHero'
+            ]),
+            async removeSpell() {
+                this.pending = true;
+                await this.removeSpellOnHero({
+                    hero: this.hero,
+                    spell: this.spell
+                });
+                this.pending = false;
             }
         }
     }
