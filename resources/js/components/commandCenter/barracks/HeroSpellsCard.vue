b4817c49133913649860b272c8153675667ea4db
Brian Rice <bwrice83@gmail.com>
2019-10-24 22:06:40 -0400

    show spell-power and mana used in hero spells card

--- resources/js/components/commandCenter/barracks/HeroSpellsCard.vue
+++ resources/js/components/commandCenter/barracks/HeroSpellsCard.vue
@@ -1,13 +1,32 @@
 <template>
     <v-card>
-        <v-card-title>Spells</v-card-title>
+        <v-card-title>
+            <v-row no-gutters align="center" justify="space-between">
+                <v-col cols="6">
+                    <span>Spells</span>
+                </v-col>
+                <v-col cols="6">
+                    <v-row no-gutters class="flex-column" align="end" style="color: rgba(255, 255, 255, .85)">
+                        <div>
+                            <span class="subtitle-1">Spell Power: </span>
+                            <span class="subtitle-1 font-weight-bold">{{hero.spellPower}}</span>
+                        </div>
+                        <div>
+                            <span class="subtitle-1">Mana Used: </span>
+                            <span class="subtitle-1 font-weight-bold">{{hero.manaUsed}}</span>
+                        </div>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-card-title>
         <v-card-text>
-            <span class="subtitle-1">Spells Casted</span>
+            <span class="subtitle-1">Current Spells on {{hero.name}}</span>
             <SpellPanelIterator :spells="hero.spells" :items-per-page="4">
                 <template v-slot:after-boosts="panelProps">
                     <RemoveSpellButton :hero="hero" :spell="panelProps.spell"></RemoveSpellButton>
                 </template>
             </SpellPanelIterator>
+            <v-divider class="my-3"></v-divider>
             <span class="subtitle-1">Spell Library</span>
             <SpellPanelIterator :spells="availableSpells">
                 <template v-slot:after-boosts="panelProps">
