59382eb1c6d68a8c447f65ff9ba174032e26add5
Brian Rice <bwrice83@gmail.com>
2019-10-25 20:51:07 -0400

    add attacks to MinionResource

--- app/Http/Resources/MinionResource.php
+++ app/Http/Resources/MinionResource.php
@@ -30,7 +30,10 @@ class MinionResource extends JsonResource
             'level' => $this->level,
             'startingHealth' => $this->getStartingHealth(),
             'protection' => $this->getProtection(),
-            'blockChance' => $this->getBlockChance()
+            'blockChance' => $this->getBlockChance(),
+            'attacks' => AttackResource::collection($this->attacks)->collection->each(function (AttackResource $attackResource) {
+                $attackResource->setHasAttacks($this->resource);
+            }),
         ];
     }
 }
