5224f8d3c3ee5eb1ce0478898f84ca79619d0570
Brian Rice <bwrice83@gmail.com>
2019-10-28 17:39:15 -0400

    update model factory for new schema

---
+++ database/factories/ResidenceModelFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(\App\Domain\Models\Residence::class, function (Faker $faker) {
+
+    /** @var \App\Domain\Models\Squad $squad */
+    $squad = factory(\App\Domain\Models\Squad::class)->create();
+    /** @var \App\Domain\Models\ResidenceType $residenceType */
+    $residenceType = \App\Domain\Models\ResidenceType::query()->where('name', '=', \App\Domain\Models\ResidenceType::SHACK)->first();
+
+    return [
+        'squad_id' => $squad->id,
+        'residence_type_id' => $residenceType->id,
+        'province_id' => $squad->province_id
+    ];
+});
