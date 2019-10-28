2e26a2ae92a51dbcb6e6fec8a82753d0ac9ff0df
Brian Rice <bwrice83@gmail.com>
2019-10-28 11:51:03 -0400

    fix conditional

--- app/Domain/Actions/UnEquipItemFromHeroAction.php
+++ app/Domain/Actions/UnEquipItemFromHeroAction.php
@@ -29,7 +29,8 @@ class UnEquipItemFromHeroAction
      */
     public function execute(Item $item, Hero $hero, Collection $hasSlots = null): Collection
     {
-        if ($item->hasItems->getMorphType() !== Hero::RELATION_MORPH_MAP_KEY && $item->hasItems->getMorphID() !== $hero->id) {
+        if (is_null($item->hasItems) ||
+            ($item->hasItems->getMorphType() !== Hero::RELATION_MORPH_MAP_KEY || $item->hasItems->getMorphID() !== $hero->id)) {
             throw new ItemTransactionException($item, "Item does not belong to hero", ItemTransactionException::CODE_INVALID_OWNERSHIP);
         }
         return $this->moveItemToBackupAction->execute($item, $hero, $hasSlots);
