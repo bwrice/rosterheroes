eb3464b54564575a8e4e003879d4b1c766ac529f
Brian Rice <bwrice83@gmail.com>
2019-10-29 17:55:59 -0400

    build out validation for domain action

--- app/Domain/Actions/EquipWagonItemForHeroAction.php
+++ app/Domain/Actions/EquipWagonItemForHeroAction.php
@@ -4,13 +4,27 @@
 namespace App\Domain\Actions;
 
 
+use App\Domain\Collections\HasItemsCollection;
 use App\Domain\Models\Hero;
 use App\Domain\Models\Item;
+use App\Domain\Models\Week;
+use App\Exceptions\ItemTransactionException;
 
 class EquipWagonItemForHeroAction
 {
-    public function execute(Item $item, Hero $hero)
+    public function execute(Item $item, Hero $hero, HasItemsCollection $hasItemsCollection = null)
     {
+        $hasItemsCollection = $hasItemsCollection ?: new HasItemsCollection();
+        $squad = $hero->getSquad();
 
+        if(! $item->belongsToHasItems($squad)) {
+            throw new ItemTransactionException($item, "Item does not belong to wagon", ItemTransactionException::CODE_INVALID_OWNERSHIP);
+        }
+
+        if(! Week::current()->adventuringOpen()) {
+            throw new ItemTransactionException($item, "Week is currently locked", ItemTransactionException::CODE_TRANSACTION_DISABLED);
+        }
+
+        $item = $item->clearHasItems();
     }
 }
