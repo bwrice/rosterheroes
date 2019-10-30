46bb1c65628d2beb7bb7452a2b13425720fec8d5
Brian Rice <bwrice83@gmail.com>
2019-10-30 11:27:50 -0400

    extend Morphable, HasUniqueIdentifier for HasItems interface

--- app/Domain/Interfaces/HasItems.php
+++ app/Domain/Interfaces/HasItems.php
@@ -7,15 +7,11 @@ namespace App\Domain\Interfaces;
 use App\Domain\Collections\ItemCollection;
 use App\Domain\Models\Item;
 
-interface HasItems
+interface HasItems extends Morphable, HasUniqueIdentifier
 {
     public function getBackupHasItems(): ?HasItems;
 
     public function hasRoomForItem(Item $item): bool;
 
     public function itemsToMoveForNewItem(Item $item): ItemCollection;
-
-    public function getMorphType(): string;
-
-    public function getMorphID(): int;
 }
