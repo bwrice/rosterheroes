5f81837a13e951c4437d76efd7213e0e8a53cdda
Brian Rice <bwrice83@gmail.com>
2019-10-29 20:45:38 -0400

    add equipping methods to collection

--- app/Domain/Collections/GearSlotCollection.php
+++ app/Domain/Collections/GearSlotCollection.php
@@ -4,7 +4,9 @@
 namespace App\Domain\Collections;
 
 
+use App\Domain\Behaviors\ItemBases\ItemBaseBehavior;
 use App\Domain\Interfaces\FillsGearSlots;
+use App\Domain\Models\Item;
 use App\Domain\Models\Support\GearSlots\GearSlot;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
@@ -39,10 +41,46 @@ class GearSlotCollection extends Collection
         });
     }
 
-    public function sortedByPriority()
+    public function sortedByEquipPriority()
     {
         return $this->sortBy(function (GearSlot $gearSlot) {
-            return $gearSlot->getPriority();
+            $emptyPriority = is_null($gearSlot->getFiller()) ? 0 : 99;
+            return $gearSlot->getPriority() + $emptyPriority;
         });
+    }
+
+    public function withItem()
+    {
+        return $this->filter(function (GearSlot $gearSlot) {
+            return $gearSlot->getFiller() instanceof Item;
+        });
+    }
+
+    public function withItemOrEmpty()
+    {
+        return $this->filter(function (GearSlot $gearSlot) {
+            $filler = $gearSlot->getFiller();
+            return is_null($filler) || $filler instanceof Item;
+        });
+    }
+
+    public function itemsToUnEquipToEquipNewItem(ItemBaseBehavior $itemBaseBehavior): ItemCollection
+    {
+        $validSlotTypes = $itemBaseBehavior->getValidGearSlotTypes();
+        $slotsNeededCount = $itemBaseBehavior->getGearSlotsCount();
+        $filtered = $this->sortedByEquipPriority()->withItemOrEmpty()->take($slotsNeededCount);
+        if ($filtered->count() < $slotsNeededCount) {
+            throw new \RuntimeException("Not enough valid gear slots for item");
+        }
+        return $filtered->mapIntoItems();
+    }
+
+    public function mapIntoItems(): ItemCollection
+    {
+        $items = new ItemCollection();
+        $this->withItem()->each(function (GearSlot $gearSlot) use ($items) {
+            $items->push($gearSlot->getFiller());
+        });
+        return $items;
     }
 }
