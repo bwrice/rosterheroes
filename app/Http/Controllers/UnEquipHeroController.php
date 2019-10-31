476864d79961f87a8ec364a09c6131f22e233b86
Brian Rice <bwrice83@gmail.com>
2019-10-31 11:34:06 -0400

    Convert empty slot controller to unequip controller

---
+++ app/Http/Controllers/UnEquipHeroController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domain\Actions\UnEquipItemFromHeroAction;
+use App\Domain\Models\Hero;
+use App\Domain\Models\Item;
+use App\Exceptions\ItemTransactionException;
+use App\Http\Resources\HasItemsResource;
+use App\Policies\HeroPolicy;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class UnEquipHeroController extends Controller
+{
+    /**
+     * @param $heroSlug
+     * @param Request $request
+     * @param UnEquipItemFromHeroAction $domainAction
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     * @throws ValidationException
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function __invoke($heroSlug, Request $request, UnEquipItemFromHeroAction $domainAction)
+    {
+        $hero = Hero::findSlugOrFail($heroSlug);
+        $this->authorize(HeroPolicy::MANAGE, $hero);
+
+        $item = Item::findUuidOrFail($request->item);
+        try {
+            $hasItemsCollection = $domainAction->execute($item, $hero);
+            return HasItemsResource::collection($hasItemsCollection);
+
+        } catch (ItemTransactionException $exception) {
+
+            throw ValidationException::withMessages([
+                'equip' => $exception->getMessage()
+            ]);
+        }
+    }
+}
