be75e6312f03ffcdfdf110b605bb22d3c7dbdd7c
Brian Rice <bwrice83@gmail.com>
2019-10-31 15:06:59 -0400

    create gear slot resource

---
+++ app/Http/Resources/GearSlotResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\Support\GearSlots\GearSlot;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class GearSlotResource
+ * @package App\Http\Resources
+ *
+ * @mixin GearSlot
+ */
+class GearSlotResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'type' => $this->getType(),
+            'item' => $this->getFiller() ? new ItemResource($this->getFiller()) : null
+        ];
+    }
+}
