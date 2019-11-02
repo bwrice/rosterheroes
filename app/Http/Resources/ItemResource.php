36b0db4f39b11b6757e26366ae4a581865dba9ef
Brian Rice <bwrice83@gmail.com>
2019-11-01 21:57:50 -0400

    set hasItems on ItemResource

--- app/Http/Resources/ItemResource.php
+++ app/Http/Resources/ItemResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Domain\Interfaces\HasItems;
 use App\Domain\Interfaces\HasSlots;
 use App\Domain\Interfaces\UsesItems;
 use App\Domain\Models\Item;
@@ -16,10 +17,10 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class ItemResource extends JsonResource
 {
     /** @var UsesItems|null */
-    public $usesItems;
+    protected $usesItems;
 
-    /** @var HasSlots|null */
-    public $hasSlots;
+    /** @var HasItems|null */
+    protected $hasItems;
 
     /** @var Item|mixed */
     public $resource;
@@ -37,9 +38,9 @@ class ItemResource extends JsonResource
             $this->resource->setUsesItems($this->usesItems);
         }
 
-        if ($this->hasSlots) {
+        if ($this->hasItems) {
             /** @var Item $this->resource */
-            $this->resource->setHasSlots($this->hasSlots);
+            $this->resource->setHasItems($this->usesItems);
         }
 
         return [
@@ -70,12 +71,12 @@ class ItemResource extends JsonResource
     }
 
     /**
-     * @param HasSlots|null $hasSlots
+     * @param HasItems|null $hasItems
      * @return ItemResource
      */
-    public function setHasSlots(?HasSlots $hasSlots): ItemResource
+    public function setHasItems(?HasItems $hasItems): ItemResource
     {
-        $this->hasSlots = $hasSlots;
+        $this->hasItems = $hasItems;
         return $this;
     }
 }
