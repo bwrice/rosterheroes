c3b6af149b66472d5a08051c5885382240fd9e81
Brian Rice <bwrice83@gmail.com>
2019-11-02 13:38:47 -0400

    update manage authorization on MeasurablePolicy

--- app/Policies/MeasurablePolicy.php
+++ app/Policies/MeasurablePolicy.php
@@ -4,23 +4,27 @@ namespace App\Policies;
 
 use App\Domain\Models\Hero;
 use App\Domain\Models\Measurable;
+use App\Domain\Models\Squad;
 use App\Domain\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class MeasurablePolicy
 {
     public const RAISE = 'raise';
+    /**
+     * @var SquadPolicy
+     */
+    private $squadPolicy;
 
     use HandlesAuthorization;
 
+    public function __construct(SquadPolicy $squadPolicy)
+    {
+        $this->squadPolicy = $squadPolicy;
+    }
+
     public function raise(User $user, Measurable $measurable)
     {
-        $squad = $measurable->hero->getSquad();
-        if (! $squad) {
-            // TODO: potential to raise measurables for heroes left at homes and not attached to squad's hero post
-            return false;
-        }
-
-        return $squad->user_id === $user->id;
+        return $this->squadPolicy->manage($user, $measurable->hero->squad);
     }
 }
