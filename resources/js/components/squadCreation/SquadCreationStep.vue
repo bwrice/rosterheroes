70fbfa08b2e5d582d22be46ec80d65a5c01692ed
Brian Rice <bwrice83@gmail.com>
2019-11-04 17:44:37 -0500

    Create step creation component

---
+++ resources/js/components/squadCreation/SquadCreationStep.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-stepper-content :step="step">
+        <v-row no-gutters align="center" v-bind:style="stepStyles">
+            <v-col cols="12">
+                <slot>
+
+                </slot>
+            </v-col>
+        </v-row>
+    </v-stepper-content>
+</template>
+
+<script>
+    export default {
+        name: "SquadCreationStep",
+        props: {
+            step: {
+                type: Number,
+                required: true
+            }
+        },
+        computed: {
+            stepStyles() {
+                return {
+                    height: window.innerHeight - (72 + 24 + 32) + 'px'
+                }
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
