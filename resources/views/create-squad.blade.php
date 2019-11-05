abb9ddd5e89f3a2cf8bf5880330d87bf6458da0c
Brian Rice <bwrice83@gmail.com>
2019-11-05 12:47:25 -0500

    fixing scrolling on squad creation

--- resources/views/create-squad.blade.php
+++ resources/views/create-squad.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <body style="overflow-y: hidden">
+    <body style="overflow-y: hidden; position: absolute; width: 100%; height: 100%">
     <div id="app">
         @if($squad)
             <create-squad :squad="{{$squad}}" :heroes="{{$heroes}}" :allowed-hero-classes-prop="{{$heroClasses}}" :allowed-hero-races-prop="{{$heroRaces}}"></create-squad>
