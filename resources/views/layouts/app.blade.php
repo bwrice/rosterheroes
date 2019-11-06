ddb79fc5ce3231b8d9c0b7756931253f0d73defa
Brian Rice <bwrice83@gmail.com>
2019-11-06 11:23:09 -0500

    add theme color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#234a4a">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
