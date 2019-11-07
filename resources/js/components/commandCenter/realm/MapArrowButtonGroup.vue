c8c2436e10623a606291eef63016e6ba6b53eae2
Brian Rice <bwrice83@gmail.com>
2019-11-06 20:29:00 -0500

    (wip) map arrow button group

---
+++ resources/js/components/commandCenter/realm/MapArrowButtonGroup.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-btn-toggle v-model="toggle_none">
+        <v-btn>
+            <v-icon>mdi-format-align-left</v-icon>
+        </v-btn>
+
+        <v-btn>
+            <v-icon>mdi-format-align-center</v-icon>
+        </v-btn>
+        <v-btn>
+
+            <v-icon>mdi-format-align-right</v-icon>
+        </v-btn>
+
+        <v-btn>
+            <v-icon>mdi-format-align-justify</v-icon>
+        </v-btn>
+    </v-btn-toggle>
+</template>
+
+<script>
+    export default {
+        name: "MapArrowButtonGroup"
+    }
+</script>
+
+<style scoped>
+
+</style>
