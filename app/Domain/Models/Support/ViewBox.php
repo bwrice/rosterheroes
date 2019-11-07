73ffbe0cd7602cc924965a3db5aa8f023006aaab
Brian Rice <bwrice83@gmail.com>
2019-11-07 17:42:18 -0500

    create ViewBox model support class

---
+++ app/Domain/Models/Support/ViewBox.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Domain\Models\Support;
+
+
+class ViewBox
+{
+    /**
+     * @var float
+     */
+    protected $panX;
+    /**
+     * @var float
+     */
+    protected $panY;
+    /**
+     * @var float
+     */
+    protected $zoomX;
+    /**
+     * @var float
+     */
+    protected $zoomY;
+
+    public function __construct(float $panX, float $panY, float $zoomX, float $zoomY)
+    {
+        $this->panX = $panX;
+        $this->panY = $panY;
+        $this->zoomX = $zoomX;
+        $this->zoomY = $zoomY;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPanX(): float
+    {
+        return $this->panX;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPanY(): float
+    {
+        return $this->panY;
+    }
+
+    /**
+     * @return float
+     */
+    public function getZoomX(): float
+    {
+        return $this->zoomX;
+    }
+
+    /**
+     * @return float
+     */
+    public function getZoomY(): float
+    {
+        return $this->zoomY;
+    }
+}
