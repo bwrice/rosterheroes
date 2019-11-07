506d7ff6cad4b2946579c3fbbf54d8a5ba75a742
Brian Rice <bwrice83@gmail.com>
2019-11-06 22:26:54 -0500

    get dragging working with map svg

--- resources/js/components/commandCenter/realm/MapViewPort.vue
+++ resources/js/components/commandCenter/realm/MapViewPort.vue
@@ -1,8 +1,10 @@
 <template>
-    <v-sheet :tile="tile" :color="oceanColor">
+    <v-sheet id="map-sheet" :tile="tile" :color="oceanColor">
         <svg xmlns="http://www.w3.org/2000/svg"
              version="1.1"
-             :viewBox="viewBoxString">
+             :viewBox="viewBoxString"
+             v-dragged="onDragged"
+        >
             <slot>
                 <!-- Default Slot: ProvinceVector components slotted here -->
             </slot>
@@ -39,9 +41,47 @@
                 default: true
             }
         },
+        created() {
+            this.initializeViewBox();
+        },
+        mounted() {
+            let mapSheet = document.getElementById('map-sheet');
+            mapSheet.addEventListener('touchmove', function(e) {
+                e.preventDefault();
+            }, false);
+        },
+        data() {
+            return {
+                currentPanX: 0,
+                currentPanY: 0,
+                currentZoomX: 315,
+                currentZoomY: 240
+            }
+        },
         computed: {
             viewBoxString() {
-                return this.viewBox.pan_x + ' ' + this.viewBox.pan_y + ' ' + this.viewBox.zoom_x + ' ' + this.viewBox.zoom_y;
+                return this.currentPanX + ' ' + this.currentPanY+ ' ' + this.currentZoomX + ' ' + this.currentZoomY;
+            }
+        },
+        methods: {
+            initializeViewBox() {
+                this.currentPanX = this.viewBox.pan_x;
+                this.currentPanY = this.viewBox.pan_y;
+                this.currentZoomX = this.viewBox.zoom_x;
+                this.currentZoomY = this.viewBox.zoom_y;
+            },
+
+            onDragged({ el, deltaX, deltaY, offsetX, offsetY, clientX, clientY, first, last }) {
+                if (first) {
+                    this.isDragging = true;
+                    return
+                }
+                if (last) {
+                    this.isDragging = false;
+                    return;
+                }
+                this.currentPanX -= deltaX;
+                this.currentPanY -= deltaY;
             }
         }
     }
