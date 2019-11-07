da0a153a517eb35191c0d42bdefef26b18342c5c
Brian Rice <bwrice83@gmail.com>
2019-11-07 17:51:20 -0500

    create ViewBox json resource

---
+++ app/Http/Resources/ViewBoxResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\Support\ViewBox;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class ViewBoxResource
+ * @package App\Http\Resources
+ *
+ * @mixin ViewBox
+ */
+class ViewBoxResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'panX' => $this->getPanX(),
+            'panY' => $this->getPanY(),
+            'zoomX' => $this->getZoomX(),
+            'zoomY' => $this->getZoomY()
+        ];
+    }
+}
