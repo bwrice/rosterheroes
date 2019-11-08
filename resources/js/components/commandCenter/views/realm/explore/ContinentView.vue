d4a65f21986ff3c2e2cab7da1d38c61a28f33f43
Brian Rice <bwrice83@gmail.com>
2019-11-08 00:07:16 -0500

    use MapViewPort directly in ContinentView component

--- resources/js/components/commandCenter/views/realm/explore/ContinentView.vue
+++ resources/js/components/commandCenter/views/realm/explore/ContinentView.vue
@@ -1,12 +1,22 @@
 <template>
-    <ExploreMapCard :view-box="continent.viewBox">
-        <ProvinceVector
-            v-for="(province, uuid) in provinces"
-            :key="uuid"
-            :province="province"
-            @provinceClicked="navigateToProvince"
-        ></ProvinceVector>
-    </ExploreMapCard>
+    <v-container>
+        <v-row>
+            <v-col cols="12" lg="8" offset-lg="2">
+                <v-row no-gutters>
+                    <v-col cols="12">
+                        <MapViewPort :view-box="continent.viewBox" :tile="false">
+                            <ProvinceVector
+                                v-for="(province, uuid) in provinces"
+                                :key="uuid"
+                                :province="province"
+                                @provinceClicked="navigateToProvince"
+                            ></ProvinceVector>
+                        </MapViewPort>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -14,13 +24,13 @@
     import {mapGetters} from 'vuex';
 
     import ProvinceVector from "../../../realm/ProvinceVector";
-    import ExploreMapCard from "../../../realm/ExploreMapCard";
     import Continent from "../../../../../models/Continent";
+    import MapViewPort from "../../../realm/MapViewPort";
 
     export default {
         name: "ContinentView",
         components: {
-            ExploreMapCard,
+            MapViewPort,
             ProvinceVector
         },
 
