65f648a77378b61dbf5775f8a3aade6b9b87cf64
Brian Rice <bwrice83@gmail.com>
2019-11-07 22:47:02 -0500

    add methods to ViewBox JS model

--- resources/js/models/ViewBox.js
+++ resources/js/models/ViewBox.js
@@ -6,4 +6,40 @@ export default class ViewBox {
         this.zoomX = zoomX;
         this.zoomY = zoomY;
     }
+
+    panUp() {
+        this.panY -= (.1 * this.zoomY);
+    }
+
+    panDown() {
+        this.panY += (.1 * this.zoomY);
+    }
+
+    panLeft() {
+        this.panX -= (.1 * this.zoomX);
+    }
+
+    panRight() {
+        this.panX += (.1 * this.zoomX);
+    }
+
+    zoomIn() {
+        /*
+        By panning 10% but zooming 80% (instead of 90), we zoom the center of the SVG
+         */
+        this.panRight();
+        this.panDown();
+        this.zoomX *= .8;
+        this.zoomY *= .8;
+    }
+
+    zoomOut() {
+        /*
+        We have to pan after we adjust the zoom to reverse zoomIn() effect
+         */
+        this.zoomX /= .8;
+        this.zoomY /= .8;
+        this.panLeft();
+        this.panUp();
+    }
 }
