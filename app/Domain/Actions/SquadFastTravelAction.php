ab99fd46e8ce137b9b14400330d547bb5543ef7f
Brian Rice <bwrice83@gmail.com>
2019-11-12 20:33:32 -0500

    update domain action to be Squad specific

---
+++ app/Domain/Actions/SquadFastTravelAction.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Collections\ProvinceCollection;
+use App\Domain\Models\Province;
+use App\Domain\Models\Squad;
+use Illuminate\Support\Facades\DB;
+
+class FastTravelAction
+{
+    /**
+     * @var SquadBorderTravelAction
+     */
+    private $borderTravelAction;
+
+    public function __construct(SquadBorderTravelAction $borderTravelAction)
+    {
+        $this->borderTravelAction = $borderTravelAction;
+    }
+
+    /**
+     * @param Squad $squad
+     * @param ProvinceCollection $travelRoute
+     */
+    public function execute(Squad $squad, ProvinceCollection $travelRoute)
+    {
+        DB::transaction(function() use ($squad, $travelRoute) {
+
+            $travelRoute->each(function(Province $border) use ($squad) {
+
+                $travelsBorders = $squad->fresh();
+                $this->borderTravelAction->execute($travelsBorders, $border);
+            });
+        });
+    }
+}
