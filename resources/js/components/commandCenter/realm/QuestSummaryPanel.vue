4c84974a8d69e50983586b96864fa7ee94f39c83
Brian Rice <bwrice83@gmail.com>
2019-11-14 18:15:50 -0500

    add quest level and % complete to QuestSummaryPanel

--- resources/js/components/commandCenter/realm/QuestSummaryPanel.vue
+++ resources/js/components/commandCenter/realm/QuestSummaryPanel.vue
@@ -6,7 +6,20 @@
     >
         <v-row no-gutters>
             <v-col cols="12">
-                <span class="subtitle-1 font-weight-light">{{quest.name}}</span>
+                <v-row no-gutters justify="space-between" align="center" class="pa-1">
+                    <span class="mx-3 title font-weight-light">{{quest.name}}</span>
+                    <v-chip>{{quest.level}}</v-chip>
+                </v-row>
+                <v-row no-gutters class="pa-2">
+                    <v-col cols="8" md="8">
+                        <GradientBar
+                            :percent="quest.percentComplete"
+                            :height="16"
+                        >
+                            <span class="font-weight-bold" style="opacity: .7">{{quest.percentComplete}}% complete</span>
+                        </GradientBar>
+                    </v-col>
+                </v-row>
             </v-col>
         </v-row>
     </ClickableSheet>
@@ -15,10 +28,11 @@
 <script>
     import Quest from "../../../models/Quest";
     import ClickableSheet from "../../shared/ClickableSheet";
+    import GradientBar from "../global/GradientBar";
 
     export default {
         name: "QuestSummaryPanel",
-        components: {ClickableSheet},
+        components: {GradientBar, ClickableSheet},
         props: {
             quest: {
                 type: Quest,
