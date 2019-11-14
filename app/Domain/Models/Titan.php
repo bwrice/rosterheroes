77f12bac05707a100b59765196124e2439116a61
Brian Rice <bwrice83@gmail.com>
2019-11-14 11:51:22 -0500

    add relations and docbloc autocompletes to Titan

--- app/Domain/Models/Titan.php
+++ app/Domain/Models/Titan.php
@@ -7,6 +7,24 @@ use App\Domain\Traits\HasNameSlug;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Sluggable\SlugOptions;
 
+
+/**
+ * Class Titan
+ * @package App\Domain\Models
+ *
+ * @property string $uuid
+ * @property string $name
+ * @property string $slug
+ * @property int $base_level
+ * @property int $base_damage_rating
+ * @property int $damage_multiplier_rating
+ * @property int $health_rating
+ * @property int $protection_rating
+ * @property int $combat_speed_rating
+ * @property int $block_rating
+ * @property int $enemy_type_id
+ * @property int $combat_position_id
+ */
 class Titan extends Model
 {
     use HasNameSlug;
@@ -22,4 +40,15 @@ class Titan extends Model
     {
         return new TitanCollection($models);
     }
+
+    public function combatPosition()
+    {
+        return $this->belongsTo(CombatPosition::class);
+    }
+
+    public function enemyType()
+    {
+        return $this->belongsTo(EnemyType::class);
+    }
+
 }
