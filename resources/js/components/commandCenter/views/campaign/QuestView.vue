08e272ff4bba9218b607e488d4aaa1cf6d17fafd
Brian Rice <bwrice83@gmail.com>
2019-11-15 15:09:49 -0500

    render SkirmishCards inside QuestView

--- resources/js/components/commandCenter/views/campaign/QuestView.vue
+++ resources/js/components/commandCenter/views/campaign/QuestView.vue
@@ -1,7 +1,12 @@
 <template>
     <v-container>
         <v-row justify="center" align="center">
-            <span class="display-1 rh-op-80">{{quest.name}}</span>
+            <span class="display-1 rh-op-85">{{quest.name}}</span>
+        </v-row>
+        <v-row>
+            <v-col v-for="(skirmish, uuid) in quest.skirmishes" :key="uuid" cols="12" sm="6" lg="4" xl="3">
+                <SkirmishCard :skirmish="skirmish"></SkirmishCard>
+            </v-col>
         </v-row>
     </v-container>
 </template>
@@ -9,9 +14,11 @@
 <script>
 
     import {mapGetters} from 'vuex';
+    import SkirmishCard from "../../campaign/SkirmishCard";
 
     export default {
         name: "QuestView",
+        components: {SkirmishCard},
         computed: {
             ...mapGetters([
                 '_questBySlug'
