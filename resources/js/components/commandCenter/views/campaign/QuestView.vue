94066a3caa3379547aa318820c7d909dd457d8c6
Brian Rice <bwrice83@gmail.com>
2019-11-15 14:35:43 -0500

    render quest name at top of QuestView

--- resources/js/components/commandCenter/views/campaign/QuestView.vue
+++ resources/js/components/commandCenter/views/campaign/QuestView.vue
@@ -1,12 +1,26 @@
 <template>
     <v-container>
-        <h1>Quest View</h1>
+        <v-row justify="center" align="center">
+            <span class="display-1 rh-op-80">{{quest.name}}</span>
+        </v-row>
     </v-container>
 </template>
 
 <script>
+
+    import {mapGetters} from 'vuex';
+
     export default {
-        name: "QuestView"
+        name: "QuestView",
+        computed: {
+            ...mapGetters([
+                '_questBySlug'
+            ]),
+            quest() {
+                let slug = this.$route.params.questSlug;
+                return this._questBySlug(slug);
+            }
+        }
     }
 </script>
 
