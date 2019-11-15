c7a9e75f6c6d5d56fceedb17bd6c218bd92d5035
Brian Rice <bwrice83@gmail.com>
2019-11-15 12:02:21 -0500

    compile custom css

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/createSquad.js', 'public/js').version();
 mix.js('resources/js/commandCenter.js', 'public/js').version();
 
-// mix.sass('resources/sass/commandCenter.scss', 'public/css').version();
+mix.sass('resources/sass/commandCenter.scss', 'public/css').version();
