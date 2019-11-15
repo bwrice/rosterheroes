bea8fabc4c61038d2bfb635f5e9247531bbde014
Brian Rice <bwrice83@gmail.com>
2019-11-15 15:09:30 -0500

    create SkirmishCard component

---
+++ resources/js/components/commandCenter/campaign/SkirmishCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-card>
+        <v-card-title>{{skirmish.name}}</v-card-title>
+    </v-card>
+</template>
+
+<script>
+    import Skirmish from "../../../models/Skirmish";
+
+    export default {
+        name: "SkirmishCard",
+        props: {
+            skirmish: {
+                type: Skirmish,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
