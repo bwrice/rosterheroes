a3854d6704cb4a2013073522f05abc36565cc721
Brian Rice <bwrice83@gmail.com>
2019-11-15 13:55:07 -0500

    create QuestView component

---
+++ resources/js/components/commandCenter/views/campaign/QuestView.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <h1>Quest View</h1>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "QuestView"
+    }
+</script>
+
+<style scoped>
+
+</style>
