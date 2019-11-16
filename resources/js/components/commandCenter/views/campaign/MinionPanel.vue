ab0dcc59d5cf8a5b604273b212ea8cb8f14c63f6
Brian Rice <bwrice83@gmail.com>
2019-11-15 20:03:19 -0500

    create MinionPanel component

---
+++ resources/js/components/commandCenter/views/campaign/MinionPanel.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-row no-gutters>
+        <v-col cols="12">
+            <v-row no-gutters>
+                <span class="subtitle-2">
+                    {{minion.name}}
+                </span>
+            </v-row>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+    import Minion from "../../../../models/Minion";
+
+    export default {
+        name: "MinionPanel",
+        props: {
+            minion: {
+                type: Minion,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
