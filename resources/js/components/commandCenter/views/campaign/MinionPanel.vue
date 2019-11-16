c202dd0cf6c1f60c3a4ef28939e2f4342a204f58
Brian Rice <bwrice83@gmail.com>
2019-11-15 23:38:14 -0500

    (wip) MinionPanel component

--- resources/js/components/commandCenter/views/campaign/MinionPanel.vue
+++ resources/js/components/commandCenter/views/campaign/MinionPanel.vue
@@ -1,6 +1,6 @@
 <template>
     <v-sheet color="#3a474a" class="pa-1">
-        <v-row no-gutters align="center">
+        <v-row no-gutters justify="center" align="center">
             <v-chip
                 label
                 color="accent darken-2"
@@ -13,16 +13,44 @@
         </v-row>
         <v-row no-gutters>
             <v-col cols="12">
-                <v-row no-gutters justify="space-between" align="center">
-                    <v-row no-gutters>
-                        <v-col cols="12">
-                            <v-row no-gutters>
-                            </v-row>
-                        </v-col>
-                    </v-row>
-                    <div style="width: 70px">
-                        <CombatPositionIcon :combat-position-id="minion.combatPositionID" :attacker-mode="false"></CombatPositionIcon>
-                    </div>
+                <v-row no-gutters align="center">
+                    <v-col cols="9">
+                        <v-row no-gutters align="center" class="pa-1">
+                            <v-progress-linear
+                                :value="45"
+                                color="error"
+                                height="15"
+                            >
+                                1184
+                            </v-progress-linear>
+                        </v-row>
+                        <v-row no-gutters align="center" class="pa-1">
+                            <v-progress-linear
+                                :value="78"
+                                color="primary"
+                                height="15"
+                            >
+                                663
+                            </v-progress-linear>
+                        </v-row>
+                        <v-row no-gutters align="center" class="pa-1">
+
+                            <v-progress-linear
+                                :value="24"
+                                color="accent"
+                                height="15"
+                            >
+                                24%
+                            </v-progress-linear>
+                        </v-row>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-row no-gutters justify="center">
+                            <div style="width: 70px">
+                                <CombatPositionIcon :combat-position-id="minion.combatPositionID" :attacker-mode="false"></CombatPositionIcon>
+                            </div>
+                        </v-row>
+                    </v-col>
                 </v-row>
             </v-col>
         </v-row>
