c021473ca586a0f8cdb1f7d644b5a7fa4232bfec
Brian Rice <bwrice83@gmail.com>
2019-11-15 21:33:48 -0500

    make quest name bold and text center with margin

--- resources/js/components/commandCenter/views/campaign/QuestView.vue
+++ resources/js/components/commandCenter/views/campaign/QuestView.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <v-row justify="center" align="center">
-            <span class="display-1 rh-op-85">{{quest.name}}</span>
+            <span class="display-1 rh-op-85 font-weight-bold mx-3 text-center">{{quest.name}}</span>
         </v-row>
         <v-row>
             <v-col v-for="(skirmish, uuid) in quest.skirmishes" :key="uuid" cols="12" sm="6" lg="4" xl="3">
