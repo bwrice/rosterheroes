3ec5cb62062d2b85813a1f18d2484f54084d13f5
Brian Rice <bwrice83@gmail.com>
2019-11-15 20:03:36 -0500

    Add carousel of MinionPanels to SkirmishCard

--- resources/js/components/commandCenter/campaign/SkirmishCard.vue
+++ resources/js/components/commandCenter/campaign/SkirmishCard.vue
@@ -1,14 +1,28 @@
 <template>
     <v-card>
-        <v-card-title>{{skirmish.name}}</v-card-title>
+        <v-card-title class="rh-op-85">{{skirmish.name}}</v-card-title>
+        <v-card-text>
+            <v-carousel
+                height="200"
+            >
+                <v-carousel-item
+                    v-for="(minion, uuid) in skirmish.minions"
+                    :key="uuid"
+                >
+                    <MinionPanel :minion="minion"></MinionPanel>
+                </v-carousel-item>
+            </v-carousel>
+        </v-card-text>
     </v-card>
 </template>
 
 <script>
     import Skirmish from "../../../models/Skirmish";
+    import MinionPanel from "../views/campaign/MinionPanel";
 
     export default {
         name: "SkirmishCard",
+        components: {MinionPanel},
         props: {
             skirmish: {
                 type: Skirmish,
