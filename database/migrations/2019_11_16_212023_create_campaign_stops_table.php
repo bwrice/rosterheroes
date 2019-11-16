6ac51459431bcbc303ad36bfc0c021c80d7af8e5
Brian Rice <bwrice83@gmail.com>
2019-11-16 16:26:01 -0500

    add migration for campaign stops table

---
+++ database/migrations/2019_11_16_212023_create_campaign_stops_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCampaignStopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaign_stops', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('uuid');
+            $table->bigInteger('campaign_id')->unsigned();
+            $table->integer('quest_id')->unsigned();
+            $table->timestamps();
+        });
+
+
+        Schema::table('campaign_stops', function (Blueprint $table) {
+            $table->foreign('campaign_id')->references('id')->on('campaigns');
+            $table->foreign('quest_id')->references('id')->on('quests');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaign_stops');
+    }
+}
