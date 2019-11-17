b2f378d3931b58212c9398d61ca781fef09efe70
Brian Rice <bwrice83@gmail.com>
2019-11-17 01:21:35 -0500

    create pivot schema for campaign stops and skirmishes

---
+++ database/migrations/2019_11_17_060909_create_campaign_stop_skirmish_pivot.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCampaignStopSkirmishPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaign_stop_skirmish', function (Blueprint $table) {
+            $table->integer('stop_id')->unsigned();
+            $table->integer('skirmish_id')->unsigned();
+            $table->primary(['stop_id', 'skirmish_id']);
+            $table->timestamps();
+        });
+
+        Schema::table('campaign_stop_skirmish', function (Blueprint $table) {
+            $table->foreign('stop_id')->references('id')->on('campaign_stops');
+            $table->foreign('skirmish_id')->references('id')->on('skirmishes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaign_stop_skirmish');
+    }
+}
