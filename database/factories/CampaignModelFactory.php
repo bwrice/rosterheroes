69bb4e9a4cb787ccc1bc6b76326e393995e05cf7
Brian Rice <bwrice83@gmail.com>
2019-11-17 12:20:27 -0500

    create CampaignModelFactory

---
+++ database/factories/CampaignModelFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Domain\Models\Campaign::class, function (Faker $faker) {
+    return [
+        'uuid' => \Illuminate\Support\Str::uuid(),
+        'squad_id' => function () {
+            factory(\App\Domain\Models\Squad::class)->create()->id;
+        },
+        'continent_id' => function () {
+            return \App\Domain\Models\Continent::query()->inRandomOrder()->first()->id;
+        },
+        'week_id' => function () {
+            return \App\Domain\Models\Week::current()->id;
+        },
+    ];
+});
