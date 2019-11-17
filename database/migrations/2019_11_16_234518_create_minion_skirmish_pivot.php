e2f72f490f16702911f05f91b2701d412c0b2e39
Brian Rice <bwrice83@gmail.com>
2019-11-16 22:14:20 -0500

    create minion-skirmish pivot

---
+++ database/migrations/2019_11_16_234518_create_minion_skirmish_pivot.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMinionSkirmishPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('minion_skirmish', function (Blueprint $table) {
+            $table->integer('minion_id')->unsigned();
+            $table->integer('skirmish_id')->unsigned();
+            $table->integer('count');
+            $table->primary(['minion_id', 'skirmish_id']);
+            $table->timestamps();
+        });
+
+        Schema::table('minion_skirmish', function (Blueprint $table) {
+            $table->foreign('minion_id')->references('id')->on('minions');
+            $table->foreign('skirmish_id')->references('id')->on('skirmishes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('minion_skirmish');
+    }
+}
