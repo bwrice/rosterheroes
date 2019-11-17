145e8d0615bb7f0a8917a720d3aea5a5acf67d0f
Brian Rice <bwrice83@gmail.com>
2019-11-17 01:47:20 -0500

    add relations to CampaignStop model

--- app/Domain/Models/CampaignStop.php
+++ app/Domain/Models/CampaignStop.php
@@ -2,17 +2,21 @@
 
 namespace App\Domain\Models;
 
+use App\Domain\Collections\SkirmishCollection;
 use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class CampaignStop
  * @package App\Domain\Models
- * 
+ *
  * @property int $id
  * @property string $uuid
  *
  * @property Campaign $campaign
  * @property Quest $quest
+ * @property Province $province
+ *
+ * @property SkirmishCollection $skirmishes
  */
 class CampaignStop extends Model
 {
@@ -26,5 +30,15 @@ class CampaignStop extends Model
     public function quest()
     {
         return $this->belongsTo(Quest::class);
+    }
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    public function skirmishes()
+    {
+        return $this->belongsToMany(Skirmish::class, 'campaign_stop_skirmish', 'stop_id')->withTimestamps();
     }
 }
