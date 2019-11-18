261f84c76ccf9830ac1af530eedd12ebc3ba2571
Brian Rice <bwrice83@gmail.com>
2019-11-17 19:48:19 -0500

    render avaible quests inside Campaign Main page

--- resources/js/components/commandCenter/views/campaign/CampaignMain.vue
+++ resources/js/components/commandCenter/views/campaign/CampaignMain.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-        <h1 class="mx-4">TODO: Campaign</h1>
-    </div>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <AvailableQuestsSection></AvailableQuestsSection>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
+    import AvailableQuestsSection from "../../campaign/AvailableQuestsSection";
     export default {
         name: "CampaignMain",
+        components: {AvailableQuestsSection},
         created: () => {
             console.log("Campaign Created!");
         }
