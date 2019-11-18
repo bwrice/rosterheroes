544f4cdb7bc8907998b6edb2317a6cf2c9c8c3f5
Brian Rice <bwrice83@gmail.com>
2019-11-17 23:03:41 -0500

    add enlist/leave buttons for QuestView

--- resources/js/components/commandCenter/views/campaign/QuestView.vue
+++ resources/js/components/commandCenter/views/campaign/QuestView.vue
@@ -1,7 +1,27 @@
 <template>
     <v-container>
-        <v-row justify="center" align="center">
-            <span class="display-1 rh-op-85 font-weight-bold mx-3 text-center">{{quest.name}}</span>
+        <v-row>
+            <v-col cols="12">
+                <v-row no-gutters justify="center" align="center">
+                    <span class="display-1 rh-op-85 font-weight-bold mx-3 text-center">{{quest.name}}</span>
+                </v-row>
+                <v-row no-gutters justify="center" align="center">
+
+                    <v-btn v-if="enlisted"
+                           block
+                           color="error"
+                        >
+                        Leave Quest
+                    </v-btn>
+                    <v-btn v-else
+                           color="primary"
+                           block
+                           :disabled="! canEnlist"
+                        >
+                        Enlist for Quest
+                    </v-btn>
+                </v-row>
+            </v-col>
         </v-row>
         <v-row>
             <v-col v-for="(skirmish, uuid) in quest.skirmishes" :key="uuid" cols="12" sm="6" lg="4" xl="3">
@@ -21,11 +41,20 @@
         components: {SkirmishCard},
         computed: {
             ...mapGetters([
-                '_currentLocationQuestBySlug'
+                '_currentLocationQuestBySlug',
+                '_currentCampaign',
+                '_enlistedForQuest',
+                '_squad'
             ]),
             quest() {
                 let slug = this.$route.params.questSlug;
                 return this._currentLocationQuestBySlug(slug);
+            },
+            enlisted() {
+                return this._enlistedForQuest(this.quest)
+            },
+            canEnlist() {
+                return this._currentCampaign.campaignStops.length < this._squad.questsPerWeek;
             }
         }
     }
