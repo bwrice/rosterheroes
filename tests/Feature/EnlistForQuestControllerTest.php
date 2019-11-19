aad6ca10cb4b947d13d4fddcd088a0e2904e708c
Brian Rice <bwrice83@gmail.com>
2019-11-19 17:29:38 -0500

    test authorization for EnlistForQuestController

---
+++ tests/Feature/EnlistForQuestControllerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Domain\Models\Quest;
+use App\Domain\Models\Squad;
+use App\Domain\Models\User;
+use App\Domain\Models\Week;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Date;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class EnlistForQuestControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @var Squad */
+    protected $squad;
+
+    /** @var Week */
+    protected $week;
+
+    /** @var Quest */
+    protected $quest;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->quest = factory(Quest::class)->create();
+        $this->squad = factory(Squad::class)->create([
+            'province_id' => $this->quest->province_id
+        ]);
+        $this->week = factory(Week::class)->create();
+        $this->week->everything_locks_at = Date::now()->addHour();
+        $this->week->save();
+        Week::setTestCurrent($this->week);
+    }
+
+    /**
+     * @test
+     */
+    public function user_must_own_squad_to_enlist()
+    {
+        Passport::actingAs(factory(User::class)->create());
+
+        $response = $this->post('/api/v1/squads/' . $this->squad->slug . '/enlist', [
+            'quest' => $this->quest->uuid
+        ]);
+
+        $response->assertStatus(403);
+    }
+}
