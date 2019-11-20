8ea37c54e25cc588634a3076c824b04b97473ac6
Brian Rice <bwrice83@gmail.com>
2019-11-20 12:21:56 -0500

    create AddSkirmishToCampaignStopAction domain action

---
+++ app/Domain/Actions/AddSkirmishToCampaignStopAction.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Models\Campaign;
+use App\Domain\Models\CampaignStop;
+use App\Domain\Models\Skirmish;
+
+class AddSkirmishToCampaignStopAction
+{
+    public function execute(CampaignStop $campaignStop, Skirmish $skirmish)
+    {
+
+    }
+}
