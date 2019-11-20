6877f4972fa3970907bf5af3044a3d42cdb2850d
Brian Rice <bwrice83@gmail.com>
2019-11-19 22:52:00 -0500

    fix removed str_random() helper

--- database/factories/SquadFactory.php
+++ database/factories/SquadFactory.php
@@ -2,6 +2,7 @@
 
 use Faker\Generator as Faker;
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(\App\Domain\Models\Squad::class, function (Faker $faker) {
 
     return [
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,6 +13,7 @@ use Faker\Generator as Faker;
 |
 */
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(\App\Domain\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
@@ -20,6 +21,6 @@ $factory->define(\App\Domain\Models\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'remember_token' => \Illuminate\Support\Str::random(10),
     ];
 });
