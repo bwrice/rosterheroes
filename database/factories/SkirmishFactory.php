a464bb67d7c4cf62767ff508436d44de8d57824e
Brian Rice <bwrice83@gmail.com>
2019-11-20 11:12:32 -0500

    add columns to Skirmish model factory

--- database/factories/SkirmishFactory.php
+++ database/factories/SkirmishFactory.php
@@ -3,10 +3,15 @@
 use Faker\Generator as Faker;
 
 $factory->define(\App\Domain\Models\Skirmish::class, function (Faker $faker) {
+
+    /** @var \App\Domain\Models\SkirmishBlueprint $blueprint */
+    $blueprint = factory(\App\Domain\Models\SkirmishBlueprint::class)->create();
     return [
         'uuid' => (string) \Ramsey\Uuid\Uuid::uuid4(),
         'quest_id' => function() {
             return factory(\App\Domain\Models\Quest::class)->create()->id;
-        }
+        },
+        'name' => $blueprint->name,
+        'skirmish_blueprint_id' => $blueprint->id
     ];
 });
