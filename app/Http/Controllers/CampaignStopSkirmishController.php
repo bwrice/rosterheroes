4c147ab64a255507e1f0fe25ea3c0ee213b05a52
Brian Rice <bwrice83@gmail.com>
2019-11-21 16:42:04 -0500

    execute domain action to add skirmish in controller

--- app/Http/Controllers/CampaignStopSkirmishController.php
+++ app/Http/Controllers/CampaignStopSkirmishController.php
@@ -2,19 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Domain\Actions\AddSkirmishToCampaignStopAction;
 use App\Domain\Models\CampaignStop;
+use App\Domain\Models\Skirmish;
+use App\Exceptions\CampaignStopException;
+use App\Http\Resources\CampaignResource;
 use App\Policies\SquadPolicy;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class CampaignStopSkirmishController extends Controller
 {
-    public function store($stopUuid)
+    /**
+     * @param $stopUuid
+     * @param Request $request
+     * @param AddSkirmishToCampaignStopAction $domainAction
+     * @return CampaignResource
+     * @throws ValidationException
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function store($stopUuid, Request $request, AddSkirmishToCampaignStopAction $domainAction)
     {
         /** @var CampaignStop $campaignStop */
         $campaignStop = CampaignStop::uuid($stopUuid)->with([
             'campaign.squad',
+            'quest',
+            'skirmishes'
+        ])->firstOrFail();
+
+        $this->authorize(SquadPolicy::MANAGE, $campaignStop->campaign->squad);
+
+        /** @var Skirmish $skirmish */
+        $skirmish = Skirmish::uuid($request->skirmishUuid)->with([
             'quest'
         ])->firstOrFail();
-        $this->authorize(SquadPolicy::MANAGE, $campaignStop->campaign->squad);
+
+        try {
+            $domainAction->execute($campaignStop, $skirmish);
+        } catch (CampaignStopException $exception) {
+            throw ValidationException::withMessages([
+                'campaign' => $exception->getMessage()
+            ]);
+        }
+
+        return new CampaignResource($campaignStop->campaign->fresh());
     }
 }
