c70d3c548f71b83a1f7e1807decbec47d3cba5d0
Brian Rice <bwrice83@gmail.com>
2019-11-22 22:58:59 -0500

    create LeaveSkirmishAction that extends CampaignStopAction

---
+++ app/Domain/Actions/LeaveSkirmishAction.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Models\CampaignStop;
+use App\Domain\Models\Skirmish;
+
+class LeaveSkirmishAction extends CampaignStopAction
+{
+    public function execute(CampaignStop $campaignStop, Skirmish $skirmish)
+    {
+        $this->setProperties($campaignStop, $skirmish);
+
+        $this->validateWeek();
+    }
+}
