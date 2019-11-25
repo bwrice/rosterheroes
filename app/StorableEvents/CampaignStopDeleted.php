9e1f57dd30b2780364ec653a4c967ef47a48ea65
Brian Rice <bwrice83@gmail.com>
2019-11-25 00:07:44 -0500

    create CampaignStopDeleted storable event

---
+++ app/StorableEvents/CampaignStopDeleted.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\StorableEvents;
+
+use App\Domain\Models\CampaignStop;
+use Spatie\EventSourcing\ShouldBeStored;
+
+final class CampaignStopDeleted implements ShouldBeStored
+{
+
+}
