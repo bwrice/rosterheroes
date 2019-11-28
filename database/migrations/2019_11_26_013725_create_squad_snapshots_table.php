a8b183bd4849acb74f9031afeabb9518cd6f9250
Brian Rice <bwrice83@gmail.com>
2019-11-27 21:26:15 -0500

    convert snapshots schema to use json data columns

--- database/migrations/2019_11_26_013725_create_squad_snapshots_table.php
+++ database/migrations/2019_11_26_013725_create_squad_snapshots_table.php
@@ -17,24 +17,13 @@ class CreateSquadSnapshotsTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('squad_id')->unsigned();
             $table->integer('week_id')->unsigned();
-            $table->integer('squad_rank_id')->unsigned();
-            $table->tinyInteger('mobile_storage_rank_id')->unsigned();
-            $table->integer('province_id')->unsigned();
-            $table->integer('nation_id')->unsigned()->nullable();
-            $table->integer('spirit_essence')->unsigned()->default(0);
-            $table->bigInteger('experience')->unsigned()->default(0);
-            $table->bigInteger('gold')->unsigned()->default(0);
-            $table->bigInteger('favor')->unsigned()->default(0);
+            $table->json('data');
             $table->timestamps();
         });
 
         Schema::table('squad_snapshots', function (Blueprint $table) {
             $table->foreign('squad_id')->references('id')->on('squads');
             $table->foreign('week_id')->references('id')->on('weeks');
-            $table->foreign('squad_rank_id')->references('id')->on('squad_ranks');
-            $table->foreign('mobile_storage_rank_id')->references('id')->on('mobile_storage_ranks');
-            $table->foreign('province_id')->references('id')->on('provinces');
-            $table->foreign('nation_id')->references('id')->on('nations');
         });
     }
 
