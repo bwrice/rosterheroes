3ced0626952c7957b314ba3f5f6a9d3741cea0e0
Brian Rice <bwrice83@gmail.com>
2019-11-30 14:18:21 -0500

    remove extra columns from WeekFactory

--- database/factories/WeekFactory.php
+++ database/factories/WeekFactory.php
@@ -1,33 +1,32 @@
 <?php
 
+use App\Domain\Models\Week;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Str;
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(\App\Domain\Models\Week::class, function (Faker $faker) {
+$factory->define(Week::class, function (Faker $faker) {
 
-    /** @var \App\Domain\Models\Week $weekForNow */
-    $weekForNow = \App\Domain\Models\Week::makeForNow();
 
     return [
         'uuid' => Str::uuid(),
-        'proposals_scheduled_to_lock_at' => $weekForNow->proposals_scheduled_to_lock_at,
-        'diplomacy_scheduled_to_lock_at' => $weekForNow->diplomacy_scheduled_to_lock_at,
-        'everything_locks_at' => $weekForNow->adventuring_locks_at,
-        'ends_at' => $weekForNow->ends_at
+        'adventuring_locks_at' => function() {
+            return Week::computeAdventuringLocksAt();
+        }
     ];
 });
 
-$factory->afterCreatingState(\App\Domain\Models\Week::class, 'adventuring-open', function(\App\Domain\Models\Week $week, Faker $faker) {
-    $week->adventuring_locks_at = \Illuminate\Support\Facades\Date::now()->addHour();
+$factory->afterCreatingState(Week::class, 'adventuring-open', function(Week $week, Faker $faker) {
+    $week->adventuring_locks_at = Date::now()->addHour();
     $week->save();
 });
 
-$factory->afterCreatingState(\App\Domain\Models\Week::class, 'adventuring-closed', function(\App\Domain\Models\Week $week, Faker $faker) {
-    $week->adventuring_locks_at = \Illuminate\Support\Facades\Date::now()->subHour();
+$factory->afterCreatingState(Week::class, 'adventuring-closed', function(Week $week, Faker $faker) {
+    $week->adventuring_locks_at = Date::now()->subHour();
     $week->save();
 });
 
-$factory->afterCreatingState(\App\Domain\Models\Week::class, 'as-current', function(\App\Domain\Models\Week $week, Faker $faker) {
-    \App\Domain\Models\Week::setTestCurrent($week);
+$factory->afterCreatingState(Week::class, 'as-current', function(Week $week, Faker $faker) {
+    Week::setTestCurrent($week);
 });
