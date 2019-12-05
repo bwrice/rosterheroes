5687a7bb7c1df5ae78e0c5f4767b88c57cdd0d7d
Brian Rice <bwrice83@gmail.com>
2019-12-05 11:31:26 -0500

    create row in stats integration for MySportsFeed

---
+++ database/migrations/2019_12_05_162654_register_msf_stats_integration.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RegisterMsfStatsIntegration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \App\StatsIntegration::query()->create([
+            'name' => \App\External\Stats\MySportsFeed\MySportsFeed::INTEGRATION_NAME
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
