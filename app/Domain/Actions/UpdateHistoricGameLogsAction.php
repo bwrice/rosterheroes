4f9191d58c8c768ef87fde880836fbacd6b51c71
Brian Rice <bwrice83@gmail.com>
2019-12-07 14:24:35 -0500

    create UpdateHistoricGameLogsAction domain action

---
+++ app/Domain/Actions/UpdateHistoricGameLogsAction.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Collections\GameCollection;
+use App\Domain\Collections\LeagueCollection;
+use App\Domain\Models\Game;
+use App\Jobs\UpdateHistoricPlayerGameLogsJob;
+use Illuminate\Support\Facades\Date;
+
+class UpdateHistoricGameLogsAction
+{
+    public const DAYS_BEFORE_QUERY_ARG = 2;
+
+    /**
+     * @param LeagueCollection $leagues
+     * @param int $yearDelta
+     * @return int
+     */
+    public function execute(LeagueCollection $leagues, int $yearDelta = 0): int
+    {
+        $count = 0;
+        $this->getGameQuery($leagues)->chunk(100, function (GameCollection $games) use (&$count, $yearDelta) {
+
+            $games->each(function (Game $game) use (&$count, $yearDelta) {
+
+                UpdateHistoricPlayerGameLogsJob::dispatch($game, $yearDelta)->onQueue('stats-integration')->delay($count * 5);
+                $count++;
+            });
+        });
+        return $count;
+    }
+
+    protected function getGameQuery(LeagueCollection $leagueCollection)
+    {
+        return Game::query()->whereDate('starts_at', '<', Date::now()->subDays(self::DAYS_BEFORE_QUERY_ARG))
+            ->forLeagues($leagueCollection->pluck('id')->toArray());
+    }
+}
