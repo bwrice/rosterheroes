08e1e9df1cc8f45c3bc6761a68b0691473b3967e
Brian Rice <bwrice83@gmail.com>
2019-12-07 14:23:55 -0500

    simplify StatsIntegrationServiceProvider by using boot() method instead of register()

--- app/Providers/StatsIntegrationServiceProvider.php
+++ app/Providers/StatsIntegrationServiceProvider.php
@@ -2,37 +2,17 @@
 
 namespace App\Providers;
 
-use App\External\Stats\MySportsFeed\Authorization;
-use App\External\Stats\MySportsFeed\GameAPI;
-use App\External\Stats\MySportsFeed\GameLogAPI;
-use App\External\Stats\MySportsFeed\LeagueSeasonConverter;
-use App\External\Stats\MySportsFeed\LeagueYearURL;
-use App\External\Stats\MySportsFeed\MSFClient;
 use App\External\Stats\MySportsFeed\MySportsFeed;
-use App\External\Stats\MySportsFeed\LeagueURL;
-use App\External\Stats\MySportsFeed\PlayerAPI;
-use App\External\Stats\MySportsFeed\StatAmountDTOs\StatAmountDTOBuilderFactory;
-use App\External\Stats\MySportsFeed\TeamAPI;
 use App\External\Stats\StatsIntegration;
-use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
 class StatsIntegrationServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
+
+    public function boot()
     {
+        // Bind MySportsFeed to container as implementation of StatsIntegration interface
         $this->app->bind(StatsIntegration::class, function ($app) {
-            return new MySportsFeed(
-                new PlayerAPI(new MSFClient(new Client(), new Authorization())),
-                new TeamAPI(new MSFClient(new Client(), new Authorization()), new LeagueSeasonConverter()),
-                new GameAPI(new MSFClient(new Client(), new Authorization()), new LeagueSeasonConverter()),
-                new GameLogAPI(new MSFClient(new Client(), new Authorization()), new LeagueSeasonConverter()),
-                new StatAmountDTOBuilderFactory()
-            );
+            return app(MySportsFeed::class);
         });
     }
 
