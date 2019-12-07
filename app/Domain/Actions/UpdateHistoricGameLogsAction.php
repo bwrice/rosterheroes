eba32121b0a88e4250b1907146e477305e0c4c7d
Brian Rice <bwrice83@gmail.com>
2019-12-07 15:14:18 -0500

    add $force argument to action to override finalized games

--- app/Domain/Actions/UpdateHistoricGameLogsAction.php
+++ app/Domain/Actions/UpdateHistoricGameLogsAction.php
@@ -7,6 +7,7 @@ namespace App\Domain\Actions;
 use App\Domain\Collections\GameCollection;
 use App\Domain\Collections\LeagueCollection;
 use App\Domain\Models\Game;
+use App\Domain\QueryBuilders\GameQueryBuilder;
 use App\Jobs\UpdateHistoricPlayerGameLogsJob;
 use Illuminate\Support\Facades\Date;
 
@@ -15,14 +16,15 @@ class UpdateHistoricGameLogsAction
     public const DAYS_BEFORE_QUERY_ARG = 2;
 
     /**
-     * @param LeagueCollection $leagues
+     * @param LeagueCollection|null $leagues
+     * @param bool $force
      * @param int $yearDelta
      * @return int
      */
-    public function execute(LeagueCollection $leagues = null, int $yearDelta = 0): int
+    public function execute(LeagueCollection $leagues = null, $force = false, int $yearDelta = 0): int
     {
         $count = 0;
-        $this->getGameQuery($leagues)->chunk(100, function (GameCollection $games) use (&$count, $yearDelta) {
+        $this->getGameQuery($leagues, $force)->chunk(100, function (GameCollection $games) use (&$count, $yearDelta) {
 
             $games->each(function (Game $game) use (&$count, $yearDelta) {
 
@@ -33,11 +35,19 @@ class UpdateHistoricGameLogsAction
         return $count;
     }
 
-    protected function getGameQuery(LeagueCollection $leagues = null)
+    /**
+     * @param LeagueCollection|null $leagues
+     * @param bool $force
+     * @return GameQueryBuilder
+     */
+    protected function getGameQuery(LeagueCollection $leagues = null, $force = false)
     {
         $query = Game::query()->whereDate('starts_at', '<', Date::now()->subDays(self::DAYS_BEFORE_QUERY_ARG));
         if ($leagues) {
             $query = $query->forLeagues($leagues->pluck('id')->toArray());
+        }
+        if (! $force) {
+            $query = $query->whereNull('finalized_at');
         }
         return $query;
     }
