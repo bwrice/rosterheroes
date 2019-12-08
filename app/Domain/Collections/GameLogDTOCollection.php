5e60221f49f8310782c116eb67c6ab83765f99f9
Brian Rice <bwrice83@gmail.com>
2019-12-08 15:39:04 -0500

    add $gameOver property to GameLogDTOCollection

--- app/Domain/Collections/GameLogDTOCollection.php
+++ app/Domain/Collections/GameLogDTOCollection.php
@@ -8,5 +8,25 @@ use Illuminate\Support\Collection;
 
 class GameLogDTOCollection extends Collection
 {
+    protected $gameOver = false;
+
+    /**
+     * @return bool
+     */
+    public function isGameOver(): bool
+    {
+        return $this->gameOver;
+    }
+
+    /**
+     * @param bool $gameOver
+     * @return $this
+     */
+    public function setGameOver(bool $gameOver)
+    {
+        $this->gameOver = $gameOver;
+        return $this;
+    }
+
 
 }
