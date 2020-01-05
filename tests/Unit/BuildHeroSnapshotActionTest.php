d8368e81e71c7cacbc213c19e118098d9bff4ce7
Brian Rice <bwrice83@gmail.com>
2020-01-05 02:05:41 -0500

    create step one domain action for finalizing week

---
+++ tests/Unit/BuildHeroSnapshotActionTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class BuildHeroSnapshotActionTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
