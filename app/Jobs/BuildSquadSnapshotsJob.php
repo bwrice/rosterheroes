18835efde4f574159eaad761a3a80e5dc431f4d3
Brian Rice <bwrice83@gmail.com>
2020-01-07 17:23:35 -0500

    create BuildSquadSnapshotsJob

---
+++ app/Jobs/BuildSquadSnapshotsJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Domain\Models\Squad;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class BuildSquadSnapshotsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * @var Squad
+     */
+    private $squad;
+
+    /**
+     * BuildSquadSnapshotsJob constructor.
+     * @param Squad $squad
+     */
+    public function __construct(Squad $squad)
+    {
+        $this->squad = $squad;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
