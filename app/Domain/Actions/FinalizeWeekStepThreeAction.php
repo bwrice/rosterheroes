cd4d56807f552b581e1a6f4b971d8ffb44247dc1
Brian Rice <bwrice83@gmail.com>
2020-01-06 21:04:41 -0500

    create step three job and action

---
+++ app/Domain/Actions/FinalizeWeekStepThreeAction.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+class FinalizeWeekStepThreeAction
+{
+    public function execute()
+    {
+
+    }
+}
---
+++ app/Jobs/FinalizeWeekStepThreeJob.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Domain\Actions\FinalizeWeekStepThreeAction;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class FinalizeWeekStepThreeJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @param FinalizeWeekStepThreeAction $domainAction
+     */
+    public function handle(FinalizeWeekStepThreeAction $domainAction)
+    {
+        $domainAction->execute();
+    }
+}
