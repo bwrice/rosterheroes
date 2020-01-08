ee64a803c71c0494835d5dbff20dad99eabd514d
Brian Rice <bwrice83@gmail.com>
2020-01-08 08:14:19 -0500

    add withPlayerSpirits() for squad query

--- app/Domain/QueryBuilders/SquadQueryBuilder.php
+++ app/Domain/QueryBuilders/SquadQueryBuilder.php
@@ -8,5 +8,10 @@ use Illuminate\Database\Eloquent\Builder;
 
 class SquadQueryBuilder extends Builder
 {
-
+    public function withPlayerSpirits()
+    {
+        return $this->whereHas('heroes', function (HeroQueryBuilder $builder) {
+            return $builder->hasPlayerSpirit();
+        });
+    }
 }
