38fd92119e76b6969ff66724daed69adc3de0c40
Brian Rice <bwrice83@gmail.com>
2020-01-08 10:11:04 -0500

    chain build squad snapshots with step four job

--- app/Domain/Actions/FinalizeWeekStepThreeAction.php
+++ app/Domain/Actions/FinalizeWeekStepThreeAction.php
@@ -4,10 +4,28 @@
 namespace App\Domain\Actions;
 
 
+use App\Domain\Models\PlayerSpirit;
+use App\Domain\Models\Squad;
+use App\Domain\Models\Week;
+use App\Jobs\BuildSquadSnapshotsJob;
+use App\Jobs\FinalizeWeekStepFourJob;
+use Bwrice\LaravelJobChainGroups\Jobs\ChainGroup;
+use Illuminate\Database\Eloquent\Collection;
+
 class FinalizeWeekStepThreeAction
 {
     public function execute()
     {
-
+        $chainGroup = ChainGroup::create([], [
+            new FinalizeWeekStepFourJob()
+        ]);
+        Squad::query()->chunk(100, function(Collection $squads) use ($chainGroup) {
+            $squads->filter(function (Squad $squad) {
+                return $squad->combatReady();
+            })->each(function (Squad $squad) use ($chainGroup) {
+                $chainGroup->push(new BuildSquadSnapshotsJob($squad));
+            });
+        });
+        $chainGroup->dispatch();
     }
 }
