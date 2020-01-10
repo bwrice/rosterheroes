8b8d22af87bf0d346120117db85e51504b216e2e
Brian Rice <bwrice83@gmail.com>
2020-01-10 06:06:17 -0500

    build step four action

---
+++ app/Domain/Actions/FinalizeWeekStepFourAction.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Models\Minion;
+use App\Jobs\BuildMinionSnapshotJob;
+use App\Jobs\FinalizeWeekStepFiveJob;
+use Bwrice\LaravelJobChainGroups\Jobs\ChainGroup;
+use Illuminate\Database\Eloquent\Collection;
+
+class FinalizeWeekStepFourAction
+{
+    public function execute()
+    {
+        $chainGroup = ChainGroup::create([], [
+            new FinalizeWeekStepFiveJob()
+        ]);
+        Minion::query()->chunk(100, function (Collection $minions) use ($chainGroup) {
+            $jobs = $minions->map(function (Minion $minion) {
+                return new BuildMinionSnapshotJob($minion);
+            });
+            $chainGroup->merge($jobs);
+        });
+        $chainGroup->dispatch();
+    }
+}
