7aabe068df409cdc366f26fdbdfeefd017a0d05d
Brian Rice <bwrice83@gmail.com>
2020-01-11 17:50:22 -0500

    test inning pitched behavior instead of calculator

--- tests/Unit/InningsPitchedBehaviorTest.php
+++ tests/Unit/InningsPitchedBehaviorTest.php
@@ -2,23 +2,27 @@
 
 namespace Tests\Unit;
 
+use App\Domain\Behaviors\StatTypes\Baseball\InningPitchedBehavior;
 use App\Domain\Behaviors\StatTypes\InningsPitchedCalculator;
 use App\Domain\Behaviors\StatTypes\MultiplierCalculator;
+use Illuminate\Validation\Rules\In;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class InningsPitchedCalculatorTest extends TestCase
+class InningsPitchedBehaviorTest extends TestCase
 {
     /**
      * @test
      */
     public function it_will_calculate_full_innings_correctly()
     {
-        $calculator = new InningsPitchedCalculator(new MultiplierCalculator(10));
+        /** @var InningPitchedBehavior $behavior */
+        $behavior = app(InningPitchedBehavior::class);
+        $pointsPer = $behavior->getPointsPer();
 
-        $total = $calculator->total(5);
-        $this->assertTrue(abs(50 - $total) < PHP_FLOAT_EPSILON);
+        $total = $behavior->getTotalPoints(5);
+        $this->assertTrue(abs(($pointsPer * 5) - $total) < PHP_FLOAT_EPSILON);
     }
 
     /**
@@ -26,11 +30,11 @@ class InningsPitchedCalculatorTest extends TestCase
      */
     public function it_will_calculate_one_third_inning_pitched_correctly()
     {
-        $pointsPer = 10;
-        $calculator = new InningsPitchedCalculator(new MultiplierCalculator($pointsPer));
+        /** @var InningPitchedBehavior $behavior */
+        $behavior = app(InningPitchedBehavior::class);
+        $pointsPer = $behavior->getPointsPer();
 
-
-        $total = $calculator->total(5.1);
+        $total = $behavior->getTotalPoints(5.1);
         $expected = round(5 * $pointsPer + ((1/3) * $pointsPer), 2);
         $this->assertTrue(abs($expected - $total) < PHP_FLOAT_EPSILON);
     }
@@ -40,11 +44,11 @@ class InningsPitchedCalculatorTest extends TestCase
      */
     public function it_will_calculate_two_thirds_inning_pitched_correctly()
     {
-        $pointsPer = 10;
-        $calculator = new InningsPitchedCalculator(new MultiplierCalculator($pointsPer));
+        /** @var InningPitchedBehavior $behavior */
+        $behavior = app(InningPitchedBehavior::class);
+        $pointsPer = $behavior->getPointsPer();
 
-
-        $total = $calculator->total(5.2);
+        $total = $behavior->getTotalPoints(5.2);
         $expected = round(5 * $pointsPer + ((2/3) * $pointsPer), 2);
         $this->assertTrue(abs($expected - $total) < PHP_FLOAT_EPSILON);
     }
