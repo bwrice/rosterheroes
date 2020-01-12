a28008520b611c525c5d5447f486687521c89195
Brian Rice <bwrice83@gmail.com>
2020-01-11 20:39:00 -0500

    create with-stats state on PlayerGameLog model factory

--- database/factories/PlayerGameLogFactory.php
+++ database/factories/PlayerGameLogFactory.php
@@ -1,18 +1,43 @@
 <?php
 
+use App\Domain\Models\Game;
+use App\Domain\Models\Player;
+use App\Domain\Models\PlayerGameLog;
+use App\Domain\Models\StatType;
+use App\Domain\Models\Team;
 use Faker\Generator as Faker;
 
-$factory->define(\App\Domain\Models\PlayerGameLog::class, function (Faker $faker) {
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(PlayerGameLog::class, function (Faker $faker) {
 
     return [
         'game_id' => function() {
-            return factory(\App\Domain\Models\Game::class)->create()->id;
+            return factory(Game::class)->create()->id;
         },
         'player_id' => function() {
-            return factory(\App\Domain\Models\Player::class)->create()->id;
+            return factory(Player::class)->create()->id;
         },
         'team_id' => function() {
-            return factory(\App\Domain\Models\Team::class)->create()->id;
+            return factory(Team::class)->create()->id;
         }
     ];
+});
+
+$factory->afterCreatingState(PlayerGameLog::class, 'with-stats', function (PlayerGameLog $gameLog, Faker $faker) {
+    $player = $gameLog->player;
+    $takeAmount = random_int(3, 7);
+    $statTypes = StatType::query()->where('sport_id', '=', $player->team->league->sport_id)->inRandomOrder()->take($takeAmount)->get();
+    $statTypes->each(function (StatType $statType) use ($gameLog) {
+        $pointsPer = $statType->getBehavior()->getPointsPer();
+        if ($pointsPer < 0) {
+            $amount = 1;
+        } else {
+            $topRange = ceil(20/$pointsPer);
+            $amount = rand(1, $topRange);
+        }
+        $gameLog->playerStats()->create([
+            'amount' => $amount,
+            'stat_type_id' => $statType->id
+        ]);
+    });
 });
