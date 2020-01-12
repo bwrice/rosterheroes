b2f2c4ad35ca08845136698c15eb035380c72179
Brian Rice <bwrice83@gmail.com>
2020-01-12 12:13:50 -0500

    add relations to SquadSnapshot

--- app/SquadSnapshot.php
+++ app/SquadSnapshot.php
@@ -2,9 +2,31 @@
 
 namespace App;
 
+use App\Domain\Models\Squad;
+use App\Domain\Models\Week;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class SquadSnapshot
+ * @package App
+ *
+ * @property int $squad_id
+ * @property int $week_id
+ *
+ * @property Squad $squad
+ * @property Week $week
+ */
 class SquadSnapshot extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function squad()
+    {
+        return $this->belongsTo(Squad::class);
+    }
+
+    public function week()
+    {
+        return $this->belongsTo(Week::class);
+    }
 }
