3c41c91d4e658e093babbe5b28c5630e9110841f
Brian Rice <bwrice83@gmail.com>
2020-01-11 23:18:47 -0500

    add ‘with-stats’ factory state to player spirit factory

--- database/factories/PlayerSpiritFactory.php
+++ database/factories/PlayerSpiritFactory.php
@@ -1,24 +1,37 @@
 <?php
 
+use App\Domain\Models\Game;
+use App\Domain\Models\Player;
+use App\Domain\Models\PlayerSpirit;
+use App\Domain\Models\Week;
 use Faker\Generator as Faker;
+use Ramsey\Uuid\Uuid;
 
-$factory->define(\App\Domain\Models\PlayerSpirit::class, function (Faker $faker) {
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(PlayerSpirit::class, function (Faker $faker) {
 
-
-    $uuid = (string) \Ramsey\Uuid\Uuid::uuid4();
+    $uuid = (string) Uuid::uuid4();
 
     return [
         'uuid' => $uuid,
         'week_id' => function () {
-            return factory(\App\Domain\Models\Week::class)->create()->id;
+            return factory(Week::class)->create()->id;
         },
         'player_id' => function () {
-            return factory(\App\Domain\Models\Player::class)->create()->id;
+            return factory(Player::class)->create()->id;
         },
         'game_id' => function () {
-            return factory(\App\Domain\Models\Game::class)->create()->id;
+            return factory(Game::class)->create()->id;
         },
         'essence_cost' => 5000,
-        'energy' => \App\Domain\Models\PlayerSpirit::STARTING_ENERGY
+        'energy' => PlayerSpirit::STARTING_ENERGY
     ];
+});
+
+$factory->afterCreatingState(PlayerSpirit::class, 'with-stats', function (PlayerSpirit $playerSpirit, Faker $faker) {
+    $gameLog = factory(\App\Domain\Models\PlayerGameLog::class)->state('with-stats')->create([
+        'player_id' => $playerSpirit->player_id,
+        'game_id' => $playerSpirit->game_id
+    ]);
+    $playerSpirit->player_game_log_id = $gameLog->id;
 });
