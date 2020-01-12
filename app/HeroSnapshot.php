67f26b5479dca9763852607941be70de77573714
Brian Rice <bwrice83@gmail.com>
2020-01-12 12:13:40 -0500

    add hero relation to HeroSnapshot

--- app/HeroSnapshot.php
+++ app/HeroSnapshot.php
@@ -2,9 +2,22 @@
 
 namespace App;
 
+use App\Domain\Models\Hero;
+use App\Domain\Models\Week;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class HeroSnapshot
+ * @package App
+ *
+ * @property Hero $hero
+ */
 class HeroSnapshot extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function hero()
+    {
+        return $this->belongsTo(Hero::class);
+    }
 }
