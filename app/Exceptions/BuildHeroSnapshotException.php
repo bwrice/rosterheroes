1a05ce02298c54fe0f81132319732d71f3118c37
Brian Rice <bwrice83@gmail.com>
2020-01-12 12:56:32 -0500

    add squad snapshot to exception

--- app/Exceptions/BuildHeroSnapshotException.php
+++ app/Exceptions/BuildHeroSnapshotException.php
@@ -5,21 +5,28 @@ namespace App\Exceptions;
 
 
 use App\Domain\Models\Hero;
+use App\SquadSnapshot;
 use Throwable;
 
 class BuildHeroSnapshotException extends \RuntimeException
 {
-    public const CODE_INVALID_PLAYER_SPIRIT = 1;
+    public const CODE_INVALID_HERO = 1;
+    public const CODE_INVALID_PLAYER_SPIRIT = 2;
 
     /**
      * @var Hero
      */
     private $hero;
+    /**
+     * @var SquadSnapshot
+     */
+    private $squadSnapshot;
 
-    public function __construct(Hero $hero, $message = "", $code = 0, Throwable $previous = null)
+    public function __construct(SquadSnapshot $squadSnapshot, Hero $hero, $message = "", $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
         $this->hero = $hero;
+        $this->squadSnapshot = $squadSnapshot;
     }
 
     /**
@@ -28,5 +35,13 @@ class BuildHeroSnapshotException extends \RuntimeException
     public function getHero(): Hero
     {
         return $this->hero;
+    }
+
+    /**
+     * @return SquadSnapshot
+     */
+    public function getSquadSnapshot(): SquadSnapshot
+    {
+        return $this->squadSnapshot;
     }
 }
