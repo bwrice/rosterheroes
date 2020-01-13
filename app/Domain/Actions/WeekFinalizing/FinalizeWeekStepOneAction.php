a14e364136ebff320d648f1201f327ec479295ee
Brian Rice <bwrice83@gmail.com>
2020-01-13 01:53:24 -0500

    use CurrentWeek facade in step one action

--- app/Domain/Actions/WeekFinalizing/FinalizeWeekStepOneAction.php
+++ app/Domain/Actions/WeekFinalizing/FinalizeWeekStepOneAction.php
@@ -5,34 +5,28 @@ namespace App\Domain\Actions\WeekFinalizing;
 
 
 use App\Domain\Models\Game;
-use App\Domain\Models\Week;
 use App\Exceptions\FinalizeWeekException;
+use App\Facades\CurrentWeek;
 use App\Jobs\FinalizeWeekStepTwoJob;
 use App\Jobs\UpdatePlayerGameLogsJob;
 use Bwrice\LaravelJobChainGroups\Jobs\ChainGroup;
-use Illuminate\Support\Facades\Date;
 
 class FinalizeWeekStepOneAction
 {
     public function execute()
     {
-        $week = Week::current();
-        $this->validateTime($week);
-        ChainGroup::create($this->getUpdatePlayerGameLogsForGameJobs($week), [
+        if (! CurrentWeek::finalizing()) {
+            throw new FinalizeWeekException(CurrentWeek::get(), "Week is not ready to be finalized", FinalizeWeekException::INVALID_TIME_TO_FINALIZE);
+        }
+        ChainGroup::create($this->getUpdatePlayerGameLogsForGameJobs(), [
             new FinalizeWeekStepTwoJob()
         ])->dispatch();
     }
 
-    protected function validateTime(Week $week)
+    protected function getUpdatePlayerGameLogsForGameJobs()
     {
-        if (Date::now()->isBefore($week->adventuring_locks_at->addHours(Week::FINALIZE_AFTER_ADVENTURING_CLOSED_HOURS))) {
-            throw new FinalizeWeekException($week, "Week is not ready to be finalized", FinalizeWeekException::INVALID_TIME_TO_FINALIZE);
-        }
-    }
-
-    protected function getUpdatePlayerGameLogsForGameJobs(Week $week)
-    {
-        $games = Game::query()->withPlayerSpiritsForWeeks([$week->id])->get();
+        $currentWeekID = CurrentWeek::id();
+        $games = Game::query()->withPlayerSpiritsForWeeks([$currentWeekID])->get();
         return $games->map(function (Game $game) {
             return new UpdatePlayerGameLogsJob($game);
         })->toArray();
