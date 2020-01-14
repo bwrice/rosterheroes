7a3f932abd3a5556b1404cc501711661c9d8a317
Brian Rice <bwrice83@gmail.com>
2020-01-14 02:09:18 -0500

    execute build hero snapshots when creating squad snapshot

--- app/Domain/Actions/BuildSquadSnapshotAction.php
+++ app/Domain/Actions/BuildSquadSnapshotAction.php
@@ -4,16 +4,26 @@
 namespace App\Domain\Actions;
 
 
+use App\Domain\Models\Hero;
 use App\Domain\Models\Squad;
-use App\Domain\Models\Week;
 use App\Exceptions\BuildSquadSnapshotException;
 use App\Facades\CurrentWeek;
+use App\Facades\HeroCombat;
 use App\SquadSnapshot;
 
 class BuildSquadSnapshotAction
 {
     /** @var Squad */
     protected $squad;
+    /**
+     * @var BuildHeroSnapshotAction
+     */
+    protected $buildHeroSnapshotAction;
+
+    public function __construct(BuildHeroSnapshotAction $buildHeroSnapshotAction)
+    {
+        $this->buildHeroSnapshotAction = $buildHeroSnapshotAction;
+    }
 
     public function execute(Squad $squad): SquadSnapshot
     {
@@ -22,12 +32,19 @@ class BuildSquadSnapshotAction
             throw new BuildSquadSnapshotException($this->squad, "Week not in finalizing state", BuildSquadSnapshotException::CODE_WEEK_NOT_FINALIZED);
         }
 
-        /** @var SquadSnapshot $snapshot */
-        $snapshot = SquadSnapshot::query()->create([
+        /** @var SquadSnapshot $squadSnapshot */
+        $squadSnapshot = SquadSnapshot::query()->create([
             'squad_id' => $this->squad->id,
             'week_id' => CurrentWeek::id(),
             'data' => []
         ]);
-        return $snapshot;
+
+        $this->squad->heroes->filter(function (Hero $hero) {
+            return HeroCombat::ready($hero);
+        })->each(function (Hero $hero) use ($squadSnapshot) {
+            $this->buildHeroSnapshotAction->execute($squadSnapshot, $hero);
+        });
+
+        return $squadSnapshot->fresh();
     }
 }
