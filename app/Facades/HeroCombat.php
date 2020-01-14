927fa7034bd922d28158ad91b92e9ada87256a5b
Brian Rice <bwrice83@gmail.com>
2020-01-14 02:09:32 -0500

    add facade method to doc block

--- app/Facades/HeroCombat.php
+++ app/Facades/HeroCombat.php
@@ -4,8 +4,15 @@
 namespace App\Facades;
 
 
+use App\Domain\Models\Hero;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class HeroCombat
+ * @package App\Facades
+ *
+ * @method static bool ready(Hero $hero)
+ */
 class HeroCombat extends Facade
 {
     protected static function getFacadeAccessor()
