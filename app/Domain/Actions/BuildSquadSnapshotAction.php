6e40a649cd8eba8ec278257549109f92e7d79d86
Brian Rice <bwrice83@gmail.com>
2020-01-13 21:07:34 -0500

    return a newly created squad snapshot

--- app/Domain/Actions/BuildSquadSnapshotAction.php
+++ app/Domain/Actions/BuildSquadSnapshotAction.php
@@ -8,17 +8,26 @@ use App\Domain\Models\Squad;
 use App\Domain\Models\Week;
 use App\Exceptions\BuildSquadSnapshotException;
 use App\Facades\CurrentWeek;
+use App\SquadSnapshot;
 
 class BuildSquadSnapshotAction
 {
     /** @var Squad */
     protected $squad;
 
-    public function execute(Squad $squad)
+    public function execute(Squad $squad): SquadSnapshot
     {
         $this->squad = $squad;
         if (! CurrentWeek::finalizing()) {
             throw new BuildSquadSnapshotException($this->squad, "Week not in finalizing state", BuildSquadSnapshotException::CODE_WEEK_NOT_FINALIZED);
         }
+
+        /** @var SquadSnapshot $snapshot */
+        $snapshot = SquadSnapshot::query()->create([
+            'squad_id' => $this->squad->id,
+            'week_id' => CurrentWeek::id(),
+            'data' => []
+        ]);
+        return $snapshot;
     }
 }
