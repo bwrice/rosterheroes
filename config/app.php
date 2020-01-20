e4fca617a4e7ceff7ad2657c21c06f43f81115a1
Brian Rice <bwrice83@gmail.com>
2020-01-20 11:56:21 -0500

    dynamically register Telescope based on environment

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
         /*
