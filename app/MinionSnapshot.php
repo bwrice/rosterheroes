4a11b1fa6c0625224df8546a934639a79584ba63
Brian Rice <bwrice83@gmail.com>
2020-01-20 17:50:56 -0500

    create MinionSnapshot model

---
+++ app/MinionSnapshot.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use App\Domain\Models\Minion;
+use App\Domain\Models\Week;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class MinionSnapshot
+ * @package App
+ *
+ * @property int $id
+ * @property int $minion_id
+ *
+ * @property Minion $minion
+ * @property Week $week
+ */
+class MinionSnapshot extends Model
+{
+    protected $guarded = [];
+    protected $casts = [
+        'data' => 'array'
+    ];
+
+    public function minion()
+    {
+        return $this->belongsTo(Minion::class);
+    }
+
+    public function week()
+    {
+        return $this->belongsTo(Week::class);
+    }
+}
