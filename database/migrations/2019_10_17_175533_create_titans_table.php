008ba431e44669bb645af6a101780b2396f6b25b
Brian Rice <bwrice83@gmail.com>
2020-01-21 14:53:14 -0500

    remove attributes from titan schema

--- database/migrations/2019_10_17_175533_create_titans_table.php
+++ database/migrations/2019_10_17_175533_create_titans_table.php
@@ -18,13 +18,6 @@ class CreateTitansTable extends Migration
             $table->uuid('uuid');
             $table->string('name');
             $table->string('slug');
-            $table->unsignedInteger('base_level');
-            $table->smallInteger('base_damage_rating');
-            $table->smallInteger('damage_multiplier_rating');
-            $table->smallInteger('health_rating');
-            $table->smallInteger('protection_rating');
-            $table->smallInteger('combat_speed_rating');
-            $table->smallInteger('block_rating');
             $table->integer('enemy_type_id')->unsigned();
             $table->integer('combat_position_id')->unsigned();
             $table->timestamps();
