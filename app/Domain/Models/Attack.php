b7552064774c09994eac1bba0a48452e48f00a4e
Brian Rice <bwrice83@gmail.com>
2020-01-22 00:39:33 -0500

    update Attack model for config yaml files

--- app/Domain/Models/Attack.php
+++ app/Domain/Models/Attack.php
@@ -12,6 +12,7 @@ use App\Domain\Models\Json\ResourceCosts\ResourceCost;
 use App\Domain\QueryBuilders\AttackQueryBuilder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class Attack
@@ -19,14 +20,10 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  *
  * @property int $id
  * @property string $name
- * @property int $grade
  * @property int $fixed_target_count
  * @property int $attacker_position_id
  * @property int $target_position_id
- * @property float $speed_rating
- * @property float $base_damage_rating
- * @property float $damage_multiplier_rating
- * @property string $resource_costs
+ * @property string $config_path
  *
  * @property DamageType $damageType
  * @property CombatPosition $attackerPosition
@@ -37,6 +34,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  */
 class Attack extends Model
 {
+    // snake case to match YAML config attributes
+    protected $grade;
+    protected $fixed_target_count;
+    protected $speed_rating;
+    protected $base_damage_rating;
+    protected $damage_multiplier_rating;
+    protected $resource_costs;
+    protected $requirements;
+
+
     public const BASIC_BLADE_ATTACK_NAME = 'Slash';
     public const BASIC_BOW_ATTACK_NAME = 'Arrow';
     public const BASIC_MAGIC_ATTACK_NAME = 'Magic Bolt';
@@ -104,9 +111,9 @@ class Attack extends Model
 
     public function getBaseDamage(): int
     {
-        $baseDamage = 10 * sqrt($this->base_damage_rating);
+        $baseDamage = 10 * sqrt($this->getBaseDamageRating());
         $attackerPositionBonus = $this->attackerPosition->getBehavior()->getBaseDamageBonus();
-        $damageTypeBonus = $this->damageType->getBehavior()->getBaseDamageBonus($this->fixed_target_count);
+        $damageTypeBonus = $this->damageType->getBehavior()->getBaseDamageBonus($this->getFixedTargetCount());
         $baseDamage *= (1 + $attackerPositionBonus + $damageTypeBonus);
         if ($this->hasAttacks) {
             $baseDamage = $this->hasAttacks->adjustBaseDamage($baseDamage);
@@ -116,9 +123,9 @@ class Attack extends Model
 
     public function getCombatSpeed(): float
     {
-        $combatSpeed = $this->speed_rating;
+        $combatSpeed = $this->getSpeedRating();
         $attackerPositionBonus = $this->attackerPosition->getBehavior()->getCombatSpeedBonus();
-        $damageTypeBonus = $this->damageType->getBehavior()->getCombatSpeedBonus($this->fixed_target_count);
+        $damageTypeBonus = $this->damageType->getBehavior()->getCombatSpeedBonus($this->getFixedTargetCount());
         $combatSpeed *= (1 + $attackerPositionBonus + $damageTypeBonus);
         if ($this->hasAttacks) {
             $combatSpeed = $this->hasAttacks->adjustCombatSpeed($combatSpeed);
@@ -128,9 +135,9 @@ class Attack extends Model
 
     public function getDamageMultiplier(): float
     {
-        $damageMultiplier = $this->damage_multiplier_rating**.25;
+        $damageMultiplier = $this->getDamageMultiplierRating()**.25;
         $attackerPositionBonus = $this->attackerPosition->getBehavior()->getDamageMultiplierBonus();
-        $damageTypeBonus = $this->damageType->getBehavior()->getDamageMultiplierBonus($this->fixed_target_count);
+        $damageTypeBonus = $this->damageType->getBehavior()->getDamageMultiplierBonus($this->getFixedTargetCount());
         $damageMultiplier *= (1 + $attackerPositionBonus + $damageTypeBonus);
         if ($this->hasAttacks) {
             $damageMultiplier = $this->hasAttacks->adjustDamageMultiplier($damageMultiplier);
@@ -138,9 +145,9 @@ class Attack extends Model
         return $damageMultiplier;
     }
 
-    public function getResourceCosts()
+    public function getResourceCostCollection()
     {
-        $resourceCostsArray = json_decode($this->resource_costs, true);
+        $resourceCostsArray = $this->getResourceCosts();
 
         $resourceCosts = array_map(function ($resourceCostArray) {
             switch ($resourceCostArray['type']) {
@@ -162,6 +169,124 @@ class Attack extends Model
     public function setHasAttacks(?HasAttacks $hasAttacks): Attack
     {
         $this->hasAttacks = $hasAttacks;
+        return $this;
+    }
+
+    public function getConfig(string $key)
+    {
+        return Yaml::parseFile(app_path() . $this->config_path)[$key];
+    }
+
+    public function getConfigAttribute(string $attribute)
+    {
+        if (! $this->$attribute) {
+            $this->$attribute = $this->getConfig($attribute);
+        }
+        return $this->$attribute;
+    }
+
+    public function getGrade()
+    {
+        return $this->getConfigAttribute('grade');
+    }
+
+    public function getFixedTargetCount()
+    {
+        return $this->getConfigAttribute('fixed_target_count');
+    }
+
+    public function getSpeedRating()
+    {
+        return $this->getConfigAttribute('speed_rating');
+    }
+
+    public function getBaseDamageRating()
+    {
+        return $this->getConfigAttribute('base_damage_rating');
+    }
+
+    public function getDamageMultiplierRating()
+    {
+        return $this->getConfigAttribute('damage_multiplier_rating');
+    }
+
+    public function getResourceCosts()
+    {
+        return $this->getConfigAttribute('resource_costs');
+    }
+
+    public function getRequirements()
+    {
+        return $this->getConfigAttribute('requirements');
+    }
+
+    /**
+     * @param int $grade
+     * @return $this
+     */
+    public function setGrade(int $grade)
+    {
+        $this->grade = $grade;
+        return $this;
+    }
+
+    /**
+     * @param int|null $fixedTargetCount
+     * @return $this
+     */
+    public function setFixedTargetCount(?int $fixedTargetCount)
+    {
+        $this->fixed_target_count = $fixedTargetCount;
+        return $this;
+    }
+
+    /**
+     * @param float $speedRating
+     * @return $this
+     */
+    public function setSpeedRating(float $speedRating)
+    {
+        $this->speed_rating = $speedRating;
+        return $this;
+    }
+
+    /**
+     * @param float $baseDamageRating
+     * @return $this
+     */
+    public function setBaseDamageRating(float $baseDamageRating)
+    {
+        $this->base_damage_rating = $baseDamageRating;
+        return $this;
+    }
+
+    /**
+     * @param float $damageMultiplierRating
+     * @return $this
+     */
+    public function setDamageMultiplierRating(float $damageMultiplierRating)
+    {
+        $this->damage_multiplier_rating = $damageMultiplierRating;
+        return $this;
+    }
+
+    /**
+     * @param array $resource_costs
+     * @return $this
+     */
+    public function setResourceCosts(array $resource_costs)
+    {
+        $this->resource_costs = $resource_costs;
+        return $this;
+    }
+
+    /**
+     * @param array $requirements
+     * @return $this
+     */
+    public function setRequirements(array $requirements)
+    {
+        $this->requirements = $requirements;
         return $this;
     }
 }
