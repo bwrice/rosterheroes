1779b8f057a0ebe4b408d531fe563f757572bdfa
Brian Rice <bwrice83@gmail.com>
2020-01-23 16:26:06 -0500

    create domain action AddTestHeroToTestSquadAction

---
+++ app/Domain/Actions/Testing/AddTestHeroToTestSquadAction.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Domain\Actions\Testing;
+
+
+use App\Domain\Models\HeroRace;
+use App\Domain\Models\Squad;
+
+class AddTestHeroToTestSquadAction
+{
+    /**
+     * @param Squad $testSquad
+     * @param HeroRace $heroRace
+     */
+    public function execute(Squad $testSquad, HeroRace $heroRace)
+    {
+
+    }
+}
