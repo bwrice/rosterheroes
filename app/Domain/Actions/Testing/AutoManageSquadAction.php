8580e6ea43ea53b471466da13308984c495a01f0
Brian Rice <bwrice83@gmail.com>
2020-01-24 01:23:53 -0500

    dispatch relevant jobs in AutoManageSquadAction

--- app/Domain/Actions/Testing/AutoManageSquadAction.php
+++ app/Domain/Actions/Testing/AutoManageSquadAction.php
@@ -4,13 +4,21 @@
 namespace App\Domain\Actions\Testing;
 
 
+use App\Domain\Models\Hero;
 use App\Domain\Models\Squad;
 use App\Exceptions\AutoManageSquadException;
 use App\Facades\CurrentWeek;
+use App\Jobs\AutoJoinQuestsJob;
+use App\Jobs\AutoManageHeroJob;
+use Bwrice\LaravelJobChainGroups\Jobs\ChainGroup;
 use Illuminate\Support\Facades\Date;
 
 class AutoManageSquadAction
 {
+    /**
+     * @param Squad $squad
+     * @throws AutoManageSquadException
+     */
     public function execute(Squad $squad)
     {
         if (! CurrentWeek::get()) {
@@ -19,5 +27,17 @@ class AutoManageSquadAction
         if (CurrentWeek::adventuringLocksAt()->isBefore(Date::now()->addHours(4))) {
             throw new AutoManageSquadException($squad, 'Current week locks too soon', AutoManageSquadException::CODE_CURRENT_WEEK_LOCKS_SOON);
         }
+
+        $heroJobs = $this->getAutoManageHeroJobs($squad);
+        ChainGroup::create($heroJobs->toArray(), [
+            new AutoJoinQuestsJob($squad)
+        ])->dispatch();
+    }
+
+    protected function getAutoManageHeroJobs(Squad $squad)
+    {
+        return $squad->heroes->map(function (Hero $hero) {
+            return new AutoManageHeroJob($hero);
+        });
     }
 }
