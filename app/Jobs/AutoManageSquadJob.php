5865f0cf8cc74d85e43ebd14563a2281d54b5196
Brian Rice <bwrice83@gmail.com>
2020-01-23 21:04:34 -0500

    create AutoManageSquadJob

---
+++ app/Jobs/AutoManageSquadJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Domain\Models\Squad;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AutoManageSquadJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * @var Squad
+     */
+    public $squad;
+
+    /**
+     * AutoManageSquadJob constructor.
+     * @param Squad $squad
+     */
+    public function __construct(Squad $squad)
+    {
+        $this->squad = $squad;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
