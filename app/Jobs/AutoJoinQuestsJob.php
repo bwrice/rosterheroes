d4db24e4188b68ea08a15d4524fcc5d570f720fd
Brian Rice <bwrice83@gmail.com>
2020-01-24 01:24:07 -0500

    create AutoJoinQuestsJob

---
+++ app/Jobs/AutoJoinQuestsJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Domain\Models\Squad;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class AutoJoinQuestsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var Squad
+     */
+    public $squad;
+
+    public function __construct(Squad $squad)
+    {
+        $this->squad = $squad;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
