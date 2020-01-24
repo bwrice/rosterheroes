0e5b7aa559dbfd37c674018f6f1baa434ada5a8b
Brian Rice <bwrice83@gmail.com>
2020-01-23 21:34:03 -0500

    create custom exception AutoManageSquadException

---
+++ app/Exceptions/AutoManageSquadException.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Exceptions;
+
+
+use App\Domain\Models\Squad;
+use Throwable;
+
+class AutoManageSquadException extends \Exception
+{
+    public const CODE_NO_CURRENT_WEEK = 1;
+    public const CODE_CURRENT_WEEK_LOCKS_SOON = 1;
+    /**
+     * @var Squad
+     */
+    protected $squad;
+
+    public function __construct(Squad $squad, $message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->squad = $squad;
+    }
+}
