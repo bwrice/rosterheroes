9c2871d17d7324f779ac0b9272f42095ed38a6b9
Brian Rice <bwrice83@gmail.com>
2020-01-25 10:34:54 -0500

    chunk query and return count of jobs dispatched

--- app/Domain/Actions/Testing/InitiateTestSquadManagementAction.php
+++ app/Domain/Actions/Testing/InitiateTestSquadManagementAction.php
@@ -4,15 +4,23 @@
 namespace App\Domain\Actions\Testing;
 
 
+use App\Domain\Collections\SquadCollection;
 use App\Domain\Models\Squad;
 use App\Jobs\AutoManageSquadJob;
+use Illuminate\Support\Facades\Log;
 
 class InitiateTestSquadManagementAction
 {
     public function execute()
     {
-        Squad::query()->isTest()->get()->each(function (Squad $squad) {
-            AutoManageSquadJob::dispatch($squad);
+        $count = 0;
+        Squad::query()->isTest()->chunk(100, function(SquadCollection $squads) use (&$count) {
+            $count += $squads->count();
+            $squads->each(function (Squad $squad) {
+                AutoManageSquadJob::dispatch($squad);
+            });
         });
+
+        return $count;
     }
 }
