d921d439b334e3feafb3bc9c8de4b12971a62dc1
Brian Rice <bwrice83@gmail.com>
2020-01-25 18:39:06 -0500

    filter spirits in use from collection instead of query

--- app/Domain/Actions/Testing/AutoAttachSpiritToHeroAction.php
+++ app/Domain/Actions/Testing/AutoAttachSpiritToHeroAction.php
@@ -5,9 +5,10 @@ namespace App\Domain\Actions\Testing;
 
 
 use App\Domain\Actions\AddSpiritToHeroAction;
+use App\Domain\Collections\PlayerSpiritCollection;
 use App\Domain\Models\Hero;
 use App\Domain\Models\PlayerSpirit;
-use App\Domain\QueryBuilders\PlayerSpiritQueryBuilder;
+use App\Domain\Models\Squad;
 
 class AutoAttachSpiritToHeroAction
 {
@@ -33,21 +34,48 @@ class AutoAttachSpiritToHeroAction
         }
 
         $squad = $hero->squad;
-        $maxEssenceCost = (int) floor($squad->spirit_essence/$squad->heroes()->count());
         $validPositionNames = $hero->heroRace->positions->pluck('name')->toArray();
 
-        /** @var PlayerSpirit $playerSpirit */
-        $playerSpirit = PlayerSpirit::query()
-            ->availableForSquad($hero->squad)
-            ->forCurrentWeek()
-            ->withPositions($validPositionNames)
-            ->maxEssenceCost($maxEssenceCost)
-            ->orderByDesc('essence_cost')
-            ->first();
+        $validPlayerSpirits = $this->getValidPlayerSpirits($validPositionNames, $squad->heroes()->count(), $squad->spirit_essence);
+        $playerSpirit = $this->filterInUsePlayerSpirits($validPlayerSpirits, $squad)->first();
 
         if ($playerSpirit) {
             return $this->addSpiritToHeroAction->execute($hero, $playerSpirit);
         }
         return $hero;
+    }
+
+    /**
+     * @param array $validPositionNames
+     * @param int $heroesCount
+     * @param int $totalEssence
+     * @return PlayerSpiritCollection
+     */
+    protected function getValidPlayerSpirits(array $validPositionNames, int $heroesCount, int $totalEssence)
+    {
+        $maxEssenceCost = (int) floor($totalEssence/$heroesCount);
+
+        return PlayerSpirit::query()
+            ->forCurrentWeek()
+            ->withPositions($validPositionNames)
+            ->maxEssenceCost($maxEssenceCost)
+            ->orderByDesc('essence_cost')
+            ->take($heroesCount)->get();
+    }
+
+    /**
+     * @param PlayerSpiritCollection $playerSpirits
+     * @param Squad $squad
+     * @return PlayerSpiritCollection
+     */
+    protected function filterInUsePlayerSpirits(PlayerSpiritCollection $playerSpirits, Squad $squad)
+    {
+        $heroes = $squad->heroes;
+        return $playerSpirits->filter(function (PlayerSpirit $playerSpirit) use ($heroes) {
+            $inUse = $heroes->first(function (Hero $hero) use ($playerSpirit) {
+                return $hero->player_spirit_id === $playerSpirit->id;
+            });
+            return is_null($inUse);
+        });
     }
 }
