87f175fa9835d02a27a3b89d07872a5a7878e2ff
Brian Rice <bwrice83@gmail.com>
2020-01-25 20:42:55 -0500

    add test it_will_use_up_essence_remaining_when_last_hero_of_squad_is_missing_a_player_spirit

--- tests/Feature/AutoAttachSpiritToHeroActionTest.php
+++ tests/Feature/AutoAttachSpiritToHeroActionTest.php
@@ -110,24 +110,19 @@ class AutoAttachSpiritToHeroActionTest extends TestCase
     /**
     * @test
     */
-    public function it_will_execute_add_spirit_with_the_higher_essence_cost_under_max_allowed()
+    public function it_will_execute_add_spirit_with_essence_cost_under_essence_per_hero_when_multiple_heroes_missing_spirits()
     {
+        $this->assertGreaterThan(1, $this->squad->heroes->filter(function (Hero $hero) {
+            return is_null($hero->player_spirit_id);
+        })->count());
         $validPositions = $this->hero->heroRace->positions;
-        /** @var Player $validPositionPlayer */
-        $validPositionPlayer = factory(Player::class)->create();
-        $validPositionPlayer->positions()->attach($validPositions->random()->id);
-        $lowEssencePlayerSpirit = factory(PlayerSpirit::class)->create([
-            'player_id' => $validPositionPlayer->id,
-            'essence_cost' => $this->essencePerHero - 2000, // Make essence cost less appealing than invalid position spirit
-            'week_id' => $this->currentWeek->id
-        ]);
 
         /** @var Player $validPositionPlayer */
         $validPositionPlayer = factory(Player::class)->create();
         $validPositionPlayer->positions()->attach($validPositions->random()->id);
         $tooHighEssencePlayerSpirit = factory(PlayerSpirit::class)->create([
             'player_id' => $validPositionPlayer->id,
-            'essence_cost' => $this->essencePerHero + 2000, // Make essence cost less appealing than invalid position spirit
+            'essence_cost' => $this->essencePerHero + 2000, // Essence cost too high
             'week_id' => $this->currentWeek->id
         ]);
 
@@ -136,7 +131,7 @@ class AutoAttachSpiritToHeroActionTest extends TestCase
         $validPositionPlayer->positions()->attach($validPositions->random()->id);
         $optimalEssencePlayerSpirit = factory(PlayerSpirit::class)->create([
             'player_id' => $validPositionPlayer->id,
-            'essence_cost' => $this->essencePerHero, // Make essence cost less appealing than invalid position spirit
+            'essence_cost' => $this->essencePerHero,
             'week_id' => $this->currentWeek->id
         ]);
 
@@ -147,6 +142,61 @@ class AutoAttachSpiritToHeroActionTest extends TestCase
         $domainAction = app(AutoAttachSpiritToHeroAction::class);
         $domainAction->execute($this->hero);
         $spy->shouldHaveReceived('execute')->with(EloquentMatcher::withExpected($this->hero), EloquentMatcher::withExpected($optimalEssencePlayerSpirit));
+    }
+
+    /**
+    * @test
+    */
+    public function it_will_use_up_essence_remaining_when_last_hero_of_squad_is_missing_a_player_spirit()
+    {
+        // attach spirit to other hero
+        $this->otherHeroForSquad->player_spirit_id = factory(PlayerSpirit::class)->create([
+            'essence_cost' => $otherSpiritCost = (int) ($this->essencePerHero - 2000),
+            'week_id' => $this->currentWeek->id
+        ])->id;
+        $this->otherHeroForSquad->save();
+
+        // assert last hero need spirit
+        $this->assertEquals(1, $this->squad->heroes->filter(function (Hero $hero) {
+            return is_null($hero->player_spirit_id);
+        })->count());
+
+        $validPositions = $this->hero->heroRace->positions;
+
+        /** @var Player $validPositionPlayer */
+        $validPositionPlayer = factory(Player::class)->create();
+        $validPositionPlayer->positions()->attach($validPositions->random()->id);
+        $tooLowEssenceSpirit = factory(PlayerSpirit::class)->create([
+            'player_id' => $validPositionPlayer->id,
+            'essence_cost' => $this->essencePerHero,
+            'week_id' => $this->currentWeek->id
+        ]);
+
+        /** @var Player $validPositionPlayer */
+        $validPositionPlayer = factory(Player::class)->create();
+        $validPositionPlayer->positions()->attach($validPositions->random()->id);
+        $expectedPlayerSpirit = factory(PlayerSpirit::class)->create([
+            'player_id' => $validPositionPlayer->id,
+            'essence_cost' => $this->squadEssence - $otherSpiritCost,
+            'week_id' => $this->currentWeek->id
+        ]);
+
+        /** @var Player $validPositionPlayer */
+        $validPositionPlayer = factory(Player::class)->create();
+        $validPositionPlayer->positions()->attach($validPositions->random()->id);
+        $tooHighSpiritEssence = factory(PlayerSpirit::class)->create([
+            'player_id' => $validPositionPlayer->id,
+            'essence_cost' => ($this->squadEssence - $otherSpiritCost) + 100,
+            'week_id' => $this->currentWeek->id
+        ]);
+
+        $spy = \Mockery::spy(AddSpiritToHeroAction::class);
+        app()->instance(AddSpiritToHeroAction::class, $spy);
+
+        /** @var AutoAttachSpiritToHeroAction $domainAction */
+        $domainAction = app(AutoAttachSpiritToHeroAction::class);
+        $domainAction->execute($this->hero);
+        $spy->shouldHaveReceived('execute')->with(EloquentMatcher::withExpected($this->hero), EloquentMatcher::withExpected($expectedPlayerSpirit));
     }
 
     /**
