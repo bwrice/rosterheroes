1c77bb46c63f676870a68df2ef27b8957792d37f
Brian Rice <bwrice83@gmail.com>
2020-01-28 19:00:11 -0500

    use HeroService facade directly in SquadService

--- app/Services/ModelServices/SquadService.php
+++ app/Services/ModelServices/SquadService.php
@@ -12,7 +12,7 @@ class SquadService
     public function combatReady(Squad $squad)
     {
         $readyHero = $squad->heroes->first(function (Hero $hero) {
-            return $hero->combatReady();
+            return \App\Facades\HeroService::combatReady($hero);
         });
         return ! is_null($readyHero);
     }
