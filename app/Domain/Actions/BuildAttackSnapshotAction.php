4b58ff7e146dd97d2d36d96b000c968dec72e8d1
Brian Rice <bwrice83@gmail.com>
2020-01-29 18:17:53 -0500

    return newly created snapshot in BuildAttackSnapshotAction

--- app/Domain/Actions/BuildAttackSnapshotAction.php
+++ app/Domain/Actions/BuildAttackSnapshotAction.php
@@ -4,12 +4,20 @@
 namespace App\Domain\Actions;
 
 
+use App\AttackSnapshot;
 use App\Domain\Models\Attack;
+use App\Facades\CurrentWeek;
 
 class BuildAttackSnapshotAction
 {
-    public function execute(Attack $attack)
+    public function execute(Attack $attack): AttackSnapshot
     {
-        // TODO
+        /** @var AttackSnapshot $snapshot */
+        $snapshot = AttackSnapshot::query()->create([
+            'attack_id' => $attack->id,
+            'week_id' => CurrentWeek::id(),
+            'data' => []
+        ]);
+        return $snapshot;
     }
 }
