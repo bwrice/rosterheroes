847e8c15c84fb3d1020057d45d74d69556547558
Brian Rice <bwrice83@gmail.com>
2020-01-29 18:27:33 -0500

    create model factory for AttackSnapshot

---
+++ database/factories/AttackSnapshotModelFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var Factory $factory */
+
+use App\AttackSnapshot;
+use App\Domain\Models\Attack;
+use App\Domain\Models\Week;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+
+$factory->define(AttackSnapshot::class, function (Faker $faker) {
+    return [
+        'squad_id' => function () {
+            return factory(Attack::class)->create()->id;
+        },
+        'week_id' => function () {
+            return factory(Week::class)->create()->id;
+        },
+        'data' => []
+    ];
+});
