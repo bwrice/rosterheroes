3615e76dce9c162620745f14e3cafedbf1850909
Brian Rice <bwrice83@gmail.com>
2020-01-29 18:09:50 -0500

    add relations and doc block props to AttackSnapshot

--- app/AttackSnapshot.php
+++ app/AttackSnapshot.php
@@ -2,9 +2,29 @@
 
 namespace App;
 
+use App\Domain\Models\Attack;
+use App\Domain\Models\Week;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class AttackSnapshot
+ * @package App
+ *
+ * @property int $id
+ * @property int $attack_id
+ * @property int $week_id
+ */
 class AttackSnapshot extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function attack()
+    {
+        return $this->belongsTo(Attack::class);
+    }
+
+    public function week()
+    {
+        return $this->belongsTo(Week::class);
+    }
 }
