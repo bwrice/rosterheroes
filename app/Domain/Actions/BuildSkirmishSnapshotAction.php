5eebb6de1b349d07d0f505cd0ec79bc86eaa12c5
Brian Rice <bwrice83@gmail.com>
2020-01-29 23:45:55 -0500

    create BuildSkirmishSnapshot job and execute associated domain action

---
+++ app/Domain/Actions/BuildSkirmishSnapshotAction.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Models\Skirmish;
+use App\SkirmishSnapshot;
+
+class BuildSkirmishSnapshotAction
+{
+    public function execute(Skirmish $skirmish): SkirmishSnapshot
+    {
+        // TODO
+    }
+}
---
+++ app/Jobs/BuildSkirmishSnapshotJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Domain\Actions\BuildSkirmishSnapshotAction;
+use App\Domain\Models\Skirmish;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class BuildSkirmishSnapshotJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var Skirmish
+     */
+    public $skirmish;
+
+    /**
+     * BuildSkirmishSnapshotJob constructor.
+     * @param Skirmish $skirmish
+     */
+    public function __construct(Skirmish $skirmish)
+    {
+        $this->skirmish = $skirmish;
+    }
+
+    /**
+     * @param BuildSkirmishSnapshotAction $domainAction
+     */
+    public function handle(BuildSkirmishSnapshotAction $domainAction)
+    {
+        $domainAction->execute($this->skirmish);
+    }
+}
