3cab861ad055380e38a3200aa49c0249ca41bdb7
Brian Rice <bwrice83@gmail.com>
2020-01-30 01:12:59 -0500

    test BuildMinionSnapshotAction creates a snapshot

--- tests/Feature/BuildMinionSnapshotActionTest.php
+++ tests/Feature/BuildMinionSnapshotActionTest.php
@@ -2,32 +2,43 @@
 
 namespace Tests\Feature;
 
+use App\Domain\Actions\BuildMinionSnapshotAction;
 use App\Domain\Models\Minion;
+use App\Domain\Models\Week;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class BuildMinionSnapshotActionTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /** @var Minion */
     protected $minion;
+
+    /** @var Week */
+    protected $currentWeek;
+
+    /** @var BuildMinionSnapshotAction */
+    protected $domainAction;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this;
+        $this->minion = factory(Minion::class)->create();
+        $this->currentWeek = factory(Week::class)->states('finalizing', 'as-current')->create();
+        $this->domainAction = app(BuildMinionSnapshotAction::class);
     }
 
     /**
-     * A basic feature test example.
-     *
-     * @return void
+     * @test
      */
-    public function testExample()
+    public function it_will_create_a_snapshot_for_the_minion_and_current_week()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $snapshot = $this->domainAction->execute($this->minion);
+        $this->assertEquals($this->minion->id, $snapshot->minion_id);
+        $this->assertEquals($this->currentWeek->id, $snapshot->week_id);
     }
 }
