3c7ca8126559d861e4e78cb1aea2009984dd7978
Brian Rice <bwrice83@gmail.com>
2020-01-30 11:56:29 -0500

    return existing minion snapshot in BuildMinionSnapshotAction

--- app/Domain/Actions/BuildMinionSnapshotAction.php
+++ app/Domain/Actions/BuildMinionSnapshotAction.php
@@ -16,10 +16,21 @@ class BuildMinionSnapshotAction
      */
     public function execute(Minion $minion): MinionSnapshot
     {
+        $currentWeekID = CurrentWeek::id();
+
+        /** @var MinionSnapshot $existing */
+        $existing = MinionSnapshot::query()
+            ->where('minion_id', '=', $minion->id)
+            ->where('week_id', '=', $currentWeekID)
+            ->first();
+        if ($existing) {
+            return $existing;
+        }
+
         /** @var MinionSnapshot $snapshot */
         $snapshot = MinionSnapshot::query()->create([
             'minion_id' => $minion->id,
-            'week_id' => CurrentWeek::id(),
+            'week_id' => $currentWeekID,
             'data' => []
         ]);
         return $snapshot;
