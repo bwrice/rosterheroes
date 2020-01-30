c99a41f054b0cc74cf58c5edde7c3f61921e7de0
Brian Rice <bwrice83@gmail.com>
2020-01-29 20:25:31 -0500

    create ProcessCurrentWeekSkirmishesAction domain action

---
+++ app/Domain/Actions/WeekFinalizing/ProcessCurrentWeekSkirmishesAction.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Domain\Actions\WeekFinalizing;
+
+
+class ProcessCurrentWeekSkirmishesAction implements FinalizeWeekDomainAction
+{
+    public function execute(int $weekFinalizingStep)
+    {
+
+    }
+}
