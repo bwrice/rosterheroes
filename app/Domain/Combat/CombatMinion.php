c37f38adb44ef65c14b4e949c6bc7286a09fa4d5
Brian Rice <bwrice83@gmail.com>
2020-02-01 12:37:02 -0500

    create combat classes for Skirmish side of SkirmishResult

---
+++ app/Domain/Combat/CombatMinion.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Domain\Combat;
+
+
+class CombatMinion
+{
+    public function getCombatAttacks()
+    {
+        return collect();
+    }
+}
---
+++ app/Domain/Combat/CombatSkirmish.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\Domain\Combat;
+
+
+class CombatSkirmish
+{
+    public function getCombatMinions()
+    {
+        return collect();
+    }
+}
