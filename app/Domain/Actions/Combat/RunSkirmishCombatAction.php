63624243d2f63227ceb3f74d36e07cf03ec00693
Brian Rice <bwrice83@gmail.com>
2020-02-01 20:28:39 -0500

    wip RunSkirmishCombatAction

---
+++ app/Domain/Actions/Combat/RunSkirmishCombatAction.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace App\Domain\Actions\Combat;
+
+
+use App\Domain\Combat\CombatAttack;
+use App\Domain\Combat\CombatHero;
+use App\Domain\Combat\CombatItem;
+use App\Domain\Combat\CombatSkirmish;
+use App\Domain\Combat\CombatSquad;
+use Illuminate\Support\Collection;
+
+class RunSkirmishCombatAction
+{
+    /**
+     * @var int
+     */
+    protected $moment;
+
+    public function __construct()
+    {
+        $this->moment = 1;
+    }
+
+    public function execute(CombatSquad $combatSquad, CombatSkirmish $combatSkirmish)
+    {
+        $squadAttacks = $this->getSquadAttacks($combatSquad);
+        $combatMinions = $combatSkirmish->getCombatMinions();
+        while (true) {
+
+        }
+    }
+
+    protected function getSquadAttacks(CombatSquad $combatSquad)
+    {
+        $attacks = collect();
+        $combatSquad->getCombatHeroes()->each(function (CombatHero $combatHero) use (&$attacks) {
+            $combatHero->getCombatItems()->each(function (CombatItem $combatItem) use (&$attacks) {
+                $attacks = $attacks->merge($combatItem->getCombatAttacks());
+            });
+        });
+        return $attacks;
+    }
+
+    protected function processSquadTurn(Collection $squadAttacks, Collection $combatMinions)
+    {
+        $rand = random_int(1, 1000);
+        $readyAttacks = $squadAttacks->filter(function (CombatAttack $attack) use ($rand) {
+            $speed = $attack->getSpeed();
+            return $speed * 10 >= $rand;
+        });
+
+        $readyAttacks->each(function (CombatAttack $combatAttack) use ($combatMinions) {
+            $this->processAttack($combatAttack, $combatMinions);
+        });
+    }
+
+    protected function processAttack(CombatAttack $attack, Collection $combatMinions)
+    {
+
+    }
+}
