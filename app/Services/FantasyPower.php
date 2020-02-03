3cc7029e6336c3b8a643f05264c7e28ab6d51550
Brian Rice <bwrice83@gmail.com>
2020-02-03 11:33:49 -0500

    create method to calculate fantasy power

--- app/Services/FantasyPower.php
+++ app/Services/FantasyPower.php
@@ -3,8 +3,19 @@
 
 namespace App\Services;
 
-
 class FantasyPower
 {
-
+    public function calculate(float $totalPoints)
+    {
+        $fantasyPower = 0;
+        $coefficient = 1;
+        $remaining = $totalPoints;
+        while ($remaining > 0) {
+            $pointsToMultiply = $remaining < 10 ? $remaining : 10;
+            $fantasyPower += ($coefficient * $pointsToMultiply);
+            $remaining -= 10;
+            $coefficient *= .8;
+        }
+        return $fantasyPower;
+    }
 }
