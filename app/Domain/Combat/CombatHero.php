4b95cabfd89d173319f2a7685dce06ccd7a570b3
Brian Rice <bwrice83@gmail.com>
2020-02-04 17:26:35 -0500

    build constructor of CombatHero

--- app/Domain/Combat/CombatHero.php
+++ app/Domain/Combat/CombatHero.php
@@ -4,10 +4,49 @@
 namespace App\Domain\Combat;
 
 
+use App\Domain\Collections\CombatAttackCollection;
+use App\Domain\Models\CombatPosition;
+
 class CombatHero
 {
-    public function getCombatItems()
+    /**
+     * @var int
+     */
+    protected $health;
+    /**
+     * @var int
+     */
+    protected $stamina;
+    /**
+     * @var int
+     */
+    protected $mana;
+    /**
+     * @var int
+     */
+    protected $protection;
+    /**
+     * @var CombatPosition
+     */
+    protected $combatPosition;
+    /**
+     * @var CombatAttackCollection
+     */
+    protected $combatAttacks;
+
+    public function __construct(
+        int $health,
+        int $stamina,
+        int $mana,
+        int $protection,
+        CombatPosition $combatPosition,
+        CombatAttackCollection $combatAttacks)
     {
-        return collect();
+        $this->health = $health;
+        $this->stamina = $stamina;
+        $this->mana = $mana;
+        $this->protection = $protection;
+        $this->combatPosition = $combatPosition;
+        $this->combatAttacks = $combatAttacks;
     }
 }
