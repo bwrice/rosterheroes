16488cfa58581e651193471c6d499635a700b924
Brian Rice <bwrice83@gmail.com>
2020-02-04 13:32:25 -0500

    build out HeroCombatAttack

--- app/Domain/Combat/HeroCombatAttack.php
+++ app/Domain/Combat/HeroCombatAttack.php
@@ -8,9 +8,12 @@ use App\Domain\Behaviors\DamageTypes\DamageTypeBehavior;
 use App\Domain\Behaviors\TargetPriorities\TargetPriorityBehavior;
 use App\Domain\Behaviors\TargetRanges\CombatPositionBehavior;
 use App\Domain\Collections\ResourceCostsCollection;
+use App\Domain\Models\CombatPosition;
+use App\Domain\Models\DamageType;
 use App\Domain\Models\Hero;
+use App\Domain\Models\TargetPriority;
 
-class HeroCombatAttack
+class HeroCombatAttack implements CombatAttack
 {
     /**
      * @var int
@@ -37,25 +40,29 @@ class HeroCombatAttack
      */
     protected $speed;
     /**
-     * @var CombatPositionBehavior
+     * @var CombatPosition
      */
-    protected $attackerPositionBehavior;
+    protected $attackerPosition;
     /**
-     * @var CombatPositionBehavior
+     * @var CombatPosition
      */
-    protected $targetPositionBehavior;
+    protected $targetPosition;
     /**
-     * @var DamageTypeBehavior
+     * @var DamageType
      */
-    protected $damageTypeBehavior;
+    protected $damageType;
     /**
-     * @var TargetPriorityBehavior
+     * @var TargetPriority
      */
-    protected $targetPriorityBehavior;
+    protected $targetPriority;
     /**
      * @var ResourceCostsCollection
      */
     protected $resourceCostsCollection;
+    /**
+     * @var int|null
+     */
+    protected $maxTargetsCount;
 
     public function __construct(
         int $heroID,
@@ -64,11 +71,12 @@ class HeroCombatAttack
         int $damage,
         int $grade,
         float $speed,
-        CombatPositionBehavior $attackerPositionBehavior,
-        CombatPositionBehavior $targetPositionBehavior,
-        DamageTypeBehavior $damageTypeBehavior,
-        TargetPriorityBehavior $targetPriorityBehavior,
-        ResourceCostsCollection $resourceCostsCollection)
+        CombatPosition $attackerPosition,
+        CombatPosition $targetPosition,
+        DamageType $damageType,
+        TargetPriority $targetPriority,
+        ResourceCostsCollection $resourceCostsCollection,
+        int $maxTargetsCount)
     {
         $this->heroID = $heroID;
         $this->itemID = $itemID;
@@ -76,10 +84,24 @@ class HeroCombatAttack
         $this->damage = $damage;
         $this->grade = $grade;
         $this->speed = $speed;
-        $this->attackerPositionBehavior = $attackerPositionBehavior;
-        $this->targetPositionBehavior = $targetPositionBehavior;
-        $this->damageTypeBehavior = $damageTypeBehavior;
-        $this->targetPriorityBehavior = $targetPriorityBehavior;
+        $this->attackerPosition = $attackerPosition;
+        $this->targetPosition = $targetPosition;
+        $this->damageType = $damageType;
+        $this->targetPriority = $targetPriority;
         $this->resourceCostsCollection = $resourceCostsCollection;
+        $this->maxTargetsCount = $maxTargetsCount;
+    }
+
+    public function getDamagePerTarget(int $targetsCount): int
+    {
+        return $this->damageType->getBehavior()->getDamagePerTarget($this->damage, $targetsCount);
+    }
+
+    public function getTargets(CombatantCollection $possibleTargets): CombatantCollection
+    {
+        return $possibleTargets
+            ->filterByCombatPosition($this->targetPosition)
+            ->sortByTargetPriority($this->targetPriority)
+            ->take($this->maxTargetsCount);
     }
 }
