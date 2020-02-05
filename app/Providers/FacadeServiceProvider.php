926bb7f7951dd77c9d1d75bc92b0ca54dbdd644e
Brian Rice <bwrice83@gmail.com>
2020-02-05 01:22:32 -0500

    delete combat service

--- app/Providers/FacadeServiceProvider.php
+++ app/Providers/FacadeServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Services\Combat;
 use App\Services\CurrentWeek;
 use App\Services\FantasyPower;
 use App\Services\ModelServices\AttackService;
@@ -41,9 +40,6 @@ class FacadeServiceProvider extends ServiceProvider
         });
         $this->app->bind('fantasy-power', function () {
             return new FantasyPower();
-        });
-        $this->app->bind('combat', function () {
-            return new Combat();
         });
     }
 
