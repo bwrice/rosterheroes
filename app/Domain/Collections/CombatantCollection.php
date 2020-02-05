eb96e1c34f392261ab5203f2dec43025de7f85e5
Brian Rice <bwrice83@gmail.com>
2020-02-05 14:52:52 -0500

    build out sortByTargetPriority method

--- app/Domain/Collections/CombatantCollection.php
+++ app/Domain/Collections/CombatantCollection.php
@@ -4,6 +4,7 @@
 namespace App\Domain\Collections;
 
 
+use App\Domain\Combat\Combatant;
 use App\Domain\Models\CombatPosition;
 use App\Domain\Models\TargetPriority;
 use Illuminate\Support\Collection;
@@ -12,7 +13,15 @@ class CombatantCollection extends Collection
 {
     public function sortByTargetPriority(TargetPriority $targetPriority)
     {
-        return $this;
+        $behavior = $targetPriority->getBehavior();
+        $sortFunc = function (Combatant $combatant) use ($behavior) {
+            return $behavior->getCombatantValue($combatant);
+        };
+        if ($behavior->sortCombatantsDescending()) {
+            return $this->sortByDesc($sortFunc);
+        } else {
+            return $this->sortBy($sortFunc);
+        }
     }
 
     public function filterByCombatPosition(CombatPosition $combatPosition)
