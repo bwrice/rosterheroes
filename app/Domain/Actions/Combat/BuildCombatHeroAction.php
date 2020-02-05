6d9c12de851f53732a7a3fa2d3ccf7c403bd30e3
Brian Rice <bwrice83@gmail.com>
2020-02-05 01:59:57 -0500

    return new CombatHero in BuildCombatHeroAction

--- app/Domain/Actions/Combat/BuildCombatHeroAction.php
+++ app/Domain/Actions/Combat/BuildCombatHeroAction.php
@@ -4,28 +4,52 @@
 namespace App\Domain\Actions\Combat;
 
 
+use App\Domain\Collections\CombatAttackCollection;
+use App\Domain\Combat\CombatHero;
 use App\Domain\Combat\HeroCombatAttack;
 use App\Domain\Models\Attack;
 use App\Domain\Models\CombatPosition;
 use App\Domain\Models\DamageType;
 use App\Domain\Models\Hero;
 use App\Domain\Models\Item;
+use App\Domain\Models\MeasurableType;
 use App\Domain\Models\TargetPriority;
 use App\Facades\FantasyPower;
 use Illuminate\Support\Collection;
 
 class BuildCombatHeroAction
 {
+    /**
+     * @param Hero $hero
+     * @param Collection $combatPositions
+     * @param Collection $targetPriorities
+     * @param Collection $damageTypes
+     * @return CombatHero
+     */
     public function execute(Hero $hero, Collection $combatPositions, Collection $targetPriorities, Collection $damageTypes)
     {
         $hero->loadMissing(Hero::heroResourceRelations());
         $fantasyPower = $this->getFantasyPower($hero);
-        $combatAttacks = collect();
+        $combatAttacks = new CombatAttackCollection();
         $hero->items->each(function (Item $item) use ($hero, $fantasyPower, &$combatAttacks, $combatPositions, $targetPriorities, $damageTypes) {
             $combatAttacks = $combatAttacks->merge($item->attacks->map(function (Attack $attack) use ($hero, $item, $fantasyPower, $combatPositions, $targetPriorities, $damageTypes) {
                 return $this->createHeroCombatAttack($attack, $item, $hero, $fantasyPower, $combatPositions, $targetPriorities, $damageTypes);
             }));
         });
+
+        $heroCombatPosition = $combatPositions->first(function (CombatPosition $combatPosition) use ($hero) {
+            return $combatPosition->id === $hero->combat_position_id;
+        });
+
+        return new CombatHero(
+            $hero->getCurrentMeasurableAmount(MeasurableType::HEALTH),
+            $hero->getCurrentMeasurableAmount(MeasurableType::STAMINA),
+            $hero->getCurrentMeasurableAmount(MeasurableType::MANA),
+            $hero->getProtection(),
+            $hero->getBlockChance(),
+            $heroCombatPosition,
+            $combatAttacks
+        );
     }
 
     protected function getFantasyPower(Hero $hero)
