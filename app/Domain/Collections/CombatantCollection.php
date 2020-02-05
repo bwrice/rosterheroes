49f3b11213fab1a6590b5cab102bdcc6055cbff6
Brian Rice <bwrice83@gmail.com>
2020-02-05 12:53:56 -0500

    delete dup collection

--- app/Domain/Collections/CombatantCollection.php
+++ app/Domain/Collections/CombatantCollection.php
@@ -4,9 +4,19 @@
 namespace App\Domain\Collections;
 
 
+use App\Domain\Models\CombatPosition;
+use App\Domain\Models\TargetPriority;
 use Illuminate\Support\Collection;
 
 class CombatantCollection extends Collection
 {
+    public function sortByTargetPriority(TargetPriority $targetPriority)
+    {
+        return $this;
+    }
 
+    public function filterByCombatPosition(CombatPosition $combatPosition)
+    {
+        return $this;
+    }
 }
