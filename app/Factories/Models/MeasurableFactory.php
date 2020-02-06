673dbae14ad05588a0eb5b8d0f8717a03cb537b0
Brian Rice <bwrice83@gmail.com>
2020-02-06 16:52:04 -0500

    create new MeasurableFactory

---
+++ app/Factories/Models/MeasurableFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace App\Factories\Models;
+
+
+use App\Domain\Models\Hero;
+use App\Domain\Models\Measurable;
+use App\Domain\Models\MeasurableType;
+use Illuminate\Support\Str;
+
+class MeasurableFactory
+{
+    /** @var int */
+    protected $measurableTypeID;
+
+    /** @var int */
+    protected $heroID;
+
+    /** @var HeroFactory */
+    protected $heroFactory;
+
+    protected function __construct()
+    {
+        $this->heroFactory = HeroFactory::new();
+    }
+
+    public static function new()
+    {
+        return new self();
+    }
+
+    /**
+     * @param array $extra
+     * @return Measurable
+     * @throws \Exception
+     */
+    public function create(array $extra = []): Measurable
+    {
+        /** @var Measurable $measurable */
+        $measurable = Measurable::query()->create([
+            'uuid' => (string) Str::uuid(),
+            'amount_raised' => 0,
+            'measurable_type_id' => $this->measurableTypeID ?: MeasurableType::query()->inRandomOrder()->first()->id,
+            'hero_id' => $this->heroID ?: $this->heroFactory->create()->id
+        ]);
+        return $measurable;
+    }
+
+    protected function forHero(Hero $hero)
+    {
+        $clone = clone $this;
+        $clone->heroID = $hero->id;
+        return $clone;
+    }
+}
