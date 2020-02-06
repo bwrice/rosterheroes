e26523c90505e0935de74634394ba7834955b3b6
Brian Rice <bwrice83@gmail.com>
2020-02-06 15:45:53 -0500

    create new SquadFactory

---
+++ app/Factories/Models/SquadFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace App\Factories\Models;
+
+
+use App\Domain\Models\MobileStorageRank;
+use App\Domain\Models\Province;
+use App\Domain\Models\Squad;
+use App\Domain\Models\SquadRank;
+use App\Domain\Models\User;
+use Illuminate\Support\Str;
+
+class SquadFactory
+{
+    protected $userID;
+
+    protected $provinceID;
+
+    protected $squadRankID;
+
+    protected $mobileStorageRankID;
+
+    public static function new(): self
+    {
+        return new self();
+    }
+
+    /**
+     * @param array $extra
+     * @return Squad
+     * @throws \Exception
+     */
+    public function create(array $extra = []): Squad
+    {
+        /** @var Squad $squad */
+        $squad = Squad::query()->create(array_merge(
+            [
+                'user_id' => $this->userID ?: factory(User::class)->create()->id,
+                'uuid' => Str::uuid(),
+                'name' => 'TestSquad_' . random_int(1,999999999),
+                'province_id' => $this->provinceID ?: Province::getStarting()->id,
+                'squad_rank_id' => $this->squadRankID ?: SquadRank::getStarting()->id,
+                'mobile_storage_rank_id' => $this->mobileStorageRankID ?: MobileStorageRank::getStarting()->id,
+                'spiritEssence' => Squad::STARTING_ESSENCE,
+                'gold' => Squad::STARTING_GOLD,
+                'experience' => Squad::STARTING_EXPERIENCE,
+                'favor' => Squad::STARTING_FAVOR,
+            ],
+            $extra
+        ));
+        return $squad;
+    }
+}
