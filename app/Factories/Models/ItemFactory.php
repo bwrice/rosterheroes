84991cd5e39fdc66c6e70f53140592527083e304
Brian Rice <bwrice83@gmail.com>
2020-02-06 21:07:22 -0500

    add more methods to ItemFactory

--- app/Factories/Models/ItemFactory.php
+++ app/Factories/Models/ItemFactory.php
@@ -4,9 +4,12 @@
 namespace App\Factories\Models;
 
 
+use App\Domain\Models\Enchantment;
+use App\Domain\Models\Hero;
 use App\Domain\Models\Item;
 use App\Domain\Models\ItemClass;
 use App\Domain\Models\ItemType;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 class ItemFactory
@@ -20,6 +23,17 @@ class ItemFactory
     /** @var int */
     protected $materialTypeID;
 
+    /** @var array */
+    protected $hasItems = [];
+
+    /** @var Collection */
+    protected $enchantments;
+
+    public function __construct()
+    {
+        $this->enchantments = collect();
+    }
+
     public static function new(): self
     {
         return new self();
@@ -30,13 +44,38 @@ class ItemFactory
         $itemType = $this->itemType ?: ItemType::query()->inRandomOrder()->first();
 
         /** @var Item $item */
-        $item = Item::query()->create([
+        $item = Item::query()->create(array_merge([
             'uuid' => (string) Str::uuid(),
             'item_class_id' => $this->itemClassID ?: ItemClass::generic()->id,
             'item_type_id' => $itemType->id,
-            'material_type_id' => $this->materialTypeID ?: $this->itemType->materials()->inRandomOrder()->first()->id
-        ]);
+            'material_id' => $this->materialTypeID ?: $itemType->materials()->inRandomOrder()->first()->id,
+            'has_items_id' => $this->hasItems['id'] ?? null,
+            'has_items_type' => $this->hasItems['type'] ?? null
+        ], $extra));
+
+        $item->enchantments()->saveMany($this->enchantments);
 
         return $item;
+    }
+
+    public function forHero(Hero $hero)
+    {
+        $clone = clone $this;
+        $clone->hasItems = [
+            'id' => $hero->id,
+            'type' => Hero::RELATION_MORPH_MAP_KEY
+        ];
+        return $clone;
+    }
+
+    public function withEnchantments(Collection $enchantments = null)
+    {
+        if (! $enchantments) {
+            $amount = rand(1, 4);
+            $enchantments = Enchantment::query()->inRandomOrder()->take($amount)->get();
+        }
+        $clone = clone $this;
+        $clone->enchantments = $enchantments;
+        return $clone;
     }
 }
