c164d78aa21e3e40a0ffec807cb61f8e9e06ab0a
Brian Rice <bwrice83@gmail.com>
2020-02-06 20:40:30 -0500

    add ItemClass helper methods

--- app/Domain/Models/ItemClass.php
+++ app/Domain/Models/ItemClass.php
@@ -19,4 +19,44 @@ class ItemClass extends Model
     const MYTHICAL = 'mythical';
 
     protected $guarded = [];
+
+    /**
+     * @return ItemType
+     */
+    public static function generic()
+    {
+        /** @var ItemType $itemType */
+        $itemType = static::query()->where('name', '=', self::GENERIC)->first();
+        return $itemType;
+    }
+
+    /**
+     * @return ItemType
+     */
+    public static function enchanted()
+    {
+        /** @var ItemType $itemType */
+        $itemType = static::query()->where('name', '=', self::ENCHANTED)->first();
+        return $itemType;
+    }
+
+    /**
+     * @return ItemType
+     */
+    public static function legendary()
+    {
+        /** @var ItemType $itemType */
+        $itemType = static::query()->where('name', '=', self::LEGENDARY)->first();
+        return $itemType;
+    }
+
+    /**
+     * @return ItemType
+     */
+    public static function mythical()
+    {
+        /** @var ItemType $itemType */
+        $itemType = static::query()->where('name', '=', self::MYTHICAL)->first();
+        return $itemType;
+    }
 }
