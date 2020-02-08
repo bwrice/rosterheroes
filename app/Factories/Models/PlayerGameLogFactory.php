a693cd75f393809c14dc500055c0fef85b4a4483
Brian Rice <bwrice83@gmail.com>
2020-02-07 23:35:09 -0500

    add property setters in PlayerGameLogFactory

--- app/Factories/Models/PlayerGameLogFactory.php
+++ app/Factories/Models/PlayerGameLogFactory.php
@@ -6,6 +6,7 @@ namespace App\Factories\Models;
 
 use App\Domain\Models\Game;
 use App\Domain\Models\Player;
+use App\Domain\Models\PlayerGameLog;
 use App\Domain\Models\Team;
 
 class PlayerGameLogFactory
@@ -24,18 +25,23 @@ class PlayerGameLogFactory
         return new self();
     }
 
+    /**
+     * @param array $extra
+     * @return PlayerGameLog
+     */
     public function create(array $extra = [])
     {
         $team = $this->getTeam();
         $player = $this->getPlayer($team);
+        $game = $this->getGame($team);
 
-        /** @var Game $game */
-        $game = Game::query()->create(array_merge([
-            'player_id' => $homeTeam->id,
-            'team_id' => $awayTeam->id,
-            'game_id' => $this->getStartsAt()
+        /** @var PlayerGameLog $playerGameLog */
+        $playerGameLog = PlayerGameLog::query()->create(array_merge([
+            'player_id' => $player->id,
+            'team_id' => $team->id,
+            'game_id' => $game->id
         ], $extra));
-        return $game;
+        return $playerGameLog;
     }
 
     /**
@@ -55,5 +61,34 @@ class PlayerGameLogFactory
             return $this->player;
         }
         return PlayerFactory::new()->forTeam($team)->create();
+    }
+
+    protected function getGame(Team $team)
+    {
+        if ($this->game) {
+            return $this->game;
+        }
+        return GameFactory::new()->forEitherTeam($team)->create();
+    }
+
+    public function forPlayer(Player $player)
+    {
+        $clone = clone $this;
+        $clone->player = $player;
+        return $clone;
+    }
+
+    public function forTeam(Team $team)
+    {
+        $clone = clone $this;
+        $clone->team = $team;
+        return $clone;
+    }
+
+    public function forGame(Game $game)
+    {
+        $clone = clone $this;
+        $clone->game = $game;
+        return $clone;
     }
 }
