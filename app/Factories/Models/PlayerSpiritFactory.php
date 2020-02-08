9ea06c2d3a0960d2000f94be04a8634b836d72d8
Brian Rice <bwrice83@gmail.com>
2020-02-07 23:53:59 -0500

    add game-log functionality to PlayerSpiritFactory

--- app/Factories/Models/PlayerSpiritFactory.php
+++ app/Factories/Models/PlayerSpiritFactory.php
@@ -13,11 +13,20 @@ use Illuminate\Support\Str;
 
 class PlayerSpiritFactory
 {
-    protected $weekID;
+    /** @var Week */
+    protected $week;
 
-    protected $playerID;
+    /** @var Player */
+    protected $player;
 
-    protected $gameID;
+    /** @var Game */
+    protected $game;
+
+    /** @var GameFactory */
+    protected $gameFactory;
+
+    /** @var PlayerGameLogFactory */
+    protected $playerGameLogFactory;
 
     public static function new(): self
     {
@@ -26,17 +35,31 @@ class PlayerSpiritFactory
 
     public function create(array $extra = [])
     {
+        $player = $this->getPlayer();
+        $week = $this->getWeek();
+        $game = $this->getGame($player, $week);
+
         /** @var PlayerSpirit $playerSpirit */
         $playerSpirit = PlayerSpirit::query()->create(array_merge([
             'uuid' => (string) Str::uuid(),
-            'week_id' => $this->weekID ?: CurrentWeek::id(),
-            'player_id' => $this->playerID ?: $this->getPlayer()->id,
-            'game_id' => $this->gameID ?: $this->getGame()->id,
+            'week_id' => $week->id,
+            'player_id' => $player->id,
+            'game_id' => $game->id,
             'essence_cost' => 5000,
             'energy' => PlayerSpirit::STARTING_ENERGY
         ], $extra));
 
-        return $playerSpirit;
+        if ($this->playerGameLogFactory) {
+            $playerGameLog = $this->playerGameLogFactory
+                ->forPlayer($player)
+                ->forTeam($player->team)
+                ->forGame($game)
+                ->create();
+            $playerSpirit->player_game_log_id = $playerGameLog->id;
+            $playerSpirit->save();
+        }
+
+        return $playerSpirit->fresh();
     }
 
     /**
@@ -44,15 +67,24 @@ class PlayerSpiritFactory
      */
     protected function getPlayer()
     {
+        if ($this->player) {
+            return $this->player;
+        }
         return factory(Player::class)->create();
     }
 
     /**
+     * @param Player $player
+     * @param Week $week
      * @return Game
      */
-    protected function getGame()
+    protected function getGame(Player $player, Week $week)
     {
-        return factory(Game::class)->create();
+        if ($this->game) {
+            return $this->game;
+        }
+        $gameFactory = $this->gameFactory ?: GameFactory::new();
+        return $gameFactory->forEitherTeam($player->team)->forWeek($week)->create();
     }
 
     /**
@@ -60,13 +92,23 @@ class PlayerSpiritFactory
      */
     protected function getWeek()
     {
+        if ($this->week) {
+            return $this->week;
+        }
         return factory(Week::class)->create();
     }
 
     public function forCurrentWeek()
     {
         $clone = clone $this;
-        $this->weekID = CurrentWeek::id();
+        $this->week = CurrentWeek::get();
+        return $clone;
+    }
+
+    public function withPlayerGameLog(PlayerGameLogFactory $playerGameLogFactory = null)
+    {
+        $clone = clone $this;
+        $clone->playerGameLogFactory = $playerGameLogFactory ?: PlayerGameLogFactory::new();
         return $clone;
     }
 }
