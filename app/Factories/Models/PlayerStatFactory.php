76acabd7a4db2df638f3723d4c6cf069311a7a71
Brian Rice <bwrice83@gmail.com>
2020-02-10 10:40:59 -0500

    create PlayerStatFactory

---
+++ app/Factories/Models/PlayerStatFactory.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace App\Factories\Models;
+
+
+use App\Domain\Models\Player;
+use App\Domain\Models\PlayerGameLog;
+use App\Domain\Models\PlayerStat;
+use App\Domain\Models\StatType;
+
+class PlayerStatFactory
+{
+    /** @var PlayerGameLog */
+    protected $playerGameLog;
+
+    /** @var StatType */
+    protected $statType;
+
+    public static function new()
+    {
+        return new self();
+    }
+
+    public function create(array $extra = [])
+    {
+        $playerGameLog = $this->getPlayerGameLog();
+        $statType = $this->getStatType($playerGameLog);
+        $amount = $this->getAmount($statType);
+
+        /** @var PlayerStat $playerStat */
+        $playerStat = PlayerStat::query()->create(array_merge([
+            'player_game_log_id' => $playerGameLog->id,
+            'stat_type_id' => $statType->id,
+            'amount' => $amount
+        ], $extra));
+        return $playerStat;
+    }
+
+    public function forGameLog(PlayerGameLog $gameLog)
+    {
+        $clone = clone $this;
+        $clone->playerGameLog = $gameLog;
+        return $clone;
+    }
+
+    public function forStatType(StatType $statType)
+    {
+        $clone = clone $this;
+        $clone->statType = $statType;
+        return $clone;
+    }
+
+    protected function getPlayerGameLog()
+    {
+        if ($this->playerGameLog) {
+            return $this->playerGameLog;
+        }
+        return PlayerGameLogFactory::new()->create();
+    }
+
+    protected function getStatType(PlayerGameLog $playerGameLog)
+    {
+        if ($this->statType) {
+            return $this->statType;
+        }
+
+        $sport = $playerGameLog->player->team->league->sport;
+        return $sport->statTypes()->inRandomOrder()->first();
+    }
+
+    protected function getAmount(StatType $statType)
+    {
+        $pointsPer = $statType->getBehavior()->getPointsPer();
+        if ($pointsPer < 0) {
+            return 1;
+        }
+        $topRange = (int) ceil(100/$pointsPer);
+        $rand = rand(1, $topRange);
+        return (int) ceil($rand/5);
+    }
+}
