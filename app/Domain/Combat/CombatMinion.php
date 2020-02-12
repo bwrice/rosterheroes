9d72ad80e0b3b9dd08c0c45214a85eb92aae7abb
Brian Rice <bwrice83@gmail.com>
2020-02-11 19:52:20 -0500

    implement Combatant in CombatMinion

--- app/Domain/Combat/CombatMinion.php
+++ app/Domain/Combat/CombatMinion.php
@@ -4,10 +4,105 @@
 namespace App\Domain\Combat;
 
 
-class CombatMinion
+use App\Domain\Models\CombatPosition;
+use Illuminate\Support\Collection;
+
+class CombatMinion implements Combatant
 {
-    public function getCombatAttacks()
+    /**
+     * @var int
+     */
+    protected $minionID;
+    /**
+     * @var int
+     */
+    protected $initialHealth;
+    /**
+     * @var int
+     */
+    protected $currentHealth;
+    /**
+     * @var int
+     */
+    protected $protection;
+    /**
+     * @var int
+     */
+    protected $blockChancePercent;
+    /**
+     * @var CombatPosition
+     */
+    protected $combatPosition;
+    /**
+     * @var Collection
+     */
+    protected $combatAttacks;
+
+    public function __construct(
+        int $minionID,
+        int $initialHealth,
+        int $protection,
+        int $blockChancePercent,
+        CombatPosition $combatPosition,
+        Collection $combatAttacks)
     {
-        return collect();
+        $this->minionID = $minionID;
+        $this->currentHealth = $this->initialHealth = $initialHealth;
+        $this->protection = $protection;
+        $this->blockChancePercent = $blockChancePercent;
+        $this->combatPosition = $combatPosition;
+        $this->combatAttacks = $combatAttacks;
+    }
+
+    /**
+     * @param int $initialDamage
+     * @return int
+     */
+    public function calculateDamageToReceive(int $initialDamage): int
+    {
+        // TODO: Implement calculateDamageToReceive() method.
+    }
+
+    /**
+     * @param int $damage
+     * @return mixed
+     */
+    public function receiveDamage(int $damage)
+    {
+        // TODO: Implement receiveDamage() method.
+    }
+
+    /**
+     * @param CombatAttack $combatAttack
+     * @return mixed
+     */
+    public function attackBlocked(CombatAttack $combatAttack)
+    {
+        // TODO: Implement attackBlocked() method.
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrentHealth(): int
+    {
+        // TODO: Implement getCurrentHealth() method.
+    }
+
+    /**
+     * @return float
+     */
+    public function getThreatLevel(): float
+    {
+        // TODO: Implement getThreatLevel() method.
+    }
+
+    /**
+     * @param CombatPosition $combatPositionToCompare
+     * @return bool
+     */
+    public function hasCombatPosition(CombatPosition $combatPositionToCompare): bool
+    {
+        // TODO: Implement hasCombatPosition() method.
     }
 }
