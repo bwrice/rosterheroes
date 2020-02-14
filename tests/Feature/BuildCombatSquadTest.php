a1743df74b090545e861785386134202a85f0165
Brian Rice <bwrice83@gmail.com>
2020-02-13 22:59:30 -0500

    test only combat squad created inside BuildCombatSquadTest

--- tests/Feature/BuildCombatSquadTest.php
+++ tests/Feature/BuildCombatSquadTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Domain\Actions\Combat\BuildCombatHero;
 use App\Domain\Actions\Combat\BuildCombatSquad;
 use App\Domain\Combat\CombatHero;
 use App\Domain\Combat\CombatSquad;
@@ -12,6 +13,7 @@ use App\Domain\Models\Squad;
 use App\Domain\Models\Week;
 use App\Exceptions\BuildCombatSquadException;
 use App\Facades\HeroService;
+use App\Factories\Combat\CombatHeroFactory;
 use App\Factories\Models\HeroFactory;
 use App\Factories\Models\PlayerFactory;
 use App\Factories\Models\PlayerGameLogFactory;
@@ -38,14 +40,15 @@ class BuildCombatSquadTest extends TestCase
         parent::setUp();
         $this->currentWeek = factory(Week::class)->states('as-current', 'finalizing')->create();
         $this->squad = SquadFactory::new()->create();
-        $player = PlayerFactory::new()->withPosition()->create();
-        $playerGameLogFactory = PlayerGameLogFactory::new()->forPlayer($player)->withStats();
-        $playerSpiritFactory = PlayerSpiritFactory::new()->withPlayerGameLog($playerGameLogFactory);
-        $this->heroFactory = HeroFactory::new()
-            ->forSquad($this->squad)
-            ->withMeasurables()
-            ->withItems()
-            ->withPlayerSpirit($playerSpiritFactory);
+        $this->heroFactory = HeroFactory::new()->forSquad($this->squad);
+//        $player = PlayerFactory::new()->withPosition()->create();
+//        $playerGameLogFactory = PlayerGameLogFactory::new()->forPlayer($player)->withStats();
+//        $playerSpiritFactory = PlayerSpiritFactory::new()->withPlayerGameLog($playerGameLogFactory);
+//        $this->heroFactory = HeroFactory::new()
+//            ->forSquad($this->squad)
+//            ->withMeasurables()
+//            ->withItems()
+//            ->withPlayerSpirit($playerSpiritFactory);
     }
 
     /**
@@ -53,7 +56,7 @@ class BuildCombatSquadTest extends TestCase
      */
     public function it_will_throw_an_exception_if_there_are_no_combat_ready_heroes()
     {
-        $hero = HeroFactory::new()->forSquad($this->squad)->create();
+        $hero = $this->heroFactory->create();
         HeroService::partialMock()->shouldReceive('combatReady')->andReturn(false);
         /** @var BuildCombatSquad $domainAction */
         $domainAction = app(BuildCombatSquad::class);
@@ -71,30 +74,24 @@ class BuildCombatSquadTest extends TestCase
      */
     public function it_will_create_a_combat_squad()
     {
-        $heroCount = 0;
-        $heroes = collect();
-        foreach([
-            HeroRace::HUMAN => HeroClass::RANGER,
-            HeroRace::ELF => HeroClass::SORCERER,
-            HeroRace::DWARF => HeroClass::WARRIOR,
-            HeroRace::ORC => HeroClass::WARRIOR,
-                ] as $heroRaceName =>  $heroClassName) {
-            $heroCount++;
-            $heroes->push($this->heroFactory->heroRace($heroRaceName)->heroClass($heroClassName)->create());
-        }
+        $hero1 = $this->heroFactory->create();
+        $hero2 = $this->heroFactory->create();
+        HeroService::partialMock()->shouldReceive('combatReady')->andReturn(true);
+
+        $combatHeroFactory = CombatHeroFactory::new();
+        $buildCombatHeroMock = \Mockery::mock(BuildCombatHero::class)
+            ->shouldReceive('execute')
+            ->andReturn($combatHeroFactory->create())
+            ->getMock();
+
+        app()->instance(BuildCombatHero::class, $buildCombatHeroMock);
+
         /** @var BuildCombatSquad $domainAction */
         $domainAction = app(BuildCombatSquad::class);
         $combatSquad = $domainAction->execute($this->squad->fresh());
         $this->assertTrue($combatSquad instanceof CombatSquad);
         $this->assertEquals($this->squad->id, $combatSquad->getSquadID());
         $combatHeroes = $combatSquad->getCombatHeroes();
-        $this->assertEquals($heroCount, $combatHeroes->count());
-        $combatHeroes->each(function (CombatHero $combatHero) use ($heroes) {
-            $match = $heroes->first(function (Hero $hero) use ($combatHero) {
-                return $hero->id === $combatHero->getHeroID();
-            });
-            $this->assertNotNull($match);
-            $this->assertGreaterThan(0, $combatHero->getCombatAttacks()->count());
-        });
+        $this->assertEquals(2, $combatHeroes->count());
     }
 }
