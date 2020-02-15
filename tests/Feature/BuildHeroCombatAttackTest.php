f0c774a4608c8c7e1da7be00d64158dbef4768a1
Brian Rice <bwrice83@gmail.com>
2020-02-14 22:34:50 -0500

    test BuildHeroCombatAttack

---
+++ tests/Feature/BuildHeroCombatAttackTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Domain\Actions\Combat\BuildHeroCombatAttack;
+use App\Domain\Combat\CombatAttack;
+use App\Factories\Models\AttackFactory;
+use App\Factories\Models\HeroFactory;
+use App\Factories\Models\ItemFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BuildHeroCombatAttackTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_will_build_a_hero_combat_attack()
+    {
+        $hero = HeroFactory::new()->create();
+        $item = ItemFactory::new()->create();
+        $attack = AttackFactory::new()->create();
+        $fantasyPower = rand(5, 50);
+        /** @var BuildHeroCombatAttack $domainAction */
+        $domainAction = app(BuildHeroCombatAttack::class);
+        $heroCombatAttack = $domainAction->execute($attack, $item, $hero, $fantasyPower);
+        $this->assertTrue($heroCombatAttack instanceof CombatAttack);
+    }
+}
