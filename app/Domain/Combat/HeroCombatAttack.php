cdd59d0f9c4d7dac2265e5c16b860451d01dc50f
Brian Rice <bwrice83@gmail.com>
2020-02-16 01:09:09 -0500

    convert HeroCombatAttack into CombatAttack decorator

--- app/Domain/Combat/HeroCombatAttack.php
+++ app/Domain/Combat/HeroCombatAttack.php
@@ -5,17 +5,9 @@ namespace App\Domain\Combat;
 
 
 use App\Domain\Collections\CombatantCollection;
-use App\Domain\Collections\ResourceCostsCollection;
-use App\Domain\Models\CombatPosition;
-use App\Domain\Models\DamageType;
-use App\Domain\Models\TargetPriority;
 
 class HeroCombatAttack implements CombatAttackInterface
 {
-    /**
-     * @var string
-     */
-    protected $name;
     /**
      * @var int
      */
@@ -25,86 +17,51 @@ class HeroCombatAttack implements CombatAttackInterface
      */
     protected $itemID;
     /**
-     * @var int
+     * @var CombatAttack
      */
-    protected $attackID;
-    /**
-     * @var int
-     */
-    protected $damage;
-    /**
-     * @var int
-     */
-    protected $grade;
-    /**
-     * @var float
-     */
-    protected $speed;
-    /**
-     * @var CombatPosition
-     */
-    protected $attackerPosition;
-    /**
-     * @var CombatPosition
-     */
-    protected $targetPosition;
-    /**
-     * @var DamageType
-     */
-    protected $damageType;
-    /**
-     * @var TargetPriority
-     */
-    protected $targetPriority;
-    /**
-     * @var ResourceCostsCollection
-     */
-    protected $resourceCostsCollection;
-    /**
-     * @var int|null
-     */
-    protected $maxTargetsCount;
+    protected $combatAttack;
 
     public function __construct(
-        string $name,
         int $heroID,
         int $itemID,
-        int $attackID,
-        int $damage,
-        int $grade,
-        float $speed,
-        CombatPosition $attackerPosition,
-        CombatPosition $targetPosition,
-        TargetPriority $targetPriority,
-        DamageType $damageType,
-        ResourceCostsCollection $resourceCostsCollection,
-        int $maxTargetsCount)
+        CombatAttack $combatAttack)
     {
-        $this->name = $name;
         $this->heroID = $heroID;
         $this->itemID = $itemID;
-        $this->attackID = $attackID;
-        $this->damage = $damage;
-        $this->grade = $grade;
-        $this->speed = $speed;
-        $this->attackerPosition = $attackerPosition;
-        $this->targetPosition = $targetPosition;
-        $this->damageType = $damageType;
-        $this->targetPriority = $targetPriority;
-        $this->resourceCostsCollection = $resourceCostsCollection;
-        $this->maxTargetsCount = $maxTargetsCount;
+        $this->combatAttack = $combatAttack;
     }
 
     public function getDamagePerTarget(int $targetsCount): int
     {
-        return $this->damageType->getBehavior()->getDamagePerTarget($this->damage, $targetsCount);
+        return $this->combatAttack->getDamagePerTarget($targetsCount);
     }
 
     public function getTargets(CombatantCollection $possibleTargets): CombatantCollection
     {
-        return $possibleTargets
-            ->filterByCombatPosition($this->targetPosition)
-            ->sortByTargetPriority($this->targetPriority)
-            ->take($this->maxTargetsCount);
+        return $this->combatAttack->getTargets($possibleTargets);
+    }
+
+    /**
+     * @return int
+     */
+    public function getHeroID(): int
+    {
+        return $this->heroID;
+    }
+
+    /**
+     * @return int
+     */
+    public function getItemID(): int
+    {
+        return $this->itemID;
+    }
+
+    /**
+     * @return CombatAttack
+     */
+    public function getCombatAttack(): CombatAttack
+    {
+        return $this->combatAttack;
     }
 }
