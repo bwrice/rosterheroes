b1acafb43c768e6b092586f8a0841e20d0cf719b
Brian Rice <bwrice83@gmail.com>
2020-02-16 12:07:20 -0500

    test BuildCombatMinion domain action

---
+++ tests/Feature/BuildCombatMinionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Domain\Actions\Combat\BuildCombatAttack;
+use App\Domain\Actions\Combat\BuildCombatMinion;
+use App\Domain\Combat\Combatant;
+use App\Domain\Combat\CombatMinion;
+use App\Factories\Combat\CombatAttackFactory;
+use App\Factories\Models\MinionFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BuildCombatMinionTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_will_correctly_build_a_combat_minion()
+    {
+        $minion = MinionFactory::new()->withAttacks()->create();
+
+        $combatAttack = CombatAttackFactory::new()->create();
+        $buildCombatAttackMock = \Mockery::mock(BuildCombatAttack::class)
+            ->shouldReceive('execute')
+            ->andReturn($combatAttack)
+            ->getMock();
+
+        app()->instance(BuildCombatAttack::class, $buildCombatAttackMock);
+
+        /** @var BuildCombatMinion $domainAction */
+        $domainAction = app(BuildCombatMinion::class);
+        $combatMinion = $domainAction->execute($minion);
+        $this->assertTrue($combatMinion instanceof CombatMinion);
+        $this->assertTrue($combatMinion instanceof Combatant);
+        $minionAttacksCount = $minion->attacks->count();
+        $this->assertGreaterThan(0, $minionAttacksCount);
+        $this->assertEquals($minionAttacksCount, $combatMinion->getCombatAttacks()->count());
+    }
+}
