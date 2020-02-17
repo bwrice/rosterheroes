ebdcba723cbe99831d74908522348debcf2756c0
Brian Rice <bwrice83@gmail.com>
2020-02-17 15:08:21 -0500

    add closestProximity() method on CombatPositionCollection

--- app/Domain/Collections/CombatPositionCollection.php
+++ app/Domain/Collections/CombatPositionCollection.php
@@ -4,9 +4,18 @@
 namespace App\Domain\Collections;
 
 
+use App\Domain\Models\CombatPosition;
 use Illuminate\Database\Eloquent\Collection;
 
 class CombatPositionCollection extends Collection
 {
-
+    /**
+     * @return CombatPosition|null
+     */
+    public function closestProximity()
+    {
+        return $this->sortBy(function (CombatPosition $combatPosition) {
+            return $combatPosition->getBehavior()->getProximity();
+        })->first();
+    }
 }
