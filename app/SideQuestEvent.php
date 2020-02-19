b2a6fd10d1821d64cb2c9f31760e170fdfb6e8ad
Brian Rice <bwrice83@gmail.com>
2020-02-19 11:19:56 -0500

    create SideQuestEvent model and schema

---
+++ app/SideQuestEvent.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SideQuestEvent extends Model
+{
+    //
+}
---
+++ database/migrations/2020_02_18_225122_create_side_quest_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSideQuestEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('side_quest_events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('combat_event_type_id')->unsigned();
+            $table->bigInteger('side_quest_result_id')->unsigned();
+            $table->json('data');
+            $table->timestamps();
+        });
+
+        Schema::table('side_quest_events', function (Blueprint $table) {
+            $table->foreign('combat_event_type_id')->references('id')->on('combat_event_types');
+            $table->foreign('side_quest_result_id')->references('id')->on('side_quest_results');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('side_quest_events');
+    }
+}
