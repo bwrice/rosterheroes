ea579f61600f9319f5f5d92625553a547217ab7d
Brian Rice <bwrice83@gmail.com>
2020-02-19 17:46:54 -0500

    add relation between SideQuest models

--- app/SideQuestEvent.php
+++ app/SideQuestEvent.php
@@ -4,7 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class SideQuestEvent
+ * @package App
+ *
+ * @property SideQuestResult $sideQuestResult
+ */
 class SideQuestEvent extends Model
 {
-    //
+    public function sideQuestResult()
+    {
+        return $this->belongsTo(SideQuestResult::class);
+    }
 }
--- app/SideQuestResult.php
+++ app/SideQuestResult.php
@@ -2,9 +2,21 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class SideQuestResult
+ * @package App
+ *
+ * @property Collection $sideQuestEvents
+ */
 class SideQuestResult extends Model
 {
     protected $guarded = [];
+
+    public function sideQuestEvents()
+    {
+        return $this->hasMany(SideQuestEvent::class);
+    }
 }
