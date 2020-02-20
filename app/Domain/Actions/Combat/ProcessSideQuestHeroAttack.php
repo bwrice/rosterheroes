a659041085c34b806815c7a88467a690885dd38a
Brian Rice <bwrice83@gmail.com>
2020-02-19 22:25:59 -0500

    return kill and block booleans with data

--- app/Domain/Actions/Combat/ProcessSideQuestHeroAttack.php
+++ app/Domain/Actions/Combat/ProcessSideQuestHeroAttack.php
@@ -11,28 +11,42 @@ use App\SideQuestResult;
 
 class ProcessSideQuestHeroAttack
 {
+    /**
+     * @param SideQuestResult $sideQuestResult
+     * @param int $moment
+     * @param int $damageReceived
+     * @param HeroCombatAttack $heroCombatAttack
+     * @param CombatMinion $combatMinion
+     * @param $block
+     * @return SideQuestEvent
+     */
     public function execute(SideQuestResult $sideQuestResult, int $moment, int $damageReceived, HeroCombatAttack $heroCombatAttack, CombatMinion $combatMinion, $block)
     {
         if ($block) {
-
+            $kill = false;
         } else {
-            SideQuestEvent::query()->create([
-                'side_quest_result_id' => $sideQuestResult->id,
-                'moment' => $moment,
-                'data' => [
-                    'type' => 'attack',
-                    'damage' => $damageReceived,
-                    'attacker' => [
-                        'type' => 'hero',
-                        'hero_id' => $heroCombatAttack->getHeroID(),
-                        'item_id' => $heroCombatAttack->getItemID()
-                    ],
-                    'defender' => [
-                        'type' => 'minion',
-                        'minion_id' => $combatMinion->getMinionID()
-                    ]
-                ]
-            ]);
+            $kill = $combatMinion->getCurrentHealth() <= 0;
         }
+        /** @var SideQuestEvent $sideQuestEvent */
+        $sideQuestEvent = SideQuestEvent::query()->create([
+            'side_quest_result_id' => $sideQuestResult->id,
+            'moment' => $moment,
+            'data' => [
+                'type' => 'hero-attack',
+                'damage' => $damageReceived,
+                'block' => $block,
+                'kill' => $kill,
+                'attacker' => [
+                    'type' => 'hero',
+                    'hero_id' => $heroCombatAttack->getHeroID(),
+                    'item_id' => $heroCombatAttack->getItemID()
+                ],
+                'defender' => [
+                    'type' => 'minion',
+                    'minion_id' => $combatMinion->getMinionID()
+                ]
+            ]
+        ]);
+        return $sideQuestEvent;
     }
 }
