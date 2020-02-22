2b336b8cad1c10533a9ab04df01c1e5b878c6251
Brian Rice <bwrice83@gmail.com>
2020-02-22 00:08:38 -0500

    pass reference model collections to BuildMinionCombatAttack

--- app/Domain/Actions/Combat/BuildMinionCombatAttack.php
+++ app/Domain/Actions/Combat/BuildMinionCombatAttack.php
@@ -7,6 +7,7 @@ namespace App\Domain\Actions\Combat;
 use App\Domain\Combat\Attacks\MinionCombatAttack;
 use App\Domain\Models\Attack;
 use App\Domain\Models\Minion;
+use Illuminate\Database\Eloquent\Collection;
 
 class BuildMinionCombatAttack
 {
@@ -23,10 +24,19 @@ class BuildMinionCombatAttack
     /**
      * @param Attack $attack
      * @param Minion $minion
+     * @param Collection|null $combatPositions
+     * @param Collection|null $targetPriorities
+     * @param Collection|null $damageTypes
      * @return MinionCombatAttack
      */
-    public function execute(Attack $attack, Minion $minion)
+    public function execute(
+        Attack $attack,
+        Minion $minion,
+        Collection $combatPositions = null,
+        Collection $targetPriorities = null,
+        Collection $damageTypes = null)
     {
-        return new MinionCombatAttack($minion->id, $this->buildCombatAttack->execute($attack, $minion));
+        $combatAttack = $this->buildCombatAttack->execute($attack, $minion, $combatPositions, $targetPriorities, $damageTypes);
+        return new MinionCombatAttack($minion->id, $combatAttack);
     }
 }
