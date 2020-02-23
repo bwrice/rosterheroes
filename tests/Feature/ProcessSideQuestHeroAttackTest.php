35e09efe4ea12a6d759d60fc1e791605e0a29cb1
Brian Rice <bwrice83@gmail.com>
2020-02-23 14:40:57 -0500

    test hero damage dealt increased

--- tests/Feature/ProcessSideQuestHeroAttackTest.php
+++ tests/Feature/ProcessSideQuestHeroAttackTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Domain\Actions\Combat\ProcessSideQuestHeroAttack;
 use App\Domain\Combat\Combatants\CombatMinion;
 use App\Domain\Combat\Attacks\HeroCombatAttack;
+use App\Domain\Models\Hero;
 use App\Domain\Models\Item;
 use App\Factories\Combat\CombatMinionFactory;
 use App\Factories\Combat\HeroCombatAttackFactory;
@@ -115,5 +116,24 @@ class ProcessSideQuestHeroAttackTest extends TestCase
 
         $item = $item->fresh();
         $this->assertEquals($damageReceived, $item->damage_dealt);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_increase_damage_dealt_for_the_hero()
+    {
+        /** @var ProcessSideQuestHeroAttack $domainAction */
+        $domainAction = app(ProcessSideQuestHeroAttack::class);
+        $damageReceived = rand(10, 200);
+        $moment = rand(1, 99);
+
+        $hero = Hero::findUuid($this->heroCombatAttack->getHeroUuid());
+        $this->assertEquals(0, $hero->damage_dealt);
+
+        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->heroCombatAttack, $this->combatMinion, false);
+
+        $hero = $hero->fresh();
+        $this->assertEquals($damageReceived, $hero->damage_dealt);
     }
 }
