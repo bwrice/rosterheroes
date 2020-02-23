87a02d9107234ddc2e3df6cf761f0122dcfcf68c
Brian Rice <bwrice83@gmail.com>
2020-02-23 11:14:22 -0500

    use actual hero and item IDs in HeroCombatAttackFactory

--- app/Factories/Combat/HeroCombatAttackFactory.php
+++ app/Factories/Combat/HeroCombatAttackFactory.php
@@ -7,10 +7,20 @@ namespace App\Factories\Combat;
 use App\Domain\Collections\ResourceCostsCollection;
 use App\Domain\Combat\Combatants\CombatHero;
 use App\Domain\Combat\Attacks\HeroCombatAttack;
+use App\Domain\QueryBuilders\Filters\HeroRaceFilter;
+use App\Factories\Models\HeroFactory;
+use App\Factories\Models\ItemFactory;
 
 class HeroCombatAttackFactory
 {
+    /** @var CombatAttackFactory */
     protected $combatAttackFactory;
+
+    /** @var HeroFactory */
+    protected $heroFactory;
+
+    /** @var ItemFactory */
+    protected $itemFactory;
 
     public static function new()
     {
@@ -19,8 +29,8 @@ class HeroCombatAttackFactory
 
     public function create()
     {
-        $heroID = rand(1, 999999);
-        $itemID = rand(1, 999999);
+        $heroID = $this->getHeroID();
+        $itemID = $this->getItemID();
         $resourceCosts = new ResourceCostsCollection();
         $combatAttackFactory = $this->combatAttackFactory ?: CombatAttackFactory::new();
         $combatAttack = $combatAttackFactory->create();
@@ -37,5 +47,31 @@ class HeroCombatAttackFactory
         $clone = clone $this;
         $clone->combatAttackFactory = $combatAttackFactory;
         return $clone;
+    }
+
+    public function fromItemFactory(ItemFactory $itemFactory)
+    {
+        $clone = clone $this;
+        $clone->itemFactory = $itemFactory;
+        return $clone;
+    }
+
+    public function fromHeroFactory(HeroFactory $heroFactory)
+    {
+        $clone = clone $this;
+        $clone->heroFactory = $heroFactory;
+        return $clone;
+    }
+
+    protected function getItemID()
+    {
+        $itemFactory = $this->itemFactory ?: ItemFactory::new();
+        return $itemFactory->create()->id;
+    }
+
+    protected function getHeroID()
+    {
+        $heroFactory = $this->heroFactory ?: HeroFactory::new();
+        return $heroFactory->create()->id;
     }
 }
