e2accc67bb3d1bf0b209da2a6fe69f49b6cb8b55
Brian Rice <bwrice83@gmail.com>
2020-02-23 01:05:30 -0500

    extend AbstractCombatant inside CombatHero

--- app/Domain/Combat/Combatants/CombatHero.php
+++ app/Domain/Combat/Combatants/CombatHero.php
@@ -4,21 +4,15 @@
 namespace App\Domain\Combat\Combatants;
 
 
-use App\Domain\Combat\Attacks\CombatAttackInterface;
-use App\Domain\Combat\Combatants\Combatant;
 use App\Domain\Models\CombatPosition;
 use Illuminate\Support\Collection;
 
-class CombatHero implements Combatant
+class CombatHero extends AbstractCombatant
 {
     /**
      * @var int
      */
     protected $heroID;
-    /**
-     * @var int
-     */
-    protected $initialHealth;
     /**
      * @var int
      */
@@ -30,36 +24,11 @@ class CombatHero implements Combatant
     /**
      * @var int
      */
-    protected $currentHealth;
-    /**
-     * @var int
-     */
     protected $currentStamina;
     /**
      * @var int
      */
     protected $currentMana;
-    /**
-     * @var int
-     */
-    protected $protection;
-    /**
-     * @var float
-     */
-    protected $blockChancePercent;
-    /**
-     * @var CombatPosition
-     */
-    protected $initialCombatPosition;
-    /**
-     * @var Collection
-     */
-    protected $combatAttacks;
-
-    /**
-     * @var Collection
-     */
-    protected $inheritedCombatPositions;
 
     public function __construct(
         int $heroID,
@@ -72,74 +41,21 @@ class CombatHero implements Combatant
         Collection $combatAttacks)
     {
         $this->heroID = $heroID;
-        $this->initialHealth = $this->currentHealth = $health;
         $this->initialStamina = $this->currentStamina = $stamina;
         $this->initialMana = $this->currentMana = $mana;
-        $this->protection = $protection;
-        $this->blockChancePercent = $blockChancePercent;
-        $this->initialCombatPosition = $combatPosition;
-        $this->combatAttacks = $combatAttacks;
-        $this->inheritedCombatPositions = collect();
-    }
-
-    public function calculateDamageToReceive(int $initialDamage): int
-    {
-        $multiplier = 500 / (500 + $this->protection);
-        return (int) ceil($multiplier * $initialDamage);
-    }
-
-    /**
-     * @param int $damage
-     * @return mixed|void
-     */
-    public function receiveDamage(int $damage)
-    {
-        $this->currentHealth = (int) max($this->currentHealth - $damage, 0);
-    }
-
-    /**
-     * @param CombatAttackInterface $combatAttack
-     * @return bool|mixed
-     * @throws \Exception
-     */
-    public function attackBlocked(CombatAttackInterface $combatAttack)
-    {
-        $rand = random_int(1, 100);
-        return $rand <= $this->blockChancePercent;
-    }
-
-    /**
-     * @return int
-     */
-    public function getCurrentHealth(): int
-    {
-        return $this->currentHealth;
-    }
-
-    /**
-     * @return float
-     */
-    public function getThreatLevel(): float
-    {
-        return $this->getDPS();
+        parent::__construct(
+            $health,
+            $protection,
+            $blockChancePercent,
+            $combatPosition,
+            $combatAttacks
+        );
     }
 
     protected function getDPS()
     {
         // TODO
         return 1;
-    }
-
-    /**
-     * @param CombatPosition $combatPositionToCompare
-     * @return bool
-     */
-    public function hasCombatPosition(CombatPosition $combatPositionToCompare): bool
-    {
-        $match = $this->allCombatPositions()->first(function (CombatPosition $combatPosition) use ($combatPositionToCompare) {
-            return $combatPosition->id === $combatPositionToCompare->id;
-        });
-        return ! is_null($match);
     }
 
     public function allCombatPositions()
