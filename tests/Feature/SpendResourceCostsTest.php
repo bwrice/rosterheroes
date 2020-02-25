71b5c6c89826b7b23a31313c23a32b372d1b6708
Brian Rice <bwrice83@gmail.com>
2020-02-25 15:55:31 -0500

    test percentage ResourceCosts for SpendResourceCosts domain action

--- tests/Feature/SpendResourceCostsTest.php
+++ tests/Feature/SpendResourceCostsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Domain\Actions\Combat\SpendResourceCosts;
 use App\Domain\Models\Json\ResourceCosts\FixedResourceCost;
+use App\Domain\Models\Json\ResourceCosts\PercentResourceCost;
 use App\Domain\Models\MeasurableType;
 use App\Factories\Combat\CombatHeroFactory;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -44,5 +45,35 @@ class SpendResourceCostsTest extends TestCase
         $result = $domainAction->execute($combatHero, $resourceCost);
         $this->assertEquals($cost, $result['mana_cost']);
         $this->assertEquals($mana - $cost, $combatHero->getCurrentMana());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_update_percent_costs_for_a_combat_hero()
+    {
+        $stamina = 200;
+        $combatHero = CombatHeroFactory::new()->withStamina($stamina)->create();
+        $percent = 25;
+        $resourceCost = new PercentResourceCost(MeasurableType::STAMINA, $percent);
+
+        /** @var SpendResourceCosts $domainAction */
+        $domainAction = app(SpendResourceCosts::class);
+        $result = $domainAction->execute($combatHero, $resourceCost);
+        $this->assertEquals(50, $result['stamina_cost']);
+        $this->assertEquals(150, $combatHero->getCurrentStamina());
+
+
+        // Mana
+        $mana = 200;
+        $combatHero = CombatHeroFactory::new()->withMana($mana)->create();
+        $percent = 25;
+        $resourceCost = new PercentResourceCost(MeasurableType::MANA, $percent);
+
+        /** @var SpendResourceCosts $domainAction */
+        $domainAction = app(SpendResourceCosts::class);
+        $result = $domainAction->execute($combatHero, $resourceCost);
+        $this->assertEquals(50, $result['mana_cost']);
+        $this->assertEquals(150, $combatHero->getCurrentMana());
     }
 }
