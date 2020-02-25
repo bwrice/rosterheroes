de9e7fae4f466a527b8cfdf0b86a9317ef3da156
Brian Rice <bwrice83@gmail.com>
2020-02-24 21:07:52 -0500

    break side quest creation events into separate methods inside SideQuestEventAggregate

--- app/Aggregates/SideQuestEventAggregate.php
+++ app/Aggregates/SideQuestEventAggregate.php
@@ -2,14 +2,50 @@
 
 namespace App\Aggregates;
 
-use App\StorableEvents\SideQuestEventCreated;
+use App\StorableEvents\HeroDamagesMinionSideQuestEvent;
+use App\StorableEvents\HeroKillsMinionSideQuestEvent;
+use App\StorableEvents\MinionBlocksHeroSideQuestEvent;
 use Spatie\EventSourcing\AggregateRoot;
 
 final class SideQuestEventAggregate extends AggregateRoot
 {
-    public function createSideQuestEvent(int $sideQuestResultID, int $moment, array $data)
+    public function createHeroDamagesMinionEvent(
+        int $sideQuestResultID,
+        int $moment,
+        string $heroUuid,
+        int $attackID,
+        string $itemUuid,
+        string $minionUuid,
+        int $damage
+    )
     {
-        $this->recordThat(new SideQuestEventCreated($sideQuestResultID, $moment, $data));
+        $this->recordThat(new HeroDamagesMinionSideQuestEvent($sideQuestResultID, $moment, $heroUuid, $attackID, $itemUuid, $minionUuid, $damage));
+        return $this;
+    }
+
+    public function createHeroKillsMinionEvent(
+        int $sideQuestResultID,
+        int $moment,
+        string $heroUuid,
+        int $attackID,
+        string $itemUuid,
+        string $minionUuid,
+        int $damage
+    ) {
+        $this->recordThat(new HeroKillsMinionSideQuestEvent($sideQuestResultID, $moment, $heroUuid, $attackID, $itemUuid, $minionUuid, $damage));
+        return $this;
+    }
+
+    public function createMinionBlocksHeroEvent(
+        int $sideQuestResultID,
+        int $moment,
+        string $heroUuid,
+        int $attackID,
+        string $itemUuid,
+        string $minionUuid
+    )
+    {
+        $this->recordThat(new MinionBlocksHeroSideQuestEvent($sideQuestResultID, $moment, $heroUuid, $attackID, $itemUuid, $minionUuid));
         return $this;
     }
 }
