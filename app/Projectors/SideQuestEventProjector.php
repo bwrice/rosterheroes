d259033346665a277dea769b15919c90dc7b4b07
Brian Rice <bwrice83@gmail.com>
2020-02-24 20:55:29 -0500

    handle all three types of Hero attacks Minion storable events in SideQuestEventProjector

--- app/Projectors/SideQuestEventProjector.php
+++ app/Projectors/SideQuestEventProjector.php
@@ -3,7 +3,9 @@
 namespace App\Projectors;
 
 use App\SideQuestEvent;
-use App\StorableEvents\SideQuestEventCreated;
+use App\StorableEvents\HeroDamagesMinionSideQuestEvent;
+use App\StorableEvents\HeroKillsMinionSideQuestEvent;
+use App\StorableEvents\MinionBlocksHeroSideQuestEvent;
 use Spatie\EventSourcing\Projectors\Projector;
 use Spatie\EventSourcing\Projectors\ProjectsEvents;
 
@@ -11,13 +13,53 @@ final class SideQuestEventProjector implements Projector
 {
     use ProjectsEvents;
 
-    public function onEventHappened(SideQuestEventCreated $event, string $aggregateUuid)
+    public function onHeroDamagesMinion(HeroDamagesMinionSideQuestEvent $event, string $aggregateUuid)
     {
         SideQuestEvent::query()->create([
             'uuid' => $aggregateUuid,
             'side_quest_result_id' => $event->sideQuestResultID,
             'moment' => $event->moment,
-            'data' => $event->data
+            'event_type' => SideQuestEvent::TYPE_HERO_DAMAGES_MINION,
+            'data' => [
+                'attackID' => $event->attackID,
+                'heroUuid' => $event->heroUuid,
+                'itemUuid' => $event->itemUuid,
+                'minionUuid' => $event->minionUuid,
+                'damage' => $event->damage
+            ]
+        ]);
+    }
+
+    public function onHeroKillsMinion(HeroKillsMinionSideQuestEvent $event, string $aggregateUuid)
+    {
+        SideQuestEvent::query()->create([
+            'uuid' => $aggregateUuid,
+            'side_quest_result_id' => $event->sideQuestResultID,
+            'moment' => $event->moment,
+            'event_type' => SideQuestEvent::TYPE_HERO_KILLS_MINION,
+            'data' => [
+                'attackID' => $event->attackID,
+                'heroUuid' => $event->heroUuid,
+                'itemUuid' => $event->itemUuid,
+                'minionUuid' => $event->minionUuid,
+                'damage' => $event->damage
+            ]
+        ]);
+    }
+
+    public function onMinionBlocksHero(MinionBlocksHeroSideQuestEvent $event, string $aggregateUuid)
+    {
+        SideQuestEvent::query()->create([
+            'uuid' => $aggregateUuid,
+            'side_quest_result_id' => $event->sideQuestResultID,
+            'moment' => $event->moment,
+            'event_type' => SideQuestEvent::TYPE_MINION_BLOCKS_HERO,
+            'data' => [
+                'attackID' => $event->attackID,
+                'heroUuid' => $event->heroUuid,
+                'itemUuid' => $event->itemUuid,
+                'minionUuid' => $event->minionUuid
+            ]
         ]);
     }
 }
