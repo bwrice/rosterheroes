823fb6169c1a55af852efff5db4b289bcf152b74
Brian Rice <bwrice83@gmail.com>
2020-02-25 15:38:15 -0500

    create SpendResourceCosts domain action

---
+++ app/Domain/Actions/Combat/SpendResourceCosts.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Domain\Actions\Combat;
+
+
+use App\Domain\Interfaces\SpendsResources;
+use App\Domain\Models\Json\ResourceCosts\ResourceCost;
+
+class SpendResourceCosts
+{
+    /**
+     * @param SpendsResources $spendsResources
+     * @param ResourceCost $resourceCost
+     * @return array
+     */
+    public function execute(SpendsResources $spendsResources, ResourceCost $resourceCost)
+    {
+        $staminaCost = $resourceCost->getStaminaCost($spendsResources);
+        $currentStamina = max(0, $spendsResources->getCurrentStamina() - $staminaCost);
+        $spendsResources->setCurrentStamina($currentStamina);
+        $manaCost = $resourceCost->getManCost($spendsResources);
+        $currentMana = max(0, $spendsResources->getCurrentMana() - $manaCost);
+        $spendsResources->setCurrentMana($currentMana);
+
+        return [
+            'stamina_cost' => $staminaCost,
+            'mana_cost' => $manaCost
+        ];
+    }
+}
