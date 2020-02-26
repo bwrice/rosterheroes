6a1385bb701750f84f0332987d030bbd4c163218
Brian Rice <bwrice83@gmail.com>
2020-02-26 01:11:16 -0500

    add constant types for minion attacks hero types in SideQuestEvent model

--- app/SideQuestEvent.php
+++ app/SideQuestEvent.php
@@ -21,6 +21,9 @@ class SideQuestEvent extends EventSourcedModel
     public const TYPE_HERO_DAMAGES_MINION = 'hero-damages-minion';
     public const TYPE_HERO_KILLS_MINION = 'hero-kills-minion';
     public const TYPE_MINION_BLOCKS_HERO = 'minion-blocks-hero';
+    public const TYPE_MINION_DAMAGES_HERO = 'minion-damages-hero';
+    public const TYPE_MINION_KILLS_HERO = 'minion-kills-hero';
+    public const TYPE_HERO_BLOCKS_MINION = 'hero-blocks-minion';
 
     protected $guarded = [];
     protected $casts = [
