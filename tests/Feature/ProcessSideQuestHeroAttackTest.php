3e7d2461bdff956e5d1479db5ceaddc2d0b2b571
Brian Rice <bwrice83@gmail.com>
2020-02-25 20:04:36 -0500

    add it_save_resource_costs_to_the_side_quest_event test

--- tests/Feature/ProcessSideQuestHeroAttackTest.php
+++ tests/Feature/ProcessSideQuestHeroAttackTest.php
@@ -3,10 +3,16 @@
 namespace Tests\Feature;
 
 use App\Domain\Actions\Combat\ProcessSideQuestHeroAttack;
+use App\Domain\Actions\Combat\SpendResourceCosts;
+use App\Domain\Collections\ResourceCostsCollection;
+use App\Domain\Combat\Combatants\CombatHero;
 use App\Domain\Combat\Combatants\CombatMinion;
 use App\Domain\Combat\Attacks\HeroCombatAttack;
 use App\Domain\Models\Hero;
 use App\Domain\Models\Item;
+use App\Domain\Models\Json\ResourceCosts\FixedResourceCost;
+use App\Domain\Models\MeasurableType;
+use App\Factories\Combat\CombatHeroFactory;
 use App\Factories\Combat\CombatMinionFactory;
 use App\Factories\Combat\HeroCombatAttackFactory;
 use App\Factories\Models\SideQuestResultFactory;
@@ -30,12 +36,16 @@ class ProcessSideQuestHeroAttackTest extends TestCase
     /** @var CombatMinion */
     protected $combatMinion;
 
+    /** @var CombatHero */
+    protected $combatHero;
+
     public function setUp(): void
     {
         parent::setUp();
         $this->sideQuestResult = SideQuestResultFactory::new()->create();
         $this->heroCombatAttack = HeroCombatAttackFactory::new()->create();
         $this->combatMinion = CombatMinionFactory::new()->create();
+        $this->combatHero = CombatHeroFactory::new()->create();
     }
 
     /**
@@ -49,7 +59,7 @@ class ProcessSideQuestHeroAttackTest extends TestCase
         $moment = rand(1, 99);
         $combatMinion = \Mockery::mock($this->combatMinion)->shouldReceive('getCurrentHealth')->andReturn(999)->getMock();
 
-        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->heroCombatAttack, $combatMinion, false);
+        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->combatHero, $this->heroCombatAttack, $combatMinion, false);
 
         $sideQuestEvents = $this->sideQuestResult->sideQuestEvents;
         $this->assertEquals(1, $sideQuestEvents->count());
@@ -70,7 +80,7 @@ class ProcessSideQuestHeroAttackTest extends TestCase
         $moment = rand(1, 99);
         $combatMinion = \Mockery::mock($this->combatMinion)->shouldReceive('getCurrentHealth')->andReturn(0)->getMock();
 
-        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->heroCombatAttack, $combatMinion, false);
+        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->combatHero, $this->heroCombatAttack, $combatMinion, false);
 
         $sideQuestEvents = $this->sideQuestResult->sideQuestEvents;
         $this->assertEquals(1, $sideQuestEvents->count());
@@ -90,7 +100,7 @@ class ProcessSideQuestHeroAttackTest extends TestCase
         $damageReceived = rand(10, 200);
         $moment = rand(1, 99);
 
-        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->heroCombatAttack, $this->combatMinion, true);
+        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->combatHero, $this->heroCombatAttack, $this->combatMinion, true);
 
         $sideQuestEvents = $this->sideQuestResult->sideQuestEvents;
         $this->assertEquals(1, $sideQuestEvents->count());
@@ -116,7 +126,7 @@ class ProcessSideQuestHeroAttackTest extends TestCase
         $this->assertEquals(0, $item->damage_dealt);
 
         $combatMinion = \Mockery::mock($this->combatMinion)->shouldReceive('getCurrentHealth')->andReturn($minionHealthReturned)->getMock();
-        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->heroCombatAttack, $combatMinion, false);
+        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->combatHero, $this->heroCombatAttack, $combatMinion, false);
 
         $item = $item->fresh();
         $this->assertEquals($damageReceived, $item->damage_dealt);
@@ -138,7 +148,7 @@ class ProcessSideQuestHeroAttackTest extends TestCase
         $this->assertEquals(0, $hero->damage_dealt);
 
         $combatMinion = \Mockery::mock($this->combatMinion)->shouldReceive('getCurrentHealth')->andReturn($minionHealthReturned)->getMock();
-        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->heroCombatAttack, $combatMinion, false);
+        $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->combatHero, $this->heroCombatAttack, $combatMinion, false);
 
         $hero = $hero->fresh();
         $this->assertEquals($damageReceived, $hero->damage_dealt);
@@ -152,6 +162,64 @@ class ProcessSideQuestHeroAttackTest extends TestCase
             ],
             'kill' => [
                 'minionHealthReturned' => 0
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     * @param $minionHealthReturned
+     * @param $block
+     * @dataProvider provides_it_save_resource_costs_to_the_side_quest_event
+     */
+    public function it_save_resource_costs_to_the_side_quest_event($minionHealthReturned, $block)
+    {
+        $resourceCost = new FixedResourceCost(MeasurableType::STAMINA, 50);
+        // Contents of the collection (besides the count) don't matter because we're mocking the return value
+        $heroCombatAttack = HeroCombatAttackFactory::new()->withResourceCosts(new ResourceCostsCollection([
+            $resourceCost,
+            $resourceCost
+        ]))->create();
+
+        $staminaCost = rand(5, 20);
+        $manaCost = rand(5, 20);
+
+        $spendResourcesMock = \Mockery::mock(SpendResourceCosts::class)
+            ->shouldReceive('execute')
+            ->times(2)
+            ->andReturn([
+                'stamina_cost' => $staminaCost,
+                'mana_cost' => $manaCost
+            ])
+            ->getMock();
+
+        app()->instance(SpendResourceCosts::class, $spendResourcesMock);
+
+        /** @var ProcessSideQuestHeroAttack $domainAction */
+        $domainAction = app(ProcessSideQuestHeroAttack::class);
+        $damageReceived = rand(10, 200);
+        $moment = rand(1, 99);
+
+        $combatMinion = \Mockery::mock($this->combatMinion)->shouldReceive('getCurrentHealth')->andReturn($minionHealthReturned)->getMock();
+        $sideQuestEvent = $domainAction->execute($this->sideQuestResult, $moment, $damageReceived, $this->combatHero, $heroCombatAttack, $combatMinion, $block);
+        $this->assertEquals(2 * $staminaCost, $sideQuestEvent->data['stamina_cost']);
+        $this->assertEquals(2 * $manaCost, $sideQuestEvent->data['mana_cost']);
+    }
+
+    public function provides_it_save_resource_costs_to_the_side_quest_event()
+    {
+        return [
+            'block' => [
+                'minionHealthReturned' => 100,
+                'block' => true
+            ],
+            'damage' => [
+                'minionHealthReturned' => 100,
+                'block' => false
+            ],
+            'kill' => [
+                'minionHealthReturned' => 0,
+                'block' => false
             ]
         ];
     }
