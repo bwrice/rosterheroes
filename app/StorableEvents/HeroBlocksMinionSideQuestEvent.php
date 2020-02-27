4b121341b2030796232865d8f56d50eab8161fc1
Brian Rice <bwrice83@gmail.com>
2020-02-27 00:34:21 -0500

    extend MinionAttacksHeroSideQuestEvent for storable events

--- app/StorableEvents/HeroBlocksMinionSideQuestEvent.php
+++ app/StorableEvents/HeroBlocksMinionSideQuestEvent.php
@@ -4,40 +4,7 @@ namespace App\StorableEvents;
 
 use Spatie\EventSourcing\ShouldBeStored;
 
-final class HeroBlocksMinionSideQuestEvent implements ShouldBeStored
+final class HeroBlocksMinionSideQuestEvent extends MinionAttacksHeroSideQuestEvent implements ShouldBeStored
 {
-    /**
-     * @var int
-     */
-    public $sideQuestResultID;
-    /**
-     * @var int
-     */
-    public $moment;
-    /**
-     * @var string
-     */
-    public $minionUuid;
-    /**
-     * @var string
-     */
-    public $attackUuid;
-    /**
-     * @var string
-     */
-    public $heroUuid;
 
-    public function __construct(
-        int $sideQuestResultID,
-        int $moment,
-        string $minionUuid,
-        string $attackUuid,
-        string $heroUuid)
-    {
-        $this->sideQuestResultID = $sideQuestResultID;
-        $this->moment = $moment;
-        $this->minionUuid = $minionUuid;
-        $this->attackUuid = $attackUuid;
-        $this->heroUuid = $heroUuid;
-    }
 }
--- app/StorableEvents/MinionKillsHeroSideQuestEvent.php
+++ app/StorableEvents/MinionKillsHeroSideQuestEvent.php
@@ -4,46 +4,7 @@ namespace App\StorableEvents;
 
 use Spatie\EventSourcing\ShouldBeStored;
 
-final class MinionKillsHeroSideQuestEvent implements ShouldBeStored
+final class MinionKillsHeroSideQuestEvent extends MinionAttacksHeroSideQuestEvent implements ShouldBeStored
 {
-    /**
-     * @var int
-     */
-    public $sideQuestResultID;
-    /**
-     * @var int
-     */
-    public $moment;
-    /**
-     * @var string
-     */
-    public $minionUuid;
-    /**
-     * @var string
-     */
-    public $attackUuid;
-    /**
-     * @var string
-     */
-    public $heroUuid;
-    /**
-     * @var int
-     */
-    public $damage;
 
-    public function __construct(
-        int $sideQuestResultID,
-        int $moment,
-        string $minionUuid,
-        string $attackUuid,
-        string $heroUuid,
-        int $damage)
-    {
-        $this->sideQuestResultID = $sideQuestResultID;
-        $this->moment = $moment;
-        $this->minionUuid = $minionUuid;
-        $this->attackUuid = $attackUuid;
-        $this->heroUuid = $heroUuid;
-        $this->damage = $damage;
-    }
 }
