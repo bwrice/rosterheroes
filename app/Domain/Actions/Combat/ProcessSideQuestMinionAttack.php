9e88dc4479f7ebb8ca04dffac86de43d6fb2b792
Brian Rice <bwrice83@gmail.com>
2020-02-27 00:37:33 -0500

    simplify data passed to aggregate

--- app/Domain/Actions/Combat/ProcessSideQuestMinionAttack.php
+++ app/Domain/Actions/Combat/ProcessSideQuestMinionAttack.php
@@ -7,6 +7,7 @@ namespace App\Domain\Actions\Combat;
 use App\Aggregates\SideQuestEventAggregate;
 use App\Domain\Combat\Attacks\MinionCombatAttack;
 use App\Domain\Combat\Combatants\CombatHero;
+use App\Domain\Combat\Combatants\CombatMinion;
 use App\SideQuestEvent;
 use App\SideQuestResult;
 use Illuminate\Support\Str;
@@ -17,12 +18,13 @@ class ProcessSideQuestMinionAttack
      * @param SideQuestResult $sideQuestResult
      * @param int $moment
      * @param int $damageReceived
+     * @param CombatMinion $combatMinion
      * @param MinionCombatAttack $minionCombatAttack
      * @param CombatHero $combatHero
      * @param $block
      * @return SideQuestEvent
      */
-    public function execute(SideQuestResult $sideQuestResult, int $moment, int $damageReceived, MinionCombatAttack $minionCombatAttack, CombatHero $combatHero, $block)
+    public function execute(SideQuestResult $sideQuestResult, int $moment, int $damageReceived, CombatMinion $combatMinion, MinionCombatAttack $minionCombatAttack, CombatHero $combatHero, $block)
     {
         $uuid = (string) Str::uuid();
         $aggregate = SideQuestEventAggregate::retrieve($uuid);
@@ -31,28 +33,34 @@ class ProcessSideQuestMinionAttack
             $aggregate->createHeroBlocksMinionEvent(
                 $sideQuestResult->id,
                 $moment,
-                $minionCombatAttack->getMinionUuid(),
-                $minionCombatAttack->getCombatAttack()->getAttackUuid(),
-                $combatHero->getHeroUuid(),
+                [
+                    'minionCombatAttack' => $minionCombatAttack->toArray(),
+                    'combatHero' => $combatHero->toArray(),
+                    'combatMinion' => $combatMinion->toArray()
+                ]
             );
         } else {
             if ($combatHero->getCurrentHealth() > 0) {
                 $aggregate->createMinionDamagesHeroEvent(
                     $sideQuestResult->id,
                     $moment,
-                    $minionCombatAttack->getMinionUuid(),
-                    $minionCombatAttack->getCombatAttack()->getAttackUuid(),
-                    $combatHero->getHeroUuid(),
-                    $damageReceived
+                    [
+                        'damage' => $damageReceived,
+                        'minionCombatAttack' => $minionCombatAttack->toArray(),
+                        'combatHero' => $combatHero->toArray(),
+                        'combatMinion' => $combatMinion->toArray()
+                    ]
                 );
             } else {
                 $aggregate->createMinionKillsHeroEvent(
                     $sideQuestResult->id,
                     $moment,
-                    $minionCombatAttack->getMinionUuid(),
-                    $minionCombatAttack->getCombatAttack()->getAttackUuid(),
-                    $combatHero->getHeroUuid(),
-                    $damageReceived
+                    [
+                        'damage' => $damageReceived,
+                        'minionCombatAttack' => $minionCombatAttack->toArray(),
+                        'combatHero' => $combatHero->toArray(),
+                        'combatMinion' => $combatMinion->toArray()
+                    ]
                 );
             }
         }
