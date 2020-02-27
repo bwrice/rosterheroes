c8469cc7d7669451baa4ac8a89ef32a1dd135455
Brian Rice <bwrice83@gmail.com>
2020-02-27 14:59:11 -0500

    extend HeroAttacksMinionSideQuestEvent for all storable hero attacks minion events

--- app/StorableEvents/HeroDamagesMinionSideQuestEvent.php
+++ app/StorableEvents/HeroDamagesMinionSideQuestEvent.php
@@ -4,64 +4,7 @@ namespace App\StorableEvents;
 
 use Spatie\EventSourcing\ShouldBeStored;
 
-final class HeroDamagesMinionSideQuestEvent implements ShouldBeStored
+final class HeroDamagesMinionSideQuestEvent extends HeroAttacksMinionSideQuestEvent implements ShouldBeStored
 {
-    /**
-     * @var int
-     */
-    public $sideQuestResultID;
-    /**
-     * @var int
-     */
-    public $moment;
-    /**
-     * @var string
-     */
-    public $heroUuid;
-    /**
-     * @var string
-     */
-    public $attackUuid;
-    /**
-     * @var string
-     */
-    public $itemUuid;
-    /**
-     * @var string
-     */
-    public $minionUuid;
-    /**
-     * @var int
-     */
-    public $damage;
-    /**
-     * @var int
-     */
-    public $staminaCost;
-    /**
-     * @var int
-     */
-    public $manaCost;
 
-    public function __construct(
-        int $sideQuestResultID,
-        int $moment,
-        string $heroUuid,
-        string $attackUuid,
-        string $itemUuid,
-        string $minionUuid,
-        int $damage,
-        int $staminaCost,
-        int $manaCost)
-    {
-        $this->sideQuestResultID = $sideQuestResultID;
-        $this->moment = $moment;
-        $this->heroUuid = $heroUuid;
-        $this->attackUuid = $attackUuid;
-        $this->itemUuid = $itemUuid;
-        $this->minionUuid = $minionUuid;
-        $this->damage = $damage;
-        $this->staminaCost = $staminaCost;
-        $this->manaCost = $manaCost;
-    }
 }
--- app/StorableEvents/MinionBlocksHeroSideQuestEvent.php
+++ app/StorableEvents/MinionBlocksHeroSideQuestEvent.php
@@ -4,58 +4,7 @@ namespace App\StorableEvents;
 
 use Spatie\EventSourcing\ShouldBeStored;
 
-final class MinionBlocksHeroSideQuestEvent implements ShouldBeStored
+final class MinionBlocksHeroSideQuestEvent extends HeroAttacksMinionSideQuestEvent implements ShouldBeStored
 {
-    /**
-     * @var int
-     */
-    public $sideQuestResultID;
-    /**
-     * @var int
-     */
-    public $moment;
-    /**
-     * @var string
-     */
-    public $heroUuid;
-    /**
-     * @var int
-     */
-    public $attackUuid;
-    /**
-     * @var string
-     */
-    public $itemUuid;
-    /**
-     * @var string
-     */
-    public $minionUuid;
-    /**
-     * @var int
-     */
-    public $staminaCost;
-    /**
-     * @var int
-     */
-    public $manaCost;
 
-    public function __construct(
-        int $sideQuestResultID,
-        int $moment,
-        string $heroUuid,
-        string $attackUuid,
-        string $itemUuid,
-        string $minionUuid,
-        int $staminaCost,
-        int $manaCost)
-    {
-        $this->sideQuestResultID = $sideQuestResultID;
-        $this->moment = $moment;
-        $this->heroUuid = $heroUuid;
-        $this->attackUuid = $attackUuid;
-        $this->itemUuid = $itemUuid;
-        $this->minionUuid = $minionUuid;
-        $this->staminaCost = $staminaCost;
-        $this->manaCost = $manaCost;
-    }
 }
