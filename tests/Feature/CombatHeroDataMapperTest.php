01abcfba3c2fce2f92d4c472b6f4ee0beb346183
Brian Rice <bwrice83@gmail.com>
2020-02-28 15:22:09 -0500

    test initial resource changes match in CombatHeroDataMapper

--- tests/Feature/CombatHeroDataMapperTest.php
+++ tests/Feature/CombatHeroDataMapperTest.php
@@ -22,4 +22,20 @@ class CombatHeroDataMapperTest extends TestCase
         $combatHero = $mapper->getCombatHero($original->toArray());
         $this->assertTrue($original == $combatHero);
     }
+
+    /**
+     * @test
+     */
+    public function it_will_map_to_matching_combat_hero_after_initial_resources_have_changed()
+    {
+        $original = CombatHeroFactory::new()->create();
+        $original->receiveDamage(10);
+        $original->setCurrentStamina($original->getCurrentStamina() - 5);
+        $original->setCurrentMana($original->getCurrentMana() - 5);
+
+        /** @var CombatHeroDataMapper $mapper */
+        $mapper = app(CombatHeroDataMapper::class);
+        $combatHero = $mapper->getCombatHero($original->toArray());
+        $this->assertTrue($original == $combatHero);
+    }
 }
