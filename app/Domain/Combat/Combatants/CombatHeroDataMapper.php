d8d7d11f89d635d620f6f5767a81afb868aa78c0
Brian Rice <bwrice83@gmail.com>
2020-02-28 15:12:36 -0500

    create CombatHeroDataMapper

---
+++ app/Domain/Combat/Combatants/CombatHeroDataMapper.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Domain\Combat\Combatants;
+
+
+use App\Domain\Combat\Attacks\HeroCombatAttackDataMapper;
+use App\Domain\Models\CombatPosition;
+use Illuminate\Database\Eloquent\Collection;
+
+class CombatHeroDataMapper
+{
+    /**
+     * @var HeroCombatAttackDataMapper
+     */
+    protected $heroCombatAttackDataMapper;
+
+    public function __construct(HeroCombatAttackDataMapper $heroCombatAttackDataMapper)
+    {
+        $this->heroCombatAttackDataMapper = $heroCombatAttackDataMapper;
+    }
+
+    public function getCombatHero(array $data, Collection $combatPositions = null)
+    {
+        $combatPositions = $combatPositions ?: CombatPosition::all();
+        $initialCombatPosition = $combatPositions->find($data['initialCombatPositionID']);
+
+        $combatAttacks = collect($data['combatAttacks'])->map(function ($combatAttackData) {
+            return $this->heroCombatAttackDataMapper->getHeroCombatAttack($combatAttackData);
+        });
+
+        $combatHero = new CombatHero(
+            $data['heroUuid'],
+            $data['initialHealth'],
+            $data['initialStamina'],
+            $data['initialMana'],
+            $data['protection'],
+            $data['blockChancePercent'],
+            $initialCombatPosition,
+            $combatAttacks
+        );
+
+        return $combatHero;
+    }
+}
