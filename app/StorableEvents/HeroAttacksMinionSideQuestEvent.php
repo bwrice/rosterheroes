0863b02b1e7be4caf3f5e8749a224f3facb392d8
Brian Rice <bwrice83@gmail.com>
2020-02-28 16:56:22 -0500

    add getters to abstract storable event HeroAttacksMinionSideQuestEvent

--- app/StorableEvents/HeroAttacksMinionSideQuestEvent.php
+++ app/StorableEvents/HeroAttacksMinionSideQuestEvent.php
@@ -4,7 +4,20 @@
 namespace App\StorableEvents;
 
 
+use App\Domain\Combat\Attacks\HeroCombatAttackDataMapper;
+
 abstract class HeroAttacksMinionSideQuestEvent extends StorableSideQuestEvent
 {
+    public function getHeroCombatAttackData()
+    {
+        return $this->data['heroCombatAttack'];
+    }
 
+    public function getDamage()
+    {
+        if (array_key_exists('damage', $this->data)) {
+            return $this->data['damage'];
+        }
+        return 0;
+    }
 }
