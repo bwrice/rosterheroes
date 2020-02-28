b38b42a6c625e69c90b1fe96aadc745d286a4b5d
Brian Rice <bwrice83@gmail.com>
2020-02-27 23:10:31 -0500

    test resource costs map correctly in HeroCombatAttackMapper

--- tests/Feature/HeroCombatAttackDataMapperTest.php
+++ tests/Feature/HeroCombatAttackDataMapperTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Domain\Collections\ResourceCostsCollection;
 use App\Domain\Combat\Attacks\CombatAttackDataMapper;
 use App\Domain\Combat\Attacks\HeroCombatAttackDataMapper;
+use App\Domain\Models\Json\ResourceCosts\FixedResourceCost;
+use App\Domain\Models\Json\ResourceCosts\PercentResourceCost;
+use App\Domain\Models\MeasurableType;
 use App\Factories\Combat\CombatAttackFactory;
 use App\Factories\Combat\HeroCombatAttackFactory;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -20,8 +24,12 @@ class HeroCombatAttackDataMapperTest extends TestCase
      */
     public function it_will_map_the_data_array_into_a_hero_combat_attack()
     {
+        $resourceCosts = new ResourceCostsCollection([
+            new FixedResourceCost(MeasurableType::STAMINA, 15),
+            new PercentResourceCost(MeasurableType::MANA, 5)
+        ]);
         $combatAttack = CombatAttackFactory::new()->create();
-        $originalHeroCombatAttack = HeroCombatAttackFactory::new()->withCombatAttack($combatAttack)->create();
+        $originalHeroCombatAttack = HeroCombatAttackFactory::new()->withResourceCosts($resourceCosts)->withCombatAttack($combatAttack)->create();
 
         $combatAttackMapperMock = \Mockery::mock(CombatAttackDataMapper::class)
             ->shouldReceive('getCombatAttack')
