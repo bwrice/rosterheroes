60f82e7cf33a344bbd96c3b84b36ff29ec7dd1f6
Brian Rice <bwrice83@gmail.com>
2020-02-28 16:26:35 -0500

    add combat attacks logic to CombatMinionFactory

--- app/Factories/Combat/CombatMinionFactory.php
+++ app/Factories/Combat/CombatMinionFactory.php
@@ -7,15 +7,27 @@ namespace App\Factories\Combat;
 use App\Domain\Combat\Combatants\CombatHero;
 use App\Domain\Combat\Combatants\CombatMinion;
 use App\Domain\Models\CombatPosition;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Str;
 
 class CombatMinionFactory extends AbstractCombatantFactory
 {
+    protected $minionCombatAttackFactories;
 
+    /**
+     * @return CombatMinion
+     */
     public function create()
     {
         /** @var CombatPosition $combatPosition */
         $combatPosition = $this->getCombatPosition();
+
+        $combatAttacks = collect();
+        if ($this->minionCombatAttackFactories) {
+            $combatAttacks = $this->minionCombatAttackFactories->map(function (MinionCombatAttackFactory $minionCombatAttackFactory) {
+                return $minionCombatAttackFactory->create();
+            });
+        }
 
         return new CombatMinion(
             (string) Str::uuid(),
@@ -23,7 +35,21 @@ class CombatMinionFactory extends AbstractCombatantFactory
             250,
             20,
             $combatPosition,
-            collect()
+            $combatAttacks
         );
+    }
+
+    public function withMinionCombatAttacks(Collection $minionCombatAttackFactories = null)
+    {
+        if (! $minionCombatAttackFactories) {
+            $minionCombatAttackFactories = collect();
+            foreach (range(1, rand(1, 4)) as $item) {
+                $minionCombatAttackFactories->push(MinionCombatAttackFactory::new());
+            }
+        }
+
+        $clone = clone $this;
+        $clone->minionCombatAttackFactories = $minionCombatAttackFactories;
+        return $clone;
     }
 }
