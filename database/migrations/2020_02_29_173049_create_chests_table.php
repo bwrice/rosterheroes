df29b1d3e702ed7583d55085c8045c159495bb98
Brian Rice <bwrice83@gmail.com>
2020-02-29 13:54:14 -0500

    create Chest model and migration

---
+++ database/migrations/2020_02_29_173049_create_chests_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('grade')->unsigned();
+            $table->bigInteger('squad_id')->unsigned();
+            $table->dateTime('opened_at');
+            $table->integer('chest_blueprint_id')->unsigned()->nullable();
+        });
+
+        Schema::table('chests', function (Blueprint $table) {
+            $table->foreign('squad_id')->references('id')->on('squads');
+            $table->foreign('chest_blueprint_id')->references('id')->on('chest_blueprints');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chests');
+    }
+}
