56fe3af4fdeffcc5699c8ac69420d6fc84511e1a
Brian Rice <bwrice83@gmail.com>
2020-02-29 13:03:11 -0500

    rename seeder

--- database/migrations/2019_08_29_171625_seed_starter_item_blueprints.php
+++ database/migrations/2019_08_29_171625_seed_starter_item_blueprints.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class SeedItemBlueprints extends Migration
+class SeedStarterItemBlueprints extends Migration
 {
     /**
      * Run the migrations.
