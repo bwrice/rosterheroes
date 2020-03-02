eb64b52de9ae22c12bd84eb4eac757e2fa3fe084
Brian Rice <bwrice83@gmail.com>
2020-03-02 11:11:27 -0500

    add updateCombatPositions() method to CombatGroup interface

--- app/Domain/Combat/CombatGroups/CombatGroup.php
+++ app/Domain/Combat/CombatGroups/CombatGroup.php
@@ -5,10 +5,13 @@ namespace App\Domain\Combat\CombatGroups;
 
 
 use App\Domain\Collections\CombatantCollection;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Support\Collection;
 
 interface CombatGroup
 {
+    public function updateCombatPositions(EloquentCollection $combatPositions);
+
     public function getReadyAttacks(int $moment): Collection;
 
     public function getPossibleTargets($moment): CombatantCollection;
