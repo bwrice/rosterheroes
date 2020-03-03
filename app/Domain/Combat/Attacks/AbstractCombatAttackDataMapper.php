969839858ecdc456f7fb6c2ce53f17681621aa87
Brian Rice <bwrice83@gmail.com>
2020-03-03 17:06:38 -0500

    convert CombatAttackDataMapper into AbstractCombatAttackDataMapper

--- app/Domain/Combat/Attacks/AbstractCombatAttackDataMapper.php
+++ app/Domain/Combat/Attacks/AbstractCombatAttackDataMapper.php
@@ -9,34 +9,16 @@ use App\Domain\Models\DamageType;
 use App\Domain\Models\TargetPriority;
 use Illuminate\Database\Eloquent\Collection;
 
-class CombatAttackDataMapper
+abstract class AbstractCombatAttackDataMapper
 {
-    public function getCombatAttack(array $data, Collection $combatPositions = null, Collection $targetPriorities = null, Collection $damageTypes = null)
-    {
-        $combatPositions = $combatPositions ?: CombatPosition::all();
-        $targetPriorities = $targetPriorities ?: TargetPriority::all();
-        $damageTypes = $damageTypes ?: DamageType::all();
 
-        return new AbstractCombatAttack(
-            $data['name'],
-            $data['attackUuid'],
-            $data['damage'],
-            $data['combatSpeed'],
-            $data['grade'],
-            $combatPositions->find($data['attackerPositionID']),
-            $combatPositions->find($data['targetPositionID']),
-            $targetPriorities->find($data['targetPriorityID']),
-            $damageTypes->find($data['damageTypeID']),
-            $data['maxTargetsCount']
-        );
-    }
 
     protected function getName(array $data)
     {
         return $data['name'];
     }
 
-    protected function attackUuid(array $data)
+    protected function getAttackUuid(array $data)
     {
         return $data['attackUuid'];
     }
