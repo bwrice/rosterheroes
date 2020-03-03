c5d7a6629c9869a26cf5c8176265b5751178c34e
Brian Rice <bwrice83@gmail.com>
2020-03-02 22:00:30 -0500

    create noobWarrior() build for CombatHeroFactory

--- app/Factories/Combat/CombatHeroFactory.php
+++ app/Factories/Combat/CombatHeroFactory.php
@@ -8,8 +8,13 @@ use App\Domain\Behaviors\HeroClasses\WarriorBehavior;
 use App\Domain\Behaviors\MeasurableTypes\Resources\HealthBehavior;
 use App\Domain\Behaviors\MeasurableTypes\Resources\ManaBehavior;
 use App\Domain\Behaviors\MeasurableTypes\Resources\StaminaBehavior;
+use App\Domain\Collections\ResourceCostsCollection;
 use App\Domain\Combat\Combatants\CombatHero;
 use App\Domain\Models\CombatPosition;
+use App\Domain\Models\DamageType;
+use App\Domain\Models\Json\ResourceCosts\FixedResourceCost;
+use App\Domain\Models\MeasurableType;
+use App\Domain\Models\TargetPriority;
 use App\Factories\Models\HeroFactory;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
@@ -113,6 +118,75 @@ class CombatHeroFactory extends AbstractCombatantFactory
     {
         $heroFactory = $this->heroFactory ?: HeroFactory::new();
         return $heroFactory->create()->uuid;
+    }
+
+    public function noobWarrior()
+    {
+        $clone = clone $this;
+        /** @var WarriorBehavior $warriorBehavior */
+        $warriorBehavior = app(WarriorBehavior::class);
+        /** @var HealthBehavior $healthBehavior */
+        $healthBehavior = app(HealthBehavior::class);
+        $health = $warriorBehavior->getMeasurableStartingAmount($healthBehavior);
+        /** @var StaminaBehavior $staminaBehavior */
+        $staminaBehavior = app(StaminaBehavior::class);
+        $stamina = $warriorBehavior->getMeasurableStartingAmount($staminaBehavior);
+        /** @var ManaBehavior $manaBehavior */
+        $manaBehavior = app(ManaBehavior::class);
+        $mana = $warriorBehavior->getMeasurableStartingAmount($manaBehavior);
+
+        $clone = $clone->withHealth($health)
+            ->withStamina($stamina)
+            ->withMana($mana)
+            ->withProtection(200)
+            ->withBlockChancePercent(10)
+            ->withCombatPosition(CombatPosition::FRONT_LINE);
+
+        $baseCombatAttackFactory = CombatAttackFactory::new()
+            ->withAttackerPosition(CombatPosition::FRONT_LINE)
+            ->withTargetPosition(CombatPosition::FRONT_LINE)
+            ->withDamageType(DamageType::FIXED_TARGET)
+            ->withTargetPriority(TargetPriority::ANY);
+
+        $meleeAttackOne = HeroCombatAttackFactory::new()
+            ->withCombatAttackFactory(
+                $baseCombatAttackFactory
+                    ->withDamage(40)
+                    ->withCombatSpeed(40)
+                    ->withMaxTargetCount(1)
+            )->withResourceCosts(new ResourceCostsCollection([
+                new FixedResourceCost(MeasurableType::STAMINA, 8)
+            ]));
+
+        $meleeAttackTwo = HeroCombatAttackFactory::new()
+            ->withCombatAttackFactory(
+                $baseCombatAttackFactory
+                    ->withDamage(50)
+                    ->withCombatSpeed(18)
+                    ->withMaxTargetCount(2)
+            )->withResourceCosts(new ResourceCostsCollection([
+                new FixedResourceCost(MeasurableType::STAMINA, 15),
+                new FixedResourceCost(MeasurableType::MANA, 3)
+            ]));
+
+        $meleeAttackThree = HeroCombatAttackFactory::new()
+            ->withCombatAttackFactory(
+                $baseCombatAttackFactory
+                    ->withDamage(75)
+                    ->withCombatSpeed(10)
+                    ->withMaxTargetCount(3)
+            )->withResourceCosts(new ResourceCostsCollection([
+                new FixedResourceCost(MeasurableType::STAMINA, 25),
+                new FixedResourceCost(MeasurableType::MANA, 8)
+            ]));
+
+        $clone = $clone->withHeroCombatAttacks(collect([
+            $meleeAttackOne,
+            $meleeAttackTwo,
+            $meleeAttackThree
+        ]));
+
+        return $clone;
     }
 
     protected function getHeroCombatAttacks(string $heroUuid)
