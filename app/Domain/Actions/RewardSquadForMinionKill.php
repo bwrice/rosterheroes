a5b3e413c825be1a22431d0331c37b9a01a506e0
Brian Rice <bwrice83@gmail.com>
2020-03-04 16:37:18 -0500

    call RewardChestToSquad for each chest blueprint attached to minion

--- app/Domain/Actions/RewardSquadForMinionKill.php
+++ app/Domain/Actions/RewardSquadForMinionKill.php
@@ -4,15 +4,29 @@
 namespace App\Domain\Actions;
 
 
+use App\ChestBlueprint;
 use App\Domain\Models\Minion;
 use App\Domain\Models\Squad;
 
 class RewardSquadForMinionKill
 {
+    /**
+     * @var RewardChestToSquad
+     */
+    protected $rewardChestToSquad;
+
+    public function __construct(RewardChestToSquad $rewardChestToSquad)
+    {
+        $this->rewardChestToSquad = $rewardChestToSquad;
+    }
+
     public function execute(Squad $squad, Minion $minion)
     {
         $experienceReward = $minion->getExperienceReward();
         $squad->getAggregate()->increaseExperience($experienceReward)->persist();
 
+        $minion->chestBlueprints->each(function (ChestBlueprint $chestBlueprint) use ($squad) {
+            $this->rewardChestToSquad->execute($chestBlueprint, $squad);
+        });
     }
 }
--- tests/Feature/RewardSquadForMinionKillTest.php
+++ tests/Feature/RewardSquadForMinionKillTest.php
@@ -2,17 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Domain\Actions\RewardChestToSquad;
 use App\Domain\Actions\RewardSquadForMinionKill;
 use App\Domain\Models\Minion;
 use App\Domain\Models\Squad;
+use App\Factories\Models\ChestBlueprintFactory;
 use App\Factories\Models\MinionFactory;
 use App\Factories\Models\SquadFactory;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RewardSquadForMinionKillTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /** @var Squad */
     protected $squad;
 
@@ -41,5 +46,22 @@ class RewardSquadForMinionKillTest extends TestCase
 
         $newExperience = $this->squad->fresh()->experience;
         $this->assertEquals($beginningExperience + $experienceReward, $newExperience);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_execute_reward_chest_to_squad_actions_for_each_chest_blueprint_belonging_to_the_minion()
+    {
+        $chestBlueFactory = ChestBlueprintFactory::new();
+        $this->minion->chestBlueprints()->save($chestBlueFactory->create());
+        $this->minion->chestBlueprints()->save($chestBlueFactory->create());
+
+        $rewardChestMock = \Mockery::mock(RewardChestToSquad::class)->shouldReceive('execute')->times(2)->getMock();
+        app()->instance(RewardChestToSquad::class, $rewardChestMock);
+
+        /** @var RewardSquadForMinionKill $domainAction */
+        $domainAction = app(RewardSquadForMinionKill::class);
+        $domainAction->execute($this->squad, $this->minion);
     }
 }
