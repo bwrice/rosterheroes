8ed9687eb183b32c7374815fd892eeec5da73fb3
Brian Rice <bwrice83@gmail.com>
2020-03-03 23:54:08 -0500

    test only survivors inherit combat positions

--- tests/Feature/AbstractCombatantCollectionTest.php
+++ tests/Feature/AbstractCombatantCollectionTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Domain\Collections\AbstractCombatantCollection;
+use App\Domain\Collections\CombatPositionCollection;
 use App\Domain\Combat\Combatants\CombatHero;
 use App\Domain\Models\CombatPosition;
 use App\Factories\Combat\AbstractCombatantFactory;
@@ -11,6 +12,7 @@ use App\Factories\Combat\CombatMinionFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use function foo\func;
 
 class AbstractCombatantCollectionTest extends TestCase
 {
@@ -19,7 +21,7 @@ class AbstractCombatantCollectionTest extends TestCase
      * @dataProvider provides_abstract_combatants_will_inherit_combat_positions_correctly
      * @param string $factoryClass
      */
-    public function abstract_combatants_will_inherit_combat_positions_correctly(string $factoryClass)
+    public function abstract_combatants_will_inherit_combat_positions_with_no_combatants(string $factoryClass)
     {
         /** @var AbstractCombatantFactory $factory */
         $factory = call_user_func($factoryClass . '::new');
@@ -52,6 +54,47 @@ class AbstractCombatantCollectionTest extends TestCase
             $this->assertNotNull($backLine);
         }
         $this->assertEquals(1, $highGroundCombatant->allCombatPositions()->count());
+    }
+    /**
+     * @test
+     * @dataProvider provides_abstract_combatants_will_inherit_combat_positions_correctly
+     * @param string $factoryClass
+     */
+    public function abstract_combatants_will_inherit_combat_positions_with_no_survivors(string $factoryClass)
+    {
+        /** @var AbstractCombatantFactory $factory */
+        $factory = call_user_func($factoryClass . '::new');
+        $frontLineCombatant = $factory->withCombatPosition(CombatPosition::FRONT_LINE)->create();
+        $backLineCombatant = $factory->withCombatPosition(CombatPosition::BACK_LINE)->create();
+        $highGroundCombatant = $factory->withCombatPosition(CombatPosition::HIGH_GROUND)->create();
+        $collection = new AbstractCombatantCollection([
+            $frontLineCombatant,
+            $backLineCombatant,
+            $highGroundCombatant
+        ]);
+        $this->assertEquals(1, $frontLineCombatant->allCombatPositions()->count());
+        $this->assertEquals(1, $backLineCombatant->allCombatPositions()->count());
+        $this->assertEquals(1, $highGroundCombatant->allCombatPositions()->count());
+
+        /** @var CombatPositionCollection $combatPositions */
+        $combatPositions = CombatPosition::all();
+        $collection->updateCombatPositions($combatPositions);
+
+        $this->assertEquals(1, $frontLineCombatant->allCombatPositions()->count());
+        $this->assertEquals(1, $backLineCombatant->allCombatPositions()->count());
+        $this->assertEquals(1, $highGroundCombatant->allCombatPositions()->count());
+
+        $frontLineCombatant->setCurrentHealth(0);
+        $backLineCombatant->setCurrentHealth(0);
+
+        $collection->updateCombatPositions($combatPositions);
+        $this->assertEquals(3, $highGroundCombatant->allCombatPositions()->count());
+        $combatPositions->each(function (CombatPosition $combatPositionToMatch) use ($highGroundCombatant) {
+            $match = $highGroundCombatant->allCombatPositions()->first(function (CombatPosition $combatPosition) use ($combatPositionToMatch) {
+                return $combatPosition->id === $combatPositionToMatch->id;
+            });
+            $this->assertNotNull($match);
+        });
     }
 
     public function provides_abstract_combatants_will_inherit_combat_positions_correctly()
