7f9b036a8175801009641acde57f9723c3be616d
Brian Rice <bwrice83@gmail.com>
2020-03-04 22:05:22 -0500

    test it_will_throw_an_exception_if_side_quest_rewards_already_processed

---
+++ tests/Feature/ProcessSideQuestRewardsTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Domain\Actions\ProcessSideQuestRewards;
+use App\Factories\Models\SideQuestResultFactory;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Date;
+use Tests\TestCase;
+
+class ProcessSideQuestRewardsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @var \App\SideQuestResult */
+    protected $sideQuestResult;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->sideQuestResult = SideQuestResultFactory::new()->create();
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_throw_an_exception_if_side_quest_rewards_already_processed()
+    {
+        $this->sideQuestResult->rewards_processed_at = Date::now();
+        $this->sideQuestResult->save();
+        $originalSquadExperience = $this->sideQuestResult->squad->experience;
+
+        try {
+            /** @var ProcessSideQuestRewards $domainAction */
+            $domainAction = app(ProcessSideQuestRewards::class);
+            $domainAction->execute($this->sideQuestResult->fresh());
+        } catch (\Exception $exception) {
+            $squad = $this->sideQuestResult->squad->fresh();
+            $this->assertEquals($originalSquadExperience, $squad->experience);
+            return;
+        }
+
+        $this->fail("Exception not thrown");
+    }
+}
