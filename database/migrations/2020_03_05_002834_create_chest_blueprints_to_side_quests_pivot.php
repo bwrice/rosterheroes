dafcdfe3736fb50ceb459896fc40d0ee2778dd18
Brian Rice <bwrice83@gmail.com>
2020-03-04 19:49:26 -0500

    create chest-blueprint side-quest pivot migration

---
+++ database/migrations/2020_03_05_002834_create_chest_blueprints_to_side_quests_pivot.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChestBlueprintsToSideQuestsPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chest_blueprint_side_quest', function (Blueprint $table) {
+            $table->integer('chest_blueprint_id')->unsigned();
+            $table->integer('side_quest_id')->unsigned();
+            $table->primary(['chest_blueprint_id', 'side_quest_id'], 'chest_bp_side_quest_composite');
+            $table->timestamps();
+        });
+
+        Schema::table('chest_blueprint_side_quest', function (Blueprint $table) {
+            $table->foreign('chest_blueprint_id')->references('id')->on('chest_blueprints');
+            $table->foreign('side_quest_id')->references('id')->on('side_quests');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chest_blueprint_side_quest');
+    }
+}
