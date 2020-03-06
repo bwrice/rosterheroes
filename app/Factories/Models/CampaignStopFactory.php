b037ffcd933ca809a541256f83420175580b31f3
Brian Rice <bwrice83@gmail.com>
2020-03-05 21:53:11 -0500

    customize campaign in CampaignStopFactory

--- app/Factories/Models/CampaignStopFactory.php
+++ app/Factories/Models/CampaignStopFactory.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Str;
 
 class CampaignStopFactory
 {
+    /** @var CampaignFactory|null */
+    protected $campaignFactory;
+
     public static function new()
     {
         return new self();
@@ -20,11 +23,26 @@ class CampaignStopFactory
         /** @var CampaignStop $campaignStop */
         $campaignStop = CampaignStop::query()->create(array_merge([
             'uuid' => (string) Str::uuid(),
-            'campaign_id' => CampaignFactory::new()->create()->id,
+            'campaign_id' => $this->getCampaignID(),
             'quest_id' => QuestFactory::new()->create()->id,
             'province_id' => Province::query()->inRandomOrder()->first()->id
         ], $extra));
         return $campaignStop;
+    }
+
+    protected function getCampaignID()
+    {
+        if ($this->campaignFactory) {
+            return $this->campaignFactory->create()->id;
+        }
+        return CampaignFactory::new()->create()->id;
+    }
+
+    public function withCampaign(CampaignFactory $campaignFactory)
+    {
+        $clone = clone $this;
+        $clone->campaignFactory = $campaignFactory;
+        return $clone;
     }
 
 }
