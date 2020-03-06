7d29e759e5b65c1d22d826c33fa253ec43ea65df
Brian Rice <bwrice83@gmail.com>
2020-03-05 21:53:24 -0500

    create ProcessSideQuestResultJob

---
+++ app/Jobs/ProcessSideQuestResultJob.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Domain\Actions\Combat\ProcessSideQuestResult;
+use App\Domain\Models\CampaignStop;
+use App\Domain\Models\SideQuest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessSideQuestResultJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * @var SideQuest
+     */
+    public $sideQuest;
+    /**
+     * @var CampaignStop
+     */
+    public $campaignStop;
+
+    /**
+     * ProcessSideQuestResultJob constructor.
+     * @param CampaignStop $campaignStop
+     * @param SideQuest $sideQuest
+     */
+    public function __construct(CampaignStop $campaignStop, SideQuest $sideQuest)
+    {
+        $this->campaignStop = $campaignStop;
+        $this->sideQuest = $sideQuest;
+    }
+
+    /**
+     * @param ProcessSideQuestResult $processSideQuestResult
+     * @throws \Exception
+     */
+    public function handle(ProcessSideQuestResult $processSideQuestResult)
+    {
+        $processSideQuestResult->execute($this->campaignStop, $this->sideQuest);
+    }
+}
