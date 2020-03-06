665b572707258f7029ab560641811adf43d2a14d
Brian Rice <bwrice83@gmail.com>
2020-03-06 12:26:29 -0500

    recursively dispatch itself if max campaigns queried

--- app/Domain/Actions/WeekFinalizing/ProcessCurrentWeekSideQuestsAction.php
+++ app/Domain/Actions/WeekFinalizing/ProcessCurrentWeekSideQuestsAction.php
@@ -15,32 +15,90 @@ use Illuminate\Database\Eloquent\Collection;
 
 class ProcessCurrentWeekSideQuestsAction implements FinalizeWeekDomainAction
 {
+    protected $maxCampaignStopsQueried = 100;
+
     /**
      * @param int $finalizeWeekStep
      * @param array $extra
      */
     public function execute(int $finalizeWeekStep, array $extra = [])
     {
-        ChainGroup::create($this->getProcessResultJobs(), [
-            new FinalizeWeekJob($finalizeWeekStep + 1)
+        $campaignStops = $this->getCampaignStops($extra);
+        $asyncJobs = $this->getProcessResultJobs($campaignStops);
+
+        $finalizeWeekArgs = $this->getFinalizeWeekArgs($campaignStops, $finalizeWeekStep);
+
+        ChainGroup::create($asyncJobs, [
+            new FinalizeWeekJob($finalizeWeekArgs['step'], $finalizeWeekArgs['extra'])
         ])->dispatch();;
     }
 
-    protected function getProcessResultJobs()
+    protected function getCampaignStops(array $extra)
     {
-        $jobs = collect();
-        CampaignStop::query()
+        $lastCampaignStopID = array_key_exists('last_campaign_stop_id', $extra)
+            ? $extra['last_campaign_stop_id'] : false;
+
+        $query = $this->buildCampaignStopsQuery($lastCampaignStopID)->take($this->maxCampaignStopsQueried);
+
+        return $query->get();
+    }
+
+    protected function buildCampaignStopsQuery($lastCampaignStopID)
+    {
+        $query = CampaignStop::query()
             ->whereHas('campaign', function (Builder $builder) {
                 return $builder->where('week_id', '=', CurrentWeek::id());
             })
             ->orderBy('id')
-            ->with('sideQuests')
-            ->get()
-            ->map(function (CampaignStop $campaignStop) use ($jobs) {
+            ->with('sideQuests');
+
+        if ($lastCampaignStopID) {
+            $query->where('id', '>', $lastCampaignStopID);
+        }
+        return $query;
+    }
+
+    protected function getProcessResultJobs(Collection $campaignStops)
+    {
+        $jobs = collect();
+        $campaignStops->map(function (CampaignStop $campaignStop) use ($jobs) {
             $campaignStop->sideQuests->each(function (SideQuest $sideQuest) use ($campaignStop, $jobs) {
                 $jobs->push(new ProcessSideQuestResultJob($campaignStop, $sideQuest));
             });
         });
         return $jobs->toArray();
+    }
+
+    protected function getFinalizeWeekArgs(Collection $campaignStops, int $currentFinalizeWeekStep)
+    {
+        if ($this->moreCampaignStopsNeedProcessing($campaignStops)) {
+            return [
+                'step' => $currentFinalizeWeekStep,
+                'extra' => [
+                    'last_campaign_stop_id' => $campaignStops->last()->id
+                ]
+            ];
+        }
+
+        return [
+            'step' => $currentFinalizeWeekStep + 1,
+            'extra' => []
+        ];
+    }
+
+    protected function moreCampaignStopsNeedProcessing(Collection $campaignStops)
+    {
+        return $campaignStops->count() >= $this->maxCampaignStopsQueried
+            && $this->buildCampaignStopsQuery($campaignStops->last()->id)->count() > 0;
+    }
+
+    /**
+     * @param int $maxCampaignStopsQueried
+     * @return ProcessCurrentWeekSideQuestsAction
+     */
+    public function setMaxCampaignStopsQueried(int $maxCampaignStopsQueried): ProcessCurrentWeekSideQuestsAction
+    {
+        $this->maxCampaignStopsQueried = $maxCampaignStopsQueried;
+        return $this;
     }
 }
