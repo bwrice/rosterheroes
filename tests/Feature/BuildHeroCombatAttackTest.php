2d96cad88ea78ff67a252f25936b017ac103a2ec
Brian Rice <bwrice83@gmail.com>
2020-03-06 21:53:33 -0500

    test damage and combat speed numbers match

--- tests/Feature/BuildHeroCombatAttackTest.php
+++ tests/Feature/BuildHeroCombatAttackTest.php
@@ -3,8 +3,12 @@
 namespace Tests\Feature;
 
 use App\Domain\Actions\Combat\BuildHeroCombatAttack;
+use App\Domain\Actions\Combat\CalculateCombatDamage;
 use App\Domain\Combat\Attacks\CombatAttackInterface;
 use App\Domain\Combat\Attacks\HeroCombatAttack;
+use App\Domain\Models\Attack;
+use App\Domain\Models\Hero;
+use App\Domain\Models\Item;
 use App\Factories\Models\AttackFactory;
 use App\Factories\Models\HeroFactory;
 use App\Factories\Models\ItemFactory;
@@ -14,18 +18,68 @@ use Tests\TestCase;
 
 class BuildHeroCombatAttackTest extends TestCase
 {
+    /** @var Hero */
+    protected $hero;
+
+    /** @var Item */
+    protected $item;
+
+    /** @var Attack */
+    protected $attack;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->hero = HeroFactory::new()->create();
+        $this->item = ItemFactory::new()->create();
+        $this->attack = AttackFactory::new()->create();
+    }
+
     /**
      * @test
      */
     public function it_will_build_a_hero_combat_attack()
     {
-        $hero = HeroFactory::new()->create();
-        $item = ItemFactory::new()->create();
-        $attack = AttackFactory::new()->create();
         /** @var BuildHeroCombatAttack $domainAction */
         $domainAction = app(BuildHeroCombatAttack::class);
-        $heroCombatAttack = $domainAction->execute($attack, $item, $hero);
+        $heroCombatAttack = $domainAction->execute($this->attack, $this->item, $this->hero);
         $this->assertTrue($heroCombatAttack instanceof HeroCombatAttack);
         $this->assertTrue($heroCombatAttack instanceof CombatAttackInterface);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_have_the_expected_combat_damage()
+    {
+        /** @var BuildHeroCombatAttack $domainAction */
+        $domainAction = app(BuildHeroCombatAttack::class);
+        $heroCombatAttack = $domainAction->execute($this->attack, $this->item, $this->hero);
+
+        $this->item->setUsesItems($this->hero);
+        $this->attack->setHasAttacks($this->item);
+
+        /** @var CalculateCombatDamage $calculateDamage */
+        $calculateDamage = app(CalculateCombatDamage::class);
+        $expectedDamage = $calculateDamage->execute($this->attack, $this->hero);
+
+        $this->assertEquals($expectedDamage, $heroCombatAttack->getDamage());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_have_the_expected_combat_speed()
+    {
+        /** @var BuildHeroCombatAttack $domainAction */
+        $domainAction = app(BuildHeroCombatAttack::class);
+        $heroCombatAttack = $domainAction->execute($this->attack, $this->item, $this->hero);
+
+        $this->item->setUsesItems($this->hero);
+        $this->attack->setHasAttacks($this->item);
+        $expectedSpeed = $this->attack->getCombatSpeed();
+        $diff = abs($expectedSpeed - $heroCombatAttack->getCombatSpeed());
+        $this->assertLessThan(PHP_FLOAT_EPSILON, $diff);
     }
 }
