69cadece4af32231da583f29d9c4686256550ee7
Brian Rice <bwrice83@gmail.com>
2020-03-10 20:59:30 -0400

    add more getReadyAttacks() tests for CombatSquad

--- tests/Feature/CombatSquadTest.php
+++ tests/Feature/CombatSquadTest.php
@@ -44,4 +44,65 @@ class CombatSquadTest extends TestCase
         $readyAttacks = $combatSquad->getReadyAttacks(1); //moment shouldn't matter
         $this->assertEquals(3, $readyAttacks->count());
     }
+
+    /**
+     * @test
+     */
+    public function a_back_line_hero_wont_have_front_line_attacks_available_if_a_front_line_hero_exists()
+    {
+        // set speed to 100 so it's always ready
+        $heroCombatAttackFactory = HeroCombatAttackFactory::new()->withCombatSpeed(100);
+        $frontLineAttackFactory = $heroCombatAttackFactory->withAttackerPosition(CombatPosition::FRONT_LINE);
+        $backLineAttackFactory = $heroCombatAttackFactory->withAttackerPosition(CombatPosition::BACK_LINE);
+        $highGroundAttackFactory = $heroCombatAttackFactory->withAttackerPosition(CombatPosition::HIGH_GROUND);
+
+        $frontLineHeroFactory = CombatHeroFactory::new()->withHeroCombatAttacks(collect([
+            $frontLineAttackFactory,
+            $backLineAttackFactory,
+            $highGroundAttackFactory
+        ]))->withCombatPosition(CombatPosition::BACK_LINE);
+
+        $backLineHeroFactory = CombatHeroFactory::new()->withCombatPosition(CombatPosition::FRONT_LINE);
+
+        $combatSquad = CombatSquadFactory::new()->withCombatHeroes(collect([
+            $frontLineHeroFactory,
+            $backLineHeroFactory
+        ]))->create();
+
+        $combatSquad->updateCombatPositions(CombatPosition::all());
+
+        $readyAttacks = $combatSquad->getReadyAttacks(1); //moment shouldn't matter
+        $this->assertEquals(2, $readyAttacks->count());
+    }
+
+    /**
+     * @test
+     */
+    public function a_high_ground_hero_will_have_combat_attacks_for_all_attacker_positions_if_no_back_or_front_line_heroes()
+    {
+        // set speed to 100 so it's always ready
+        $heroCombatAttackFactory = HeroCombatAttackFactory::new()->withCombatSpeed(100);
+        $frontLineAttackFactory = $heroCombatAttackFactory->withAttackerPosition(CombatPosition::FRONT_LINE);
+        $backLineAttackFactory = $heroCombatAttackFactory->withAttackerPosition(CombatPosition::BACK_LINE);
+        $highGroundAttackFactory = $heroCombatAttackFactory->withAttackerPosition(CombatPosition::HIGH_GROUND);
+
+        $frontLineHeroFactory = CombatHeroFactory::new()->withHeroCombatAttacks(collect([
+            $frontLineAttackFactory,
+            $backLineAttackFactory,
+            $highGroundAttackFactory
+        ]))->withCombatPosition(CombatPosition::HIGH_GROUND);
+
+        // Another high-ground hero
+        $backLineHeroFactory = CombatHeroFactory::new()->withCombatPosition(CombatPosition::HIGH_GROUND);
+
+        $combatSquad = CombatSquadFactory::new()->withCombatHeroes(collect([
+            $frontLineHeroFactory,
+            $backLineHeroFactory
+        ]))->create();
+
+        $combatSquad->updateCombatPositions(CombatPosition::all());
+
+        $readyAttacks = $combatSquad->getReadyAttacks(1); //moment shouldn't matter
+        $this->assertEquals(3, $readyAttacks->count());
+    }
 }
