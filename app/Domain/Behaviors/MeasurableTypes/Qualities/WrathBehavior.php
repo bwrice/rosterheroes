897f892d180c3eee7c25f215297d4a8da63b6e58
Brian Rice <bwrice83@gmail.com>
2020-03-13 20:23:20 -0400

    add wrath behavior stat type relations

--- app/Domain/Behaviors/MeasurableTypes/Qualities/WrathBehavior.php
+++ app/Domain/Behaviors/MeasurableTypes/Qualities/WrathBehavior.php
@@ -5,9 +5,21 @@ namespace App\Domain\Behaviors\MeasurableTypes\Qualities;
 
 
 use App\Domain\Models\MeasurableType;
+use App\Domain\Models\StatType;
 
 class WrathBehavior extends QualityBehavior
 {
+    protected $statTypeNames = [
+        StatType::REC_YARD,
+        StatType::HOME_RUN,
+        StatType::RUN_SCORED,
+        StatType::PITCHING_WIN,
+        StatType::PITCHING_SAVE,
+        StatType::HIT_AGAINST,
+        StatType::SHOT_ON_GOAL,
+        StatType::STEAL,
+        StatType::TURNOVER,
+    ];
 
     public function __construct()
     {
