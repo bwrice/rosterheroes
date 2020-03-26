244b8444c70b4361c2c6a1bd07d65a3b8d519491
Brian Rice <bwrice83@gmail.com>
2020-03-26 17:23:57 -0400

    convert item-type, material pivot to item-base, material-type

---
+++ database/migrations/2018_12_03_031901_create_item_base_material_type_pivot.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemBaseMaterialTypePivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_base_material_type', function (Blueprint $table) {
+            $table->integer('item_base_id')->unsigned();
+            $table->integer('material_type_id')->unsigned();
+            $table->primary(['item_base_id', 'material_type_id'], 'item_base_material_type_primary_key');
+            $table->timestamps();
+        });
+
+        Schema::table('item_base_material_type', function (Blueprint $table) {
+            $table->foreign('item_base_id')->references('id')->on('item_bases');
+            $table->foreign('material_type_id')->references('id')->on('material_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_type_material_type');
+    }
+}
