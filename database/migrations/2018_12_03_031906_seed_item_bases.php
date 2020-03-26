710c38a07ebdcf14917aaffb822b0f5c49770398
Brian Rice <bwrice83@gmail.com>
2020-03-26 19:39:41 -0400

    add material type relations to item bases when seeding

--- database/migrations/2018_12_03_031906_seed_item_bases.php
+++ database/migrations/2018_12_03_031906_seed_item_bases.php
@@ -1,57 +1,254 @@
 <?php
 
 use App\Domain\Models\ItemBase;
-use App\Helpers\ModelNameSeeding\ModelNameSeederMigration;
+use App\Domain\Models\MaterialType;
+use Illuminate\Database\Migrations\Migration;
 
-class SeedItemBases extends ModelNameSeederMigration
+class SeedItemBases extends Migration
 {
-    /**
-     * @return string
-     */
-    protected function getModelClass(): string
+    public function up()
     {
-        return ItemBase::class;
-    }
-
-    /**
-     * @return array
-     */
-    protected function getSeedNames(): array
-    {
-        return [
-            ItemBase::DAGGER,
-            ItemBase::SWORD,
-            ItemBase::AXE,
-            ItemBase::MACE,
-            ItemBase::BOW,
-            ItemBase::CROSSBOW,
-            ItemBase::THROWING_WEAPON,
-            ItemBase::POLEARM,
-            ItemBase::TWO_HAND_SWORD,
-            ItemBase::TWO_HAND_AXE,
-            ItemBase::WAND,
-            ItemBase::ORB,
-            ItemBase::STAFF,
-            ItemBase::PSIONIC_ONE_HAND,
-            ItemBase::PSIONIC_TWO_HAND,
-            ItemBase::SHIELD,
-            ItemBase::PSIONIC_SHIELD,
-            ItemBase::HELMET,
-            ItemBase::CAP,
-            ItemBase::HEAVY_ARMOR,
-            ItemBase::LIGHT_ARMOR,
-            ItemBase::LEGGINGS,
-            ItemBase::ROBES,
-            ItemBase::GLOVES,
-            ItemBase::GAUNTLETS,
-            ItemBase::SHOES,
-            ItemBase::BOOTS,
-            ItemBase::BELT,
-            ItemBase::SASH,
-            ItemBase::NECKLACE,
-            ItemBase::BRACELET,
-            ItemBase::RING,
-            ItemBase::CROWN
+        $data = [
+            [
+                'name' => ItemBase::DAGGER,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::METAL,
+                    MaterialType::GEMSTONE,
+                ]
+            ],
+            [
+                'name' => ItemBase::SWORD,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::METAL
+                ]
+            ],
+            [
+                'name' => ItemBase::AXE,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::METAL
+                ]
+            ],
+            [
+                'name' => ItemBase::MACE,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::METAL
+                ]
+            ],
+            [
+                'name' => ItemBase::BOW,
+                'material_types' => [
+                    MaterialType::WOOD
+                ]
+            ],
+            [
+                'name' => ItemBase::CROSSBOW,
+                'material_types' => [
+                    MaterialType::WOOD
+                ]
+            ],
+            [
+                'name' => ItemBase::THROWING_WEAPON,
+                'material_types' => [
+                    MaterialType::WOOD,
+                    MaterialType::METAL,
+                ]
+            ],
+            [
+                'name' => ItemBase::POLEARM,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::METAL
+                ]
+            ],
+            [
+                'name' => ItemBase::TWO_HAND_SWORD,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::METAL
+                ]
+            ],
+            [
+                'name' => ItemBase::TWO_HAND_AXE,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::METAL
+                ]
+            ],
+            [
+                'name' => ItemBase::WAND,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::GEMSTONE,
+                    MaterialType::WOOD
+                ]
+            ],
+            [
+                'name' => ItemBase::ORB,
+                'material_types' => [
+                    MaterialType::GEMSTONE
+                ]
+            ],
+            [
+                'name' => ItemBase::STAFF,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::GEMSTONE,
+                    MaterialType::WOOD
+                ]
+            ],
+            [
+                'name' => ItemBase::PSIONIC_ONE_HAND,
+                'material_types' => [
+                    MaterialType::PSIONIC
+                ]
+            ],
+            [
+                'name' => ItemBase::PSIONIC_TWO_HAND,
+                'material_types' => [
+                    MaterialType::PSIONIC
+                ]
+            ],
+            [
+                'name' => ItemBase::SHIELD,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::METAL
+                ]
+            ],
+            [
+                'name' => ItemBase::PSIONIC_SHIELD,
+                'material_types' => [
+                    MaterialType::PSIONIC
+                ]
+            ],
+            [
+                'name' => ItemBase::HELMET,
+                'material_types' => [
+                    MaterialType::BONE,
+                    MaterialType::METAL
+                ]
+            ],
+            [
+                'name' => ItemBase::CAP,
+                'material_types' => [
+                    MaterialType::CLOTH,
+                    MaterialType::HIDE
+                ]
+            ],
+            [
+                'name' => ItemBase::HEAVY_ARMOR,
+                'material_types' => [
+                    MaterialType::METAL,
+                    MaterialType::HIDE
+                ]
+            ],
+            [
+                'name' => ItemBase::LIGHT_ARMOR,
+                'material_types' => [
+                    MaterialType::METAL,
+                    MaterialType::HIDE
+                ]
+            ],
+            [
+                'name' => ItemBase::LEGGINGS,
+                'material_types' => [
+                    MaterialType::CLOTH,
+                    MaterialType::HIDE
+                ]
+            ],
+            [
+                'name' => ItemBase::ROBES,
+                'material_types' => [
+                    MaterialType::CLOTH,
+                ]
+            ],
+            [
+                'name' => ItemBase::GLOVES,
+                'material_types' => [
+                    MaterialType::CLOTH,
+                    MaterialType::HIDE
+                ]
+            ],
+            [
+                'name' => ItemBase::GAUNTLETS,
+                'material_types' => [
+                    MaterialType::METAL,
+                    MaterialType::HIDE
+                ]
+            ],
+            [
+                'name' => ItemBase::SHOES,
+                'material_types' => [
+                    MaterialType::CLOTH,
+                ]
+            ],
+            [
+                'name' => ItemBase::BOOTS,
+                'material_types' => [
+                    MaterialType::METAL,
+                    MaterialType::HIDE
+                ]
+            ],
+            [
+                'name' => ItemBase::BELT,
+                'material_types' => [
+                    MaterialType::METAL,
+                    MaterialType::HIDE
+                ]
+            ],
+            [
+                'name' => ItemBase::SASH,
+                'material_types' => [
+                    MaterialType::CLOTH,
+                ]
+            ],
+            [
+                'name' => ItemBase::NECKLACE,
+                'material_types' => [
+                    MaterialType::GEMSTONE,
+                    MaterialType::PRECIOUS_METAL,
+                ]
+            ],
+            [
+                'name' => ItemBase::BRACELET,
+                'material_types' => [
+                    MaterialType::GEMSTONE,
+                    MaterialType::PRECIOUS_METAL,
+                ]
+            ],
+            [
+                'name' => ItemBase::RING,
+                'material_types' => [
+                    MaterialType::GEMSTONE,
+                    MaterialType::PRECIOUS_METAL,
+                ]
+            ],
+            [
+                'name' => ItemBase::CROWN,
+                'material_types' => [
+                    MaterialType::GEMSTONE,
+                    MaterialType::PRECIOUS_METAL,
+                ]
+            ],
         ];
+
+        $materialTypes = MaterialType::all();
+
+        foreach ($data as $itemBaseData) {
+            /** @var ItemBase $itemBase */
+            $itemBase = ItemBase::query()->create([
+                'name' => $itemBaseData['name']
+            ]);
+
+            $typesToAttach = $materialTypes->filter(function (MaterialType $materialType) use ($itemBaseData) {
+                return in_array($materialType->name, $itemBaseData['material_types']);
+            });
+
+            $itemBase->materialTypes()->saveMany($typesToAttach);
+        }
     }
 }
