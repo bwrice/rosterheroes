63dd93fdd32acead8ae4114fdc253d076014e77b
Brian Rice <bwrice83@gmail.com>
2020-03-29 12:35:17 -0400

    save schedule status changes when updating games

--- app/Domain/Models/Game.php
+++ app/Domain/Models/Game.php
@@ -20,6 +20,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $home_team_id
  * @property int $away_team_id
  * @property string $external_id
+ * @property string $schedule_status
  * @property CarbonImmutable $starts_at
  * @property CarbonImmutable|null $finalized_at
  *
