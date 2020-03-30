59aa555df729b9292541906562420576a78a9533
Brian Rice <bwrice83@gmail.com>
2020-03-29 20:03:57 -0400

    create SideQuestEventFactory

---
+++ app/Factories/Models/SideQuestEventFactory.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace App\Factories\Models;
+
+
+use App\SideQuestEvent;
+use Illuminate\Support\Str;
+
+class SideQuestEventFactory
+{
+    protected $moment = 1;
+
+    protected $sideQuestResultID;
+
+    protected $eventType = SideQuestEvent::TYPE_BATTLEGROUND_SET;
+
+    public static function new()
+    {
+        return new self();
+    }
+
+    public function create(array $extra = [])
+    {
+        /** @var SideQuestEvent $sideQuestEvent */
+        $sideQuestEvent = SideQuestEvent::query()->create(array_merge([
+            'uuid' => (string) Str::uuid(),
+            'moment' => $this->moment,
+            'side_quest_result' => $this->getSideQuestResultID(),
+            'event_type' => $this->eventType,
+            'data' => []
+        ], $extra));
+
+        return $sideQuestEvent->fresh();
+    }
+
+    protected function withMoment(int $moment)
+    {
+        $clone = clone $this;
+        $clone->moment = $moment;
+        return $clone;
+    }
+
+    protected function withSideQuestResultID(int $sideQuestResultID)
+    {
+        $clone = clone $this;
+        $clone->sideQuestResultID = $sideQuestResultID;
+        return $clone;
+    }
+
+    protected function withEventType(string $eventType)
+    {
+        $clone = clone $this;
+        $clone->eventType = $eventType;
+        return $clone;
+    }
+
+    protected function getSideQuestResultID()
+    {
+        if ($this->sideQuestResultID) {
+            return $this->sideQuestResultID;
+        }
+        return SideQuestResultFactory::new()->create()->id;
+    }
+}
