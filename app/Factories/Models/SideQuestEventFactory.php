2a725d826485336883cb90b67ae7b6284280eea8
Brian Rice <bwrice83@gmail.com>
2020-03-30 13:53:08 -0400

    add heroKillsMinion() method to SideQuestEventFactory

--- app/Factories/Models/SideQuestEventFactory.php
+++ app/Factories/Models/SideQuestEventFactory.php
@@ -4,6 +4,9 @@
 namespace App\Factories\Models;
 
 
+use App\Domain\Combat\Attacks\HeroCombatAttack;
+use App\Domain\Combat\Combatants\CombatHero;
+use App\Domain\Combat\Combatants\CombatMinion;
 use App\SideQuestEvent;
 use Illuminate\Support\Str;
 
@@ -12,6 +15,8 @@ class SideQuestEventFactory
     protected $moment = 1;
 
     protected $sideQuestResultID;
+
+    protected $data = [];
 
     protected $eventType = SideQuestEvent::TYPE_BATTLEGROUND_SET;
 
@@ -28,7 +33,7 @@ class SideQuestEventFactory
             'moment' => $this->moment,
             'side_quest_result_id' => $this->getSideQuestResultID(),
             'event_type' => $this->eventType,
-            'data' => []
+            'data' => $this->data
         ], $extra));
 
         return $sideQuestEvent->fresh();
@@ -61,5 +66,20 @@ class SideQuestEventFactory
             return $this->sideQuestResultID;
         }
         return SideQuestResultFactory::new()->create()->id;
+    }
+
+    public function heroKillsMinion(CombatHero $combatHero, HeroCombatAttack $heroCombatAttack, CombatMinion $combatMinion)
+    {
+        $clone = clone $this;
+        $clone->eventType = SideQuestEvent::TYPE_HERO_KILLS_MINION;
+        $clone->data = [
+            'damage' => rand(10, 999),
+            'combatHero' => $combatHero->toArray(),
+            'heroCombatAttack' => $heroCombatAttack->toArray(),
+            'combatMinion' => $combatMinion->toArray(),
+            'staminaCost' => rand(5, 15),
+            'manaCost' => rand(2, 10)
+        ];
+        return $clone;
     }
 }
