59fb164b57da52a665e75d86cb4417a8e3d8932a
Brian Rice <bwrice83@gmail.com>
2020-03-31 10:48:48 -0400

    create ProcessSideQuestRewardsJob

---
+++ app/Jobs/ProcessSideQuestRewardsJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Domain\Actions\ProcessSideQuestRewards;
+use App\SideQuestResult;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessSideQuestRewardsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    /**
+     * @var SideQuestResult
+     */
+    public $sideQuestResult;
+
+    /**
+     * ProcessSideQuestRewardsJob constructor.
+     * @param SideQuestResult $sideQuestResult
+     */
+    public function __construct(SideQuestResult $sideQuestResult)
+    {
+        $this->sideQuestResult = $sideQuestResult;
+    }
+
+    /**
+     * @param ProcessSideQuestRewards $processSideQuestRewards
+     * @throws \Exception
+     */
+    public function handle(ProcessSideQuestRewards $processSideQuestRewards)
+    {
+        $processSideQuestRewards->execute($this->sideQuestResult);
+    }
+}
