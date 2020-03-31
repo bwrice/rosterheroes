2634ff614926b126a866ee2cc1758bafb2f8cddd
Brian Rice <bwrice83@gmail.com>
2020-03-31 14:53:42 -0400

    rename domain action

---
+++ app/Domain/Actions/Testing/AutoJoinQuests.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Domain\Actions\Testing;
+
+
+use App\Domain\Actions\JoinQuestAction;
+use App\Domain\Actions\SquadBorderTravelAction;
+use App\Domain\Models\Squad;
+
+class AutoJoinQuests
+{
+    public function __construct(SquadBorderTravelAction $borderTravelAction, JoinQuestAction $joinQuestAction)
+    {
+    }
+
+    public function execute(Squad $squad)
+    {
+        foreach (range (1, $squad->getQuestsPerWeek()) as $count) {
+            if ($count > 1) {
+                $squad = $squad->fresh();
+            }
+        }
+    }
+}
