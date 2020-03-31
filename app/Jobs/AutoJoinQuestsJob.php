56206d48ba28dbae739b0e89da57e80afd265565
Brian Rice <bwrice83@gmail.com>
2020-03-31 14:55:06 -0400

    move loop logic outside of the job

--- app/Jobs/AutoJoinQuestsJob.php
+++ app/Jobs/AutoJoinQuestsJob.php
@@ -2,7 +2,7 @@
 
 namespace App\Jobs;
 
-use App\Domain\Actions\Testing\AutoJoinQuestAction;
+use App\Domain\Actions\Testing\AutoJoinQuests;
 use App\Domain\Models\Squad;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -25,15 +25,10 @@ class AutoJoinQuestsJob implements ShouldQueue
     }
 
     /**
-     * @param AutoJoinQuestAction $domainAction
+     * @param AutoJoinQuests $domainAction
      */
-    public function handle(AutoJoinQuestAction $domainAction)
+    public function handle(AutoJoinQuests $domainAction)
     {
-        foreach (range (1, $this->squad->getQuestsPerWeek()) as $count) {
-            if ($count > 1) {
-                $this->squad = $this->squad->fresh();
-            }
-            $domainAction->execute($this->squad);
-        }
+        $domainAction->execute($this->squad);
     }
 }
