ab88d6698ff4d2f23531f094406324c6b85481a2
Brian Rice <bwrice83@gmail.com>
2020-03-31 14:54:53 -0400

    add methods to QuestFactory

--- app/Factories/Models/QuestFactory.php
+++ app/Factories/Models/QuestFactory.php
@@ -7,10 +7,16 @@ namespace App\Factories\Models;
 use App\Domain\Models\Province;
 use App\Domain\Models\Quest;
 use App\Domain\Models\TravelType;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
 class QuestFactory
 {
+    protected $provinceID;
+
+    /** @var Collection|null */
+    protected $sideQuestFactories;
+
     public static function new(): self
     {
         return new self();
@@ -28,9 +34,35 @@ class QuestFactory
             'name' => 'Test Quest ' . rand(1, 999999),
             'level' => 200,
             'percent' => 100,
-            'province_id' => Province::query()->inRandomOrder()->first()->id,
+            'province_id' => $this->getProvinceID(),
             'travel_type_id' => TravelType::query()->inRandomOrder()->first()->id,
         ], $extra));
+
+        if ($this->sideQuestFactories) {
+            $this->sideQuestFactories->each(function (SideQuestFactory $sideQuestFactory) use ($quest) {
+                $sideQuestFactory->forQuestID($quest->id)->create();
+            });
+        }
+
         return $quest;
+    }
+
+    public function withProvinceID(int $provinceID)
+    {
+        $clone = clone $this;
+        $clone->provinceID = $provinceID;
+        return $clone;
+    }
+
+    protected function getProvinceID()
+    {
+        return $this->provinceID ?: Province::query()->inRandomOrder()->first()->id;
+    }
+
+    public function withSideQuests(Collection $sideQuestFactories)
+    {
+        $clone = clone $this;
+        $clone->sideQuestFactories = $sideQuestFactories;
+        return $clone;
     }
 }
