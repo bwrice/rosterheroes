42c53056c865553193b9af9ae1d81c7e9c9bf672
Brian Rice <bwrice83@gmail.com>
2020-04-04 09:33:06 -0400

    increase werewolf combat speed

--- app/Domain/Behaviors/EnemyTypes/WerewolfBehavior.php
+++ app/Domain/Behaviors/EnemyTypes/WerewolfBehavior.php
@@ -74,7 +74,7 @@ class WerewolfBehavior extends EnemyTypeBehavior
      */
     public function adjustCombatSpeed(float $combatSpeed, int $enemyLevel, CombatPosition $startingCombatPosition): float
     {
-        return $combatSpeed * (1.5 + $enemyLevel/80);
+        return $combatSpeed * (1.6 + $enemyLevel/80);
     }
 
     /**
