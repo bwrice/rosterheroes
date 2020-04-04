c61355cdafb268beafbd51700cf5e9719f788807
Brian Rice <bwrice83@gmail.com>
2020-04-04 09:53:53 -0400

    implement combat getters in VampireBehavior

--- app/Domain/Behaviors/EnemyTypes/VampireBehavior.php
+++ app/Domain/Behaviors/EnemyTypes/VampireBehavior.php
@@ -4,12 +4,120 @@
 namespace App\Domain\Behaviors\EnemyTypes;
 
 
+use App\Domain\Models\CombatPosition;
+
 class VampireBehavior extends EnemyTypeBehavior
 {
-    protected $healthModifierBonus = .3;
-    protected $protectionModifierBonus = -.25;
-    protected $blockModifierBonus = -.5;
-    protected $baseDamageModifierBonus = .4;
-    protected $damageMultiplierModifierBonus = .4;
-    protected $combatSpeedModifierBonus = 0;
+    /**
+     * @param int $enemyLevel
+     * @param CombatPosition $startingCombatPosition
+     * @return int
+     */
+    public function getStartingHealth(int $enemyLevel, CombatPosition $startingCombatPosition): int
+    {
+        $base = 300;
+        $levelModifier = 11;
+        switch ($startingCombatPosition->name) {
+            case CombatPosition::FRONT_LINE:
+                $base += 300;
+                $levelModifier += 22;
+                break;
+            case CombatPosition::BACK_LINE:
+                $base += 125;
+                $levelModifier += 11;
+                break;
+        }
+        return $base + ($levelModifier * $enemyLevel);
+    }
+
+    /**
+     * @param int $enemyLevel
+     * @param CombatPosition $startingCombatPosition
+     * @return int
+     */
+    public function getProtection(int $enemyLevel, CombatPosition $startingCombatPosition): int
+    {
+        $base = 15;
+        $levelModifier = 2;
+        switch ($startingCombatPosition->name) {
+            case CombatPosition::FRONT_LINE:
+                $base += 120;
+                $levelModifier += 3;
+                break;
+            case CombatPosition::BACK_LINE:
+                $base += 20;
+                $levelModifier += 1;
+                break;
+        }
+        return $base + ($levelModifier * $enemyLevel);
+    }
+
+    /**
+     * @param float $damageProperty
+     * @param int $enemyLevel
+     * @param CombatPosition $startingCombatPosition
+     * @return int|float
+     */
+    protected function adjustDamageProperty(float $damageProperty, int $enemyLevel, CombatPosition $startingCombatPosition)
+    {
+        $modifier = 1.65 + ($enemyLevel / 30);
+        switch ($startingCombatPosition->name) {
+            case CombatPosition::HIGH_GROUND:
+                $modifier *= 1.8;
+                break;
+            case CombatPosition::BACK_LINE:
+                $modifier *= 1.7;
+                break;
+        }
+        return $damageProperty * $modifier;
+    }
+
+    /**
+     * @param float $baseDamage
+     * @param int $enemyLevel
+     * @param CombatPosition $startingCombatPosition
+     * @return float
+     */
+    public function adjustBaseDamage(float $baseDamage, int $enemyLevel, CombatPosition $startingCombatPosition): float
+    {
+        return $this->adjustDamageProperty($baseDamage, $enemyLevel, $startingCombatPosition);
+    }
+
+    /**
+     * @param float $damageMultiplier
+     * @param int $enemyLevel
+     * @param CombatPosition $startingCombatPosition
+     * @return float
+     */
+    public function adjustDamageMultiplier(float $damageMultiplier, int $enemyLevel, CombatPosition $startingCombatPosition): float
+    {
+        return $this->adjustDamageProperty($damageMultiplier, $enemyLevel, $startingCombatPosition);
+    }
+
+    /**
+     * @param float $combatSpeed
+     * @param int $enemyLevel
+     * @param CombatPosition $startingCombatPosition
+     * @return float
+     */
+    public function adjustCombatSpeed(float $combatSpeed, int $enemyLevel, CombatPosition $startingCombatPosition): float
+    {
+        return $combatSpeed * (.75 + $enemyLevel/150);
+    }
+
+    /**
+     * @param int $enemyLevel
+     * @param CombatPosition $startingCombatPosition
+     * @return float
+     */
+    public function getBlockChancePercent(int $enemyLevel, CombatPosition $startingCombatPosition): float
+    {
+        switch ($startingCombatPosition->name) {
+            case CombatPosition::HIGH_GROUND:
+            case CombatPosition::BACK_LINE:
+                return 0;
+        }
+        $blockChance = 20 + $enemyLevel/5;
+        return min(70, $blockChance);
+    }
 }
