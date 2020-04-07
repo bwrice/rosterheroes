3d56e5d59cc5f4839d1b9170e615f959a3fd5dbb
Brian Rice <bwrice83@gmail.com>
2020-04-07 00:26:14 -0400

    add reference_id to old SkirmishBlueprint factory

--- database/factories/SkirmishBlueprintModelFactory.php
+++ database/factories/SkirmishBlueprintModelFactory.php
@@ -7,5 +7,6 @@ use Faker\Generator as Faker;
 $factory->define(\App\Domain\Models\SideQuestBlueprint::class, function (Faker $faker) {
     return [
         'name' => $faker->streetName,
+        'reference_id' => (string) \Illuminate\Support\Str::random(8)
     ];
 });
