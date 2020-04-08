09ea646226bc0d86fa1916dbd26b037bc7d30b38
Brian Rice <bwrice83@gmail.com>
2020-04-08 01:03:23 -0400

    optimize query in BuildWeeklyPlayerSpiritsAction

--- app/Domain/Actions/BuildWeeklyPlayerSpiritsAction.php
+++ app/Domain/Actions/BuildWeeklyPlayerSpiritsAction.php
@@ -4,21 +4,29 @@
 namespace App\Domain\Actions;
 
 
+use App\Domain\Collections\GameCollection;
+use App\Domain\Collections\PlayerCollection;
 use App\Domain\Models\Game;
 use App\Domain\Models\Player;
 use App\Domain\Models\Week;
 use App\Exceptions\InvalidWeekException;
 use App\Jobs\CreatePlayerSpiritJob;
+use Laravel\Telescope\Telescope;
 
 class BuildWeeklyPlayerSpiritsAction
 {
     public function execute(Week $week)
     {
-        $games = Game::query()->validForWeek($week)->get();
-
-        $games->each(function (Game $game) use ($week) {
-            Player::query()->withNoSpiritForGame($game)->get()->each(function (Player $player) use ($game, $week) {
-                CreatePlayerSpiritJob::dispatch($week, $game, $player);
+        Game::query()->validForWeek($week)->with([
+            'homeTeam.players',
+            'awayTeam.players'
+        ])->chunk(10, function (GameCollection $games) use ($week) {
+            $games->each(function (Game $game) use ($week) {
+                Player::query()->withNoSpiritForGame($game)->chunk(100, function (PlayerCollection $players) use ($game, $week) {
+                    $players ->each(function (Player $player) use ($game, $week) {
+                        CreatePlayerSpiritJob::dispatch($week, $game, $player);
+                    });
+                });
             });
         });
     }
