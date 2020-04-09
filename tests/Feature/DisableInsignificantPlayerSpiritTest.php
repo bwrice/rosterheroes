57ebb56f6f9b36dce783f5930b27d43587d5ee2b
Brian Rice <bwrice83@gmail.com>
2020-04-08 21:27:57 -0400

    add test provides_it_will_disable_player_spirits_with_multiple_games_without_any_stats

--- tests/Feature/DisableInsignificantPlayerSpiritTest.php
+++ tests/Feature/DisableInsignificantPlayerSpiritTest.php
@@ -9,6 +9,7 @@ use App\Factories\Models\GameFactory;
 use App\Factories\Models\PlayerFactory;
 use App\Factories\Models\PlayerGameLogFactory;
 use App\Factories\Models\PlayerSpiritFactory;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Date;
@@ -16,6 +17,8 @@ use Tests\TestCase;
 
 class DisableInsignificantPlayerSpiritTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * @test
      */
@@ -29,6 +32,7 @@ class DisableInsignificantPlayerSpiritTest extends TestCase
         $playerSpirit = PlayerSpiritFactory::new()->withPlayerGameLog($gameLogFactory)->create();
 
         $disableSpy = \Mockery::spy(DisablePlayerSpirit::class);
+        app()->instance(DisablePlayerSpirit::class, $disableSpy);
 
         /** @var DisableInsignificantPlayerSpirit $domainAction */
         $domainAction = app(DisableInsignificantPlayerSpirit::class);
@@ -36,5 +40,67 @@ class DisableInsignificantPlayerSpiritTest extends TestCase
 
         $this->assertFalse($result);
         $disableSpy->shouldNotHaveReceived('execute');
+    }
+
+    /**
+     * @test
+     * @dataProvider provides_it_will_disable_player_spirits_with_multiple_games_without_any_stats
+     * @param $positionName
+     * @param $previousGamesCount
+     */
+    public function it_will_disable_player_spirits_with_multiple_games_without_any_stats($positionName, $previousGamesCount)
+    {
+        $game = GameFactory::new()->create([
+            'starts_at' => Date::now()->addDays(5)
+        ]);
+        $player = PlayerFactory::new()->withPosition(Position::forName($positionName))->create();
+        $baseGameLogFactory = PlayerGameLogFactory::new()->forPlayer($player);
+        $playerSpiritsGameLog = $baseGameLogFactory->forGame($game);
+        $playerSpirit = PlayerSpiritFactory::new()->withPlayerGameLog($playerSpiritsGameLog)->create();
+
+
+        /*
+         * Make game logs without stats
+         */
+        foreach (range(1, $previousGamesCount) as $count) {
+            $game = GameFactory::new()->create([
+                'starts_at' => Date::now()->subWeeks($count)
+            ]);
+            $baseGameLogFactory->forGame($game)->create();
+        }
+
+        /*
+         * Make older game logs with stats
+         */
+        foreach (range(1, $previousGamesCount) as $count) {
+            $game = GameFactory::new()->create([
+                'starts_at' => Date::now()->subWeeks($count + 1 + $previousGamesCount)
+            ]);
+            $baseGameLogFactory->forGame($game)->withStats()->create();
+        }
+
+        // verify we have previous game logs and the one for the created player spirit
+        $expectedLogsCount = (2 * $previousGamesCount) + 1;
+        $this->assertEquals($expectedLogsCount, $player->playerGameLogs()->count());
+
+        $disableSpy = \Mockery::spy(DisablePlayerSpirit::class);
+        app()->instance(DisablePlayerSpirit::class, $disableSpy);
+
+        /** @var DisableInsignificantPlayerSpirit $domainAction */
+        $domainAction = app(DisableInsignificantPlayerSpirit::class);
+        $result = $domainAction->execute($playerSpirit);
+
+        $this->assertTrue($result);
+        $disableSpy->shouldHaveReceived('execute');
+    }
+
+    public function provides_it_will_disable_player_spirits_with_multiple_games_without_any_stats()
+    {
+        return [
+            Position::QUARTERBACK => [
+                'positionName' => Position::QUARTERBACK,
+                'previousGamesCount' => 5
+            ]
+        ];
     }
 }
