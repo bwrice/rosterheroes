65464e70cdb437286dc2ee36bbbbf1311209b3dc
Brian Rice <bwrice83@gmail.com>
2020-04-09 23:46:16 -0400

    return player-spirits with offset and limit

--- app/Http/Controllers/WeekPlayerSpiritController.php
+++ app/Http/Controllers/WeekPlayerSpiritController.php
@@ -4,35 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Domain\Models\Week;
 use App\Domain\Models\PlayerSpirit;
-use App\Domain\QueryBuilders\Filters\HeroRaceFilter;
-use App\Domain\QueryBuilders\Filters\MaxEssenceCostFilter;
-use App\Domain\QueryBuilders\Filters\MinEssenceCostFilter;
-use App\Domain\QueryBuilders\Filters\PositionFilter;
 use App\Http\Resources\PlayerSpiritResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Spatie\QueryBuilder\Filter;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class WeekPlayerSpiritController extends Controller
 {
-    public function index($weekUuid)
+    public function index($weekUuid, Request $request)
     {
         if ('current' === $weekUuid) {
             $week = Week::current();
         } else {
             $week = Week::findUuidOrFail($weekUuid);
         }
+        $offset = $request->offset;
+        $limit = $request->limit;
+        $query = PlayerSpirit::query()->forWeek($week);
 
-        return Cache::remember('player_spirits_for_week_' . $week->id, 60 * 60 * 1, function() use ($week) {
+        $playerSpirits = $query->with([
+            'playerGameLog.player.positions',
+        ])->orderByDesc('essence_cost')
+            ->offset($offset)
+            ->limit($limit)
+            ->get();
 
-            $query = PlayerSpirit::query()->forWeek($week);
+        return PlayerSpiritResource::collection($playerSpirits);
 
-            $playerSpirits = $query->with([
-                'playerGameLog.player.positions',
-            ])->orderByDesc('essence_cost')->get();
-
-            return PlayerSpiritResource::collection($playerSpirits);
-        });
+//        return Cache::remember('player_spirits_for_week_' . $week->id, 60 * 60 * 1, function() use ($week) {
+//
+//            $query = PlayerSpirit::query()->forWeek($week);
+//
+//            $playerSpirits = $query->with([
+//                'playerGameLog.player.positions',
+//            ])->orderByDesc('essence_cost')->get();
+//
+//            return PlayerSpiritResource::collection($playerSpirits);
+//        });
     }
 }
