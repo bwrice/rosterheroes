0101bf0814a96031c66bd4596a4d4b0fa80b10c3
Brian Rice <bwrice83@gmail.com>
2020-04-11 00:02:00 -0400

    update ProcessSideQuestResultJob for associated action args

--- app/Jobs/ProcessSideQuestResultJob.php
+++ app/Jobs/ProcessSideQuestResultJob.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Domain\Actions\Combat\ProcessSideQuestResult;
 use App\Domain\Models\CampaignStop;
 use App\Domain\Models\SideQuest;
+use App\SideQuestResult;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -14,24 +15,19 @@ use Illuminate\Queue\SerializesModels;
 class ProcessSideQuestResultJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
     /**
-     * @var SideQuest
+     * @var SideQuestResult
      */
-    public $sideQuest;
-    /**
-     * @var CampaignStop
-     */
-    public $campaignStop;
+    public $sideQuestResult;
 
     /**
      * ProcessSideQuestResultJob constructor.
-     * @param CampaignStop $campaignStop
-     * @param SideQuest $sideQuest
+     * @param SideQuestResult $sideQuestResult
      */
-    public function __construct(CampaignStop $campaignStop, SideQuest $sideQuest)
+    public function __construct(SideQuestResult $sideQuestResult)
     {
-        $this->campaignStop = $campaignStop;
-        $this->sideQuest = $sideQuest;
+        $this->sideQuestResult = $sideQuestResult;
     }
 
     /**
@@ -40,22 +36,6 @@ class ProcessSideQuestResultJob implements ShouldQueue
      */
     public function handle(ProcessSideQuestResult $processSideQuestResult)
     {
-        $processSideQuestResult->execute($this->campaignStop, $this->sideQuest);
-    }
-
-    /**
-     * @return SideQuest
-     */
-    public function getSideQuest(): SideQuest
-    {
-        return $this->sideQuest;
-    }
-
-    /**
-     * @return CampaignStop
-     */
-    public function getCampaignStop(): CampaignStop
-    {
-        return $this->campaignStop;
+        $processSideQuestResult->execute($this->sideQuestResult);
     }
 }
