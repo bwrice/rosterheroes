b944b9fcab46faf65e67300e287ad5ad546eb7c9
Brian Rice <bwrice83@gmail.com>
2020-04-11 13:50:41 -0400

    Use CurrentWeek facade in CampaignQueryBuilder

--- app/Domain/QueryBuilders/CampaignQueryBuilder.php
+++ app/Domain/QueryBuilders/CampaignQueryBuilder.php
@@ -5,13 +5,13 @@ namespace App\Domain\QueryBuilders;
 
 
 use App\Domain\Models\Week;
+use App\Facades\CurrentWeek;
 use Illuminate\Database\Eloquent\Builder;
 
 class CampaignQueryBuilder extends Builder
 {
     public function forCurrentWeek()
     {
-        $week = Week::current();
-        return $this->where('week_id', '=', $week->id);
+        return $this->where('week_id', '=', CurrentWeek::id());
     }
 }
