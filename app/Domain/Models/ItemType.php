d9e03dc68939501a92cdb029d0a260bbbe80fa7b
Brian Rice <bwrice83@gmail.com>
2020-04-12 21:16:12 -0400

    add doc block property to ItemType

--- app/Domain/Models/ItemType.php
+++ app/Domain/Models/ItemType.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $id
  * @property string $name
  * @property int $tier
+ * @property int $item_base_id
  *
  * @property ItemBase $itemBase
  *
