696138bcd8612a76b4bdab9ba8a8157619ba636d
Brian Rice <bwrice83@gmail.com>
2020-04-13 19:45:27 -0400

    move migration dates around

--- database/migrations/2019_08_31_172605_create_chest_blueprints_table.php
+++ database/migrations/2019_08_31_172605_create_chest_blueprints_table.php
@@ -15,6 +15,7 @@ class CreateChestBlueprintsTable extends Migration
     {
         Schema::create('chest_blueprints', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('reference_id')->unique();
             $table->integer('grade')->unsigned();
             $table->bigInteger('min_gold')->unsigned();
             $table->bigInteger('max_gold')->unsigned();
