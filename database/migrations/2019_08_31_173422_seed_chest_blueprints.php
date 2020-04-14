468257454f06e8ce8acaa34758a267738c46ce50
Brian Rice <bwrice83@gmail.com>
2020-04-14 11:26:31 -0400

    seed more chest-blueprints

--- database/migrations/2019_08_31_173422_seed_chest_blueprints.php
+++ database/migrations/2019_08_31_173422_seed_chest_blueprints.php
@@ -25,6 +25,62 @@ class SeedChestBlueprints extends Migration
                 'max_gold' => 1000,
                 'item_blueprints' => [
                     [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_ITEM,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_SWORD,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_AXE,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_MACE,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_POLEARM,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_TWO_HAND_SWORD,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_TWO_HAND_AXE,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_SHIELD,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_HELMET,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_HEAVY_ARMOR,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_GAUNTLETS,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_BELT,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_BOOTS,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_ITEM,
+                        'chance' => 10
+                    ],
+                    [
                         'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_SWORD,
                         'chance' => 25
                     ],
@@ -87,6 +143,226 @@ class SeedChestBlueprints extends Migration
                     [
                         'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_CROWN,
                         'chance' => 10
+                    ],
+                ]
+            ],
+            [
+                'reference_id' => ChestBlueprint::LOW_TIER_RANGER_CHEST,
+                'grade' => 10,
+                'min_gold' => 100,
+                'max_gold' => 1000,
+                'item_blueprints' => [
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_ITEM,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_DAGGER,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_BOW,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_CROSSBOW,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_THROWING_WEAPON,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LIGHT_ARMOR,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_HELMET,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_GAUNTLETS,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_BELT,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LEGGINGS,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_ITEM,
+                        'chance' => 10
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_DAGGER,
+                        'chance' => 25
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_BOW,
+                        'chance' => 50
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_CROSSBOW,
+                        'chance' => 50
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_THROWING_WEAPON,
+                        'chance' => 25
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_LIGHT_ARMOR,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_HELMET,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_GAUNTLETS,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_BELT,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_LEGGINGS,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_NECKLACE,
+                        'chance' => 10
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_BRACELET,
+                        'chance' => 10
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_RING,
+                        'chance' => 10
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_CROWN,
+                        'chance' => 10
+                    ],
+                ]
+            ],
+            [
+                'reference_id' => ChestBlueprint::LOW_TIER_SORCERER_CHEST,
+                'grade' => 10,
+                'min_gold' => 100,
+                'max_gold' => 1000,
+                'item_blueprints' => [
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_ITEM,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_WAND,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_ORB,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_STAFF,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_PSIONIC_ONE_HAND,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_PSIONIC_TWO_HAND,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_PSIONIC_SHIELD,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_CAP,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_ROBES,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_GLOVES,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_SHOES,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_SASH,
+                        'chance' => 2.5
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_ITEM,
+                        'chance' => 10
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_WAND,
+                        'chance' => 35
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_ORB,
+                        'chance' => 35
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_STAFF,
+                        'chance' => 35
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_PSIONIC_ONE_HAND,
+                        'chance' => 15
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_PSIONIC_TWO_HAND,
+                        'chance' => 15
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_PSIONIC_SHIELD,
+                        'chance' => 15
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_CAP,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_ROBES,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_GLOVES,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_SHOES,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_SASH,
+                        'chance' => 20
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_BRACELET,
+                        'chance' => 15
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_RING,
+                        'chance' => 15
+                    ],
+                    [
+                        'reference_id' => ItemBlueprint::RANDOM_ENCHANTED_LOW_TIER_CROWN,
+                        'chance' => 15
                     ],
                 ]
             ],
