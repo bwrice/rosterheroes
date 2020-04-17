d574e1f86c3a8080be6237c5b2bd350975744d15
Brian Rice <bwrice83@gmail.com>
2020-04-17 19:04:09 -0400

    create ClearWeeklyPlayerSpirit domain action

---
+++ app/Domain/Actions/ClearWeeklyPlayerSpirit.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Models\Hero;
+use App\Facades\CurrentWeek;
+
+class ClearWeeklyPlayerSpirit
+{
+    public function execute(Hero $hero)
+    {
+        $playerSpirit = $hero->playerSpirit;
+        if (is_null($playerSpirit)) {
+            throw new \Exception("No player-spirit attached to hero with id: " . $hero->id);
+        }
+        if (CurrentWeek::id() === $playerSpirit->week_id && ! CurrentWeek::finalizing()) {
+            throw new \Exception("Week not finalizing: Cannot remove player-spirit for hero: " . $hero->id);
+        }
+        $hero->getAggregate()->clearWeeklyPlayerSpirit($playerSpirit->id)->persist();
+    }
+}
