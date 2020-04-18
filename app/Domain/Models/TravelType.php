42baa26c1cd3f7ac59ecab437a5546b7a1d943f1
Brian Rice <bwrice83@gmail.com>
2020-04-17 20:42:17 -0400

    add factory method for getting behavior from travel-types

--- app/Domain/Models/TravelType.php
+++ app/Domain/Models/TravelType.php
@@ -2,7 +2,14 @@
 
 namespace App\Domain\Models;
 
+use App\Domain\Behaviors\TravelTypes\BorderTravelBehavior;
+use App\Domain\Behaviors\TravelTypes\ContinentTravelBehavior;
+use App\Domain\Behaviors\TravelTypes\RealmTravelBehavior;
+use App\Domain\Behaviors\TravelTypes\StationaryTravelBehavior;
+use App\Domain\Behaviors\TravelTypes\TerritoryTravelBehavior;
+use App\Domain\Behaviors\TravelTypes\TravelTypeBehavior;
 use App\Domain\Models\Traits\HasUniqueNames;
+use App\Exceptions\UnknownBehaviorException;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -10,6 +17,7 @@ use Illuminate\Database\Eloquent\Model;
  * @package App\Domain\Models
  *
  * @property int $id
+ * @property string $name
  */
 class TravelType extends Model
 {
@@ -22,4 +30,21 @@ class TravelType extends Model
     public const TERRITORY = 'Territory';
     public const CONTINENT = 'Continent';
     public const REALM = 'Realm';
+
+    public function getBehavior(): TravelTypeBehavior
+    {
+        switch ($this->name) {
+            case self::STATIONARY:
+                return app(StationaryTravelBehavior::class);
+            case self::BORDER:
+                return app(BorderTravelBehavior::class);
+            case self::TERRITORY:
+                return app(TerritoryTravelBehavior::class);
+            case self::CONTINENT:
+                return app(ContinentTravelBehavior::class);
+            case self::REALM:
+                return app(RealmTravelBehavior::class);
+        }
+        throw new UnknownBehaviorException($this->name, TravelTypeBehavior::class);
+    }
 }
