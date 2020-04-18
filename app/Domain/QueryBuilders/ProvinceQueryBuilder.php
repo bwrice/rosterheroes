8ad97a6840adb58340d95e26e81db0f469938212
Brian Rice <bwrice83@gmail.com>
2020-04-17 20:42:51 -0400

    add first() method autocomplete for ProvinceQueryBuilder

--- app/Domain/QueryBuilders/ProvinceQueryBuilder.php
+++ app/Domain/QueryBuilders/ProvinceQueryBuilder.php
@@ -5,8 +5,15 @@ namespace App\Domain\QueryBuilders;
 
 
 use App\Domain\Models\Continent;
+use App\Domain\Models\Province;
 use Illuminate\Database\Eloquent\Builder;
 
+/**
+ * Class ProvinceQueryBuilder
+ * @package App\Domain\QueryBuilders
+ *
+ * @method Province|mixed first($columns = ['*'])
+ */
 class ProvinceQueryBuilder extends Builder
 {
     public function ofContinent(int $continentID)
