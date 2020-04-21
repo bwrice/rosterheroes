56bd9a38b76b90269007ee6477fdfe8de6084988
Brian Rice <bwrice83@gmail.com>
2020-04-20 22:56:52 -0400

    create UnopenedChestPanel component

---
+++ resources/js/components/commandCenter/barracks/UnopenedChestPanel.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-sheet color="#67856f"
+             tile
+             style="margin: 1px 0 1px 0"
+    >
+        <v-row align="center" justify="center" class="mx-2">
+            <span class="subtitle-2 font-weight-light pa-2">{{unopenedChest.description}}</span>
+            <div class="flex-grow-1"></div>
+            <v-btn
+                fab
+                dark
+                x-small
+                color="rgba(0, 0, 0, .4)"
+            >
+                <v-icon>lock_open</v-icon>
+            </v-btn>
+        </v-row>
+    </v-sheet>
+</template>
+
+<script>
+    import UnopenedChest from "../../../models/UnopenedChest";
+
+    export default {
+        name: "UnopenedChestPanel",
+        props: {
+            unopenedChest: {
+                type: UnopenedChest,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
