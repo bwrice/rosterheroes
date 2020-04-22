56378fafe0a25279ef7c654b47f5d05a1450fce1
Brian Rice <bwrice83@gmail.com>
2020-04-21 21:10:12 -0400

    return OpenChestResult from OpenChest domain action

--- app/Domain/Actions/OpenChest.php
+++ app/Domain/Actions/OpenChest.php
@@ -5,6 +5,8 @@ namespace App\Domain\Actions;
 
 
 use App\Chest;
+use App\Domain\Collections\HasItemsCollection;
+use App\Domain\DataTransferObjects\OpenedChestResult;
 use App\Domain\Models\Item;
 use Illuminate\Support\Facades\DB;
 
@@ -20,24 +22,34 @@ class OpenChest
         $this->addItemToHasItems = $addItemToHasItems;
     }
 
+    /**
+     * @param Chest $chest
+     * @return OpenedChestResult
+     * @throws \Exception
+     */
     public function execute(Chest $chest)
     {
         if ($chest->opened_at) {
             throw new \Exception("Chest already opened");
         }
 
-        DB::transaction(function () use ($chest) {
+        return DB::transaction(function () use ($chest) {
 
             $chest->opened_at = now();
             $chest->save();
 
             $squad = $chest->squad;
 
-            $squad->getAggregate()->increaseGold($chest->gold)->persist();
+            $gold = $chest->gold;
+            $squad->getAggregate()->increaseGold($gold)->persist();
 
-            $chest->items->each(function (Item $item) use ($squad) {
-                $this->addItemToHasItems->execute($item, $squad, null, false);
+            $hasItemsCollection = new HasItemsCollection();
+
+            $chest->items->each(function (Item $item) use ($squad, &$hasItemsCollection) {
+                $hasItemsCollection = $this->addItemToHasItems->execute($item, $squad, $hasItemsCollection, false);
             });
+
+            return new OpenedChestResult($gold, $hasItemsCollection);
         });
     }
 }
