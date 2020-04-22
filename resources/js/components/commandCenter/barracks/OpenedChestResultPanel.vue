8b2960e085465a5a39c6198690232c66a2d2ef0c
Brian Rice <bwrice83@gmail.com>
2020-04-22 14:10:21 -0400

    create OpenedChestResultPanel vue component

---
+++ resources/js/components/commandCenter/barracks/OpenedChestResultPanel.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-row no-gutters>
+        <v-col
+            cols="12"
+            v-for="(item, uuid) in openedChestResult.items"
+            :key="uuid"
+        >
+            <ItemCard :item="item"></ItemCard>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+    import OpenedChestResult from "../../../models/OpenedChestResult";
+    import ItemCard from "../global/ItemCard";
+
+    export default {
+        name: "OpenedChestResultPanel",
+        components: {ItemCard},
+        props: {
+            openedChestResult: {
+                type: OpenedChestResult,
+                required: true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
