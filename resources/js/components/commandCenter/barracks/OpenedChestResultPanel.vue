388790ecd765c40d0c46fc72a9eb0d37bbe44cdf
Brian Rice <bwrice83@gmail.com>
2020-04-23 00:20:20 -0400

    separate items in chest by where they were stored

--- resources/js/components/commandCenter/barracks/OpenedChestResultPanel.vue
+++ resources/js/components/commandCenter/barracks/OpenedChestResultPanel.vue
@@ -1,16 +1,24 @@
 <template>
     <v-row no-gutters>
-        <v-col
-            cols="12"
-            v-for="(item, uuid) in openedChestResult.items"
-            :key="uuid"
-        >
-            <ItemExpandPanel :item="item"></ItemExpandPanel>
+        <v-col cols="12" v-if="openedChestResult.itemsMovedToMobileStorage.length">
+            <v-row no-gutters>
+                <span class="font-weight-light"></span>
+            </v-row>
+            <v-row no-gutters>
+                <v-col
+                    cols="12"
+                    v-for="(item, uuid) in openedChestResult.itemsMovedToMobileStorage"
+                    :key="uuid"
+                >
+                    <ItemExpandPanel :item="item"></ItemExpandPanel>
+                </v-col>
+            </v-row>
         </v-col>
     </v-row>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
     import OpenedChestResult from "../../../models/OpenedChestResult";
     import ItemExpandPanel from "../global/ItemExpandPanel";
 
@@ -23,6 +31,11 @@
                 required: true
             }
         },
+        computed: {
+            ...mapGetters([
+                '_mobileStorageRankName',
+            ]),
+        }
     }
 </script>
 
