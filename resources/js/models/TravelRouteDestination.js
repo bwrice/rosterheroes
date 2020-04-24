159ef8c1c9164900524f4b35dc9e37ef5db7a576
Brian Rice <bwrice83@gmail.com>
2020-04-24 12:30:38 -0400

    fix TravelRouteDestination not using Province JS model correctly

--- resources/js/models/TravelRouteDestination.js
+++ resources/js/models/TravelRouteDestination.js
@@ -2,8 +2,8 @@ import Province from "./Province";
 
 export default class TravelRouteDestination {
 
-    constructor({cost, province = new Province({})}) {
+    constructor({cost, province}) {
         this.cost = cost;
-        this.province = province;
+        this.province = province ? new Province(province) : new Province({});
     }
 }
