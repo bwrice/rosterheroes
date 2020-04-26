5980e86fba6024e0ac5f3544416ad6564bd87076
Brian Rice <bwrice83@gmail.com>
2020-04-25 23:08:39 -0400

    return HeroResource from show method on HeroController

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -3,22 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Domain\Models\Hero;
+use App\Http\Resources\HeroResource;
 use App\Http\Resources\SquadCreationHeroResource;
+use App\Policies\SquadPolicy;
 use Illuminate\Http\Request;
 
 class HeroController extends Controller
 {
     public function show($heroSlug)
     {
-        // TODO: auth
         $hero = Hero::findSlugOrFail($heroSlug);
-        return new SquadCreationHeroResource($hero->loadMissing([
-            'heroClass',
-            'heroRace.positions',
-            'playerSpirit.game.homeTeam',
-            'playerSpirit.game.awayTeam',
-            'playerSpirit.player.positions',
-            'playerSpirit.player.team'
-        ]));
+        $this->authorize(SquadPolicy::MANAGE, $hero->squad);
+        return new HeroResource($hero->loadMissing(Hero::heroResourceRelations()));
     }
 }
