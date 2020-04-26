064af892ef274c0845d94c086a0c58763bbe6a9c
Brian Rice <bwrice83@gmail.com>
2020-04-26 15:06:44 -0400

    add hero getter to heroAPI

--- resources/js/api/heroApi.js
+++ resources/js/api/heroApi.js
@@ -1,39 +1,46 @@
 
+const ROUTE_PREFIX = '/api/v1/heroes/';
+
+export async function getHero(heroSlug) {
+    let response = await axios.get(ROUTE_PREFIX + heroSlug);
+    return response.data;
+}
+
 export async function addSpirit(heroSlug, spiritUuid) {
-    let response = await axios.post('/api/v1/heroes/' + heroSlug + '/player-spirit', {
+    let response = await axios.post(ROUTE_PREFIX + heroSlug + '/player-spirit', {
         spirit: spiritUuid
     });
     return response.data;
 }
 
 export async function removeSpirit(heroSlug, spiritUuid) {
-    let response = await axios.delete('/api/v1/heroes/' + heroSlug + '/player-spirit/' + spiritUuid);
+    let response = await axios.delete(ROUTE_PREFIX + heroSlug + '/player-spirit/' + spiritUuid);
     return response.data;
 }
 
 export async function changeCombatPosition(heroSlug, combatPositionID) {
-    let response = await axios.post('/api/v1/heroes/' + heroSlug + '/combat-position', {
+    let response = await axios.post(ROUTE_PREFIX + heroSlug + '/combat-position', {
         position: combatPositionID
     });
     return response.data;
 }
 
 export async function unequipItem(heroSlug, itemUuid) {
-    let response = await axios.post('/api/v1/heroes/' + heroSlug + '/unequip', {
+    let response = await axios.post(ROUTE_PREFIX + heroSlug + '/unequip', {
         item: itemUuid
     });
     return response.data;
 }
 
 export async function equipFromWagon(heroSlug, itemUuid) {
-    let response = await axios.post('/api/v1/heroes/' + heroSlug + '/equip', {
+    let response = await axios.post(ROUTE_PREFIX + heroSlug + '/equip', {
         item: itemUuid
     });
     return response.data;
 }
 
 export async function getCostToRaiseMeasurable(heroSlug, measurableType, raiseAmount) {
-    let response = await axios.get('/api/v1/heroes/' + heroSlug + '/raise-measurable', {
+    let response = await axios.get(ROUTE_PREFIX + heroSlug + '/raise-measurable', {
         params: {
             type: measurableType,
             amount: raiseAmount
@@ -43,7 +50,7 @@ export async function getCostToRaiseMeasurable(heroSlug, measurableType, raiseAm
 }
 
 export async function raiseMeasurable(heroSlug, measurableType, raiseAmount) {
-    let response = await axios.post('/api/v1/heroes/' + heroSlug + '/raise-measurable', {
+    let response = await axios.post(ROUTE_PREFIX + heroSlug + '/raise-measurable', {
         type: measurableType,
         amount: raiseAmount
     });
@@ -51,14 +58,14 @@ export async function raiseMeasurable(heroSlug, measurableType, raiseAmount) {
 }
 
 export async function castSpell(heroSlug, spellID) {
-    let response = await axios.post('/api/v1/heroes/' + heroSlug + '/cast-spell', {
+    let response = await axios.post(ROUTE_PREFIX + heroSlug + '/cast-spell', {
         spell: spellID
     });
     return response.data;
 }
 
 export async function removeSpell(heroSlug, spellID) {
-    let response = await axios.post('/api/v1/heroes/' + heroSlug + '/remove-spell', {
+    let response = await axios.post(ROUTE_PREFIX + heroSlug + '/remove-spell', {
         spell: spellID
     });
     return response.data;
