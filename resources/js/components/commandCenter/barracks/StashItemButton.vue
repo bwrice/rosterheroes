3dd27a02ddbb6b60ea3979f0f30322fc62c14f1f
Brian Rice <bwrice83@gmail.com>
2020-04-26 23:33:58 -0400

    create StashItemButton vue component

---
+++ resources/js/components/commandCenter/barracks/StashItemButton.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-btn
+        fab
+        x-small
+        color="warning"
+        @click="stash"
+        :disabled="pending"
+    >
+        <v-icon>archive</v-icon>
+    </v-btn>
+</template>
+
+<script>
+
+    import {mapActions} from 'vuex';
+    import Item from "../../../models/Item";
+
+    export default {
+        name: "StashItemButton",
+        props: {
+            item: {
+                type: Item,
+                required: true
+            }
+        },
+        data() {
+            return {
+                pending: false
+            }
+        },
+        methods: {
+            ...mapActions([
+                'stashItem'
+            ]),
+            async stash() {
+                this.pending = true;
+                await this.stashItem(this.item);
+                this.pending = false;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
