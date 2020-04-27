6ef01d2de3929bdacac3323960a274f57f45ad04
Brian Rice <bwrice83@gmail.com>
2020-04-27 16:41:51 -0400

    create MobileStoreItemButton vue component

---
+++ resources/js/components/commandCenter/barracks/MobileStoreItemButton.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-btn
+        fab
+        x-small
+        color="success"
+        @click="storeItem"
+        :disabled="pending"
+    >
+        <v-icon>unarchive</v-icon>
+    </v-btn>
+</template>
+
+<script>
+    import Item from "../../../models/Item";
+
+    import {mapActions} from 'vuex';
+    export default {
+        name: "MobileStoreItemButton",
+        props: {
+            item: {
+                type: Item,
+                required: true
+            }
+        },
+        data() {
+            return {
+                pending: false
+            }
+        },
+        methods: {
+            ...mapActions([
+                'mobileStoreItem'
+            ]),
+            async storeItem() {
+                this.pending = true;
+                await this.mobileStoreItem(this.item);
+                this.pending = false;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
