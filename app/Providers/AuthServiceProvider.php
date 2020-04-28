c96c43a80b3106b6530f3da70c834a20e307d8c3
Brian Rice <bwrice83@gmail.com>
2020-04-28 13:41:27 -0400

    remove Passport routes from Auth boot

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -10,7 +10,6 @@ use App\Policies\SquadPolicy;
 use App\Domain\Models\Squad;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -33,7 +32,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        Passport::routes();
     }
 }
