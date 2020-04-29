8b6645848cc0a4659857410b80c3fdf035df5749
Brian Rice <bwrice83@gmail.com>
2020-04-29 10:43:56 -0400

    check authorization in CommandCenterController show method

--- app/Http/Controllers/CommandCenterController.php
+++ app/Http/Controllers/CommandCenterController.php
@@ -7,18 +7,20 @@ use App\Http\Resources\HeroClassResource;
 use App\Http\Resources\HeroRaceResource;
 use App\Http\Resources\SquadCreationHeroResource;
 use App\Http\Resources\SquadResource;
+use App\Policies\SquadPolicy;
 use Illuminate\Http\Request;
 
 class CommandCenterController extends Controller
 {
     public function show(Request $request, $squadSlug, $any = null)
     {
+        $squad = Squad::findSlugOrFail($squadSlug);
+        $this->authorize(SquadPolicy::MANAGE, $squad);
+
         if (! $any) {
             // redirect to barracks if no sub-page is set
             return redirect('/command-center/' . $squadSlug . '/barracks');
         }
-
-        $squad = Squad::findSlugOrFail($squadSlug);
 
         if ($squad->inCreationState()) {
 
