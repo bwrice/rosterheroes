1ae38ee019a755a56822edc6ea3d28e21b407cc5
Brian Rice <bwrice83@gmail.com>
2020-05-02 13:24:56 -0400

    add CurrentCampaignCard to CampaignMain view

--- resources/js/components/commandCenter/views/campaign/CampaignMain.vue
+++ resources/js/components/commandCenter/views/campaign/CampaignMain.vue
@@ -1,7 +1,10 @@
 <template>
     <v-container>
         <v-row>
-            <v-col cols="12">
+            <v-col cols="12" offset-sm="2" sm="8" offset-md="0" md="6" lg="5" offset-lg="1" xl="4" offset-xl="2">
+                <CurrentCampaignCard></CurrentCampaignCard>
+            </v-col>
+            <v-col cols="12" offset-sm="2" sm="8" offset-md="0" md="6" lg="5" xl="4">
                 <AvailableQuestsSection></AvailableQuestsSection>
             </v-col>
         </v-row>
@@ -10,9 +13,10 @@
 
 <script>
     import AvailableQuestsSection from "../../campaign/AvailableQuestsSection";
+    import CurrentCampaignCard from "../../campaign/CurrentCampaignCard";
     export default {
         name: "CampaignMain",
-        components: {AvailableQuestsSection},
+        components: {CurrentCampaignCard, AvailableQuestsSection},
     }
 </script>
 
