b5429efaac88ae5c329410198039b8a827a35f6e
Brian Rice <bwrice83@gmail.com>
2020-05-02 17:28:10 -0400

    create CompactQuest JS model

--- resources/js/models/CampaignStop.js
+++ resources/js/models/CampaignStop.js
@@ -1,12 +1,14 @@
+import CompactQuest from "./compact/CompactQuest";
 
 export default class CampaignStop {
 
-    constructor({uuid, name, questUuid, provinceUuid, campaignUuid, sideQuestUuids = []}) {
+    constructor({uuid, name, questUuid, provinceUuid, campaignUuid, sideQuestUuids = []}, compactQuest) {
         this.uuid = uuid;
         this.name = name;
         this.questUuid = questUuid;
         this.provinceUuid = provinceUuid;
         this.campaignUuid = campaignUuid;
         this.sideQuestUuids = sideQuestUuids;
+        this.compactQuest = compactQuest ? new CompactQuest(compactQuest) : new CompactQuest({});
     }
 }
---
+++ resources/js/models/compact/CompactQuest.js
@@ -0,0 +1,12 @@
+
+export default class CompactQuest {
+
+    constructor({uuid, name, slug, level, provinceID, percent}) {
+        this.uuid = uuid;
+        this.name = name;
+        this.slug = slug;
+        this.level = level;
+        this.provinceID = provinceID;
+        this.percent = percent;
+    }
+}
