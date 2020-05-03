58200723bb6051f50f5054dbc87604bee25ed48d
Brian Rice <bwrice83@gmail.com>
2020-05-03 01:52:53 -0400

    create leave quest dialog and trigger leaveQuest action

--- resources/js/components/commandCenter/campaign/CurrentCampaignCard.vue
+++ resources/js/components/commandCenter/campaign/CurrentCampaignCard.vue
@@ -31,6 +31,13 @@
                             </ProvinceVector>
                         </MapViewPort>
                     </v-col>
+                    <CampaignStopCard
+                        v-for="(campaignStop, uuid) in _currentCampaign.campaignStops"
+                        :key="uuid"
+                        :campaign-stop="campaignStop"
+                        @leaveQuestClicked="handleLeaveQuestClicked"
+                    >
+                    </CampaignStopCard>
                 </v-row>
             </v-sheet>
         </v-col>
@@ -41,22 +48,85 @@
                 </v-row>
             </v-sheet>
         </v-col>
+        <v-dialog
+            v-model="leaveQuestDialog"
+            max-width="500"
+        >
+            <v-sheet
+            >
+                <v-row no-gutters>
+                    <v-col cols="12">
+                        <v-row no-gutters justify="center" class="px-2 pt-1">
+                            <span class="title font-weight-bolder">{{leaveQuestTitle}}</span>
+                        </v-row>
+                        <v-row no-gutters justify="center" class="pa-1">
+                            <p class="font-weight-thin">{{leaveQuestMessage}}</p>
+                        </v-row>
+                    </v-col>
+                </v-row>
+                <v-row no-gutters justify="end" align="center" class="pa-2">
+                    <v-btn
+                        outlined
+                        color="error"
+                        @click="leaveQuestDialog = false"
+                        class="mx-1"
+                    >
+                        Cancel
+                    </v-btn>
+                    <v-btn
+                        color="success"
+                        class="mx-1"
+                        @click="confirmLeaveQuest"
+                        :disabled="pendingLeaveQuest"
+                    >
+                        Leave Quest
+                    </v-btn>
+                </v-row>
+            </v-sheet>
+        </v-dialog>
     </v-row>
 </template>
 
 <script>
     import {mapGetters} from 'vuex';
+    import {mapActions} from 'vuex';
     import MapViewPort from "../realm/MapViewPort";
     import Continent from "../../../models/Continent";
     import ContinentVector from "../realm/ContinentVector";
     import ProvinceVector from "../realm/ProvinceVector";
+    import CampaignStopCard from "./CampaignStopCard";
+    import CampaignStop from "../../../models/CampaignStop";
+    import {leaveQuest} from "../../../api/squadApi";
     export default {
         name: "CurrentCampaignCard",
-        components: {ProvinceVector, ContinentVector, MapViewPort},
+        components: {CampaignStopCard, ProvinceVector, ContinentVector, MapViewPort},
+        data() {
+            return {
+                leaveQuestDialog: false,
+                campaignStopToLeave: new CampaignStop({}),
+                pendingLeaveQuest: false
+            }
+        },
         methods: {
+            ...mapActions([
+                'leaveQuest'
+            ]),
             continentProvinceFillColor(province) {
                 let provinceInCampaignStops = this._currentCampaign.campaignStops.find(campaignStop => campaignStop.provinceUuid === province.uuid);
                 return provinceInCampaignStops ? '#28bf5b' : '#808080';
+            },
+            handleLeaveQuestClicked(campaignStop) {
+                this.campaignStopToLeave = campaignStop;
+                this.leaveQuestDialog = true;
+            },
+            async confirmLeaveQuest() {
+                this.pendingLeaveQuest = true;
+                await this.leaveQuest({
+                    questUuid: this.campaignStopToLeave.questUuid,
+                    questName: this.campaignStopToLeave.compactQuest.name
+                });
+                this.leaveQuestDialog = false;
+                this.pendingLeaveQuest = false;
             }
         },
         computed: {
@@ -64,6 +134,7 @@
                 '_currentCampaign',
                 '_continentByID',
                 '_provincesByContinentID',
+                '_provinceByUuid',
                 '_squad'
             ]),
             continent() {
@@ -77,6 +148,17 @@
             },
             questRatioText() {
                 return '(' + this._currentCampaign.campaignStops.length + '/' + this._squad.questsPerWeek + ')';
+            },
+            leaveQuestTitle() {
+                return 'Leave ' + this.campaignStopToLeave.compactQuest.name + '?';
+            },
+            leaveQuestMessage() {
+                let message = 'Are you sure you want to leave the quest, ';
+                message += this.campaignStopToLeave.compactQuest.name + '? ';
+                message += 'You will have to travel back to the province of ';
+                message += this._provinceByUuid(this.campaignStopToLeave.provinceUuid).name;
+                message += ' to add the quest back to your campaign';
+                return message;
             }
         }
     }
