4f6ff53075e22f290b62d08181e1806d526d99cb
Brian Rice <bwrice83@gmail.com>
2020-05-02 21:54:45 -0400

    fix sideQuestsPerQuest property on Squad js model

--- resources/js/models/Squad.js
+++ resources/js/models/Squad.js
@@ -1,7 +1,7 @@
 
 export default class Squad {
 
-    constructor({uuid, name = '', slug = '', spiritEssence = 0, gold = 0, experience = 0, favor = 0, questsPerWeek, sideQuestsPerWeek}) {
+    constructor({uuid, name = '', slug = '', spiritEssence = 0, gold = 0, experience = 0, favor = 0, questsPerWeek, sideQuestsPerQuest}) {
         this.name = name;
         this.uuid = uuid;
         this.slug = slug;
@@ -10,6 +10,6 @@ export default class Squad {
         this.experience = experience;
         this.favor = favor;
         this.questsPerWeek = questsPerWeek;
-        this.sideQuestsPerWeek = sideQuestsPerWeek;
+        this.sideQuestsPerQuest = sideQuestsPerQuest;
     }
 }
