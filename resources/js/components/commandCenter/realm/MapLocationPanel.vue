b1a25f3c873aefdfcf8a1ecc2dac60a2b372c271
Brian Rice <bwrice83@gmail.com>
2020-05-04 00:16:47 -0400

    extract out MapLocationPanel component from TerritoryPanel

---
+++ resources/js/components/commandCenter/realm/MapLocationPanel.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-sheet color="#466661" class="my-1">
+        <v-row no-gutters>
+            <v-col cols="12">
+                <v-row no-gutters align="center">
+                    <v-col cols="3" class="pa-1">
+                        <MapViewPort :view-box="viewBox">
+                            <slot>
+                                <!-- Default Slot -->
+                            </slot>
+                        </MapViewPort>
+                    </v-col>
+                    <v-col cols="7">
+                        <v-row no-gutters justify="center">
+                            <span class="subtitle-1 font-weight-light">{{locationName}}</span>
+                        </v-row>
+                    </v-col>
+                    <v-col cols="2">
+                        <v-row no-gutters justify="center">
+                            <v-btn
+                                fab
+                                dark
+                                small
+                                color="#219cc2"
+                                elevation="2"
+                                @click="handleExplore"
+                            >
+                                <v-icon>explore</v-icon>
+                            </v-btn>
+                        </v-row>
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-sheet>
+</template>
+
+<script>
+    import MapViewPort from "./MapViewPort";
+    import ViewBox from "../../../models/ViewBox";
+    export default {
+        name: "MapLocationPanel",
+        components: {MapViewPort},
+        props: {
+            viewBox: {
+                type: ViewBox,
+                required: true
+            },
+            locationName: {
+                type: String,
+                required: true
+            },
+            handleExplore: {
+                type: Function,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/commandCenter/realm/TerritoryPanel.vue
+++ resources/js/components/commandCenter/realm/TerritoryPanel.vue
@@ -1,49 +1,25 @@
 <template>
-    <v-sheet color="#466661" class="my-1">
-        <v-row no-gutters>
-            <v-col cols="12">
-                <v-row no-gutters align="center">
-                    <v-col cols="3" class="pa-1">
-                        <MapViewPort :view-box="territory.viewBox">
-                            <TerritoryVector
-                                :territory="territory"
-                                :interactive="false"
-                            ></TerritoryVector>
-                        </MapViewPort>
-                    </v-col>
-                    <v-col cols="7">
-                        <v-row no-gutters justify="center">
-                            <span class="subtitle-1 font-weight-light">{{territory.name}}</span>
-                        </v-row>
-                    </v-col>
-                    <v-col cols="2">
-                        <v-row no-gutters justify="center">
-                            <v-btn
-                                fab
-                                dark
-                                small
-                                color="#219cc2"
-                                elevation="2"
-                                @click="exploreTerritory"
-                            >
-                                <v-icon>explore</v-icon>
-                            </v-btn>
-                        </v-row>
-                    </v-col>
-                </v-row>
-            </v-col>
-        </v-row>
-    </v-sheet>
+    <MapLocationPanel
+        :view-box="territory.viewBox"
+        :location-name="territory.name"
+        :handle-explore="exploreTerritory"
+    >
+        <TerritoryVector
+            :territory="territory"
+            :interactive="false"
+        ></TerritoryVector>
+    </MapLocationPanel>
 </template>
 
 <script>
     import Territory from "../../../models/Territory";
     import MapViewPort from "./MapViewPort";
     import TerritoryVector from "./TerritoryVector";
+    import MapLocationPanel from "./MapLocationPanel";
 
     export default {
         name: "TerritoryPanel",
-        components: {TerritoryVector, MapViewPort},
+        components: {MapLocationPanel, TerritoryVector, MapViewPort},
         props: {
             territory: {
                 type: Territory,
