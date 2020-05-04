7e96c9f8b1596c08044b905ce0c47f29ce67889b
Brian Rice <bwrice83@gmail.com>
2020-05-04 16:28:59 -0400

    add province pagination block to TerritoryView

--- resources/js/components/commandCenter/views/realm/explore/TerritoryView.vue
+++ resources/js/components/commandCenter/views/realm/explore/TerritoryView.vue
@@ -16,7 +16,21 @@
             </v-row>
         </template>
         <template v-slot:column-two>
-            <!-- TODO -->
+            <v-row no-gutters>
+                <v-col cols="12">
+                    <span class="title font-weight-thin">PROVINCES</span>
+                </v-col>
+                <v-col cols="12">
+                    <PaginationBlock
+                        :items-per-page="6"
+                        :items="provinces"
+                    >
+                        <template v-slot:default="slotProps">
+                            <ProvincePanel :province="slotProps.item"></ProvincePanel>
+                        </template>
+                    </PaginationBlock>
+                </v-col>
+            </v-row>
         </template>
     </TwoColumnWideLayout>
 </template>
@@ -29,11 +43,15 @@
     import ExploreMapCard from "../../../realm/ExploreMapCard";
     import MapViewPortWithControls from "../../../realm/MapViewPortWithControls";
     import TwoColumnWideLayout from "../../../layouts/TwoColumnWideLayout";
+    import PaginationBlock from "../../../global/PaginationBlock";
+    import ProvincePanel from "../../../realm/ProvincePanel";
 
     export default {
         name: "TerritoryView",
 
         components: {
+            ProvincePanel,
+            PaginationBlock,
             TwoColumnWideLayout,
             MapViewPortWithControls,
             ExploreMapCard,
