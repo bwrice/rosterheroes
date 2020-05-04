f5e27ec252709c354af790d9c861c70e1e62efe4
Brian Rice <bwrice83@gmail.com>
2020-05-04 10:49:24 -0400

    create ProvincePanel

---
+++ resources/js/components/commandCenter/realm/ProvincePanel.vue
@@ -0,0 +1,35 @@
+<template>
+    <MapLocationPanel
+        :view-box="province.viewBox"
+        :location-name="province.name"
+        :handle-explore="exploreProvince"
+    >
+        <ProvinceVector :province="province"></ProvinceVector>
+    </MapLocationPanel>
+</template>
+
+<script>
+    import Province from "../../../models/Province";
+    import MapLocationPanel from "./MapLocationPanel";
+    import ProvinceVector from "./ProvinceVector";
+
+    export default {
+        name: "ProvincePanel",
+        components: {ProvinceVector, MapLocationPanel},
+        props: {
+            province: {
+                type: Province,
+                required: true
+            }
+        },
+        methods: {
+            exploreProvince() {
+                this.province.goToRoute(this.$router, this.$route);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
