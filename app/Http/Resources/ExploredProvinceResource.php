4d02daa19d25f431dd94fa5cefb977c121c51317
Brian Rice <bwrice83@gmail.com>
2020-05-04 23:42:43 -0400

    create ExploredProvinceResource

---
+++ app/Http/Resources/ExploredProvinceResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\Province;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class ExploredProvinceResource
+ * @package App\Http\Resources
+ *
+ * @mixin Province
+ */
+class ExploredProvinceResource extends JsonResource
+{
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     * @throws \Exception
+     */
+    public function toArray($request)
+    {
+        if ($this->stashes->count() > 1) {
+            throw new \Exception(self::class . " should be used in conjunction with a single stash belonging to a specific squad");
+        }
+
+        return [
+            'provinceUuid' => $this->uuid,
+            'squadStash' => new CompactStash($this->stashes->first())
+        ];
+    }
+}
