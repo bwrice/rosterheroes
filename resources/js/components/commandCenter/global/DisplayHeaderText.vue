4bd70af5a4982bc1a271d918a0c0ca9c510e95df
Brian Rice <bwrice83@gmail.com>
2020-05-05 13:50:33 -0400

    create header text vue component

---
+++ resources/js/components/commandCenter/global/DisplayHeaderText.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-row no-gutters align="center" justify="center" :class="[textSizeClass, fontWeightClass]">
+        {{displayText}}
+    </v-row>
+</template>
+
+<script>
+    export default {
+        name: "DisplayHeaderText",
+        props: {
+            displayText: {
+                type: String,
+                required: true
+            }
+        },
+        computed: {
+            textSizeClass() {
+                switch (this.$vuetify.breakpoint.name) {
+                    case 'xs':
+                    case 'sm':
+                        return 'display-1';
+                    case 'md':
+                    case 'lg':
+                    case 'xl':
+                        return 'display-3'
+                }
+            },
+            fontWeightClass() {
+                switch (this.$vuetify.breakpoint.name) {
+                    case 'xs':
+                    case 'sm':
+                        return 'font-weight-medium';
+                    case 'md':
+                    case 'lg':
+                    case 'xl':
+                        return 'font-weight-bold'
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
