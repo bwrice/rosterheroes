37f7215042ae571dd1caeaa0d48b2c2bd5656b8e
Brian Rice <bwrice83@gmail.com>
2020-05-05 20:41:10 -0400

    create CardBlock component

---
+++ resources/js/components/commandCenter/global/CardBlock.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-row no-gutters>
+        <v-col cols="12">
+            <span class="title font-weight-thin">{{title.toUpperCase()}}</span>
+        </v-col>
+        <slot>
+            <!-- Default Slot -->
+        </slot>
+    </v-row>
+</template>
+
+<script>
+    export default {
+        name: "CardBlock",
+        props: {
+            title: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
