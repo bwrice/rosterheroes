890917d807e6ea3331027329c716e186b6bf9f1f
Brian Rice <bwrice83@gmail.com>
2020-05-05 22:51:16 -0400

    create EmptyNotifier component

---
+++ resources/js/components/commandCenter/global/EmptyNotifier.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-col cols="12">
+        <v-sheet color="rgba(255,255,255, 0.25)" class="my-2">
+            <v-row no-gutters class="pa-2" justify="center" align="center">
+                <span class="subtitle-1 font-weight-light">{{notificationText}}</span>
+            </v-row>
+        </v-sheet>
+    </v-col>
+</template>
+
+<script>
+    export default {
+        name: "EmptyNotifier",
+        props: {
+            notificationText: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
