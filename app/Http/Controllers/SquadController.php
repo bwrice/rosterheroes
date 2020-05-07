8d436797a769992d8e2d67c9f8f69450327df86f
Brian Rice <bwrice83@gmail.com>
2020-05-06 21:41:55 -0400

    prevent squad creation beyond max-squad-count reached for user

--- app/Http/Controllers/SquadController.php
+++ app/Http/Controllers/SquadController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Aggregates\SquadAggregate;
 use App\Domain\Actions\CreateSquadAction;
+use App\Domain\Models\User;
 use App\Policies\SquadPolicy;
 use App\Http\Resources\SquadResource;
 use App\Domain\Models\Squad;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SquadController extends Controller
 {
@@ -16,9 +18,21 @@ class SquadController extends Controller
         $this->middleware('auth');
     }
 
+    /**
+     * @param Request $request
+     * @param CreateSquadAction $createSquadAction
+     * @return SquadResource
+     * @throws ValidationException
+     */
     public function store(Request $request, CreateSquadAction $createSquadAction)
     {
-        //TODO authorize (limit squad creation to 1?)
+        /** @var User $user */
+        $user = auth()->user();
+        if ($user->maxSquadsReached()) {
+            throw ValidationException::withMessages([
+                'squads' => 'Max squad count reached'
+            ]);
+        }
         $request->validate([
             'name' => 'required|unique:squads|between:4,20|regex:/^[\w\s]+$/'
         ]);
@@ -30,6 +44,12 @@ class SquadController extends Controller
 
     public function create()
     {
+        /** @var User $user */
+        $user = auth()->user();
+        if ($user->maxSquadsReached()) {
+            return redirect('dashboard');
+        }
+
         return view('create-squad', [
             // If squad is still in creation state, Command Center Controller reuses this view
             'squad' => null
