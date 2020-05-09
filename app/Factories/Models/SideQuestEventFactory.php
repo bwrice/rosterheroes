cbc867999f1fb366660db6fa4af5b126190f55be
Brian Rice <bwrice83@gmail.com>
2020-05-09 12:28:37 -0400

    add heroDamagesMinion to SideQuestEventFactory

--- app/Factories/Models/SideQuestEventFactory.php
+++ app/Factories/Models/SideQuestEventFactory.php
@@ -7,6 +7,9 @@ namespace App\Factories\Models;
 use App\Domain\Combat\Attacks\HeroCombatAttack;
 use App\Domain\Combat\Combatants\CombatHero;
 use App\Domain\Combat\Combatants\CombatMinion;
+use App\Factories\Combat\CombatHeroFactory;
+use App\Factories\Combat\CombatMinionFactory;
+use App\Factories\Combat\HeroCombatAttackFactory;
 use App\SideQuestEvent;
 use Illuminate\Support\Str;
 
@@ -68,17 +71,62 @@ class SideQuestEventFactory
         return SideQuestResultFactory::new()->create()->id;
     }
 
-    public function heroKillsMinion(CombatHero $combatHero, HeroCombatAttack $heroCombatAttack, CombatMinion $combatMinion)
+    public function heroKillsMinion(
+        CombatHero $combatHero = null,
+        HeroCombatAttack $heroCombatAttack = null,
+        CombatMinion $combatMinion = null,
+        $damage = null,
+        $staminaCost = null,
+        $manaCost = null)
     {
+        $eventType = SideQuestEvent::TYPE_HERO_KILLS_MINION;
+        return $this->heroAttacksMinion($eventType, $combatHero, $heroCombatAttack, $combatMinion, $damage, $staminaCost, $manaCost);
+    }
+
+    public function heroDamagesMinion(
+        CombatHero $combatHero = null,
+        HeroCombatAttack $heroCombatAttack = null,
+        CombatMinion $combatMinion = null,
+        $damage = null,
+        $staminaCost = null,
+        $manaCost = null)
+    {
+        $eventType = SideQuestEvent::TYPE_HERO_DAMAGES_MINION;
+        return $this->heroAttacksMinion($eventType, $combatHero, $heroCombatAttack, $combatMinion, $damage, $staminaCost, $manaCost);
+    }
+
+    /**
+     * @param CombatHero $combatHero
+     * @param HeroCombatAttack $heroCombatAttack
+     * @param CombatMinion $combatMinion
+     * @param $damage
+     * @param $staminaCost
+     * @param $manaCost
+     * @param string $eventType
+     * @return SideQuestEventFactory
+     */
+    protected function heroAttacksMinion(
+        string $eventType,
+        CombatHero $combatHero = null,
+        HeroCombatAttack $heroCombatAttack = null,
+        CombatMinion $combatMinion = null,
+        $damage = null,
+        $staminaCost = null,
+        $manaCost = null): SideQuestEventFactory
+    {
+        $combatHero = $combatHero ?: CombatHeroFactory::new()->create();
+        $heroCombatAttack = $heroCombatAttack ?: HeroCombatAttackFactory::new()->create();
+        $combatMinion = $combatMinion ?: CombatMinionFactory::new()->create();
+
         $clone = clone $this;
-        $clone->eventType = SideQuestEvent::TYPE_HERO_KILLS_MINION;
+        $clone->eventType = $eventType;
         $clone->data = [
-            'damage' => rand(10, 999),
+            'damage' => $damage ?: rand(10, 999),
             'combatHero' => $combatHero->toArray(),
             'heroCombatAttack' => $heroCombatAttack->toArray(),
             'combatMinion' => $combatMinion->toArray(),
-            'staminaCost' => rand(5, 15),
-            'manaCost' => rand(2, 10)
+            'staminaCost' => $staminaCost ?: rand(5, 15),
+            'manaCost' => $manaCost ?: rand(2, 10)
         ];
         return $clone;
     }
