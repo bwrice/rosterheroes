ff70d73ad8595026326ac227880a2f9e5da6d19a
Brian Rice <bwrice83@gmail.com>
2020-05-09 20:09:49 -0400

    add combat stat columns to squads

---
+++ database/migrations/2020_05_09_230156_add_combat_stat_columns_to_squads.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCombatStatColumnsToSquads extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('squads', function (Blueprint $table) {
+            $table->bigInteger('damage_dealt')->default(0);
+            $table->bigInteger('damage_taken')->default(0);
+            $table->bigInteger('attacks_blocked')->default(0);
+            $table->bigInteger('minion_kills')->default(0);
+            $table->bigInteger('titan_kills')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
