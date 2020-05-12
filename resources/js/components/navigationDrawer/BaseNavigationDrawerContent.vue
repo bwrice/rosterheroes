8deaa886d16df3c9fcc85935e1e0711de89f2768
Brian Rice <bwrice83@gmail.com>
2020-05-12 17:18:52 -0400

    extract out NavListItem component

--- resources/js/components/navigationDrawer/BaseNavigationDrawerContent.vue
+++ resources/js/components/navigationDrawer/BaseNavigationDrawerContent.vue
@@ -1,10 +1,43 @@
 <template>
-
+    <v-list nav>
+        <v-list-group color="primary">
+            <template v-slot:activator>
+                <v-list-item-content>
+                    <v-list-item-title>Barracks</v-list-item-title>
+                </v-list-item-content>
+            </template>
+            <BarracksHeroNavListItem
+                v-for="(hero, uuid) in _heroes"
+                :key="uuid"
+                :hero="hero"
+            ></BarracksHeroNavListItem>
+        </v-list-group>
+        <v-list-item>
+            <v-list-item-content>Roster</v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+            <v-list-item-content>Realm</v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+            <v-list-item-content>Campaign</v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+            <v-list-item-content>Nation</v-list-item-content>
+        </v-list-item>
+    </v-list>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
+    import BarracksHeroNavListItem from "./BarracksHeroNavListItem";
     export default {
-        name: "BaseNavigationDrawerContent"
+        name: "BaseNavigationDrawerContent",
+        components: {BarracksHeroNavListItem},
+        computed: {
+            ...mapGetters([
+                '_heroes',
+            ]),
+        }
     }
 </script>
 
---
+++ resources/js/components/navigationDrawer/NavListItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-list-item :to="to" color="accent">
+        <span class="font-weight-light">{{title}}</span>
+    </v-list-item>
+</template>
+
+<script>
+    export default {
+        name: "NavListItem",
+        props: {
+            to: {
+                required: true
+            },
+            title: {
+                required: true,
+                type: String
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
