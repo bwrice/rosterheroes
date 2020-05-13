11a427454b7971e843acd41441b40f80d7c6fdd3
Brian Rice <bwrice83@gmail.com>
2020-05-12 21:55:15 -0400

    create RosterHeroNavListItem component

---
+++ resources/js/components/commandCenter/navigationDrawer/RosterHeroNavListItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <NavListItem
+        :to="rosterRoute"
+        :title="hero.name"
+    ></NavListItem>
+</template>
+
+<script>
+    import * as routerHelpers from "../../../helpers/routerHelpers"
+    import Hero from "../../../models/Hero";
+    import NavListItem from "./NavListItem";
+
+    export default {
+        name: "RosterHeroNavListItem",
+        components: {NavListItem},
+        props: {
+            hero: {
+                type: Hero,
+                required: true
+            }
+        },
+        computed: {
+            rosterRoute() {
+                return routerHelpers.getRosterHeroRoute(this.hero, this.$route);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
