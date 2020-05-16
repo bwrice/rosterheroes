7ca3620a2df5a6055043e137354742b97db22c08
Brian Rice <bwrice83@gmail.com>
2020-05-16 10:31:22 -0400

    add hero-logo to landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -73,19 +73,19 @@
 
     <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
         <!--Left Col-->
-        <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+        <div class="flex pt-4 flex-col w-full md:w-2/5 justify-center items-start text-center lg:text-left">
             <p class="uppercase tracking-loose w-full">Where Fantasy Sports meet MMORPGs</p>
-            <h1 class="my-4 text-5xl font-bold leading-tight">Main Hero Message to sell yourself!</h1>
-            <p class="leading-normal text-2xl mb-8">Sign-up today and start playing. It's totally free!</p>
+            <div class="flex flex-col w-full justify-center">
+                @svg('rh-hero-logo', 'my-4 h-40 lg:h-64 lg:mt-4 lg:mb-8')
+            </div>
 
+            <p class="leading-normal text-center text-2xl mb-8 w-full">Register today and start playing. It's free to play!</p>
 
-
-            <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Subscribe</button>
-
+            <button class="mx-auto hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Register</button>
         </div>
         <!--Right Col-->
         <div class="w-full md:w-3/5 py-6 text-center">
-            <img class="w-full md:w-4/5 z-50" src="hero.png">
+{{--            <img class="w-full md:w-4/5 z-50" src="hero.png">--}}
         </div>
 
     </div>
@@ -101,7 +101,7 @@
                 <path d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z" opacity="0.100000001"></path>
                 <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" id="Path-4" opacity="0.200000003"></path>
             </g>
-            <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
+            <g transform="translate(-4.000000, 76.000000)" fill="#bfcdd6" fill-rule="nonzero">
                 <path d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"></path>
             </g>
         </g>
@@ -110,9 +110,9 @@
 
 
 
-<section class="bg-white border-b py-8">
+<section class="border-b py-8" style="background-color: #bfcdd6">
     <div class="container max-w-5xl mx-auto m-8">
-        <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Title</h1>
+        <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Features</h1>
         <div class="w-full mb-4">
             <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
