9933ca69cec0d7edbfff07673fe308a58cdc4ea6
Brian Rice <bwrice83@gmail.com>
2020-05-16 21:15:08 -0400

    show roster mobile screenshot on landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -85,7 +85,7 @@
         </div>
         <!--Right Col-->
         <div class="w-full md:w-3/5 py-6 text-center">
-{{--            <img class="w-full md:w-4/5 z-50" src="hero.png">--}}
+            <img class="w-full z-50" src="{{asset('/images/rh_mobile_roster_main.png')}}">
         </div>
 
     </div>
