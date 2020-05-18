98dc8f8d20cbff4d68af20ec4d3762b0202b4cd1
Brian Rice <bwrice83@gmail.com>
2020-05-18 15:56:49 -0400

    WIP landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -177,6 +177,22 @@
                 </p>
             </div>
         </div>
+
+
+
+        <div class="flex flex-wrap md:pb-16">
+            <div class="w-full sm:w-1/2 p-6 mt-6 md:pt-16">
+                <div class="align-middle">
+                    <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">Cheer on your heroes during game day</h3>
+                    <p class="text-gray-700 mb-8">Embody your heroes with players from the NFL, NBA, MLB and NHL. There's no longer an off-season in fantasy
+                        because games from these leagues are happening all year long... Well... with the exception of a global pandemic.</p>
+                </div>
+            </div>
+            <div class="w-full sm:w-1/2 px-6 mt-3 lg:py-6 lg:mt-6">
+                @svg('game_day')
+            </div>
+        </div>
+
     </div>
 </section>
 
@@ -214,21 +230,22 @@
 
 
 <!--Footer-->
-<footer class="bg-white">
+<footer style="background-color: #d5e1e8">
     <div class="container mx-auto  px-8">
 
         <div class="w-full flex flex-col md:flex-row py-6">
 
             <div class="flex-1 mb-6">
 
-                <a class="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"  href="#">
-                    <!--Icon from: http://www.potlabicons.com/ -->
-                    <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
-                        <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
-                        <path class="plane-take-off" d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "/>
-                    </svg> LANDING
+                @svg('rh_color_simple_logo')
+{{--                <a class="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"  href="#">--}}
+{{--                    <!--Icon from: http://www.potlabicons.com/ -->--}}
+{{--                    <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">--}}
+{{--                        <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />--}}
+{{--                        <path class="plane-take-off" d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "/>--}}
+{{--                    </svg> LANDING--}}
 
-                </a>
+{{--                </a>--}}
             </div>
 
 
