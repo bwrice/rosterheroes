66fd63a33ee551a6ee5dfc8bd72a84e52949ad52
Brian Rice <bwrice83@gmail.com>
2020-05-19 16:56:33 -0400

    create login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.tailwind')
+
+@section('body')
+    <body style="background-image: linear-gradient(#234a4a, #222626); background-attachment: fixed">
+
+    <div class="min-h-screen flex flex-col">
+        <form  method="POST" action="{{ route('login') }}" class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+            @csrf
+            <div class="px-6 py-8 rounded shadow-md text-black w-full" style="background-color: #d5e1e8">
+                <h1 class="mb-8 text-3xl text-center text-teal-900">Login</h1>
+                <input
+                    id="email"
+                    type="email"
+                    class="block border w-full p-3 rounded mb-4 {{ $errors->has('email') ? 'border-red-700' : 'border-gray-500' }}"
+                    name="email"
+                    placeholder="Email"
+                    value="{{ old('email') }}"
+                    required
+                >
+                @if ($errors->has('email'))
+                    <div class="rh-error bg-red-100 border border-red-400 text-red-700 px-4 py-4 mb-4 rounded relative" role="alert">
+                        <span class="block sm:inline">{{ $errors->first('email') }}</span>
+                    </div>
+                @endif
+                <input
+                    type="password"
+                    class="block border w-full p-3 rounded mb-4 {{ $errors->has('password') ? 'border-red-700' : 'border-gray-500' }}"
+                    name="password"
+                    placeholder="Password"
+                    required
+                >
+                @if ($errors->has('password'))
+                    <div class="rh-error bg-red-100 border border-red-400 text-red-700 px-4 py-4 mb-4 rounded relative" role="alert">
+                        <span class="block sm:inline">{{ $errors->first('password') }}</span>
+                    </div>
+                @endif
+
+                <div class="block text-teal-900 px-2 pb-4">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label for="remember" class="mx-2">Remember me</label>
+                </div>
+
+                <button
+                    type="submit"
+                    class="w-full text-center py-3 rounded bg-teal-700 text-white hover:bg-teal-600 focus:outline-none my-1"
+                >Login</button>
+
+                <div class="text-center text-sm text-grey-dark mt-4">
+                    <a class="no-underline border-b border-teal-900 text-teal-900" href="{{ route('password.request') }}">
+                        Forgot Your Password?
+                    </a>
+                </div>
+            </div>
+
+            <div class="text-gray-400 mt-6">
+                Don't have an account yet?
+                <a class="no-underline border-b border-teal-300 text-teal-300" href="/register">
+                    Register
+                </a>.
+            </div>
+        </form>
+    </div>
+
+    <script>
+        let formGroups = document.getElementsByTagName('input');
+        for (let i = 0; i < formGroups.length; i++) {
+            formGroups[i].addEventListener('focus', function (event) {
+                let input = event.target;
+                if (input.classList.contains('border-red-700')) {
+                    console.log(input);
+                    input.classList.remove('border-red-700');
+                    input.classList.add('border-gray-500');
+
+                    let errorMessage = input.nextElementSibling;
+                    console.log("Error");
+                    console.log(errorMessage);
+                    if (errorMessage.classList.contains('rh-error')) {
+                        errorMessage.parentNode.removeChild(errorMessage);
+                    }
+                }
+            });
+        }
+    </script>
+    </body>
+
+
+@endsection
+
