c163642ed9835146c7d005e61ed02433bc0cbff4
Brian Rice <bwrice83@gmail.com>
2020-05-19 05:52:21 -0400

    WIP landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,7 +41,7 @@
         </div>
 
         <div class="block lg:hidden pr-4">
-            <button id="nav-toggle" class="flex items-center p-1 text-orange-800 hover:text-gray-900">
+            <button id="nav-toggle" class="flex items-center p-1 toggleColour hover:text-gray-900">
                 <svg class="fill-current h-6 w-6" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
         </div>
@@ -49,16 +49,12 @@
         <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
             <ul class="list-reset lg:flex justify-end flex-1 items-center">
                 <li class="mr-3">
-                    <a class="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Active</a>
-                </li>
-                <li class="mr-3">
-                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
-                </li>
-                <li class="mr-3">
-                    <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
+                    <a class="toggleColour inline-block text-white no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="/login">Login</a>
                 </li>
             </ul>
-            <button id="navAction" class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75">Action</button>
+            <a class="mx-auto lg:mx-0 mt-4 lg:mt-0" href="/register">
+                <button id="navAction" class="hover:underline bg-white text-gray-800 font-bold rounded-full py-4 px-8 shadow opacity-75">Register</button>
+            </a>
         </div>
     </div>
 
@@ -69,8 +65,8 @@
 
 
 <!--Hero-->
-<div class="pt-24 pb-6">
-    <div class="bg-orange-300 text-center py-4 lg:px-4">
+<div class="pt-12 mt-2 md:pt-24 pb-6">
+    <div class="bg-orange-300 text-center py-2 md:py-4 lg:px-4">
         <div class="p-2 bg-orange-600 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
             <span class="flex rounded-full bg-orange-900 uppercase px-2 py-1 text-xs font-bold mr-3">Beta</span>
             <span class="md:font-semibold mr-2 text-left flex-auto">Roster Heroes is currently in beta. There will possibly be rollbacks (likely) or complete server resets (less likely)</span>
@@ -87,7 +83,9 @@
 
             <p class="leading-normal text-center text-2xl mb-8 w-full">Register today and start playing. <br/>It's free!</p>
 
-            <button class="mx-auto hover:underline bg-white text-gray-800 font-bold rounded-full my-4 py-4 px-8 shadow-lg">Register</button>
+            <a class="mx-auto" href="/register">
+                <button class="hover:underline bg-white text-gray-800 font-bold rounded-full my-4 py-4 px-8 shadow-lg">Register</button>
+            </a>
         </div>
         <!--Right Col-->
         <div class="w-full md:w-2/3 py-6 text-center">
@@ -201,7 +199,7 @@
             <div class="w-full sm:w-1/2 p-6 mt-6 md:pt-16">
                 <div class="align-middle">
                     <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">Game Day means a whole lot more</h3>
-                    <p class="text-gray-700 mb-8">That Christian McCaffrey touchdown no longer just means you've crushed Bob from Accounting,
+                    <p class="text-gray-700 mb-8">That Christian McCaffrey touchdown no longer just ensures you've predictably crushed Bob from Accounting,
                     who hasn't set his lineup in over a month. Now it could be the difference between slaying
                     an elder vampire or falling in battle to a lowly skeleton scout.</p>
                 </div>
@@ -232,14 +230,16 @@
 
 <section class="container mx-auto text-center py-6 mb-12">
 
-    <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-white">Call to Action</h1>
+    <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-white">Join the Action</h1>
     <div class="w-full mb-4">
         <div class="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
     </div>
 
-    <h3 class="my-4 text-3xl leading-tight">Main Hero Message to sell yourself!</h3>
+    <h3 class="my-4 text-3xl leading-tight">Register today and start playing for free!</h3>
 
-    <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Action!</button>
+    <a class="mx-auto lg:mx-0 mt-4 lg:mt-0" href="/register">
+        <button class="hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Register</button>
+    </a>
 
 </section>
 
@@ -253,14 +253,6 @@
             <div class="flex-1 mb-6">
 
                 @svg('rh_color_simple_logo')
-{{--                <a class="text-orange-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"  href="#">--}}
-{{--                    <!--Icon from: http://www.potlabicons.com/ -->--}}
-{{--                    <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">--}}
-{{--                        <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />--}}
-{{--                        <path class="plane-take-off" d=" M 510.7 189.151 C 505.271 168.95 484.565 156.956 464.365 162.385 L 330.156 198.367 L 155.924 35.878 L 107.19 49.008 L 211.729 230.183 L 86.232 263.767 L 36.614 224.754 L 0 234.603 L 45.957 314.27 L 65.274 347.727 L 105.802 336.869 L 240.011 300.886 L 349.726 271.469 L 483.935 235.486 C 504.134 230.057 516.129 209.352 510.7 189.151 Z "/>--}}
-{{--                    </svg> LANDING--}}
-
-{{--                </a>--}}
             </div>
 
 
