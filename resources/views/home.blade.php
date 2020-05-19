0fa1e791bc42d003c666ff57a6bfc46e2b7fbe8d
Brian Rice <bwrice83@gmail.com>
2020-05-19 06:07:48 -0400

    extract tailwind blade out of home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('tailwind')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Roster Heroes</title>
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
-
-    <!-- Font Awesome if you need it
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
-    -->
-    <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css">
-    <!--Replace with your tailwind.css once created-->
-
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
-
-    <!-- Define your gradient here - use online tools to find a gradient matching your branding-->
-    <style>
-        .gradient {
-            background: linear-gradient(90deg, #234a4a 0%, #3fa391 100%);
-        }
-    </style>
-
-</head>
+@section('body')
 
 <body class="leading-normal tracking-normal text-white gradient" style="font-family: 'Source Sans Pro', sans-serif;">
 
@@ -413,4 +388,4 @@
 
 </body>
 
-</html>
+@endsection
---
+++ resources/views/tailwind.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Roster Heroes</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+    <!-- Font Awesome if you need it
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    -->
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css">
+    <!--Replace with your tailwind.css once created-->
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
+
+    <!-- Define your gradient here - use online tools to find a gradient matching your branding-->
+    <style>
+        .gradient {
+            background: linear-gradient(90deg, #234a4a 0%, #3fa391 100%);
+        }
+    </style>
+
+</head>
+
+@yield('body')
+
+</html>
