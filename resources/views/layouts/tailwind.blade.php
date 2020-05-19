8edb5b7695b7e2df49d34fb24802b3efcc70e7fe
Brian Rice <bwrice83@gmail.com>
2020-05-19 07:18:01 -0400

    add csrf_token to tailwind layout

--- resources/views/layouts/tailwind.blade.php
+++ resources/views/layouts/tailwind.blade.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Roster Heroes</title>
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
 
-    <!-- Font Awesome if you need it
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
-    -->
+    <title>{{ config('app.name', 'Roster Heroes') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css">
     <!--Replace with your tailwind.css once created-->
 
