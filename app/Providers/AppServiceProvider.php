15f55b5aa07a12cd95849ded6f8f8ee67917e65d
Brian Rice <bwrice83@gmail.com>
2020-05-20 19:03:08 -0400

    clean-up unused namespaces from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,9 @@
 
 namespace App\Providers;
 
-use App\Exceptions\NoCurrentWeekException;
-use App\Projectors\ItemBlueprintProjector;
-use App\Domain\Models\Week;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-use Spatie\EventSourcing\Projectionist;
 
 class AppServiceProvider extends ServiceProvider
 {
