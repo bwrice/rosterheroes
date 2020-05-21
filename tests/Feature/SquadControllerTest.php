84e9bc94815599342c70b01883338d41a6b22fdf
Brian Rice <bwrice83@gmail.com>
2020-05-20 21:18:58 -0400

    add name validation tests for SquadController

--- tests/Feature/SquadControllerTest.php
+++ tests/Feature/SquadControllerTest.php
@@ -7,6 +7,7 @@ use App\Domain\Models\User;
 use App\Domain\Models\Week;
 use Carbon\CarbonImmutable;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Validation\ValidationException;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -36,5 +37,62 @@ class SquadControllerTest extends TestCase
                 'name' => $name
             ]
         ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_throw_an_exception_if_the_name_is_white_space_invalid()
+    {
+        $this->withoutExceptionHandling();
+
+        /** @var User $user */
+        $user = factory(User::class)->create();
+        Passport::actingAs($user);
+        $this->assertEquals(0, $user->squads()->count());
+
+        $whiteSpaceName = '       B';
+
+        try {
+            $response = $this->json('POST','api/v1/squads', [
+                'name' => $whiteSpaceName
+            ]);
+
+        } catch (ValidationException $exception) {
+            $nameErrors = $exception->validator->errors()->get('name');
+            $this->assertNotEmpty($nameErrors);
+            $this->assertEquals(0, $user->squads()->count());
+            return;
+        }
+
+        $this->fail("Exception Not Thrown");
+    }
+    /**
+     * @test
+     */
+    public function it_will_throw_an_exception_if_the_name_has_special_characters()
+    {
+        $this->withoutExceptionHandling();
+
+        /** @var User $user */
+        $user = factory(User::class)->create();
+        Passport::actingAs($user);
+        $this->assertEquals(0, $user->squads()->count());
+
+        $specialCharName = 'ValidExcept&';
+
+        try {
+            $response = $this->json('POST','api/v1/squads', [
+                'name' => $specialCharName
+            ]);
+
+        } catch (ValidationException $exception) {
+            $nameErrors = $exception->validator->errors()->get('name');
+            $this->assertNotEmpty($nameErrors);
+            $this->assertEquals(0, $user->squads()->count());
+            return;
+        }
+
+        $this->fail("Exception Not Thrown");
     }
 }
