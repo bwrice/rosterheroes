6c5730d2ecc56b536629bb86c222af31c357c18c
Brian Rice <bwrice83@gmail.com>
2020-05-23 01:31:34 -0400

    create HeroStatMeasurableBonusesCard component

---
+++ resources/js/components/commandCenter/barracks/HeroStatMeasurableBonusesCard.vue
@@ -0,0 +1,83 @@
+<template>
+    <v-row no-gutters>
+        <v-col cols="12">
+            <span class="title font-weight-thin">STAT BONUSES</span>
+        </v-col>
+        <v-col cols="12">
+            <v-card
+                color="#3c4d4c"
+            >
+                <v-tabs
+                    v-model="tab"
+                    mobile-break-point="10"
+                    centered
+                    background-color="#3c4d4c"
+                    color="#b3c9c3"
+                    slider-color="primary"
+                >
+                    <v-tab
+                        v-for="(bonusGroup, sport) in statBonusGroups"
+                        :key="sport"
+                    >
+                        {{ bonusGroup.sport.name }}
+                    </v-tab>
+                </v-tabs>
+                <v-tabs-items v-model="tab" style="background-color: transparent">
+                    <StatBonusGroupTabItem
+                        v-for="(bonusGroup, sport) in statBonusGroups"
+                        :key="sport"
+                        :sport="bonusGroup.sport"
+                        :stat-measurable-bonuses="bonusGroup.statMeasurableBonuses"
+                    ></StatBonusGroupTabItem>
+                </v-tabs-items>
+            </v-card>
+        </v-col>
+        <v-sheet>
+        </v-sheet>
+    </v-row>
+</template>
+
+<script>
+    import Hero from "../../../models/Hero";
+    import {mapGetters} from 'vuex';
+    import StatBonusGroupTabItem from "./StatBonusGroupTabItem";
+
+    export default {
+        name: "HeroStatMeasurableBonusesCard",
+        components: {StatBonusGroupTabItem},
+        props: {
+            hero: {
+                type: Hero,
+                required: true
+            }
+        },
+        data() {
+            return {
+                tab: null
+            }
+        },
+        computed: {
+            ...mapGetters([
+                '_sports',
+                '_statTypeByID'
+            ]),
+            statBonusGroups() {
+                let self = this;
+                return this._sports.map(function (sport) {
+                    let statMeasurableBonuses = self.hero.statMeasurableBonuses.filter(function (statMeasurableBonus) {
+                        let statType = self._statTypeByID(statMeasurableBonus.statTypeID);
+                        return statType.sportID === sport.id;
+                    });
+                    return {
+                        sport,
+                        statMeasurableBonuses
+                    }
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
