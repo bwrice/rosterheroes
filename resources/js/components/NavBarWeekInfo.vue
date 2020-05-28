3c20641c87511832de2a6a9c3282e69ffd5866e7
Brian Rice <bwrice83@gmail.com>
2020-05-28 16:27:33 -0400

    create NavBarWeekInfo vue component

---
+++ resources/js/components/NavBarWeekInfo.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-menu
+        bottom
+        left
+        v-model="opened"
+    >
+        <template v-slot:activator="{ on }">
+            <v-btn
+                :color="iconColor"
+                icon
+                v-on="on"
+            >
+                <v-icon>schedule</v-icon>
+            </v-btn>
+        </template>
+
+        <v-list>
+            <v-list-item>
+                <v-spacer></v-spacer>
+                <v-icon @click="opened = false">close</v-icon>
+            </v-list-item>
+        </v-list>
+    </v-menu>
+</template>
+
+<script>
+    export default {
+        name: "NavBarWeekInfo",
+        data() {
+            return {
+                opened: false
+            }
+        },
+        computed: {
+            iconColor() {
+                return 'success';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
