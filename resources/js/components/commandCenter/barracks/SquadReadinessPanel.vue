2f1603f4b8fd4eca456ca077bf56c42bcae2f310
Brian Rice <bwrice83@gmail.com>
2020-05-29 12:30:12 -0400

    create SquadReadinessPanel vue component

---
+++ resources/js/components/commandCenter/barracks/SquadReadinessPanel.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-sheet color="rgba(0,0,0,.4)" class="ma-1 pa-1">
+        <v-row no-gutters align="center">
+            <v-col cols="3" sm="2">
+                <v-row no-gutters justify="center">
+                    <slot>
+                        <!-- Default Slot -->
+                    </slot>
+                </v-row>
+            </v-col>
+            <v-col cols="9" sm="10">
+                <v-row no-gutters justify="center">
+                    <span class="title font-weight-regular">{{name.toUpperCase()}}</span>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-sheet>
+</template>
+
+<script>
+    export default {
+        name: "SquadReadinessPanel",
+        props: {
+            name: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
