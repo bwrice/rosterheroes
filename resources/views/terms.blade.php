0f80825ccff16344587a5d4a4720e58fd4cf239d
Brian Rice <bwrice83@gmail.com>
2020-05-29 21:50:24 -0400

    create terms and conditions blade view

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.tailwind')
+
+@section('body')
+
+    <body style="background-image: linear-gradient(#234a4a, #222626); background-attachment: fixed">
+
+    <div class="min-h-screen flex flex-col">
+        <div class="container max-w-5xl mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <div class="px-6 py-8 my-12 rounded shadow-md text-black w-full" style="background-color: #d5e1e8">
+
+        <h1 class="font-bold text-3xl my-2 text-center">Terms and Conditions for Roster Heroes</h1>
+
+        <h2 class="font-semibold text-xl my-1 underline">Introduction</h2>
+
+        <p>These Website Standard Terms and Conditions written on this webpage shall manage your use of our website,
+            Roster Heroes accessible at rosterheroes.com.</p>
+
+        <p>These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed
+            to accept all terms and conditions written in here. You must not use this Website if you disagree with any
+            of these Website Standard Terms and Conditions. These Terms and Conditions have been generated with the help
+            of the <a href="https://www.termsandcondiitionssample.com">Terms And Conditions Template</a> and the <a
+                href="https://www.terms-conditions-generator.com">Terms and Conditions Generator</a>.</p>
+
+        <p>Anyone under the age of 13 must have permission from their legal guardian to use this site and have their
+            legal guardian reviews these terms and conditions.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Intellectual Property Rights</h2>
+
+        <p>Other than the content you own, under these Terms, Roster Heroes and/or its licensors own all the
+            intellectual property rights and materials contained in this Website.</p>
+
+        <p>You are granted limited license only for purposes of viewing the material contained on this Website.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Restrictions</h2>
+
+        <p>You are specifically restricted from all of the following:</p>
+
+        <ul class="list-disc px-8 my-2">
+            <li>publishing any Website material in any other media;</li>
+            <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
+            <li>publicly performing and/or showing any Website material;</li>
+            <li>using this Website in any way that is or may be damaging to this Website;</li>
+            <li>using this Website in any way that impacts user access to this Website;</li>
+            <li>using this Website contrary to applicable laws and regulations, or in any way may cause harm to the
+                Website, or to any person or business entity;
+            </li>
+            <li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation
+                to this Website;
+            </li>
+            <li>using this Website to engage in any advertising or marketing.</li>
+        </ul>
+
+        <p>Certain areas of this Website are restricted from being access by you and Roster Heroes may further restrict
+            access by you to any areas of this Website, at any time, in absolute discretion. Any user ID and password
+            you may have for this Website are confidential and you must maintain confidentiality as well.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Your Content</h2>
+
+        <p>In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, video text, images or
+            other material you choose to display on this Website. By displaying Your Content, you grant Roster Heroes a
+            non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate
+            and distribute it in any and all media.</p>
+
+        <p>Your Content must be your own and must not be invading any third-party’s rights. Roster Heroes reserves the
+            right to remove any of Your Content from this Website at any time without notice.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Your Privacy</h2>
+
+        <p>Please read Privacy Policy.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">No warranties</h2>
+
+        <p>This Website is provided "as is," with all faults, and Roster Heroes express no representations or
+            warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing
+            contained on this Website shall be interpreted as advising you.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Limitation of liability</h2>
+
+        <p>In no event shall Roster Heroes, nor any of its officers, directors and employees, shall be held liable for
+            anything arising out of or in any way connected with your use of this Website whether such liability is
+            under contract.  Roster Heroes, including its officers, directors and employees shall not be held liable for
+            any indirect, consequential or special liability arising out of or in any way related to your use of this
+            Website.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Indemnification</h2>
+
+        <p>You hereby indemnify to the fullest extent Roster Heroes from and against any and/or all liabilities, costs,
+            demands, causes of action, damages and expenses arising in any way related to your breach of any of the
+            provisions of these Terms.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Severability</h2>
+
+        <p>If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be
+            deleted without affecting the remaining provisions herein.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Variation of Terms</h2>
+
+        <p>Roster Heroes is permitted to revise these Terms at any time as it sees fit, and by using this Website you
+            are expected to review these Terms on a regular basis.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Assignment</h2>
+
+        <p>Roster Heroes is allowed to assign, transfer, and subcontract its rights and/or obligations under these
+            Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your
+            rights and/or obligations under these Terms.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Entire Agreement</h2>
+
+        <p>These Terms constitute the entire agreement between Roster Heroes and you in relation to your use of this
+            Website, and supersede all prior agreements and understandings.</p>
+
+        <h2 class="font-semibold text-xl my-1 underline">Governing Law & Jurisdiction</h2>
+
+        <p>These Terms will be governed by and interpreted in accordance with the laws of the State of us, and you
+            submit to the non-exclusive jurisdiction of the state and federal courts located in us for the resolution of
+            any disputes.</p>
+
+        </div>
+        </div>
+    </div>
+    </body>
+@endsection
