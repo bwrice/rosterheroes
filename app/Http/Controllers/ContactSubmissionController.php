a476168f4434c21969be1ec1f228731bfc11a128
Brian Rice <bwrice83@gmail.com>
2020-05-30 13:39:25 -0400

    add create method on ContactSubmissionController

--- app/Http/Controllers/ContactSubmissionController.php
+++ app/Http/Controllers/ContactSubmissionController.php
@@ -3,10 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\ContactSubmission;
+use App\Domain\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ContactSubmissionController extends Controller
 {
+    public function create(Request $request)
+    {
+        $type = $request->type;
+        if (! in_array($type, ['contact', 'support'])) {
+            $type = 'contact';
+        }
+
+        if ($type === 'contact') {
+            $buttonText = 'Contact Us';
+            $messagePlaceHolder = 'What would you like to say?';
+        } else {
+            $buttonText = 'Contact Support';
+            $messagePlaceHolder = 'What can we help you with?';
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+        if ($user) {
+            $userEmail = $user->email;
+            $userName = $user->name;
+        } else {
+            $userEmail = null;
+            $userName = null;
+        }
+
+        return view('contact', [
+            'type' => $type,
+            'buttonText' => $buttonText,
+            'messagePlaceHolder' => $messagePlaceHolder,
+            'userEmail' => $userEmail,
+            'userName' => $userName
+        ]);
+    }
+
     public function store(Request $request)
     {
         $request->validate([
