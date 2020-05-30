5fff760b5e69451a4cd52e9949e760780e919b40
Brian Rice <bwrice83@gmail.com>
2020-05-30 11:14:49 -0400

    create privacy policy view

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,243 @@
+@extends('layouts.tailwind')
+
+@section('body')
+
+
+    <body style="background-image: linear-gradient(#234a4a, #222626); background-attachment: fixed">
+
+    <div class="min-h-screen flex flex-col">
+        <div class="container max-w-5xl mx-auto flex-1 flex flex-col items-center justify-center px-2">
+            <div class="px-6 py-8 my-12 rounded shadow-md text-black w-full" style="background-color: #d5e1e8">
+                <h1 class="font-bold text-3xl my-2 text-center">Privacy Policy for Roster Heroes</h1>
+
+                <p>This privacy policy has been compiled to better serve those who are concerned with how their 'Personally Identifiable Information' (PII) is being used online. PII, as described in US privacy law and information security, is information that can be used on its own or with other information to identify, contact, or locate a single person, or to identify an individual in context. Please read our privacy policy carefully to get a clear understanding of how we collect, use, protect or otherwise handle your Personally Identifiable Information in accordance with our website.
+                </p>
+
+                <h2 class="font-semibold text-xl my-1 underline">What personal information do we collect from the people that visit our blog, website or app?</h2>
+
+                <p>When ordering or registering on our site, as appropriate, you may be asked to enter your name, email
+                    address or other details to help you with your experience.</p>
+
+                <h2 class="font-semibold text-xl my-1 underline">When do we collect information?</h2>
+
+                <p>We collect information from you when you register on our site, fill out a form or enter information on
+                    our site.</p>
+
+                <h2 class="font-semibold text-xl my-1 underline">How do we use your information?</h2>
+
+                <p>We may use the information we collect from you when you register, make a purchase, sign up for our
+                    newsletter, respond to a survey or marketing communication, surf the website, or use certain other site
+                    features in the following ways:</p>
+
+                <ul class="list-disc px-8 my-2">
+                <li>To personalize your experience and to allow us to deliver the type of content and product offerings in
+                    which you are most interested.</li>
+                <li>To improve our website in order to better serve you.</li>
+                <li>To allow us to better service you in responding to your customer service requests.</li>
+                <li>To quickly process your transactions.</li>
+                <li>To follow up with them after correspondence (live chat, email or phone inquiries)</li>
+                </ul>
+
+                <h2 class="font-semibold text-xl my-1 underline">How do we protect your information?</h2>
+
+                <p>
+                    We do not use vulnerability scanning and/or scanning to PCI standards.
+                    We only provide articles and information. We never ask for credit card numbers.
+                    We use regular Malware Scanning.
+                </p>
+
+                <p>
+                    Your personal information is contained behind secured networks and is only accessible by a limited
+                    number of persons who have special access rights to such systems, and are required to keep the
+                    information confidential. In addition, all sensitive/credit information you supply is encrypted via
+                    Secure Socket Layer (SSL) technology.
+                </p>
+
+                <p>
+                    We implement a variety of security measures when a user enters, submits, or accesses their information
+                    to maintain the safety of your personal information.
+                </p>
+
+                <p>
+                    All transactions are processed through a gateway provider and are not stored or processed on our
+                    servers.
+                </p>
+
+                <h2 class="font-semibold text-xl my-1 underline">Do we use 'cookies'?</h2>
+
+                <p>
+                    Yes. Cookies are small files that a site or its service provider transfers to your computer's hard drive
+                    through your Web browser (if you allow) that enables the site's or service provider's systems to
+                    recognize your browser and capture and remember certain information. For instance, we use cookies to
+                    help us remember and process the items in your shopping cart. They are also used to help us understand
+                    your preferences based on previous or current site activity, which enables us to provide you with
+                    improved services. We also use cookies to help us compile aggregate data about site traffic and site
+                    interaction so that we can offer better site experiences and tools in the future.
+                </p>
+
+                <p>We use cookies to:</p>
+
+                <ul class="list-disc px-8 my-2">
+                    <li>Understand and save user's preferences for future visits.</li>
+                </ul>
+
+                <p>You can choose to have your computer warn you each time a cookie is being sent, or you can choose to
+                    turn off all cookies. You do this through your browser settings. Since browser is a little
+                    different,
+                    look at your browser's Help Menu to learn the correct way to modify your cookies.</p>
+
+                <p>If you turn cookies off, Some of the features that make your site experience more efficient may not
+                    function properly.It won't affect the user's experience that make your site experience more
+                    efficient
+                    and may not function properly.</p>
+
+
+                <h2 class="font-semibold text-xl my-1 underline">Third-party disclosure</h2>
+
+                <p>We do not sell, trade, or otherwise transfer to outside parties your Personally Identifiable
+                    Information
+                    unless we provide users with advance notice. This does not include website hosting partners and
+                    other
+                    parties who assist us in operating our website, conducting our business, or serving our users, so
+                    long
+                    as those parties agree to keep this information confidential. We may also release information when
+                    it's
+                    release is appropriate to comply with the law, enforce our site policies, or protect ours or others'
+                    rights, property or safety.</p>
+
+                <p>However, non-personally identifiable visitor information may be provided to other parties for
+                    marketing,
+                    advertising, or other uses.</p>
+
+                <h2 class="font-semibold text-xl my-1 underline">Third-party links</h2>
+
+                <p>We do not include or offer third-party products or services on our website.</p>
+
+                <h2 class="font-semibold text-xl my-1 underline">Google</h2>
+
+                <p>Google's advertising requirements can be summed up by Google's Advertising Principles. They are put
+                    in
+                    place to provide a positive experience for users.</p>
+                <a href="https://support.google.com/adwordspolicy/answer/1316548?hl=en">https://support.google.com/adwordspolicy/answer/1316548?hl=en</a>
+
+                <p>We have not enabled Google AdSense on our site but we may do so in the future.</p>
+
+                <h2 class="font-semibold text-xl my-1 underline">California Online Privacy Protection Act</h2>
+
+                <p>CalOPPA is the first state law in the nation to require commercial websites and online services to
+                    post
+                    a privacy policy. The law's reach stretches well beyond California to require any person or company
+                    in
+                    the United States (and conceivably the world) that operates websites collecting Personally
+                    Identifiable
+                    Information from California consumers to post a conspicuous privacy policy on its website stating
+                    exactly the information being collected and those individuals or companies with whom it is being
+                    shared. See more at:</p>
+                <a href="http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf">http://consumercal.org/california-online-privacy-protection-act-caloppa/#sthash.0FdRbT51.dpuf</a>
+
+                <p>According to CalOPPA, we agree to the following:<br>
+                    Users can visit our site anonymously.
+                    Once this privacy policy is created, we will add a link to it on our home page or as a minimum, on
+                    the
+                    first significant page after entering our website.
+                    Our Privacy Policy link includes the word 'Privacy' and can easily be found on the page specified
+                    above.</p>
+
+
+                <p>You will be notified of any Privacy Policy changes:</p>
+                <ul class="list-disc px-8 my-2">
+                    <li>On our Privacy Policy Page</li>
+                </ul>
+
+                <p>Can change your personal information:</p>
+                <ul class="list-disc px-8 my-2">
+                    <li>By emailing us</li>
+                    <li>By logging in to your account</li>
+                </ul>
+
+                <h2 class="font-semibold text-xl my-1 underline">COPPA (Children Online Privacy Protection Act)</h2>
+
+                <p>When it comes to the collection of personal information from children under the age of 13 years old,
+                    the
+                    Children's Online Privacy Protection Act (COPPA) puts parents in control. The Federal Trade
+                    Commission,
+                    United States' consumer protection agency, enforces the COPPA Rule, which spells out what operators
+                    of
+                    websites and online services must do to protect children's privacy and safety online.</p>
+
+                <p>We do not specifically market to children under the age of 13 years old.</p>
+
+                <h2 class="font-semibold text-xl my-1 underline">Fair Information Practices</h2>
+
+                <p>The Fair Information Practices Principles form the backbone of privacy law in the United States and
+                    the
+                    concepts they include have played a significant role in the development of data protection laws
+                    around
+                    the globe. Understanding the Fair Information Practice Principles and how they should be implemented
+                    is
+                    critical to comply with the various privacy laws that protect personal information.</p>
+
+                <p>In order to be in line with Fair Information Practices we will take the following responsive action,
+                    should a data breach occur:</p>
+                <p>
+                    We will notify you via email
+                </p>
+                <ul class="list-disc px-8 my-2">
+                    <li>Within 7 business days</li>
+                </ul>
+
+                <p>We also agree to the Individual Redress Principle which requires that individuals have the right to
+                    legally pursue enforceable rights against data collectors and processors who fail to adhere to the
+                    law.
+                    This principle requires not only that individuals have enforceable rights against data users, but
+                    also
+                    that individuals have recourse to courts or government agencies to investigate and/or prosecute
+                    non-compliance by data processors.</p>
+
+                <h2 class="font-semibold text-xl my-1 underline">CAN SPAM Act</h2>
+
+                <p>The CAN-SPAM Act is a law that sets the rules for commercial email, establishes requirements for
+                    commercial messages, gives recipients the right to have emails stopped from being sent to them, and
+                    spells out tough penalties for violations.</p>
+
+                <p>We collect your email address in order to:</p>
+
+                <ul class="list-disc px-8 my-2">
+                    <li>Process orders and to send information and updates pertaining to orders.</li>
+                    <li>Send you additional information related to your product and/or service</li>
+                    <li>Market to our mailing list or continue to send emails to our clients after the original transaction
+                        has occurred.</li>
+                </ul>
+
+                <p>To be in accordance with CANSPAM, we agree to the following:</p>
+
+                <ul class="list-disc px-8 my-2">
+                    <li>Not use false or misleading subjects or email addresses.</li>
+                    <li>Identify the message as an advertisement in some reasonable way.</li>
+                    <li>Include the physical address of our business or site headquarters.</li>
+                    <li>Monitor third-party email marketing services for compliance, if one is used.</li>
+                    <li>Honor opt-out/unsubscribe requests quickly.</li>
+                    <li>Allow users to unsubscribe by using the link at the bottom of each email.</li>
+                </ul>
+
+                <p>If at any time you would like to unsubscribe from receiving future emails, you can email us at:</p>
+                <ul class="list-disc px-8 my-2">
+                    <li>Follow the instructions at the bottom of each email.
+                        and we will promptly remove you from ALL correspondence.</li>
+                </ul>
+
+                <h2 class="font-semibold text-xl my-1 underline">Contacting Us</h2>
+
+                <p>If there are any questions regarding this privacy policy, you may contact us using the information
+                    below.</p>
+
+                <p>rosterheroes.com</p>
+                <p>contact@rosterheroes.com</p>
+
+                <p>Last Edited on 05-29-2020</p>
+
+            </div>
+        </div>
+    </div>
+    </body>
+@endsection
