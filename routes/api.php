e3f9fd35642975f2d80822cc1c1ccdf0a5a02184
Brian Rice <bwrice83@gmail.com>
2020-06-03 01:09:54 -0400

    remove commented out route from original laravel install

--- routes/api.php
+++ routes/api.php
@@ -59,10 +59,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 Route::prefix('v1')->group(function () {
 
     Route::prefix('weeks')->group(function () {
