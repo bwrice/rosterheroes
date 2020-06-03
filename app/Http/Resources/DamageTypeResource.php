e117db32ed18800ffa70b6e8be132ce12056c8d4
Brian Rice <bwrice83@gmail.com>
2020-06-02 21:05:05 -0400

    add damage-type id to resource

--- app/Http/Resources/DamageTypeResource.php
+++ app/Http/Resources/DamageTypeResource.php
@@ -22,6 +22,7 @@ class DamageTypeResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name
         ];
     }
