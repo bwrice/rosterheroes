fcc91fb6f3b4ad33a7ab27fa19798d0507c3a366
Brian Rice <bwrice83@gmail.com>
2020-06-03 10:09:34 -0400

    create target-priority svg icon components

---
+++ resources/js/components/icons/targetPriorities/TargetPriorityIcon.vue
@@ -0,0 +1,40 @@
+<template>
+    <SvgIconSheet>
+        <component :is="targetPriorityComponent"></component>
+    </SvgIconSheet>
+</template>
+
+<script>
+
+    import {mapGetters} from 'vuex';
+    import SvgIconSheet from "../../commandCenter/global/SvgIconSheet";
+    import AnyTargetIcon from "./AnyTargetIcon";
+
+    export default {
+        name: "TargetPriorityIcon",
+        components: {SvgIconSheet, AnyTargetIcon},
+        props: {
+            targetPriorityId: {
+                type: Number,
+                required: true
+            }
+        },
+        computed: {
+            ...mapGetters([
+                '_targetPriorityByID'
+            ]),
+            targetPriority() {
+                return this._targetPriorityByID(this.targetPriorityId);
+            },
+            targetPriorityComponent() {
+                if (this.targetPriority.name === 'Any') {
+                    return 'AnyTargetIcon';
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
