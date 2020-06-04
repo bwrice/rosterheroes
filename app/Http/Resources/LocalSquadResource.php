e6b1d33e295414eb7ce935ca208305d982e7634c
Brian Rice <bwrice83@gmail.com>
2020-06-03 21:12:20 -0400

    create LocalSquadResource

---
+++ app/Http/Resources/LocalSquadResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\Squad;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class LocalSquadResource
+ * @package App\Http\Resources
+ *
+ * @mixin Squad
+ */
+class LocalSquadResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'uuid' => $this->uuid,
+            'slug' => $this->slug,
+            'name' => $this->name,
+            'level' => $this->level(),
+            'heroes' => LocalHeroResource::collection($this->heroes)
+        ];
+    }
+}
