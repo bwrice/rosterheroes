51803a0951be6a6ad75afbc077ebfdee10647e48
Brian Rice <bwrice83@gmail.com>
2020-06-04 00:03:11 -0400

    create LocalHero JS model

---
+++ resources/js/models/LocalHero.js
@@ -0,0 +1,11 @@
+
+export default class LocalHero {
+
+    constructor({uuid, slug, name, heroClassID, heroRaceID}) {
+        this.uuid = uuid;
+        this.slug = slug;
+        this.name = name;
+        this.heroClassID = heroClassID;
+        this.heroRaceID = heroRaceID;
+    }
+}
