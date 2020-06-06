331e3ddc601910eeafcd883f1bcc9feebf42cd1c
Brian Rice <bwrice83@gmail.com>
2020-06-06 16:42:28 -0400

    create CardSection Component

---
+++ resources/js/components/commandCenter/global/CardSection.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-row no-gutters>
+        <v-col cols="12">
+            <span class="title font-weight-thin">{{title}}</span>
+        </v-col>
+        <slot>
+            <!-- Default Slot -->
+        </slot>
+    </v-row>
+</template>
+
+<script>
+    export default {
+        name: "CardSection",
+        props: {
+            title: {
+                type: String
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
