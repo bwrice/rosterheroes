554a154c483ff817e25b47d55bdaffbbd2e346f3
Brian Rice <bwrice83@gmail.com>
2020-06-06 23:20:31 -0400

    create HistoricCampaignPanel component

---
+++ resources/js/components/commandCenter/campaign/HistoricCampaignPanel.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-sheet color="#524c59" class="py-1 px-2">
+        <v-row no-gutters justify="center" class="pb-1">
+            <span class="title rh-op-90">{{historicCampaign.description}}</span>
+        </v-row>
+        <v-sheet
+            v-for="(campaignStop, uuid) in historicCampaign.stops"
+            :key="uuid"
+            color="rgba(0,0,0,.2)"
+            class="px-2 py-1 mb-1"
+
+        >
+            <v-row
+                no-gutters
+                justify="space-between"
+                align="center"
+            >
+                <span class="subtitle-2 rh-op-85">{{campaignStop.questName}}</span>
+                <v-btn color="primary">
+                    view
+                </v-btn>
+            </v-row>
+        </v-sheet>
+    </v-sheet>
+</template>
+
+<script>
+    import HistoricCampaign from "../../../models/HistoricCampaign";
+
+    export default {
+        name: "HistoricCampaignPanel",
+        props: {
+            historicCampaign: {
+                type: HistoricCampaign,
+                required: true
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
