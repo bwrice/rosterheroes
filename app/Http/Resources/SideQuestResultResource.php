e049b789d254db9dc6ed165e004bcbb62293c0ec
Brian Rice <bwrice83@gmail.com>
2020-06-07 17:10:34 -0400

    create JSON resources for quest results

---
+++ app/Http/Resources/SideQuestResultResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\SideQuestResult;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class SideQuestResultResource
+ * @package App\Http\Resources
+ *
+ * @mixin SideQuestResult
+ */
+class SideQuestResultResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'uuid' => $this->uuid,
+            'sideQuest' => new SideQuestResource($this->sideQuest),
+            'experienceRewarded' => $this->experience_rewarded,
+            'favorRewarded' => $this->favor_rewarded
+        ];
+    }
+}
