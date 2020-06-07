082ed89fa9b849d34c7f223e23be5461c48d45b0
Brian Rice <bwrice83@gmail.com>
2020-06-07 13:32:55 -0400

    create QuestResultView

---
+++ resources/js/components/commandCenter/views/campaign/QuestResultView.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-container>
+        <v-row justify="center">
+            <span class="headline">{{historicCampaignStop.questName}}</span>
+        </v-row>
+        <v-row>
+            <v-col cols="12" offset-sm="2" sm="8" offset-md="0" md="6" lg="5" offset-lg="1" xl="4" offset-xl="2">
+            </v-col>
+            <v-col cols="12" offset-sm="2" sm="8" offset-md="0" md="6" lg="5" xl="4">
+                <v-row no-gutters>
+                    <v-col cols="12" class="pb-3">
+                    </v-col>
+                    <v-col cols="12" class="pt-3">
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    export default {
+        name: "QuestResultView",
+        computed: {
+            ...mapGetters([
+                '_historicCampaignStopByUuid',
+            ]),
+            historicCampaignStop() {
+                return this._historicCampaignStopByUuid(this.$route.params.campaignStopUuid);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
