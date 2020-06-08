ea42093bd86c9f53dd2a81087874018970665a74
Brian Rice <bwrice83@gmail.com>
2020-06-07 22:09:35 -0400

    WIP SideQuestResultReplay

---
+++ resources/js/components/commandCenter/campaign/SideQuestResultReplay.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-sheet
+    >
+        <v-row no-gutters justify="center">
+            <span class="title rh-op-85">{{sideQuestResult.sideQuest.name}}</span>
+        </v-row>
+    </v-sheet>
+</template>
+
+<script>
+    import SideQuestResult from "../../../models/SideQuestResult";
+
+    export default {
+        name: "SideQuestResultReplay",
+        props: {
+            sideQuestResult: {
+                type: SideQuestResult,
+                required: true
+            },
+            sideQuestEvents: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
