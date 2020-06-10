63d192b3ad8a189cbf1df7b7c0363e7fb476beb6
Brian Rice <bwrice83@gmail.com>
2020-06-10 00:03:34 -0400

    remove jquery comment

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -260,12 +260,6 @@
 </footer>
 
 
-
-
-<!-- jQuery if you need it
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
--->
-
 <script>
 
     var scrollpos = window.scrollY;
