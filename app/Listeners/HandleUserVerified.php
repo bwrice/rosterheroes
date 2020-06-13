5a29360aee6b66bb263ad3f65ae4d1d62320727c
Brian Rice <bwrice83@gmail.com>
2020-06-13 18:36:39 -0400

    rename event listener for verified user

--- app/Listeners/HandleUserVerified.php
+++ app/Listeners/HandleUserVerified.php
@@ -4,15 +4,11 @@ namespace App\Listeners;
 
 use App\Domain\Models\User;
 use App\Facades\Admin;
-use App\Notifications\AdminNotifiable;
 use App\Notifications\NewVerifiedUserNotification;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Notification;
 
-class AlertOfNewVerifiedUser implements ShouldQueue
+class HandleUserVerified implements ShouldQueue
 {
 
     /**
