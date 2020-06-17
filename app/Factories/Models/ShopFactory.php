0be1c73541b4f2ed4b628e4fa152edb3e9bc60a4
Brian Rice <bwrice83@gmail.com>
2020-06-17 11:37:53 -0400

    create ShopFactory

---
+++ app/Factories/Models/ShopFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace App\Factories\Models;
+
+
+use App\Domain\Models\Province;
+use App\Domain\Models\Shop;
+use Illuminate\Support\Str;
+
+class ShopFactory
+{
+    protected $provinceID;
+
+    public static function new(): self
+    {
+        return new self();
+    }
+
+    /**
+     * @param array $extra
+     * @return Shop
+     */
+    public function create(array $extra = []): Shop
+    {
+        /** @var Shop $shop */
+        $shop = Shop::query()->create(array_merge([
+            'uuid' => (string) Str::uuid(),
+            'name' => 'Test Shop ' . rand(1, 999999),
+            'province_id' => $this->getProvinceID(),
+        ], $extra));
+
+
+        return $shop;
+    }
+
+    public function withProvinceID(int $provinceID)
+    {
+        $clone = clone $this;
+        $clone->provinceID = $provinceID;
+        return $clone;
+    }
+
+    protected function getProvinceID()
+    {
+        return $this->provinceID ?: Province::query()->inRandomOrder()->first()->id;
+    }
+}
