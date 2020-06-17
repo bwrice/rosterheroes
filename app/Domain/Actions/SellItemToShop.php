c9442c197aa9261e3e75b9f40f6f0aefaa909089
Brian Rice <bwrice83@gmail.com>
2020-06-17 11:38:13 -0400

    create SellItemToShop domain action

---
+++ app/Domain/Actions/SellItemToShop.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Models\Item;
+use App\Domain\Models\Shop;
+use App\Domain\Models\Squad;
+use App\Exceptions\SellItemToShopException;
+use Illuminate\Support\Facades\DB;
+
+class SellItemToShop
+{
+
+    /**
+     * @param Item $item
+     * @param Squad $squad
+     * @param Shop $shop
+     * @throws SellItemToShopException
+     */
+    public function execute(Item $item, Squad $squad, Shop $shop)
+    {
+        if (! $item->ownedByMorphable($squad)) {
+            $message = $item->getItemName() . ' does not belong to ' . $squad->name;
+            throw new SellItemToShopException($item, $squad, $shop, $message, SellItemToShopException::CODE_INVALID_ITEM_OWNERSHIP);
+        }
+
+        if ($squad->province_id !== $shop->province_id) {
+            $message = $squad->name . ' is not in the province of ' . $shop->province->name;
+            throw new SellItemToShopException($item, $squad, $shop, $message, SellItemToShopException::CODE_INVALID_PROVINCE);
+        }
+
+        $salePrice = floor(0.6 * $item->getValue());
+
+        DB::transaction(function () use ($item, $squad, $shop, $salePrice) {
+
+            $squad->gold += $salePrice;
+            $squad->save();
+            $item->hasItems()->associate($shop);
+            $item->save();
+        });
+    }
+
+
+}
