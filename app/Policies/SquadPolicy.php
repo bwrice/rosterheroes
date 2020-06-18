fc7fb074b6c1771a59ea9467ce3043a335385e87
Brian Rice <bwrice83@gmail.com>
2020-06-17 20:13:07 -0400

    create visitShop Squad Policy

--- app/Policies/SquadPolicy.php
+++ app/Policies/SquadPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Domain\Models\Shop;
 use App\Domain\Models\User;
 use App\Domain\Models\Squad;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -10,6 +11,7 @@ class SquadPolicy
 {
     public const MANAGE = 'manage';
     public const VIEW = 'view';
+    public const VISIT_SHOP = 'visit-shop';
 
     use HandlesAuthorization;
 
@@ -21,5 +23,14 @@ class SquadPolicy
     public function view(User $user, Squad $squad)
     {
         return $user->id === $squad->user_id;
+    }
+
+    public function visitShop(User $user, Squad $squad, Shop $shop)
+    {
+        if ($squad->user_id !== $user->id) {
+            return false;
+        }
+
+        return $squad->province_id === $shop->province_id;
     }
 }
