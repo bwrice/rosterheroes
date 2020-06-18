eb376f6e3d13faaba05f8c2f84301504896ee98e
Brian Rice <bwrice83@gmail.com>
2020-06-18 12:05:14 -0400

    add all seeders to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(DefaultUsersSeeder::class);
+        $this->call(PlayerSpiritsSeeder::class);
+        $this->call(ShopsSeeder::class);
     }
 }
