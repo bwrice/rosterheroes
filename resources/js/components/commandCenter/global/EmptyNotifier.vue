9cd86af536ef0ab04353314071c2dd3f01355fa6
Brian Rice <bwrice83@gmail.com>
2020-06-18 19:21:42 -0400

    remove redundant columsn to EmptyNotifier

--- resources/js/components/commandCenter/global/EmptyNotifier.vue
+++ resources/js/components/commandCenter/global/EmptyNotifier.vue
@@ -1,11 +1,9 @@
 <template>
-    <v-col cols="12">
-        <v-sheet color="rgba(255,255,255, 0.25)" class="my-2">
-            <v-row no-gutters class="pa-2" justify="center" align="center">
-                <span class="subtitle-1 font-weight-light">{{notificationText}}</span>
-            </v-row>
-        </v-sheet>
-    </v-col>
+    <v-sheet color="rgba(255,255,255, 0.25)" class="my-2">
+        <v-row no-gutters class="pa-2" justify="center" align="center">
+            <span class="subtitle-1 rh-op-80">{{notificationText}}</span>
+        </v-row>
+    </v-sheet>
 </template>
 
 <script>
