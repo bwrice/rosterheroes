c3c12d12a5b8702cb59734313634519b25d86899
Brian Rice <bwrice83@gmail.com>
2020-06-19 20:46:49 -0400

    create buttons for adding and removing from array of items to sell

---
+++ resources/js/components/commandCenter/realm/AddItemToSellButton.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-btn
+        x-small
+        color="primary"
+        @click="handleClick"
+    >
+        sell
+    </v-btn>
+</template>
+
+<script>
+
+    import {mapActions} from 'vuex';
+    import Item from "../../../models/Item";
+
+    export default {
+        name: "AddItemToSellButton",
+        props: {
+            item: {
+                type: Item,
+                required: true
+            }
+        },
+        methods: {
+            ...mapActions([
+                'addItemToSell'
+            ]),
+            handleClick() {
+                this.addItemToSell(this.item);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/commandCenter/realm/RemoveItemToSellButton.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-btn
+        x-small
+        color="accent"
+        @click="handleClick"
+    >
+        remove
+    </v-btn>
+</template>
+
+<script>
+
+    import {mapActions} from 'vuex';
+    import Item from "../../../models/Item";
+
+    export default {
+        name: "RemoveItemToSellButton",
+        props: {
+            item: {
+                type: Item,
+                required: true
+            }
+        },
+        methods: {
+            ...mapActions([
+                'removeItemToSell'
+            ]),
+            handleClick() {
+                this.removeItemToSell(this.item);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
