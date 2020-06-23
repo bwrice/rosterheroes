b24af3c6d2f03af10dc887514d678dd59cc65b81
Brian Rice <bwrice83@gmail.com>
2020-06-23 16:45:07 -0400

    create SellItemBundleToShop domain action

---
+++ app/Domain/Actions/SellItemBundleToShop.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Domain\Actions;
+
+
+use App\Domain\Collections\ItemCollection;
+use App\Domain\Models\Item;
+use App\Domain\Models\Shop;
+use App\Domain\Models\Squad;
+use Illuminate\Support\Facades\DB;
+
+class SellItemBundleToShop
+{
+    /**
+     * @var SellItemToShop
+     */
+    protected $sellItemToShop;
+
+    public function __construct(SellItemToShop $sellItemToShop)
+    {
+        $this->sellItemToShop = $sellItemToShop;
+    }
+
+    /**
+     * @param ItemCollection $items
+     * @param Squad $squad
+     * @param Shop $shop
+     * @return ItemCollection
+     */
+    public function execute(ItemCollection $items, Squad $squad, Shop $shop)
+    {
+        return DB::transaction(function () use ($items, $squad, $shop) {
+
+            $itemsSold = new ItemCollection();
+
+            $items->each(function (Item $item) use ($itemsSold, $squad, $shop) {
+                $items = $this->sellItemToShop->execute($item, $squad, $shop);
+                $itemsSold->merge($items);
+            });
+        });
+    }
+}
