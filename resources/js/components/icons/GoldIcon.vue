18aabaddbae61c088ae6e57b5a84966c2fceccdc
Brian Rice <bwrice83@gmail.com>
2020-06-23 14:22:32 -0400

    create GoldIcon

---
+++ resources/js/components/icons/GoldIcon.vue
@@ -0,0 +1,186 @@
+<template>
+    <svg
+        xmlns:svg="http://www.w3.org/2000/svg"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+        viewBox="0 0 193.81583 136.06972"
+    >
+        <defs
+            id="defs2" />
+        <g
+            transform="translate(-4.3346999,-43.62199)"
+            id="layer1">
+            <g
+                id="g854">
+                <path
+                    id="ellipse843"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    transform="scale(0.26458333)" />
+                <ellipse
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="path833"
+                    cx="95.068138"
+                    cy="127.06916"
+                    rx="38.734165"
+                    ry="13.260345" />
+            </g>
+            <g
+                transform="translate(5.6115437,-18.129603)"
+                id="g860">
+                <path
+                    transform="scale(0.26458333)"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="path856" />
+                <ellipse
+                    ry="13.260345"
+                    rx="38.734165"
+                    cy="127.06916"
+                    cx="95.068138"
+                    id="ellipse858"
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+            </g>
+            <g
+                id="g866"
+                transform="translate(23.093661,-34.10092)">
+                <path
+                    id="path862"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    transform="scale(0.26458333)" />
+                <ellipse
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="ellipse864"
+                    cx="95.068138"
+                    cy="127.06916"
+                    rx="38.734165"
+                    ry="13.260345" />
+            </g>
+            <g
+                transform="translate(7.9856588,-52.230523)"
+                id="g872">
+                <path
+                    transform="scale(0.26458333)"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="path868" />
+                <ellipse
+                    ry="13.260345"
+                    rx="38.734165"
+                    cy="127.06916"
+                    cx="95.068138"
+                    id="ellipse870"
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+            </g>
+            <g
+                id="g878"
+                transform="translate(-51.499273,27.566337)">
+                <path
+                    id="path874"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    transform="scale(0.26458333)" />
+                <ellipse
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="ellipse876"
+                    cx="95.068138"
+                    cy="127.06916"
+                    rx="38.734165"
+                    ry="13.260345" />
+            </g>
+            <g
+                transform="translate(-38.504627,12.281421)"
+                id="g884">
+                <path
+                    transform="scale(0.26458333)"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="path880" />
+                <ellipse
+                    ry="13.260345"
+                    rx="38.734165"
+                    cy="127.06916"
+                    cx="95.068138"
+                    id="ellipse882"
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+            </g>
+            <g
+                id="g890"
+                transform="translate(57.00071,22.448118)">
+                <path
+                    id="path886"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    transform="scale(0.26458333)" />
+                <ellipse
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="ellipse888"
+                    cx="95.068138"
+                    cy="127.06916"
+                    rx="38.734165"
+                    ry="13.260345" />
+            </g>
+            <g
+                transform="translate(45.206608,9.247695)"
+                id="g896">
+                <path
+                    transform="scale(0.26458333)"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="path892" />
+                <ellipse
+                    ry="13.260345"
+                    rx="38.734165"
+                    cy="127.06916"
+                    cx="95.068138"
+                    id="ellipse894"
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+            </g>
+            <g
+                id="g902"
+                transform="translate(63.673172,-6.6855583)">
+                <path
+                    id="path898"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    transform="scale(0.26458333)" />
+                <ellipse
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="ellipse900"
+                    cx="95.068138"
+                    cy="127.06916"
+                    rx="38.734165"
+                    ry="13.260345" />
+            </g>
+            <g
+                transform="translate(-1.5631108,-69.686825)"
+                id="g908">
+                <path
+                    transform="scale(0.26458333)"
+                    d="m 359.50586,472.83594 a 146.39684,50.117839 0 0 0 -63.05274,4.96484 h -82.47656 v 41.45313 a 146.39684,50.117839 0 0 0 -0.40039,3.70117 146.39684,50.117839 0 0 0 146.39649,50.11719 146.39684,50.117839 0 0 0 146.39843,-50.11719 146.39684,50.117839 0 0 0 -0.78515,-5.17774 v -39.97656 h -82.08985 a 146.39684,50.117839 0 0 0 -63.52343,-4.96484 146.39684,50.117839 0 0 0 -0.4668,0 z"
+                    style="display:inline;fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:3.77953;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+                    id="path904" />
+                <ellipse
+                    ry="13.260345"
+                    rx="38.734165"
+                    cy="127.06916"
+                    cx="95.068138"
+                    id="ellipse906"
+                    style="fill:#d3cc28;fill-opacity:1;stroke:#000000;stroke-width:1;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" />
+            </g>
+        </g>
+    </svg>
+</template>
+
+<script>
+    import SvgIconSheet from "../commandCenter/global/SvgIconSheet";
+    export default {
+        name: "GoldIcon",
+        components: {SvgIconSheet}
+    }
+</script>
+
+<style scoped>
+
+</style>
