6eaf3098c2a62267b9e945835cb62e8c68b2d721
Brian Rice <bwrice83@gmail.com>
2020-06-25 11:11:59 -0400

    pass tier and blueprints to CreateShop action

--- app/Domain/Actions/CreateShop.php
+++ app/Domain/Actions/CreateShop.php
@@ -4,6 +4,7 @@
 namespace App\Domain\Actions;
 
 
+use App\Domain\Collections\ItemBlueprintCollection;
 use App\Domain\Models\Province;
 use App\Domain\Models\Shop;
 use Illuminate\Support\Str;
@@ -12,18 +13,23 @@ class CreateShop
 {
     /**
      * @param string $name
+     * @param int $tier
      * @param Province $province
+     * @param ItemBlueprintCollection $itemBlueprints
      * @return Shop
      */
-    public function execute(string $name, Province $province)
+    public function execute(string $name, int $tier, Province $province, ItemBlueprintCollection $itemBlueprints)
     {
         /** @var Shop $shop */
         $shop = Shop::query()->create([
             'uuid' => Str::uuid(),
             'name' => $name,
             'province_id' => $province->id,
-            'tier' => 1
+            'tier' => $tier
         ]);
+
+        $shop->itemBlueprints()->saveMany($itemBlueprints);
+
         return $shop;
     }
 }
--- tests/Feature/CreateShopTest.php
+++ tests/Feature/CreateShopTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Domain\Actions\CreateShop;
+use App\Domain\Collections\ItemBlueprintCollection;
+use App\Domain\Models\ItemBlueprint;
 use App\Domain\Models\Province;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -25,29 +27,33 @@ class CreateShopTest extends TestCase
     /**
      * @test
      */
-    public function it_will_create_a_tier_1_shop_with_the_name_and_province()
+    public function it_will_create_a_shop_with_the_correct_atributes()
     {
         $name = $this->faker->streetName;
         $province = Province::query()->inRandomOrder()->first();
 
-        $shop = $this->getDomainAction()->execute($name, $province);
+        $tier = rand(1,3);
+        $shop = $this->getDomainAction()->execute($name, $tier, $province, new ItemBlueprintCollection());
 
         $this->assertEquals($name, $shop->name);
         $this->assertEquals($province->id, $shop->province_id);
-        $this->assertEquals(1, $shop->tier);
+        $this->assertEquals($tier, $shop->tier);
     }
 
     /**
      * @test
      */
-    public function it_will_create_shop_with_a_handful_of_item_blueprints()
+    public function it_will_create_shop_and_attach_item_blueprints()
     {
         $name = $this->faker->streetName;
         $province = Province::query()->inRandomOrder()->first();
 
-        $shop = $this->getDomainAction()->execute($name, $province);
+        $amount = rand(3, 8);
+        /** @var ItemBlueprintCollection $itemBlueprints */
+        $itemBlueprints = ItemBlueprint::query()->inRandomOrder()->take($amount)->get();
 
+        $shop = $this->getDomainAction()->execute($name, 1, $province, $itemBlueprints);
 
-        $this->assertGreaterThan(5, $shop->itemBlueprints);
+        $this->assertEquals($amount, $shop->itemBlueprints->count());
     }
 }
