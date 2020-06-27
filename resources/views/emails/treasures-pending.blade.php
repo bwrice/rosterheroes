023ac89c93c720a21aacee6cd9f8ed19fa6074e5
Brian Rice <bwrice83@gmail.com>
2020-06-27 11:56:38 -0400

    tweak treasures pending message

--- resources/views/emails/treasures-pending.blade.php
+++ resources/views/emails/treasures-pending.blade.php
@@ -6,12 +6,13 @@
 ?>
 
 @component('mail::message')
+
 # {{$unopenedChestsCount}} Unopened Chests Awaiting!
 
 ![Treasure Chest]({{ config('app.url') . '/images/treasures.png' }})
 
-Your squad, {{$squad->name}}, has {{$unopenedChestsCount}} treasure chests
-won from campaigns that still haven't been opened. There's loot to be had!
+Your squad, {{$squad->name}}, has {{$unopenedChestsCount}} treasure chests that
+won from campaigns and still haven't been opened. There's loot to be had!
 Visit {{$squad->name}}'s command center and open those chests.
 
 @component('mail::button', ['url' => config('app.url') . '/command-center/' . $squad->slug])
