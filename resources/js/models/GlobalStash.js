68dd4feede7002a246d6a8d5ecf69f35a0b2abd6
Brian Rice <bwrice83@gmail.com>
2020-07-14 19:48:52 -0400

    create GlobalStash JS model

---
+++ resources/js/models/GlobalStash.js
@@ -0,0 +1,9 @@
+
+export default class GlobalStash {
+
+    constructor({uuid, provinceUuid, itemsCount}) {
+        this.uuid = uuid;
+        this.provinceUuid = provinceUuid;
+        this.itemsCount = itemsCount;
+    }
+}
