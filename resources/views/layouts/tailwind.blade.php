65334bc346114cb7aeefa9e8be020352510198fc
Brian Rice <bwrice83@gmail.com>
2020-07-21 14:54:18 -0400

    add google analytics scripts to base layouts.

--- resources/views/layouts/tailwind.blade.php
+++ resources/views/layouts/tailwind.blade.php
@@ -2,6 +2,16 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?{{config('app.g_tag_id')}}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', {{config('app.g_tag_id')}});
+    </script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
