98e6b06bf71bf40391bdbf8d8ac979dd95631011
Brian Rice <bwrice83@gmail.com>
2020-07-25 17:03:42 -0400

    add static helper methods to EmailSubscription model

--- app/Domain/Models/EmailSubscription.php
+++ app/Domain/Models/EmailSubscription.php
@@ -19,12 +19,32 @@ class EmailSubscription extends Model
 {
 
     public const NEWSLETTER = 'newsletter';
-    public const SQUAD_NOTIFICATIONS = 'squad-notifications';
+    public const SQUAD_NOTIFICATIONS = 'squad notifications';
 
     protected $guarded = [];
 
     public function users()
     {
         return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
+    /**
+     * @return static
+     */
+    public static function squadNotifications()
+    {
+        /** @var static $sub */
+        $sub = static::query()->where('name', '=', self::SQUAD_NOTIFICATIONS)->firstOrFail();
+        return $sub;
+    }
+
+    /**
+     * @return static
+     */
+    public static function newsletter()
+    {
+        /** @var static $sub */
+        $sub = static::query()->where('name', '=', self::NEWSLETTER)->firstOrFail();
+        return $sub;
     }
 }
