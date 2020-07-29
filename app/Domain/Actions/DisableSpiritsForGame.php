76ca72d642e21d61275fe231af06358ab0e0fa48
Brian Rice <bwrice83@gmail.com>
2020-07-29 10:47:45 -0400

    notify admin for spirits disabled

--- app/Domain/Actions/DisableSpiritsForGame.php
+++ app/Domain/Actions/DisableSpiritsForGame.php
@@ -7,26 +7,34 @@ namespace App\Domain\Actions;
 use App\Domain\Models\Game;
 use App\Domain\Models\Hero;
 use App\Domain\Models\PlayerSpirit;
+use App\Facades\Admin;
+use App\Notifications\SpiritsDisabledForGame;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 
 class DisableSpiritsForGame
 {
-    public function execute(Game $game)
+    public function execute(Game $game, string $reason = 'N/A')
     {
         $spirits = PlayerSpirit::query()->whereHas('playerGameLog', function (Builder $builder) use ($game) {
             return $builder->where('game_id', '=', $game->id);
         });
 
-        $spirits->each(function (PlayerSpirit $playerSpirit) {
-            $playerSpirit->heroes()->chunk(200, function (Collection $heroes) {
+        $spiritsCount = $spirits->count();
+        $heroesCount = 0;
+
+        $spirits->each(function (PlayerSpirit $playerSpirit) use (&$heroesCount) {
+            $playerSpirit->heroes()->chunk(200, function (Collection $heroes) use (&$heroesCount) {
                 $heroes->each(function (Hero $hero) {
                     $hero->player_spirit_id = null;
                     $hero->save();
                 });
+                $heroesCount += $heroes->count();
             });
         });
 
         $spirits->delete();
+
+        Admin::notify(new SpiritsDisabledForGame($game, $spiritsCount, $heroesCount, $reason));
     }
 }
