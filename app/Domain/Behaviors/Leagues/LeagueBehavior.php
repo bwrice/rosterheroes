e54d414894e73b67cd8346c803c9801f6b92f1ec
Brian Rice <bwrice83@gmail.com>
2020-07-29 16:49:32 -0400

    Simplify LeagueBehavior

--- app/Domain/Behaviors/Leagues/LeagueBehavior.php
+++ app/Domain/Behaviors/Leagues/LeagueBehavior.php
@@ -8,85 +8,22 @@
 
 namespace App\Domain\Behaviors\Leagues;
 
-
-use Carbon\CarbonImmutable;
+use Carbon\CarbonInterface;
 
 class LeagueBehavior
 {
     /**
-     * @var string
-     */
-    protected $key;
-    /**
      * @var int
      */
-    protected $dayOfYearStart;
-    /**
-     * @var int
-     */
-    protected $dayOfYearEnd;
-    /**
-     * @var int
-     */
-    protected $totalTeams;
+    protected $seasonChangeOverDayOfYear = 1;
 
-    public function __construct(string $key, int $dayOfYearStart, int $dayOfYearEnd, int $totalTeams)
+    public function getSeason(CarbonInterface $date = null): int
     {
-        $this->key = $key;
-        $this->dayOfYearStart = $dayOfYearStart;
-        $this->dayOfYearEnd = $dayOfYearEnd;
-        $this->totalTeams = $totalTeams;
-    }
-
-    public function isLive(): bool
-    {
-        $currentDayOfYear = CarbonImmutable::now()->dayOfYear;
-        if ( $this->spansOverYearChange() ) {
-            return $currentDayOfYear >= $this->getDayOfYearStart() || $currentDayOfYear <= $this->getDayOfYearEnd();
-        } else {
-            return $currentDayOfYear >= $this->getDayOfYearStart() && $currentDayOfYear <= $this->getDayOfYearEnd();
-        }
-    }
-
-    public function getSeason(): int
-    {
-        $now = CarbonImmutable::now();
-        if (! $this->isLive()) {
-            return $now->year;
+        $date = $date ?: now();
+        if ($date->dayOfYear >= $this->seasonChangeOverDayOfYear) {
+            return $date->year;
         }
 
-        if ($this->spansOverYearChange() && ($now->dayOfYear <= $this->getDayOfYearEnd())) {
-            return $now->year - 1;
-        }
-        return $now->year;
-    }
-
-    public function spansOverYearChange(): bool
-    {
-        return $this->getDayOfYearEnd() < $this->getDayOfYearStart();
-    }
-
-    /**
-     * @return string
-     */
-    public function getKey(): string
-    {
-        return $this->key;
-    }
-
-    /**
-     * @return int
-     */
-    public function getDayOfYearStart(): int
-    {
-        return $this->dayOfYearStart;
-    }
-
-    /**
-     * @return int
-     */
-    public function getDayOfYearEnd(): int
-    {
-        return $this->dayOfYearEnd;
+        return $date->year - 1;
     }
 }
