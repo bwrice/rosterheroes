9ace638e0c7bfa0869dc1421744723b0bda18b70
Brian Rice <bwrice83@gmail.com>
2020-08-01 23:02:52 -0400

    update logic for more scenarios when dispatching AlmostReady emails

--- app/Domain/Actions/Emails/DispatchAlmostReadyForWeekEmails.php
+++ app/Domain/Actions/Emails/DispatchAlmostReadyForWeekEmails.php
@@ -5,6 +5,8 @@ namespace App\Domain\Actions\Emails;
 
 
 use App\Domain\Collections\SquadCollection;
+use App\Domain\Models\Campaign;
+use App\Domain\Models\CampaignStop;
 use App\Domain\Models\EmailSubscription;
 use App\Domain\Models\Hero;
 use App\Domain\Models\Squad;
@@ -13,6 +15,7 @@ use App\Facades\CurrentWeek;
 use App\Mail\AlmostReadyForWeek;
 use App\Notifications\BulkEmailsDispatched;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Mail;
 
 class DispatchAlmostReadyForWeekEmails
@@ -32,25 +35,47 @@ class DispatchAlmostReadyForWeekEmails
             });
 
         })->chunk(200, function (SquadCollection $squads)  use ($now, $currentWeek, &$count) {
+
             $squadsToMail = $squads->filter(function (Squad $squad) {
+
                 $heroesWithoutSpirits = $squad->heroes->filter(function (Hero $hero) {
                     return is_null($hero->player_spirit_id);
                 });
-                if ($heroesWithoutSpirits->isNotEmpty()) {
-                    return true;
-                }
+
                 $currentCampaign = $squad->getCurrentCampaign(['campaignStops.sideQuestResults']);
-                if (!$currentCampaign) {
+
+                if (! $currentCampaign) {
+                    /*
+                     * If there is no current campaign, the squad is consider active based on
+                     * if any heroes have spirits attached and because they have no campaign,
+                     * we can send them an almost ready email
+                     */
+                    return $heroesWithoutSpirits->count() !== $squad->heroes->count();
+                }
+
+                if (! $this->campaignIsFull($squad, $currentCampaign->campaignStops)) {
+                    /*
+                     * We know they have a current week's campaign, and if it's not full we can
+                     * send them an "almost ready" email
+                     */
                     return true;
                 }
-                return false;
+
+                /*
+                 * Now we know they have a full campaign, so we only send an email if they have heroes
+                 * missing spirits
+                 */
+                return $heroesWithoutSpirits->isNotEmpty();
             });
+
             $delayCounter = 0;
+
             $squadsToMail->each(function (Squad $squad) use (&$delayCounter, $now, $currentWeek) {
 
                 $secondsDelay = (20 * $delayCounter + (rand(0, 3)) + rand(0, 20));
                 $when = $now->clone()->addSeconds($secondsDelay);
                 Mail::to($squad->user)->later($when, new AlmostReadyForWeek($squad, $currentWeek));
+                $delayCounter++;
             });
 
             $count += $squadsToMail->count();
@@ -58,5 +83,19 @@ class DispatchAlmostReadyForWeekEmails
 
         Admin::notify(new BulkEmailsDispatched('Squad Almost Ready For Week', $count));
         return $count;
+    }
+
+    protected function campaignIsFull(Squad $squad, Collection $campaignStops)
+    {
+        if ($campaignStops->count() < $squad->getQuestsPerWeek()) {
+            return false;
+        }
+
+        $sideQuestPerQuest = $squad->getSideQuestsPerQuest();
+        $unfilledStop = $campaignStops->first(function (CampaignStop $campaignStop) use ($sideQuestPerQuest) {
+            return $campaignStop->sideQuestResults->count() < $sideQuestPerQuest;
+        });
+
+        return is_null($unfilledStop);
     }
 }
