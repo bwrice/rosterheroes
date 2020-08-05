f983338a4a9cb4a7d2cf4edc8af56da324596ce6
Brian Rice <bwrice83@gmail.com>
2020-08-05 16:03:53 -0400

    create HeroPostTypeResource

---
+++ app/Http/Resources/HeroPostTypeResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\HeroPostType;
+use App\Domain\Models\HeroRace;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class HeroPostTypeResource
+ * @package App\Http\Resources
+ *
+ * @mixin HeroPostType
+ */
+class HeroPostTypeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'heroRaceIDs' => $this->heroRaces->map(function (HeroRace $heroRace) {
+                return $heroRace->id;
+            })
+        ];
+    }
+}
