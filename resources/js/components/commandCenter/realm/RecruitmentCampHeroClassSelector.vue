c510582aad2c7e109b4e999dc62c119f70d3bf82
Brian Rice <bwrice83@gmail.com>
2020-08-06 00:35:10 -0400

    create RecruitmentCampHeroClassSelector vue component

---
+++ resources/js/components/commandCenter/realm/RecruitmentCampHeroClassSelector.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-col cols="6">
+        <span class="">{{heroClass.name}}</span>
+        <HeroClassIcon :hero-class="heroClass"></HeroClassIcon>
+    </v-col>
+</template>
+
+<script>
+    import HeroClass from "../../../models/HeroClass";
+    import HeroClassIcon from "../../icons/heroClasses/HeroClassIcon";
+
+    export default {
+        name: "RecruitmentCampHeroClassSelector",
+        components: {HeroClassIcon},
+        props: {
+            heroClass: {
+                type: HeroClass,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
