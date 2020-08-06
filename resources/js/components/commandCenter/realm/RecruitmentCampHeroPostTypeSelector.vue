dacf0fbad1945a6e1038cb5e061b5465c3009ce7
Brian Rice <bwrice83@gmail.com>
2020-08-06 00:35:20 -0400

    create RecruitmentCampHeroPostTypeSelector vue component

---
+++ resources/js/components/commandCenter/realm/RecruitmentCampHeroPostTypeSelector.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-col cols="6">
+        <span class="">{{heroRace.name}}</span>
+        <HeroRaceIcon :hero-race="heroRace"></HeroRaceIcon>
+    </v-col>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import {mapActions} from 'vuex';
+    import HeroPostType from "../../../models/HeroPostType";
+    import HeroRaceIcon from "../../icons/heroRaces/HeroRaceIcon";
+
+    export default {
+        name: "RecruitmentCampHeroPostTypeSelector",
+        components: {HeroRaceIcon},
+        props: {
+            heroPostType: {
+                type: HeroPostType,
+                required: true
+            }
+        },
+        mounted() {
+            console.log("Hero Post Type");
+            console.log(this.heroPostType);
+        },
+        computed: {
+            ...mapGetters([
+                '_heroRaceByID'
+            ]),
+            heroRace() {
+                let heroRaceID = this.heroPostType.heroRaceIDs[0];
+                return this._heroRaceByID(heroRaceID);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
