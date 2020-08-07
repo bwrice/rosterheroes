7efd52479152d7d4946413bb7ab1701aaac887c4
Brian Rice <bwrice83@gmail.com>
2020-08-06 20:30:35 -0400

    set hero-class in Hero Class selector

--- resources/js/components/commandCenter/realm/RecruitmentCampHeroClassSelector.vue
+++ resources/js/components/commandCenter/realm/RecruitmentCampHeroClassSelector.vue
@@ -1,11 +1,19 @@
 <template>
-    <v-col cols="6">
-        <span class="">{{heroClass.name}}</span>
-        <HeroClassIcon :hero-class="heroClass"></HeroClassIcon>
-    </v-col>
+    <v-sheet :color="sheetColor" :elevation="elevation" class="rounded rh-clickable" @click.native="handleClick">
+        <v-row no-gutters class="pa-1">
+            <v-col cols="12">
+                <HeroClassIcon :hero-class="heroClass"></HeroClassIcon>
+            </v-col>
+        </v-row>
+        <v-row no-gutters justify="center">
+            <span class="text-body-1 text-md-h6 font-weight-light text-primary">{{heroClass.name.toUpperCase()}}</span>
+        </v-row>
+    </v-sheet>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
+    import {mapActions} from 'vuex';
     import HeroClass from "../../../models/HeroClass";
     import HeroClassIcon from "../../icons/heroClasses/HeroClassIcon";
 
@@ -16,6 +24,31 @@
             heroClass: {
                 type: HeroClass,
                 required: true
+            }
+        },
+        methods: {
+            ...mapActions([
+                'setRecruitmentHeroClass'
+            ]),
+            handleClick() {
+                this.setRecruitmentHeroClass(this.heroClass);
+            }
+        },
+        computed: {
+            ...mapGetters([
+                '_recruitmentHeroClass'
+            ]),
+            isSelected() {
+                if (!this._recruitmentHeroClass) {
+                    return false;
+                }
+                return this._recruitmentHeroClass.id === this.heroClass.id;
+            },
+            elevation() {
+                return this.isSelected ? 0 : 6;
+            },
+            sheetColor() {
+                return this.isSelected ? '#447075' : '#445866';
             }
         }
     }
