9fd479cf3ac74bc2e87a0dcc9933664d7bb190c5
Brian Rice <bwrice83@gmail.com>
2020-08-06 20:31:35 -0400

    add setters and getters to recruitment-camp module

--- resources/js/store/commandCenter/recruitmentCampModule.js
+++ resources/js/store/commandCenter/recruitmentCampModule.js
@@ -5,16 +5,37 @@ export default {
 
     state: {
         recruitmentCamp: new RecruitmentCamp({}),
+        recruitmentHeroRace: null,
+        recruitmentHeroPostType: null,
+        recruitmentHeroClass: null,
     },
 
     getters: {
         _recruitmentCamp(state) {
             return state.recruitmentCamp;
+        },
+        _recruitmentHeroRace(state) {
+            return state.recruitmentHeroRace;
+        },
+        _recruitmentHeroPostType(state) {
+            return state.recruitmentHeroPostType;
+        },
+        _recruitmentHeroClass(state) {
+            return state.recruitmentHeroClass;
         }
     },
     mutations: {
         SET_RECRUITMENT_CAMP(state, recruitmentCamp) {
             state.recruitmentCamp = recruitmentCamp;
+        },
+        SET_RECRUITMENT_HERO_RACE(state, heroRace) {
+            state.recruitmentHeroRace = heroRace;
+        },
+        SET_RECRUITMENT_HERO_CLASS(state, heroClass) {
+            state.recruitmentHeroClass = heroClass;
+        },
+        SET_RECRUITMENT_HERO_POST_TYPE(state, heroPostType) {
+            state.recruitmentHeroPostType = heroPostType;
         },
     },
     actions: {
@@ -27,5 +48,14 @@ export default {
                 console.warn("Failed to update shop");
             }
         },
+        setRecruitmentHeroRace({commit}, heroRace) {
+            commit('SET_RECRUITMENT_HERO_RACE', heroRace);
+        },
+        setRecruitmentHeroClass({commit}, heroClass) {
+            commit('SET_RECRUITMENT_HERO_CLASS', heroClass);
+        },
+        setRecruitmentHeroPostType({commit}, heroPostType) {
+            commit('SET_RECRUITMENT_HERO_POST_TYPE', heroPostType);
+        }
     }
 };
