396eee72788a001dfc44b6c8f750b1642b7326f6
Brian Rice <bwrice83@gmail.com>
2020-08-08 01:58:58 -0400

    add recruitmentBonusSpiritEssence to HeroPostType JS model

--- resources/js/models/HeroPostType.js
+++ resources/js/models/HeroPostType.js
@@ -1,9 +1,10 @@
 export default class HeroPostType {
 
-    constructor({id, name = '', recruitmentCost = 0, heroRaceIDs = []}) {
+    constructor({id, name = '', recruitmentCost = 0, recruitmentBonusSpiritEssence = 0, heroRaceIDs = []}) {
         this.id = id;
         this.name = name;
         this.recruitmentCost = recruitmentCost;
+        this.recruitmentBonusSpiritEssence = recruitmentBonusSpiritEssence;
         this.heroRaceIDs = heroRaceIDs;
     }
 }
