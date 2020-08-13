27d55c4cdfa0b23a2de683c226d1df020b72f39b
Brian Rice <bwrice83@gmail.com>
2020-08-12 21:41:36 -0400

    fix UpdateContent always using attacks path

--- app/Admin/Content/Actions/UpdateContent.php
+++ app/Admin/Content/Actions/UpdateContent.php
@@ -4,9 +4,7 @@
 namespace App\Admin\Content\Actions;
 
 
-use App\Admin\Content\Sources\AttackSource;
 use App\Admin\Content\Sources\ContentSource;
-use App\Facades\Content;
 use Illuminate\Support\Collection;
 
 class UpdateContent
@@ -24,7 +22,7 @@ class UpdateContent
         $updatedContent = $content->replace([$matchingKey => $updatedSource]);
         $lastUpdated = now()->timestamp;
 
-        file_put_contents(Content::attacksPath(), json_encode([
+        file_put_contents($contentPath, json_encode([
             'last_updated' => $lastUpdated,
             'data' => $updatedContent->toArray()
         ], JSON_PRETTY_PRINT));
