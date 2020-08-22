cf09a952d6740f4dd5ec5d37d06563d448e15e28
Brian Rice <bwrice83@gmail.com>
2020-08-21 23:43:03 -0400

    create questsToJoin() method for NPCService

--- app/Services/NPCService.php
+++ app/Services/NPCService.php
@@ -4,9 +4,13 @@
 namespace App\Services;
 
 
+use App\Domain\Models\Continent;
 use App\Domain\Models\Hero;
+use App\Domain\Models\Quest;
+use App\Domain\Models\SideQuest;
 use App\Domain\Models\Squad;
 use App\Domain\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Str;
 
 class NPCService
@@ -70,5 +74,57 @@ class NPCService
         }
 
         return $name;
+    }
+
+    public function questsToJoin(Squad $npc)
+    {
+        $npcLevel = $npc->level();
+
+        // Get quests located in province npc can visit
+        $validContinentIDs = Continent::query()->get()->filter(function (Continent $continent) use ($npcLevel) {
+            return $continent->getBehavior()->getMinLevelRequirement() <= $npcLevel;
+        })->pluck('id')->toArray();
+
+        // If there are over 50 quests, we can safely grab 50 randomly and find some ideal quests to join
+        $quests = Quest::query()->with('sideQuests.minions')->whereHas('province', function (Builder $builder) use ($validContinentIDs) {
+            $builder->whereIn('continent_id', $validContinentIDs);
+        })->inRandomOrder()->limit(50)->get();
+
+        $idealDifficulty = ($npcLevel * 2) + 5;
+
+        // Sort quests by average difficulty of side-quest relative to ideal difficulty for npc
+        $orderedQuests = $quests->sortBy(function (Quest $quest) use ($npcLevel, $idealDifficulty) {
+            $sideQuestDifficultyAvg = $quest->sideQuests->average(function (SideQuest $sideQuest) {
+                return $sideQuest->difficulty();
+            });
+            return abs($idealDifficulty - $sideQuestDifficultyAvg);
+        });
+
+        $questsCount = $orderedQuests->count();
+        $questsPerWeek = $npc->getQuestsPerWeek();
+
+        // Get a rand int between quests-per-week and total quests available
+        $amountOfQuestsToTake = $questsCount <= $questsPerWeek ? $questsCount : rand($questsPerWeek, $questsCount);
+
+        $questsToJoin = $orderedQuests->take($amountOfQuestsToTake)->random($questsPerWeek);
+
+        $sideQuestsPerQuest = $npc->getSideQuestsPerQuest();
+        return $questsToJoin->map(function (Quest $quest) use ($sideQuestsPerQuest, $idealDifficulty) {
+
+            $sideQuests = $quest->sideQuests->sortBy(function (SideQuest $sideQuest) use ($idealDifficulty) {
+                return abs($idealDifficulty - $sideQuest->difficulty());
+            });
+
+            $sideQuestsCount = $sideQuests->count();
+
+            // Get a rand int between side-quests-per-quest and total side-quests available
+            $amountOfQuestsToTake = $sideQuestsCount <= $sideQuestsPerQuest ? $sideQuestsCount : rand($sideQuestsPerQuest, $sideQuestsCount);
+            $sideQuestsToJoin = $sideQuests->take($amountOfQuestsToTake)->random($sideQuestsPerQuest);
+
+            return [
+                'quest' => $quest,
+                'side_quests' =>  $sideQuestsToJoin->values()->all()
+            ];
+        });
     }
 }
