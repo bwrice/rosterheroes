5c16bfeb045e8486040e702317a37f0c32c0daae
Brian Rice <bwrice83@gmail.com>
2020-08-23 01:30:33 -0400

    add local key to npc config

--- config/npc.php
+++ config/npc.php
@@ -179,6 +179,7 @@ if (! function_exists('getDefaultNPCConfig')) {
 
 return [
     'development' => getDefaultNPCConfig(),
+    'local' => getDefaultNPCConfig(),
     'beta' => getDefaultNPCConfig(),
     'production' => [
         'squads' => [
