2bc238a1bc586e1503f1332d46f9cd93fe7f0604
Brian Rice <bwrice83@gmail.com>
2020-09-01 23:35:15 -0400

    adjust damage calculations for materials

--- app/Domain/Models/Material.php
+++ app/Domain/Models/Material.php
@@ -31,12 +31,12 @@ class Material extends Model
 
     public function getBaseDamageModifierBonus()
     {
-        return ($this->grade**1.15)/50;
+        return ($this->grade**1.08)/250;
     }
 
     public function getDamageMultiplierModifierBonus()
     {
-        return ($this->grade**1.1)/200;
+        return ($this->grade**1.08)/250;
     }
 
     public function getWeightModifier()
