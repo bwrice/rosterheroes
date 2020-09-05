ba65b8564fa6ec481470c3ffb55b4c48d63c1eaf
Brian Rice <bwrice83@gmail.com>
2020-09-05 18:15:45 -0400

    create SquadSnapshot model

---
+++ app/Domain/Models/SquadSnapshot.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Domain\Models;
+
+use App\Domain\Models\Squad;
+use App\Domain\Models\SquadRank;
+use App\Domain\Models\Week;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class SquadSnapshot
+ * @package App
+ *
+ * @property int $week_id
+ * @property int $squad_id
+ * @property int $experience
+ * @property int $squad_rank_id
+ *
+ * @property Week $week
+ * @property Squad $squad
+ * @property SquadRank $squadRank
+ */
+class SquadSnapshot extends Model
+{
+    public function week()
+    {
+        return $this->belongsTo(Week::class);
+    }
+
+    public function squad()
+    {
+        return $this->belongsTo(Squad::class);
+    }
+
+    public function squadRank()
+    {
+        return $this->belongsTo(SquadRank::class);
+    }
+}
