3354214dcb67821ac809040f0bc20fd23da42f38
Brian Rice <bwrice83@gmail.com>
2020-09-05 22:13:31 -0400

    throw exception if building snapshot before week finalizing

--- app/Domain/Actions/BuildSquadSnapshot.php
+++ app/Domain/Actions/BuildSquadSnapshot.php
@@ -7,13 +7,25 @@ namespace App\Domain\Actions;
 use App\Domain\Models\Squad;
 use App\Domain\Models\SquadSnapshot;
 use App\Domain\Models\Week;
+use App\Facades\WeekService;
+use Illuminate\Support\Str;
 
 class BuildSquadSnapshot
 {
     public const EXCEPTION_CODE_SNAPSHOT_EXISTS = 1;
+    public const EXCEPTION_CODE_WEEK_NOT_FINALIZING = 2;
 
+    /**
+     * @param Squad $squad
+     * @param Week $week
+     * @throws \Exception
+     */
     public function execute(Squad $squad, Week $week)
     {
+        if (now()->isBefore(WeekService::finalizingStartsAt($week->adventuring_locks_at))) {
+            throw new \Exception("Too early to create Squad Snapshot", self::EXCEPTION_CODE_WEEK_NOT_FINALIZING);
+        }
+
         $existingSnapshot = SquadSnapshot::query()
             ->where('squad_id', '=', $squad->id)
             ->where('week_id', '=', $week->id)
--- tests/Feature/BuildSquadSnapshotTest.php
+++ tests/Feature/BuildSquadSnapshotTest.php
@@ -4,14 +4,19 @@ namespace Tests\Feature;
 
 use App\Domain\Actions\BuildSquadSnapshot;
 use App\Domain\Models\Week;
+use App\Facades\WeekService;
 use App\Factories\Models\SquadFactory;
 use App\Factories\Models\SquadSnapshotFactory;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Date;
 use Tests\TestCase;
 
 class BuildSquadSnapshotTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * @return BuildSquadSnapshot
      */
@@ -34,6 +39,27 @@ class BuildSquadSnapshotTest extends TestCase
             $this->getDomainAction()->execute($squad, $week);
         } catch (\Exception $exception) {
             $this->assertEquals(BuildSquadSnapshot::EXCEPTION_CODE_SNAPSHOT_EXISTS, $exception->getCode());
+            return;
+        }
+        $this->fail("Exception not thrown");
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_throw_an_exception_if_the_week_is_not_past_finalizing()
+    {
+        /** @var Week $week */
+        $week = factory(Week::class)->create();
+        $finalizingStartsAt = WeekService::finalizingStartsAt($week->adventuring_locks_at);
+        Date::setTestNow($finalizingStartsAt->subHour());
+
+        $squad = SquadFactory::new()->create();
+
+        try {
+            $this->getDomainAction()->execute($squad, $week);
+        } catch (\Exception $exception) {
+            $this->assertEquals(BuildSquadSnapshot::EXCEPTION_CODE_WEEK_NOT_FINALIZING, $exception->getCode());
             return;
         }
         $this->fail("Exception not thrown");
