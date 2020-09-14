865c465013da51df85c062d296fdcb06b0d93ada
Brian Rice <bwrice83@gmail.com>
2020-09-14 15:15:04 -0400

    test BuildAttackSnapshot using ItemSnapshot

--- tests/Feature/BuildAttackSnapshotTest.php
+++ tests/Feature/BuildAttackSnapshotTest.php
@@ -4,8 +4,10 @@ namespace Tests\Feature;
 
 use App\Domain\Actions\BuildAttackSnapshot;
 use App\Domain\Actions\Combat\CalculateCombatDamage;
+use App\Domain\Models\ItemSnapshot;
 use App\Factories\Models\AttackFactory;
 use App\Factories\Models\HeroSnapshotFactory;
+use App\Factories\Models\ItemSnapshotFactory;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -14,7 +16,7 @@ use Tests\TestCase;
 class BuildAttackSnapshotTest extends TestCase
 {
     use DatabaseTransactions;
-    
+
     /**
      * @return BuildAttackSnapshot
      */
@@ -29,10 +31,10 @@ class BuildAttackSnapshotTest extends TestCase
     public function it_will_build_an_attack_snapshot_for_an_attack_and_hero_snapshot()
     {
         $attack = AttackFactory::new()->create();
-        $heroSnapshot = HeroSnapshotFactory::new()->create();
+        $itemSnapshot = ItemSnapshotFactory::new()->create();
         $fantasyPower = round(rand(100, 5000)/100, 2);
 
-        $attackSnapshot = $this->getDomainAction()->execute($attack, $heroSnapshot, $fantasyPower);
+        $attackSnapshot = $this->getDomainAction()->execute($attack, $itemSnapshot, $fantasyPower);
 
         $this->assertTrue(abs($attack->getCombatSpeed() - $attackSnapshot->combat_speed) < 0.01);
         $this->assertEquals($attack->name, $attackSnapshot->name);
@@ -47,17 +49,32 @@ class BuildAttackSnapshotTest extends TestCase
     /**
      * @test
      */
+    public function the_attack_snapshot_will_belong_to_the_item_snapshot()
+    {
+        $attack = AttackFactory::new()->create();
+        $itemSnapshot = ItemSnapshotFactory::new()->create();
+        $fantasyPower = round(rand(100, 5000)/100, 2);
+
+        $attackSnapshot = $this->getDomainAction()->execute($attack, $itemSnapshot, $fantasyPower);
+
+        $this->assertEquals($attackSnapshot->attacker_id, $itemSnapshot->id);
+        $this->assertEquals($attackSnapshot->attacker_type, ItemSnapshot::RELATION_MORPH_MAP_KEY);
+    }
+
+    /**
+     * @test
+     */
     public function it_will_build_a_snapshot_attack_with_the_expected_damage()
     {
         $attack = AttackFactory::new()->create();
-        $heroSnapshot = HeroSnapshotFactory::new()->create();
+        $itemSnapshot = ItemSnapshotFactory::new()->create();
         $fantasyPower = round(rand(100, 5000)/100, 2);
 
         /** @var CalculateCombatDamage $calculateDamage */
         $calculateDamage = app(CalculateCombatDamage::class);
         $damage = $calculateDamage->execute($attack, $fantasyPower);
 
-        $attackSnapshot = $this->getDomainAction()->execute($attack, $heroSnapshot, $fantasyPower);
+        $attackSnapshot = $this->getDomainAction()->execute($attack, $itemSnapshot, $fantasyPower);
 
         $this->assertEquals($damage, $attackSnapshot->damage);
     }
