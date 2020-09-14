6fef1f8bb8508ca795cb9764de4a589b8c8351aa
Brian Rice <bwrice83@gmail.com>
2020-09-14 19:21:13 -0400

    create MinionSnapshot model

---
+++ app/Domain/Models/MinionSnapshot.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class MinionSnapshot
+ * @package App
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property int $week_id
+ * @property int $minion_id
+ * @property int $level
+ * @property int $combat_position_id
+ * @property int $enemy_type_id
+ * @property int $health
+ * @property int $protection
+ * @property float $block_chance
+ * @property float $fantasy_power
+ * @property int $experience_reward
+ * @property int $favor_reward
+ *
+ * @property Week $week
+ * @property Minion $minion
+ * @property CombatPosition $combatPosition
+ * @property EnemyType $enemyType
+ */
+class MinionSnapshot extends Model
+{
+    protected $guarded = [];
+
+    public function week()
+    {
+        return $this->belongsTo(Week::class);
+    }
+
+    public function minion()
+    {
+        return $this->belongsTo(Minion::class);
+    }
+
+    public function combatPosition()
+    {
+        return $this->belongsTo(CombatPosition::class);
+    }
+
+    public function enemyType()
+    {
+        return $this->belongsTo(EnemyType::class);
+    }
+}
