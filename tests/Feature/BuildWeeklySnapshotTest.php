bc70a52c42fbde5ced70cb3f25b048f701dbd18f
Brian Rice <bwrice83@gmail.com>
2020-09-15 18:44:23 -0400

    create abstract BuildWeeklySnapshotTest test-case

---
+++ tests/Feature/BuildWeeklySnapshotTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Domain\Actions\Snapshots\BuildWeeklySnapshot;
+use App\Domain\Models\Week;
+use App\Facades\WeekService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Date;
+use Tests\TestCase;
+
+abstract class BuildWeeklySnapshotTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function building_a_weekly_snapshot_will_throw_an_exception_if_the_week_is_not_finalizing()
+    {
+        /** @var Week $currentWeek */
+        $currentWeek = factory(Week::class)->states('as-current')->create();
+        Date::setTestNow(WeekService::finalizingStartsAt($currentWeek->adventuring_locks_at)->subHour());
+
+        try {
+            $this->getDomainAction()->execute();
+        } catch (\Exception $exception) {
+            $this->assertEquals(BuildWeeklySnapshot::EXCEPTION_CODE_WEEK_NOT_FINALIZING, $exception->getCode());
+            return;
+        }
+        $this->fail("Exception not thrown");
+    }
+
+    /**
+     * @return mixed
+     */
+    abstract protected function getDomainAction();
+}
