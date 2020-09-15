7e64324c14123db0d315b22d3931250403d74169
Brian Rice <bwrice83@gmail.com>
2020-09-15 18:45:57 -0400

    extend BuildWeeklySnapshotTest in BuildSquadSnapshotTest and remove dupe tests

--- tests/Feature/BuildSquadSnapshotTest.php
+++ tests/Feature/BuildSquadSnapshotTest.php
@@ -5,22 +5,17 @@ namespace Tests\Feature;
 use App\Domain\Actions\Snapshots\BuildHeroSnapshot;
 use App\Domain\Actions\Snapshots\BuildSquadSnapshot;
 use App\Domain\Models\Hero;
-use App\Domain\Models\Squad;
 use App\Domain\Models\SquadSnapshot;
 use App\Domain\Models\Week;
-use App\Facades\HeroService;
 use App\Facades\WeekService;
 use App\Factories\Models\HeroFactory;
 use App\Factories\Models\SquadFactory;
-use App\Factories\Models\SquadSnapshotFactory;
-use App\Helpers\EloquentMatcher;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Date;
-use Tests\TestCase;
 
-class BuildSquadSnapshotTest extends TestCase
+class BuildSquadSnapshotTest extends BuildWeeklySnapshotTest
 {
     use DatabaseTransactions;
 
@@ -30,48 +25,6 @@ class BuildSquadSnapshotTest extends TestCase
     protected function getDomainAction()
     {
         return app(BuildSquadSnapshot::class);
-    }
-
-    /**
-     * @test
-     */
-    public function it_will_throw_an_exception_if_there_is_an_existing_snapshot_for_the_squad_and_week()
-    {
-        /** @var Week $week */
-        $week = factory(Week::class)->states('as-current')->create();
-        $finalizingStartsAt = WeekService::finalizingStartsAt($week->adventuring_locks_at);
-        Date::setTestNow($finalizingStartsAt->addHour());
-        $squad = SquadFactory::new()->create();
-        $existingSnapshot = SquadSnapshotFactory::new()->withSquadID($squad->id)->withWeekID($week->id)->create();
-
-        try {
-            $this->getDomainAction()->execute($squad);
-        } catch (\Exception $exception) {
-            $this->assertEquals(BuildSquadSnapshot::EXCEPTION_CODE_SNAPSHOT_EXISTS, $exception->getCode());
-            return;
-        }
-        $this->fail("Exception not thrown");
-    }
-
-    /**
-     * @test
-     */
-    public function it_will_throw_an_exception_if_the_week_is_not_past_finalizing()
-    {
-        /** @var Week $week */
-        $week = factory(Week::class)->states('as-current')->create();
-        $finalizingStartsAt = WeekService::finalizingStartsAt($week->adventuring_locks_at);
-        Date::setTestNow($finalizingStartsAt->subHour());
-
-        $squad = SquadFactory::new()->create();
-
-        try {
-            $this->getDomainAction()->execute($squad);
-        } catch (\Exception $exception) {
-            $this->assertEquals(BuildSquadSnapshot::EXCEPTION_CODE_WEEK_NOT_FINALIZING, $exception->getCode());
-            return;
-        }
-        $this->fail("Exception not thrown");
     }
 
     /**
