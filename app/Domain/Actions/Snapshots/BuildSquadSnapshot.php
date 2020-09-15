f3e4c70c94f4dc2f53a24723d5b80b042a4a6ed2
Brian Rice <bwrice83@gmail.com>
2020-09-15 18:44:43 -0400

    extend BuildWeeklySnapshot in BuildSquadSnapshot

--- app/Domain/Actions/Snapshots/BuildSquadSnapshot.php
+++ app/Domain/Actions/Snapshots/BuildSquadSnapshot.php
@@ -7,15 +7,11 @@ namespace App\Domain\Actions\Snapshots;
 use App\Domain\Models\Hero;
 use App\Domain\Models\Squad;
 use App\Domain\Models\SquadSnapshot;
-use App\Domain\Models\Week;
 use App\Facades\CurrentWeek;
 use Illuminate\Support\Str;
 
-class BuildSquadSnapshot
+class BuildSquadSnapshot extends BuildWeeklySnapshot
 {
-    public const EXCEPTION_CODE_SNAPSHOT_EXISTS = 1;
-    public const EXCEPTION_CODE_WEEK_NOT_FINALIZING = 2;
-
     /**
      * @var BuildHeroSnapshot
      */
@@ -28,25 +24,11 @@ class BuildSquadSnapshot
 
     /**
      * @param Squad $squad
-     * @param Week $week
      * @return SquadSnapshot
      * @throws \Exception
      */
-    public function execute(Squad $squad): SquadSnapshot
+    public function handle(Squad $squad): SquadSnapshot
     {
-        if (! CurrentWeek::finalizing()) {
-            throw new \Exception("Cannot create squad snapshot when week is not finalizing", self::EXCEPTION_CODE_WEEK_NOT_FINALIZING);
-        }
-
-        $existingSnapshot = SquadSnapshot::query()
-            ->where('squad_id', '=', $squad->id)
-            ->where('week_id', '=', CurrentWeek::id())
-            ->first();
-
-        if ($existingSnapshot) {
-            throw new \Exception("Snapshot for squad and week already exists", self::EXCEPTION_CODE_SNAPSHOT_EXISTS);
-        }
-
         /** @var SquadSnapshot $squadSnapshot */
         $squadSnapshot = SquadSnapshot::query()->create([
             'uuid' => Str::uuid(),
