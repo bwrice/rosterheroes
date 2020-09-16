e96c1641ff435954c2e05fa179c168fb53082fad
Brian Rice <bwrice83@gmail.com>
2020-09-16 16:37:30 -0400

    create minion-snapshot/side-quest-snapshot pivot migration

---
+++ database/migrations/2020_09_16_201802_create_minion_snapshots_to_side_quest_snapshots_pivot.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMinionSnapshotsToSideQuestSnapshotsPivot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('m_snapshot_sq_snapshot', function (Blueprint $table) {
+            $table->bigInteger('minion_snapshot_id')->unsigned();
+            $table->bigInteger('side_quest_snapshot_id')->unsigned();
+            $table->primary(['minion_snapshot_id', 'side_quest_snapshot_id'], 'm_snapshot_sq_snapshot_primary');
+            $table->timestamps();
+        });
+
+        Schema::table('m_snapshot_sq_snapshot', function (Blueprint $table) {
+            $table->foreign('minion_snapshot_id')->references('id')->on('minion_snapshots');
+            $table->foreign('side_quest_snapshot_id')->references('id')->on('side_quest_snapshots');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('minion_snapshot_side_quest_snapshot');
+    }
+}
