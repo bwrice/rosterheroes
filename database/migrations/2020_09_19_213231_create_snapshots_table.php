d3ab5c5bd7c36120622ece78777fa63a5fef2ee3
Brian Rice <bwrice83@gmail.com>
2020-09-19 21:32:49 -0400

    add migration for new table in laravel-event-sourcing update

---
+++ database/migrations/2020_09_19_213231_create_snapshots_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSnapshotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('snapshots', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('aggregate_uuid');
+            $table->unsignedInteger('aggregate_version');
+            $table->json('state');
+
+            $table->timestamps();
+
+            $table->index('aggregate_uuid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('snapshots');
+    }
+}
