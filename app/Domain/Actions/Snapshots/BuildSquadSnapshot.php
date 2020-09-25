98f8be171783aacdf017a9b8d2e8a99707adeb47
Brian Rice <bwrice83@gmail.com>
2020-09-24 20:01:59 -0400

    throw exception when building SquadSnapshot that already exists

--- app/Domain/Actions/Snapshots/BuildSquadSnapshot.php
+++ app/Domain/Actions/Snapshots/BuildSquadSnapshot.php
@@ -10,12 +10,11 @@ use App\Domain\Models\SquadSnapshot;
 use App\Facades\CurrentWeek;
 use Illuminate\Support\Str;
 
-class BuildSquadSnapshot extends BuildWeeklySnapshot
+class BuildSquadSnapshot extends BuildSnapshot
 {
-    /**
-     * @var BuildHeroSnapshot
-     */
-    protected $buildHeroSnapshot;
+    public const EXCEPTION_CODE_SNAPSHOT_EXISTS = 7;
+
+    protected BuildHeroSnapshot $buildHeroSnapshot;
 
     public function __construct(BuildHeroSnapshot $buildHeroSnapshot)
     {
@@ -24,11 +23,25 @@ class BuildSquadSnapshot extends BuildWeeklySnapshot
 
     /**
      * @param Squad $squad
+     * @param $weekly
      * @return SquadSnapshot
      * @throws \Exception
      */
     public function handle(Squad $squad): SquadSnapshot
     {
+        $weekID = $this->weekly ? CurrentWeek::id() : null;
+
+        if ($weekID) {
+            $existing = SquadSnapshot::query()
+                ->where('week_id', '=', $weekID)
+                ->where('squad_id', '=', $squad->id)
+                ->first();
+
+            if ($existing) {
+                throw new \Exception("Cannot build weekly squad snapshot because snapshot already exists", self::EXCEPTION_CODE_SNAPSHOT_EXISTS);
+            }
+        }
+
         /** @var SquadSnapshot $squadSnapshot */
         $squadSnapshot = SquadSnapshot::query()->create([
             'uuid' => Str::uuid(),
