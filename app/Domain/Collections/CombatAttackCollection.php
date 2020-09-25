e3cfdcd489358663d7862ab1ecc2729e88338624
Brian Rice <bwrice83@gmail.com>
2020-09-25 16:32:54 -0400

    rename collection and update class references to match CombatAttack changes

--- app/Domain/Collections/CombatAttackCollection.php
+++ app/Domain/Collections/CombatAttackCollection.php
@@ -4,28 +4,28 @@
 namespace App\Domain\Collections;
 
 
-use App\Domain\Combat\Attacks\AbstractCombatAttack;
+use App\Domain\Combat\Attacks\CombatAttack;
 use Illuminate\Support\Collection;
 
-class AbstractCombatAttackCollection extends Collection
+class CombatAttackCollection extends Collection
 {
     public function withinAttackerProximity(int $proximity)
     {
-        return $this->filter(function (AbstractCombatAttack $combatAttack) use ($proximity) {
+        return $this->filter(function (CombatAttack $combatAttack) use ($proximity) {
             return $combatAttack->getAttackerPosition()->isWithinProximity($proximity);
         });
     }
 
     public function filterByAttackerPositions(Collection $combatPositions)
     {
-        return $this->filter(function (AbstractCombatAttack $combatAttack) use ($combatPositions) {
+        return $this->filter(function (CombatAttack $combatAttack) use ($combatPositions) {
             return in_array($combatAttack->getAttackerPosition()->id, $combatPositions->pluck('id')->toArray());
         });
     }
 
     public function ready()
     {
-        return $this->filter(function (AbstractCombatAttack $combatAttack) {
+        return $this->filter(function (CombatAttack $combatAttack) {
             // Use 10,000 and multiply speed by 100 so precision up to 2 decimals on combat speed matters
             $rand = rand(1, 10000);
             return ($combatAttack->getCombatSpeed() * 100) >= $rand;
