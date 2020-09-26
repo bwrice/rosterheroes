3f766568fb15c7299bd69f642fc3411af76a1518
Brian Rice <bwrice83@gmail.com>
2020-09-26 08:28:51 -0400

    build out CombatPosition facade

--- app/Facades/CombatPositionFacade.php
+++ app/Facades/CombatPositionFacade.php
@@ -4,7 +4,24 @@
 namespace App\Facades;
 
 
-class CombatPositionFacade
-{
+use App\Domain\Behaviors\TargetRanges\CombatPositionBehavior;
+use App\Domain\Models\CombatPosition;
+use App\Services\Models\Reference\CombatPositionService;
+use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class CombatPositionFacade
+ * @package App\Facades
+ *
+ * @method static CombatPositionBehavior getBehavior(int|string $identifier)
+ * @method static CombatPosition closestProximity(array $combatPositionIDs)
+ *
+ * @see CombatPositionService
+ */
+class CombatPositionFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return CombatPositionService::class;
+    }
 }
