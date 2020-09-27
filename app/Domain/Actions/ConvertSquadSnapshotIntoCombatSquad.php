290ffdf5c60c72bf51093b506106ef9390c0cbad
Brian Rice <bwrice83@gmail.com>
2020-09-27 14:05:06 -0400

    convert hero-snapshots to combat heroes when converting squad-snapshot into combat squad

--- app/Domain/Actions/ConvertSquadSnapshotIntoCombatSquad.php
+++ app/Domain/Actions/ConvertSquadSnapshotIntoCombatSquad.php
@@ -6,21 +6,34 @@ namespace App\Domain\Actions;
 
 use App\Domain\Collections\AbstractCombatantCollection;
 use App\Domain\Combat\CombatGroups\CombatSquad;
+use App\Domain\Models\HeroSnapshot;
 use App\Domain\Models\SquadSnapshot;
 
 class ConvertSquadSnapshotIntoCombatSquad
 {
+    protected ConvertHeroSnapshotToCombatHero $convertHeroSnapshotToCombatHero;
+
+    public function __construct(ConvertHeroSnapshotToCombatHero $convertHeroSnapshotToCombatHero)
+    {
+        $this->convertHeroSnapshotToCombatHero = $convertHeroSnapshotToCombatHero;
+    }
+
     /**
      * @param SquadSnapshot $squadSnapshot
      * @return CombatSquad
      */
     public function execute(SquadSnapshot $squadSnapshot)
     {
+        $combatHeroes = new AbstractCombatantCollection();
+        $squadSnapshot->heroSnapshots->each(function (HeroSnapshot $heroSnapshot) use ($combatHeroes) {
+            $combatHeroes->push($this->convertHeroSnapshotToCombatHero->execute($heroSnapshot));
+        });
+
         return new CombatSquad(
             $squadSnapshot->uuid,
             $squadSnapshot->experience,
             $squadSnapshot->squad_rank_id,
-            new AbstractCombatantCollection()
+            $combatHeroes
         );
     }
 }
--- tests/Feature/ConvertSquadSnapshotIntoCombatSquadTest.php
+++ tests/Feature/ConvertSquadSnapshotIntoCombatSquadTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Domain\Actions\ConvertHeroSnapshotToCombatHero;
 use App\Domain\Actions\ConvertSquadSnapshotIntoCombatSquad;
+use App\Domain\Models\HeroSnapshot;
+use App\Factories\Models\HeroSnapshotFactory;
 use App\Factories\Models\SquadSnapshotFactory;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -32,5 +35,46 @@ class ConvertSquadSnapshotIntoCombatSquadTest extends TestCase
         $this->assertEquals($squadSnapshot->uuid, $combatSquad->getSourceUuid());
         $this->assertEquals($squadSnapshot->experience, $combatSquad->getExperience());
         $this->assertEquals($squadSnapshot->squad_rank_id, $combatSquad->getSquadRankID());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_execute_convert_combat_hero_on_each_hero_snapshot_belonging_to_the_squad_snapshot()
+    {
+        $squadSnapshot = SquadSnapshotFactory::new()->create();
+
+        $heroSnapshots = collect();
+        $heroSnapshotFactory = HeroSnapshotFactory::new()->forSquadSnapshot($squadSnapshot);
+        for ($i = 1; $i <= rand(2, 4); $i++) {
+            $heroSnapshots->push($heroSnapshotFactory->create());
+        }
+
+        $this->assertGreaterThan(1, $heroSnapshots->count());
+
+        $heroSnapshotUuids = $heroSnapshots->map(function (HeroSnapshot $heroSnapshot) {
+            return $heroSnapshot->uuid;
+        });
+
+        $convertCombatHeroMock = $this->getMockBuilder(ConvertHeroSnapshotToCombatHero::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $convertCombatHeroMock->expects($this->exactly($heroSnapshots->count()))
+            ->method('execute')
+            ->with($this->callback(function (HeroSnapshot $heroSnapshot) use ($heroSnapshotUuids) {
+                $matchingKey = $heroSnapshotUuids->search($heroSnapshot->uuid);
+                if ($matchingKey === false) {
+                    return false;
+                }
+                $heroSnapshotUuids->forget($matchingKey);
+                return true;
+            }))
+            ->willReturn('anything');
+
+        $this->instance(ConvertHeroSnapshotToCombatHero::class, $convertCombatHeroMock);
+
+        $combatSquad = $this->getDomainAction()->execute($squadSnapshot);
+        $this->assertEquals($heroSnapshots->count(), $combatSquad->getCombatHeroes()->count());
     }
 }
