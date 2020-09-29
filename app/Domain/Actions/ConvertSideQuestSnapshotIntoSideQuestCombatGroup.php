c679edf7c64b94dd6c4bfc391553412cfbb421ec
Brian Rice <bwrice83@gmail.com>
2020-09-29 19:19:24 -0400

    execute ConvertMinionSnapshotIntoCombatMinion for each minion-snapshot in ConvertSideQuestSnapshotIntoSideQuestCombatGroup

--- app/Domain/Actions/ConvertSideQuestSnapshotIntoSideQuestCombatGroup.php
+++ app/Domain/Actions/ConvertSideQuestSnapshotIntoSideQuestCombatGroup.php
@@ -6,19 +6,33 @@ namespace App\Domain\Actions;
 
 use App\Domain\Collections\AbstractCombatantCollection;
 use App\Domain\Combat\CombatGroups\SideQuestCombatGroup;
+use App\Domain\Models\MinionSnapshot;
 use App\Domain\Models\SideQuestSnapshot;
 
 class ConvertSideQuestSnapshotIntoSideQuestCombatGroup
 {
+    protected ConvertMinionSnapshotIntoCombatMinion $convertMinionSnapshotIntoCombatMinion;
+
+    public function __construct(ConvertMinionSnapshotIntoCombatMinion $convertMinionSnapshotIntoCombatMinion)
+    {
+        $this->convertMinionSnapshotIntoCombatMinion = $convertMinionSnapshotIntoCombatMinion;
+    }
+
     /**
      * @param SideQuestSnapshot $sideQuestSnapshot
      * @return SideQuestCombatGroup
      */
     public function execute(SideQuestSnapshot $sideQuestSnapshot)
     {
+        $combatMinions = new AbstractCombatantCollection();
+        $sideQuestSnapshot->minionSnapshots->each(function (MinionSnapshot $minionSnapshot) use ($combatMinions) {
+            for ($i = 1; $i <= $minionSnapshot->pivot->count; $i++) {
+                $combatMinions->push($this->convertMinionSnapshotIntoCombatMinion->execute($minionSnapshot));
+            }
+        });
         return new SideQuestCombatGroup(
             $sideQuestSnapshot->uuid,
-            new AbstractCombatantCollection()
+            $combatMinions
         );
     }
 }
