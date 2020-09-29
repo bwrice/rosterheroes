8c2b59631214c1516210d6a78b0907fd271b8a8f
Brian Rice <bwrice83@gmail.com>
2020-09-29 16:41:19 -0400

    execute ConvertAttackSnapshotToCombatAttack in ConvertMinionSnapshotIntoCombatMinion for each attack snapshot

--- app/Domain/Actions/ConvertMinionSnapshotIntoCombatMinion.php
+++ app/Domain/Actions/ConvertMinionSnapshotIntoCombatMinion.php
@@ -4,25 +4,39 @@
 namespace App\Domain\Actions;
 
 
+use App\Domain\Actions\Combat\ConvertAttackSnapshotToCombatAttack;
 use App\Domain\Collections\CombatAttackCollection;
 use App\Domain\Combat\Combatants\CombatMinion;
+use App\Domain\Models\AttackSnapshot;
 use App\Domain\Models\MinionSnapshot;
 
 class ConvertMinionSnapshotIntoCombatMinion
 {
+    protected ConvertAttackSnapshotToCombatAttack $convertAttackSnapshotToCombatAttack;
+
+    public function __construct(ConvertAttackSnapshotToCombatAttack $convertAttackSnapshotToCombatAttack)
+    {
+        $this->convertAttackSnapshotToCombatAttack = $convertAttackSnapshotToCombatAttack;
+    }
+
     /**
      * @param MinionSnapshot $minionSnapshot
      * @return CombatMinion
      */
     public function execute(MinionSnapshot $minionSnapshot)
     {
+        $combatAttacks = new CombatAttackCollection();
+        $minionSnapshot->attackSnapshots->each(function (AttackSnapshot $attackSnapshot) use ($combatAttacks) {
+            $combatAttacks->push($this->convertAttackSnapshotToCombatAttack->execute($attackSnapshot));
+        });
+
         return new CombatMinion(
             $minionSnapshot->uuid,
             $minionSnapshot->starting_health,
             $minionSnapshot->protection,
             $minionSnapshot->block_chance,
             $minionSnapshot->combat_position_id,
-            new CombatAttackCollection()
+            $combatAttacks
         );
     }
 }
