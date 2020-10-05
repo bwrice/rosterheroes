bf1f501fb0cc4b80ed3016549d85cf9542f275e6
Brian Rice <bwrice83@gmail.com>
2020-10-05 06:28:50 -0400

    return damage and kill events from ExecuteCombatAttackOnCombatant

--- app/Domain/Actions/Combat/ExecuteCombatAttackOnCombatant.php
+++ app/Domain/Actions/Combat/ExecuteCombatAttackOnCombatant.php
@@ -7,6 +7,8 @@ namespace App\Domain\Actions\Combat;
 use App\Domain\Combat\Attacks\CombatAttackInterface;
 use App\Domain\Combat\Combatants\Combatant;
 use App\Domain\Combat\Events\AttackBlocked;
+use App\Domain\Combat\Events\AttackDamagesCombatant;
+use App\Domain\Combat\Events\AttackKillsCombatant;
 use App\Domain\Combat\Events\CombatEvent;
 
 class ExecuteCombatAttackOnCombatant
@@ -15,12 +17,25 @@ class ExecuteCombatAttackOnCombatant
      * @param CombatAttackInterface $combatAttack
      * @param Combatant $combatant
      * @param int $moment
+     * @param int $targetsCount
      * @return CombatEvent
      */
-    public function execute(CombatAttackInterface $combatAttack, Combatant $combatant, int $moment)
+    public function execute(CombatAttackInterface $combatAttack, Combatant $combatant, int $moment, int $targetsCount)
     {
         if ($combatant->attackBlocked($combatAttack)) {
             return new AttackBlocked($combatAttack, $combatant, $moment);
         }
+
+        $initialDamage = $combatAttack->getDamagePerTarget($targetsCount);
+        $damageToReceive = $combatant->calculateDamageToReceive($initialDamage);
+        $currentHealth = $combatant->getCurrentHealth();
+        $updatedHealth = max($currentHealth - $damageToReceive, 0);
+        $combatant->updateCurrentHealth($updatedHealth);
+
+        if ($updatedHealth > 0) {
+            return new AttackDamagesCombatant($combatAttack, $combatant, $damageToReceive, $moment);
+        }
+
+        return new AttackKillsCombatant($combatAttack, $combatant, $damageToReceive, $currentHealth, $moment);
     }
 }
