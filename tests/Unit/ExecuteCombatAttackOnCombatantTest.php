57decd757fdb1fd581b1c8a9a47752d3b015b284
Brian Rice <bwrice83@gmail.com>
2020-10-05 06:31:15 -0400

    test damage and kill events returned from ExecuteCombatAttackOnCombatant

--- tests/Unit/ExecuteCombatAttackOnCombatantTest.php
+++ tests/Unit/ExecuteCombatAttackOnCombatantTest.php
@@ -6,6 +6,8 @@ use App\Domain\Actions\Combat\ExecuteCombatAttackOnCombatant;
 use App\Domain\Combat\Attacks\CombatAttackInterface;
 use App\Domain\Combat\Combatants\Combatant;
 use App\Domain\Combat\Events\AttackBlocked;
+use App\Domain\Combat\Events\AttackDamagesCombatant;
+use App\Domain\Combat\Events\AttackKillsCombatant;
 use PHPUnit\Framework\TestCase;
 
 class ExecuteCombatAttackOnCombatantTest extends TestCase
@@ -30,8 +32,81 @@ class ExecuteCombatAttackOnCombatantTest extends TestCase
             ->andReturn(true)
             ->getMock();
 
-        $event = $this->getDomainAction()->execute($combatAttack, $combatant, $moment);
+        $event = $this->getDomainAction()->execute($combatAttack, $combatant, $moment, 1);
         $this->assertTrue($event instanceof AttackBlocked);
         $this->assertEquals($moment, $event->moment());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_return_a_attack_damages_combatant_event_if_not_blocked_and_combatant_health_above_zero()
+    {
+        $moment = rand(1, 100);
+        $initialDamage = rand(101, 500);
+        $damageToReceive = $initialDamage - rand(1, 100);
+        $currentHealth = $damageToReceive + rand(1, 1000);
+        $targetsCount = rand(1, 5);
+
+        $combatAttack = \Mockery::mock(CombatAttackInterface::class);
+        $combatAttack->shouldReceive('getDamagePerTarget')
+            ->with($targetsCount)
+            ->andReturn($initialDamage);
+
+
+        $combatant = \Mockery::spy(Combatant::class);
+        $combatant->shouldReceive('attackBlocked')
+            ->andReturn(false);
+        $combatant->shouldReceive('calculateDamageToReceive')
+            ->with($initialDamage)
+            ->andReturn($damageToReceive);
+        $combatant->shouldReceive('getCurrentHealth')
+            ->andReturn($currentHealth);
+
+        /** @var AttackDamagesCombatant $combatEvent */
+        $combatEvent = $this->getDomainAction()->execute($combatAttack, $combatant, $moment, $targetsCount);
+
+        $combatant->shouldHaveReceived('updateCurrentHealth')->with($currentHealth - $damageToReceive);
+
+        $this->assertTrue($combatEvent instanceof AttackDamagesCombatant);
+        $this->assertEquals($moment, $combatEvent->moment());
+        $this->assertEquals($damageToReceive, $combatEvent->getDamage());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_return_a_combatant_killed_event_if_the_damage_received_is_higher_than_the_combatant_current_health()
+    {
+        $moment = rand(1, 100);
+        $initialDamage = rand(101, 500);
+        $damageToReceive = $initialDamage - rand(1, 100);
+        $currentHealth = $damageToReceive - rand(1, 50);
+        $targetsCount = rand(1, 5);
+
+        $combatAttack = \Mockery::mock(CombatAttackInterface::class);
+        $combatAttack->shouldReceive('getDamagePerTarget')
+            ->with($targetsCount)
+            ->andReturn($initialDamage);
+
+
+        $combatant = \Mockery::spy(Combatant::class);
+        $combatant->shouldReceive('attackBlocked')
+            ->andReturn(false);
+        $combatant->shouldReceive('calculateDamageToReceive')
+            ->with($initialDamage)
+            ->andReturn($damageToReceive);
+        $combatant->shouldReceive('getCurrentHealth')
+            ->andReturn($currentHealth);
+
+        /** @var AttackKillsCombatant $combatEvent */
+        $combatEvent = $this->getDomainAction()->execute($combatAttack, $combatant, $moment, $targetsCount);
+
+        $combatant->shouldHaveReceived('updateCurrentHealth')->with(0);
+
+        $this->assertTrue($combatEvent instanceof AttackKillsCombatant);
+        $this->assertEquals($moment, $combatEvent->moment());
+        $this->assertEquals($damageToReceive, $combatEvent->getInitialDamageToReceive());
+        $this->assertEquals($currentHealth, $combatEvent->getActualDamageReceived());
     }
 }
