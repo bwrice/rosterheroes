cbbb4b4d000a08aa188f6d81ec0a2a437abfc4ea
Brian Rice <bwrice83@gmail.com>
2020-10-05 06:28:08 -0400

    create AttackKillsCombatant event

---
+++ app/Domain/Combat/Events/AttackKillsCombatant.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace App\Domain\Combat\Events;
+
+
+use App\Domain\Combat\Attacks\CombatAttackInterface;
+use App\Domain\Combat\Combatants\Combatant;
+
+class AttackKillsCombatant implements CombatEvent
+{
+    public const EVENT_STREAM = 'attack-damages-combatant';
+
+    protected CombatAttackInterface $combatAttack;
+    protected Combatant $combatant;
+    protected int $moment, $initialDamageToReceive, $actualDamageReceived;
+
+    public function __construct(
+        CombatAttackInterface $combatAttack,
+        Combatant $combatant,
+        int $initialDamageToReceive,
+        int $actualDamageReceived,
+        int $moment)
+    {
+        $this->combatAttack = $combatAttack;
+        $this->combatant = $combatant;
+        $this->initialDamageToReceive = $initialDamageToReceive;
+        $this->actualDamageReceived = $actualDamageReceived;
+        $this->moment = $moment;
+    }
+
+    public function moment(): int
+    {
+        return $this->moment;
+    }
+
+    public function eventStream(): string
+    {
+        return self::EVENT_STREAM;
+    }
+
+    public function getInitialDamageToReceive(): int
+    {
+        return $this->initialDamageToReceive;
+    }
+
+    public function getActualDamageReceived(): int
+    {
+        return $this->actualDamageReceived;
+    }
+}
