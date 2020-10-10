68c5767efdf9e809d5f0b3b3fe4005d2fe783807
Brian Rice <bwrice83@gmail.com>
2020-10-10 19:13:03 -0400

    convert AbstractCombatantFactory in CombatantFactory

--- app/Factories/Combat/CombatantFactory.php
+++ app/Factories/Combat/CombatantFactory.php
@@ -4,68 +4,78 @@
 namespace App\Factories\Combat;
 
 
+use App\Domain\Collections\CombatAttackCollection;
 use App\Domain\Combat\Combatants\Combatant;
 use App\Domain\Combat\Combatants\CombatHero;
 use App\Domain\Models\CombatPosition;
 use App\Domain\Models\Squad;
 use App\Factories\Models\HeroFactory;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
-abstract class AbstractCombatantFactory
+class CombatantFactory
 {
-    /** @var string|null */
-    protected $combatPositionName;
-
+    protected ?string $combatPositionName = null, $sourceUuid = null;
+    protected ?int $initialHealth = null, $initialStamina = null, $initialMana = null, $protection = null;
+    protected ?float $blockChancePercent = null;
     /**
      * @return static
      */
     public static function new()
     {
         return new static();
-    }/** @var string|null */
-    protected $heroUuid;
-
-    /** @var HeroFactory */
-    protected $heroFactory;
-
-    protected $health;
-
-    protected $stamina;
-
-    protected $mana;
-
-    protected $protection;
-
-    protected $blockChancePercent;
-
-    /** @var Squad|null */
-    protected $squad;
-
-    /** @var Collection|null */
-    protected $heroCombatAttackFactories;
+    }
 
     public function create()
     {
         $combatPosition = $this->getCombatPosition();
-        $heroUuid = $this->getHeroUuid();
-        $heroCombatAttacks = $this->getHeroCombatAttacks($heroUuid);
 
-        return new CombatHero(
-            $heroUuid,
-            is_null($this->health) ? 800 : $this->health,
-            is_null($this->stamina) ? 500 : $this->stamina,
-            is_null($this->mana) ? 400 : $this->mana,
+        return new Combatant(
+            $this->sourceUuid ?: (string) Str::uuid(),
+            is_null($this->initialHealth) ? 800 : $this->initialHealth,
+            is_null($this->initialStamina) ? 500 : $this->initialStamina,
+            is_null($this->initialMana) ? 400 : $this->initialMana,
             is_null($this->protection) ? 100 : $this->protection,
             is_null($this->blockChancePercent) ? 10: $this->blockChancePercent,
             $combatPosition->id,
-            $heroCombatAttacks
+            new CombatAttackCollection()
         );
     }
 
-    /**
-     * @return Combatant
-     */
-    abstract public function create();
+    public function withInitialMana(int $initialMana)
+    {
+        $clone = clone $this;
+        $clone->initialMana = $initialMana;
+        return $clone;
+    }
+
+    public function withInitialStamina(int $initialStamina)
+    {
+        $clone = clone $this;
+        $clone->initialStamina = $initialStamina;
+        return $clone;
+    }
+
+    public function withInitialHealth(int $initialHealth)
+    {
+        $clone = clone $this;
+        $clone->initialHealth = $initialHealth;
+        return $clone;
+    }
+
+    public function withProtection(int $protection)
+    {
+        $clone = clone $this;
+        $clone->protection = $protection;
+        return $clone;
+    }
+
+    public function withBlockChancePercent(float $blockChancePercent)
+    {
+        $clone = clone $this;
+        $clone->blockChancePercent = $blockChancePercent;
+        return $clone;
+    }
 
     protected function getCombatPosition()
     {
