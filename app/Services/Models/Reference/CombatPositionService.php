b8307a045f0055e936673a1203954067e76ade38
Brian Rice <bwrice83@gmail.com>
2020-10-09 21:41:02 -0400

    set behaviors for CombatPositionService

--- app/Services/Models/Reference/CombatPositionService.php
+++ app/Services/Models/Reference/CombatPositionService.php
@@ -4,11 +4,22 @@
 namespace App\Services\Models\Reference;
 
 
+use App\Domain\Behaviors\TargetRanges\BackLineBehavior;
+use App\Domain\Behaviors\TargetRanges\CombatPositionBehavior;
+use App\Domain\Behaviors\TargetRanges\FrontLineBehavior;
+use App\Domain\Behaviors\TargetRanges\HighGroundBehavior;
 use App\Domain\Models\CombatPosition;
 use Illuminate\Database\Eloquent\Collection;
 
 class CombatPositionService extends ReferenceService
 {
+
+    public function __construct()
+    {
+        $this->behaviors[CombatPosition::FRONT_LINE] = app(FrontLineBehavior::class);
+        $this->behaviors[CombatPosition::BACK_LINE] = app(BackLineBehavior::class);
+        $this->behaviors[CombatPosition::HIGH_GROUND] = app(HighGroundBehavior::class);
+    }
 
     protected function all(): Collection
     {
