169e5585356c46b7f437b4068570700360f52d08
Brian Rice <bwrice83@gmail.com>
2020-10-11 21:08:11 -0400

    add doc block methods to CombatPositionFacade

--- app/Facades/CombatPositionFacade.php
+++ app/Facades/CombatPositionFacade.php
@@ -8,6 +8,7 @@ use App\Domain\Behaviors\TargetRanges\CombatPositionBehavior;
 use App\Domain\Models\CombatPosition;
 use App\Services\Models\Reference\CombatPositionService;
 use Illuminate\Support\Facades\Facade;
+use phpDocumentor\Reflection\Types\Integer;
 
 /**
  * Class CombatPositionFacade
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Facade;
  *
  * @method static CombatPositionBehavior getBehavior(int|string $identifier)
  * @method static CombatPosition closestProximity(array $combatPositionIDs)
+ * @method static int proximity(int|string $identifier)
+ * @method static int id(string $combatPositionName);
  *
  * @see CombatPositionService
  */
