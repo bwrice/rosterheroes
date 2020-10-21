04510cfb6bf2fc0d0f29e9e6fd1ddec4cd2985ca
Brian Rice <bwrice83@gmail.com>
2020-10-20 21:06:19 -0400

    create MeasurableType facade

---
+++ app/Facades/MeasurableTypeFacade.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Facades;
+
+
+use App\Domain\Behaviors\MeasurableTypes\MeasurableTypeBehavior;
+use App\Services\Models\Reference\MeasurableTypeService;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Class MeasurableTypeFacade
+ * @package App\Facades
+ *
+ * @method static string name(int $id)
+ * @method static MeasurableTypeBehavior getBehavior($identifier)
+ *
+ */
+class MeasurableTypeFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return MeasurableTypeService::class;
+    }
+}
