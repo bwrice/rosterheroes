730b3b71abb3d6efbd714fb073820503aa6dce5a
Brian Rice <bwrice83@gmail.com>
2020-10-24 19:35:58 -0400

    add database-uuids driver to queue for laravel job batching

--- config/queue.php
+++ config/queue.php
@@ -99,6 +99,7 @@ return [
     */
 
     'failed' => [
+        'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
         'database' => env('DB_CONNECTION', 'mysql'),
         'table' => 'failed_jobs',
     ],
