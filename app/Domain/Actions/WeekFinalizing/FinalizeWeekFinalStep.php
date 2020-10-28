d222b8a2160d9eaf9c36e6ddbef2664ec7ebd11a
Brian Rice <bwrice83@gmail.com>
2020-10-28 15:04:00 -0400

    execute DispatchProcessSideQuestResultJobs in FinalizeWeekFinalStep

--- app/Domain/Actions/WeekFinalizing/FinalizeWeekFinalStep.php
+++ app/Domain/Actions/WeekFinalizing/FinalizeWeekFinalStep.php
@@ -3,31 +3,28 @@
 
 namespace App\Domain\Actions\WeekFinalizing;
 
-
 use App\Domain\Actions\SetupNextWeekAction;
+
 class FinalizeWeekFinalStep implements FinalizeWeekDomainAction
 {
-    /**
-     * @var SetupNextWeekAction
-     */
-    protected $setupNextWeekAction;
-    /**
-     * @var ClearWeeklyPlayerSpiritsFromHeroes
-     */
-    protected $clearSpirits;
+    protected SetupNextWeekAction $setupNextWeekAction;
+    protected DispatchProcessSideQuestResultJobs $dispatchProcessSideQuestResultJobs;
+    protected ClearWeeklyPlayerSpiritsFromHeroes $clearSpirits;
 
     public function __construct(
         ClearWeeklyPlayerSpiritsFromHeroes $clearSpirits,
+        DispatchProcessSideQuestResultJobs $dispatchProcessSideQuestResultJobs,
         SetupNextWeekAction $setupNextWeekAction)
     {
         $this->clearSpirits = $clearSpirits;
+        $this->dispatchProcessSideQuestResultJobs = $dispatchProcessSideQuestResultJobs;
         $this->setupNextWeekAction = $setupNextWeekAction;
     }
 
     public function execute(int $finalizeWeekStep, array $extra = [])
     {
         $this->clearSpirits->execute();
-
+        $this->dispatchProcessSideQuestResultJobs->execute();
         $this->setupNextWeekAction->execute();
     }
 }
