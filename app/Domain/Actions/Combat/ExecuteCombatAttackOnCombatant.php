aff728c7da0ebd15ac9c00c86e019c450a1aca06
Brian Rice <bwrice83@gmail.com>
2020-10-29 17:44:07 -0400

    return damage and kill event when ExecuteCombatAttack kills combatant

--- app/Domain/Actions/Combat/ExecuteCombatAttackOnCombatant.php
+++ app/Domain/Actions/Combat/ExecuteCombatAttackOnCombatant.php
@@ -10,8 +10,8 @@ use App\Domain\Combat\Combatants\CombatantInterface;
 use App\Domain\Combat\Events\AttackBlocked;
 use App\Domain\Combat\Events\AttackDamagesCombatant;
 use App\Domain\Combat\Events\AttackKillsCombatant;
-use App\Domain\Combat\Events\CombatEvent;
 use App\Facades\DamageTypeFacade;
+use Illuminate\Support\Collection;
 
 class ExecuteCombatAttackOnCombatant
 {
@@ -32,12 +32,14 @@ class ExecuteCombatAttackOnCombatant
      * @param CombatantInterface $target
      * @param int $moment
      * @param int $targetsCount
-     * @return CombatEvent
+     * @return Collection
      */
     public function execute(CombatAttackInterface $combatAttack, CombatantInterface $attacker, CombatantInterface $target, int $moment, int $targetsCount)
     {
+        $events = collect();
         if ($this->determineIfAttackIsBlocked->execute($target->getBlockChancePercent())) {
-            return new AttackBlocked($combatAttack, $attacker, $target, $moment);
+            $events->push(new AttackBlocked($combatAttack, $attacker, $target, $moment));
+            return $events;
         }
 
         $initialDamage = DamageTypeFacade::damagePerTarget($combatAttack->getDamageTypeID(), $combatAttack->getDamage(), $targetsCount);
@@ -47,10 +49,14 @@ class ExecuteCombatAttackOnCombatant
         $target->updateCurrentHealth($updatedHealth);
 
         if ($updatedHealth > 0) {
-            return new AttackDamagesCombatant($combatAttack, $attacker, $target, $damageToReceive, $moment);
+            $events->push(new AttackDamagesCombatant($combatAttack, $attacker, $target, $damageToReceive, $moment));
+            return $events;
         }
 
         // If attack kills the combatant, the actual damage received would be the health before the attack
-        return new AttackKillsCombatant($combatAttack, $attacker, $target, $currentHealth, $moment);
+        $damageTaken = $currentHealth;
+        $events->push(new AttackDamagesCombatant($combatAttack, $attacker, $target, $damageTaken, $moment));
+        $events->push(new AttackKillsCombatant($combatAttack, $attacker, $target, $currentHealth, $moment));
+        return $events;
     }
 }
