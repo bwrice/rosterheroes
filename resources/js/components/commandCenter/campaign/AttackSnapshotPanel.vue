86e4384ce75fa48227df302ebd6edb36b594aaa8
Brian Rice <bwrice83@gmail.com>
2020-11-11 13:40:44 -0500

    create AttackSnapshotPanel vue component

---
+++ resources/js/components/commandCenter/campaign/AttackSnapshotPanel.vue
@@ -0,0 +1,25 @@
+<template>
+    <BaseAttackPanel :attack="attackSnapshot">
+
+    </BaseAttackPanel>
+</template>
+
+<script>
+    import AttackSnapshot from "../../../models/AttackSnapshot";
+    import BaseAttackPanel from "../global/BaseAttackPanel";
+
+    export default {
+        name: "AttackSnapshotPanel",
+        components: {BaseAttackPanel},
+        props: {
+            attackSnapshot: {
+                type: AttackSnapshot,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
