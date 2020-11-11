8fb494763425ace0b998275b69d8ff4f16c255f9
Brian Rice <bwrice83@gmail.com>
2020-11-11 16:35:17 -0500

    build out AttackSnapshotPanel component

--- resources/js/components/commandCenter/campaign/AttackSnapshotPanel.vue
+++ resources/js/components/commandCenter/campaign/AttackSnapshotPanel.vue
@@ -1,6 +1,45 @@
 <template>
     <BaseAttackPanel :attack="attackSnapshot">
-
+        <v-row no-gutters align="center">
+            <v-col cols="4">
+                <v-row no-gutters justify="center">
+                    <v-tooltip bottom>
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-progress-circular
+                                :rotate="90"
+                                :size="52"
+                                :width="6"
+                                :value="speedProgress"
+                                color="accent"
+                                v-bind="attrs"
+                                v-on="on"
+                            >
+                            <span class="caption">
+                                {{attackSnapshot.combatSpeed}}
+                            </span>
+                            </v-progress-circular>
+                        </template>
+                        <span>Speed</span>
+                    </v-tooltip>
+                </v-row>
+            </v-col>
+            <v-col cols="8">
+                <v-tooltip bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-progress-linear
+                            v-model="damageProgress"
+                            color="error"
+                            height="25"
+                            v-bind="attrs"
+                            v-on="on"
+                        >
+                            {{attackSnapshot.damage}}
+                        </v-progress-linear>
+                    </template>
+                    <span>Damage</span>
+                </v-tooltip>
+            </v-col>
+        </v-row>
     </BaseAttackPanel>
 </template>
 
@@ -15,6 +54,14 @@
             attackSnapshot: {
                 type: AttackSnapshot,
                 required: true
+            }
+        },
+        computed: {
+            speedProgress() {
+                return 22 * Math.cbrt(this.attackSnapshot.combatSpeed);
+            },
+            damageProgress() {
+                return Math.sqrt(this.attackSnapshot.damage);
             }
         }
     }
