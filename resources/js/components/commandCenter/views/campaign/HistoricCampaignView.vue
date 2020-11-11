5f39a0d72fd75e0a1ec776b99bec7125bad7716a
Brian Rice <bwrice83@gmail.com>
2020-11-11 13:47:50 -0500

    render HeroSnapshotTabItems in HistoricCampaignView

--- resources/js/components/commandCenter/views/campaign/HistoricCampaignView.vue
+++ resources/js/components/commandCenter/views/campaign/HistoricCampaignView.vue
@@ -2,7 +2,19 @@
     <v-container>
         <v-row>
             <v-col cols="12" offset-sm="2" sm="8" offset-md="0" md="6" lg="5" offset-lg="1" xl="4" offset-xl="2">
-                {{_focusedCampaign.uuid}}
+                <v-card v-if="_squadSnapshot">
+                    <TabbedItems :items="_squadSnapshot.heroSnapshots">
+                        <template v-slot:tab="{item}">
+                            {{item.name}}
+                        </template>
+                        <template v-slot:default="{item}">
+                            <HeroSnapshotTabItem :hero-snapshot="item"></HeroSnapshotTabItem>
+                        </template>
+                    </TabbedItems>
+                </v-card>
+                <v-skeleton-loader v-else
+                    type="table-heading, list-item-two-line, image, table-tfoot"
+                ></v-skeleton-loader>
             </v-col>
             <v-col cols="12" offset-sm="2" sm="8" offset-md="0" md="6" lg="5" xl="4">
 
@@ -13,12 +25,21 @@
 
 <script>
     import {mapGetters} from 'vuex';
+    import TabbedItems from "../../global/TabbedItems";
+    import HeroSnapshotTabItem from "../../campaign/HeroSnapshotTabItem";
 
     export default {
         name: "HistoricCampaignView",
+        components: {HeroSnapshotTabItem, TabbedItems},
+        data() {
+            return {
+                heroSnapshotTab: null
+            }
+        },
         computed: {
             ...mapGetters([
                 '_focusedCampaign',
+                '_squadSnapshot'
             ]),
         }
     }
