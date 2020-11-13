8040e9f14973deee5a1d81cc3eee3e17eb4ae265
Brian Rice <bwrice83@gmail.com>
2020-11-12 23:49:31 -0500

    create MinionSnapshotResource

---
+++ app/Http/Resources/MinionSnapshotResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Domain\Models\MinionSnapshot;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Class MinionSnapshotResource
+ * @package App\Http\Resources
+ *
+ * @mixin MinionSnapshot
+ */
+class MinionSnapshotResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'uuid' => $this->uuid,
+            'name' => $this->name,
+            'level' => $this->level,
+            'combatPositionID' => $this->combat_position_id,
+            'enemyTypeID' => $this->enemy_type_id,
+            'startingHealth' => $this->starting_health,
+            'startingStamina' => $this->starting_stamina,
+            'startingMana' => $this->starting_mana,
+            'protection' => $this->protection,
+            'blockChance' => $this->block_chance,
+            'fantasyPower' => $this->fantasy_power,
+            'attackSnapshots' => AttackSnapshotResource::collection($this->attackSnapshots)
+        ];
+    }
+}
