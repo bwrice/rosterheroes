f0bc0c763264078e4bd5d3a2d266abc1efcb6343
Brian Rice <bwrice83@gmail.com>
2020-11-14 11:21:31 -0500

    setup card component for historic campaigns

---
+++ resources/js/components/commandCenter/campaign/HistoricCampaignStopCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-card>
+        Historic Campaign
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "HistoricCampaignStopCard",
+
+    }
+</script>
+
+<style scoped>
+
+</style>
