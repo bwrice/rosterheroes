9483bc9598bbff651757bd8d1ea2dea2eadfe2ae
Brian Rice <bwrice83@gmail.com>
2020-11-13 22:51:43 -0500

    add campaign-stops to focused-campaign vuex module

--- resources/js/store/commandCenter/focusedCampaignModule.js
+++ resources/js/store/commandCenter/focusedCampaignModule.js
@@ -1,12 +1,15 @@
 import HistoricCampaign from "../../models/HistoricCampaign";
 import * as squadApi from '../../api/squadApi';
+import * as campaignApi from '../../api/campaignApi';
 import SquadSnapshot from "../../models/SquadSnapshot";
+import CampaignStop from "../../models/CampaignStop";
 
 export default {
 
     state: {
         focusedCampaign: new HistoricCampaign({}),
-        squadSnapshot: null
+        squadSnapshot: null,
+        campaignStops: [],
     },
 
     getters: {
@@ -15,6 +18,9 @@ export default {
         },
         _squadSnapshot(state) {
             return state.squadSnapshot;
+        },
+        _campaignStops(state) {
+            return state.campaignStops;
         }
     },
 
@@ -24,6 +30,9 @@ export default {
         },
         SET_SQUAD_SNAPSHOT(state, squadSnapshot) {
             state.squadSnapshot = squadSnapshot;
+        },
+        SET_CAMPAIGN_STOPS(state, campaignStops) {
+            state.campaignStops = campaignStops;
         }
     },
 
@@ -33,12 +42,21 @@ export default {
             dispatch('updateSquadSnapshot', {
                 squadSlug: squadSlug,
                 weekID: focusedCampaign.weekID
+            });
+            dispatch('updateCampaignStops', {
+                campaignUuid: focusedCampaign.uuid
             })
         },
 
         async updateSquadSnapshot({commit}, {squadSlug, weekID}) {
             let response = await squadApi.getSquadSnapshot(squadSlug, weekID);
             commit('SET_SQUAD_SNAPSHOT', new SquadSnapshot(response.data));
+        },
+
+        async updateCampaignStops({commit}, {campaignUuid}) {
+            let response = await campaignApi.campaignStops(campaignUuid);
+            let campaignStops = response.data.map(stop => new CampaignStop(stop))
+            commit('SET_CAMPAIGN_STOPS', campaignStops);
         }
     }
 };
