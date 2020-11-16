6298a172b0799d4afb49d4eb94d035ffe19e73ed
Brian Rice <bwrice83@gmail.com>
2020-11-16 18:37:15 -0500

    create MinionSnapshotTab component

---
+++ resources/js/components/commandCenter/campaign/MinionSnapshotTab.vue
@@ -0,0 +1,36 @@
+<template>
+    <BaseMinionTab :minion="minionSnapshot">
+        <v-row class="no-gutters">
+            <v-col cols="12" class="px-1">
+                <span class="subtitle-2 rh-op-75">ATTACKS:</span>
+
+                <AttackSnapshotPanel
+                    v-for="(snapshot, uuid) in minionSnapshot.attackSnapshots"
+                    :key="uuid"
+                    :attack-snapshot="snapshot"
+                >
+                </AttackSnapshotPanel>
+            </v-col>
+        </v-row>
+    </BaseMinionTab>
+</template>
+
+<script>
+    import BaseMinionTab from "./BaseMinionTab";
+    import MinionSnapshot from "../../../models/MinionSnapshot";
+    import AttackSnapshotPanel from "./AttackSnapshotPanel";
+    export default {
+        name: "MinionSnapshotTab",
+        components: {AttackSnapshotPanel, BaseMinionTab},
+        props: {
+            minionSnapshot: {
+                type: MinionSnapshot,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
