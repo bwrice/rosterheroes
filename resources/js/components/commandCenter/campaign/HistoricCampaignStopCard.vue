f5a54cb56c40ba5513c0f0fe7653d88f7f299637
Brian Rice <bwrice83@gmail.com>
2020-11-16 18:38:30 -0500

    render side-quest-result panels in HistoricCampaignStopCard

--- resources/js/components/commandCenter/campaign/HistoricCampaignStopCard.vue
+++ resources/js/components/commandCenter/campaign/HistoricCampaignStopCard.vue
@@ -1,13 +1,30 @@
 <template>
-    <v-card>
-        Historic Campaign
+    <v-card outlined>
+        <v-card-title>
+            {{historicCampaignStop.name}}
+        </v-card-title>
+        <SideQuestResultExpandPanel
+            v-for="(sideQuestResult, uuid) in historicCampaignStop.sideQuestResults"
+            :key="uuid"
+            :side-quest-result="sideQuestResult"
+            class="mb-1 mx-1"
+        ></SideQuestResultExpandPanel>
     </v-card>
 </template>
 
 <script>
+    import HistoricCampaignStop from "../../../models/HistoricCampaignStop";
+    import SideQuestResultExpandPanel from "./SideQuestResultExpandPanel";
+
     export default {
         name: "HistoricCampaignStopCard",
-
+        components: {SideQuestResultExpandPanel},
+        props: {
+            historicCampaignStop: {
+                type: HistoricCampaignStop,
+                required: true
+            }
+        }
     }
 </script>
 
