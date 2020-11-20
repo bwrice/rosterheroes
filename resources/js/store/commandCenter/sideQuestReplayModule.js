7dda5afc0e090ef4dfc2309d6b01b012035b700a
Brian Rice <bwrice83@gmail.com>
2020-11-19 19:22:12 -0500

    add setupSideQuestReplay() action to module

--- resources/js/store/commandCenter/sideQuestReplayModule.js
+++ resources/js/store/commandCenter/sideQuestReplayModule.js
@@ -1,10 +1,12 @@
-import SideQuestResult from "../../models/SideQuestResult";
-
+import * as sideQuestResultApi from '../../api/sideQuestResultApi';
+import CombatGroup from "../../models/CombatGroup";
 export default {
 
     state: {
         sideQuestResult: null,
-        currentMoment: 0
+        currentMoment: 0,
+        sideQuestCombatSquad: null,
+        sideQuestCombatGroup: null
     },
 
     getters: {
@@ -15,10 +17,25 @@ export default {
     mutations: {
         SET_SIDE_QUEST_RESULT(state, sideQuestResult) {
             state.sideQuestResult = sideQuestResult;
+        },
+        SET_SIDE_QUEST_COMBAT_SQUAD(state, combatSquad) {
+            state.sideQuestCombatSquad = combatSquad;
+        },
+        SET_SIDE_QUEST_COMBAT_GROUP(state, combatGroup) {
+            state.sideQuestCombatGroup = combatGroup;
         }
     },
 
     actions: {
-
+        async setupSideQuestReplay({commit}, {sideQuestResult}) {
+            commit('SET_SIDE_QUEST_RESULT', sideQuestResult);
+            let battleGround = await sideQuestResultApi.getBattleground(sideQuestResult.uuid);
+            commit('SET_SIDE_QUEST_COMBAT_SQUAD', new CombatGroup({
+                combatants: battleGround.data.combat_squad.combat_heroes
+            }));
+            commit('SET_SIDE_QUEST_COMBAT_GROUP', new CombatGroup({
+                combatants: battleGround.data.side_quest_group.combat_minions
+            }));
+        }
     }
 };
