2640ebe5e5f6ac593a16f665488a7fef67e8e54f
Brian Rice <bwrice83@gmail.com>
2020-11-21 14:35:50 -0500

    extract out BattlefieldSide component

---
+++ resources/js/components/commandCenter/global/battlefield/BattlefieldSide.vue
@@ -0,0 +1,34 @@
+<template>
+    <g>
+        <BattlefieldCombatPosition
+            v-for="(combatPosition, id) in _combatPositions"
+            :key="id"
+            :combat-position="combatPosition"
+            :ally-side="allySide"
+        ></BattlefieldCombatPosition>
+    </g>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+    import BattlefieldCombatPosition from "./BattlefieldCombatPosition";
+    export default {
+        name: "BattlefieldSide",
+        components: {BattlefieldCombatPosition},
+        props: {
+            allySide: {
+                type: Boolean,
+                required: true
+            }
+        },
+        computed: {
+            ...mapGetters([
+                '_combatPositions'
+            ])
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/components/commandCenter/global/battlefield/CombatBattlefield.vue
+++ resources/js/components/commandCenter/global/battlefield/CombatBattlefield.vue
@@ -1,33 +1,15 @@
 <template>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,1000,1000" style="display: block">
-        <!-- ally side -->
-        <BattlefieldCombatPosition
-            v-for="(combatPosition, id) in _combatPositions"
-            :key="id"
-            :combat-position="combatPosition"
-            :ally-side="true"
-        ></BattlefieldCombatPosition>
-        <!-- enemy side -->
-        <BattlefieldCombatPosition
-            v-for="(combatPosition, id) in _combatPositions"
-            :key="id"
-            :combat-position="combatPosition"
-            :ally-side="false"
-        ></BattlefieldCombatPosition>
+        <BattlefieldSide :ally-side="true"></BattlefieldSide>
+        <BattlefieldSide :ally-side="false"></BattlefieldSide>
     </svg>
 </template>
 
 <script>
-    import {mapGetters} from 'vuex';
-    import BattlefieldCombatPosition from "./BattlefieldCombatPosition";
+    import BattlefieldSide from "./BattlefieldSide";
     export default {
         name: "CombatBattlefield",
-        components: {BattlefieldCombatPosition},
-        computed: {
-            ...mapGetters([
-                '_combatPositions'
-            ])
-        }
+        components: {BattlefieldSide}
     }
 </script>
 
