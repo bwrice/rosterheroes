3a36167cbce315df039919d0beee6058e5cc8c1a
Brian Rice <bwrice83@gmail.com>
2020-11-24 01:31:29 -0500

    fix initialMana key in Combatant JS model

--- resources/js/models/Combatant.js
+++ resources/js/models/Combatant.js
@@ -20,7 +20,7 @@ export default class Combatant {
         this.combatPositionID = combat_position_id;
         this.currentHealth = this.initialHealth = initial_health;
         this.currentStamina = this.initialStamina = initial_stamina;
-        this.currentMana = this.initial_mana = initial_mana;
+        this.currentMana = this.initialMana = initial_mana;
         this.combatAttacks = combat_attacks.map(attack => new CombatAttack(attack));
     }
 }
