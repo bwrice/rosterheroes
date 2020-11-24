bb91acc3663131464c60def0b51f26654d400221
Brian Rice <bwrice83@gmail.com>
2020-11-24 11:20:05 -0500

    add getHealthSum() method to CombatGroup

--- resources/js/models/CombatGroup.js
+++ resources/js/models/CombatGroup.js
@@ -5,4 +5,13 @@ export default class CombatGroup {
     constructor({combatants = []}) {
         this.combatants = combatants.map(combatant => new Combatant(combatant));
     }
+
+    getHealthSum({combatPositionID, healthProperty}) {
+        return this.combatants.filter(combatant => combatant.combatPositionID === combatPositionID)
+            .map(combatant => combatant[healthProperty])
+            .reduce(function (health, sum) {
+                return health + sum;
+            }, 0);
+    }
+
 }
