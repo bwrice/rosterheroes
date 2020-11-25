96870926b696f94bd9d6f6b62bf3daad7a2d3998
Brian Rice <bwrice83@gmail.com>
2020-11-25 01:01:20 -0500

    create BattlefieldDamage component

---
+++ resources/js/components/commandCenter/global/battlefield/BattlefieldDamage.vue
@@ -0,0 +1,29 @@
+<template>
+    <g>
+        <circle
+            color="#fff"
+            opacity=".7"
+            :cx="battlefieldEvent.xPosition"
+            :cy="battlefieldEvent.yPosition"
+            :r="battlefieldEvent.magnitude"
+        ></circle>
+    </g>
+</template>
+
+<script>
+    import BattlefieldEvent from "../../../../models/BattlefieldEvent";
+
+    export default {
+        name: "BattlefieldDamage",
+        props: {
+            battlefieldEvent: {
+                type: BattlefieldEvent,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
