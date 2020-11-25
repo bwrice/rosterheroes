833762c754f7c7b216aabaef61eb4530e59dc3d2
Brian Rice <bwrice83@gmail.com>
2020-11-25 01:00:21 -0500

    create BattlefieldEvent JS model

---
+++ resources/js/models/BattlefieldEvent.js
@@ -0,0 +1,8 @@
+export default class BattlefieldEvent {
+
+    constructor({magnitude, xPosition, yPosition}) {
+        this.magnitude = magnitude;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+    }
+}
