4bc537667b9363f071c4508e402ee65bb9247bc1
Brian Rice <bwrice83@gmail.com>
2020-11-25 23:23:40 -0500

    move rendering of BattlefieldDamage component into new BattlefieldEventGroup

--- resources/js/components/commandCenter/global/battlefield/BattlefieldCombatPosition.vue
+++ resources/js/components/commandCenter/global/battlefield/BattlefieldCombatPosition.vue
@@ -2,21 +2,14 @@
     <g>
         <path :d="fullArcPath" :fill="color" opacity="0.3" stroke="#333333"></path>
         <path :d="healthArcPath" :fill="color" stroke="#333333"></path>
-        <BattlefieldDamage
-            v-for="(battlefieldEvent, id) in battleFieldEvents"
-            :key="id"
-            :battlefield-damage-event="battlefieldEvent"
-        ></BattlefieldDamage>
     </g>
 </template>
 
 <script>
-    import BattlefieldDamage from "./BattlefieldDamage";
     import * as arcHelpers from "../../../../helpers/battlefieldArcHelpers";
 
     export default {
         name: "BattlefieldCombatPosition",
-        components: {BattlefieldDamage},
         props: {
             outerRadius: {
                 type: Number,
@@ -37,25 +30,6 @@
             color: {
                 type: String,
                 required: true
-            },
-            damages: {
-                type: Array,
-                required: true
-            }
-        },
-        data() {
-            return {
-                battleFieldEvents: []
-            }
-        },
-        created() {
-            let vm = this;
-            this.battleFieldEvents = createBattlefieldEventsFromDamages(this.damages, vm);
-        },
-        watch: {
-            damages(newDamages) {
-                let vm = this;
-                this.battleFieldEvents = createBattlefieldEventsFromDamages(newDamages, vm);
             }
         },
         computed: {
@@ -68,28 +42,6 @@
         }
     }
 
-    function createBattlefieldEventsFromDamages(damages, vm) {
-        let innerRadius = vm.innerRadius;
-        let thickness = vm.outerRadius - vm.innerRadius;
-        let allySide = vm.allySide;
-        let xOrigin = allySide ? 480 : 520;
-        let yOrigin = 500;
-
-        return damages.map(function (damage) {
-
-            // Create a random radius and percent value to calculate x and y coords
-            let radius = innerRadius + (thickness/10) + (Math.random() * (thickness/2));
-            let percent = 20 + (Math.random() * 60);
-            let xPosition = arcHelpers.getXPosition(xOrigin, radius, percent, allySide);
-            let yPosition = arcHelpers.getYPosition(yOrigin, radius, percent, allySide);
-
-            return {
-                damage,
-                xPosition,
-                yPosition
-            };
-        });
-    }
 </script>
 
 <style scoped>
---
+++ resources/js/components/commandCenter/global/battlefield/BattlefieldEventGroup.vue
@@ -0,0 +1,84 @@
+<template>
+    <g>
+        <BattlefieldDamage
+            v-for="(battlefieldDamage, id) in battlefieldDamages"
+            :key="id"
+            :battlefield-damage-event="battlefieldDamage"
+            :color="allySide ? '#fc7e23' : '#29b1cf'"
+        ></BattlefieldDamage>
+    </g>
+</template>
+
+<script>
+    import BattlefieldDamage from "./BattlefieldDamage";
+    import * as arcHelpers from "../../../../helpers/battlefieldArcHelpers";
+
+    export default {
+        name: "BattlefieldEventGroup",
+        components: {BattlefieldDamage},
+        props: {
+            outerRadius: {
+                type: Number,
+                required: true
+            },
+            innerRadius: {
+                type: Number,
+                required: true
+            },
+            healthPercent: {
+                type: Number,
+                required: true
+            },
+            allySide: {
+                type: Boolean,
+                required: true
+            },
+            damages: {
+                type: Array,
+                required: true
+            }
+        },
+        data() {
+            return {
+                battlefieldDamages: []
+            }
+        },
+        created() {
+            let vm = this;
+            this.battlefieldDamages = createBattlefieldDamagesFromDamages(this.damages, vm);
+        },
+        watch: {
+            damages(newDamages) {
+                let vm = this;
+                this.battlefieldDamages = createBattlefieldDamagesFromDamages(newDamages, vm);
+            }
+        }
+    }
+
+    function createBattlefieldDamagesFromDamages(damages, vm) {
+        let innerRadius = vm.innerRadius;
+        let thickness = vm.outerRadius - vm.innerRadius;
+        let allySide = vm.allySide;
+        let xOrigin = allySide ? 480 : 520;
+        let yOrigin = 500;
+
+        return damages.map(function (damage) {
+
+            // Create a random radius and percent value to calculate x and y coords
+            let radius = innerRadius + (thickness/10) + (Math.random() * (thickness/2));
+            let percent = 20 + (Math.random() * 60);
+            let xPosition = arcHelpers.getXPosition(xOrigin, radius, percent, allySide);
+            let yPosition = arcHelpers.getYPosition(yOrigin, radius, percent, allySide);
+
+            return {
+                damage,
+                xPosition,
+                yPosition
+            };
+        });
+    }
+</script>
+
+<style scoped>
+
+</style>
