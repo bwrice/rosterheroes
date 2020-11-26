8fae7301d67224f7cf434d3f840660cc52593ecb
Brian Rice <bwrice83@gmail.com>
2020-11-25 21:08:04 -0500

    delete BattlefieldEvent

--- resources/js/models/BattlefieldEvent.js
+++
@@ -1,8 +0,0 @@
-export default class BattlefieldEvent {
-
-    constructor({magnitude, xPosition, yPosition}) {
-        this.magnitude = magnitude;
-        this.xPosition = xPosition;
-        this.yPosition = yPosition;
-    }
-}
