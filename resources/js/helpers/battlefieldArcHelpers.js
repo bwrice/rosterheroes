17a7aff51c2f319dd6d1f8730f3b56083ffe954e
Brian Rice <bwrice83@gmail.com>
2020-11-27 22:06:49 -0500

    calculate arc radii inside battlefieldArcHelper

--- resources/js/helpers/battlefieldArcHelpers.js
+++ resources/js/helpers/battlefieldArcHelpers.js
@@ -1,37 +1,70 @@
+const Y_ORIGIN = 500;
 
-
-export function getXPosition(xOrigin, radius, percent, allySide) {
+export function getXPosition(radius, percent, allySide) {
+    let xOrigin = getXOrigin(allySide);
     let radians = getRadians(percent, allySide);
     return xOrigin + (radius * Math.cos(radians));
 }
 
-export function getYPosition(yOrigin, radius, percent, allySide) {
+export function getYPosition(radius, percent, allySide) {
     let radians = getRadians(percent, allySide);
-    return yOrigin - (radius * Math.sin(radians));
+    return 500 - (radius * Math.sin(radians));
 }
 
-export function buildArcPath(outerRadius, innerRadius, percent, allySide = true) {
-    let xOrigin = allySide ? 480 : 520;
-    let yOrigin = 500;
+export function buildArcPath(combatPositionName, percent, allySide) {
+
+    let xOrigin = getXOrigin(allySide);
+    let innerRadius = getInnerRadius(combatPositionName);
+    let outerRadius = getOuterRadius(combatPositionName);
+
     let outerArcEnd = {
-        x: getXPosition(xOrigin, outerRadius, percent, allySide),
-        y: getYPosition(yOrigin, outerRadius, percent, allySide)
+        x: getXPosition(outerRadius, percent, allySide),
+        y: getYPosition(outerRadius, percent, allySide)
     };
     let innerArcStart = {
-        x: getXPosition(xOrigin, innerRadius, percent, allySide),
-        y: getYPosition(yOrigin, innerRadius, percent, allySide)
+        x: getXPosition(innerRadius, percent, allySide),
+        y: getYPosition(innerRadius, percent, allySide)
     };
+
     return [
-        "M", xOrigin, (yOrigin + innerRadius),
-        "L", xOrigin, (yOrigin + outerRadius),
+        "M", xOrigin, (Y_ORIGIN + innerRadius),
+        "L", xOrigin, (Y_ORIGIN + outerRadius),
         "A", outerRadius, outerRadius, 0, 0, allySide ? 1 : 0, outerArcEnd.x, outerArcEnd.y,
         "L", innerArcStart.x, innerArcStart.y,
-        "A", innerRadius, innerRadius, 0, 0, allySide ? 0 : 1, xOrigin, (yOrigin + innerRadius)
+        "A", innerRadius, innerRadius, 0, 0, allySide ? 0 : 1, xOrigin, (Y_ORIGIN + innerRadius)
     ].join(" ");
+}
+
+function getXOrigin(allySide) {
+    return allySide ? 480: 520;
 }
 
 function getRadians(percent, allySide = true) {
     let offset = (100 - percent)/100 * 180;
     let degrees = allySide ? offset + 90 : 90 - offset;
     return degrees * (Math.PI/180);
+}
+
+export function getInnerRadius(combatPositionName) {
+    switch (combatPositionName) {
+        case 'front-line':
+            return 0;
+        case 'back-line':
+            return 220;
+        case 'high-ground':
+        default:
+            return 350;
+    }
+}
+
+export function getOuterRadius(combatPositionName) {
+    switch (combatPositionName) {
+        case 'front-line':
+            return 220;
+        case 'back-line':
+            return 350;
+        case 'high-ground':
+        default:
+            return 450;
+    }
 }
