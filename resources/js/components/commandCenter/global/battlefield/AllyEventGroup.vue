4fcf30a3835e4570b77b6386d414ebdb9d3f5c51
Brian Rice <bwrice83@gmail.com>
2020-11-30 12:12:58 -0500

    create ally and enemy versions of EventGroup component

---
+++ resources/js/components/commandCenter/global/battlefield/AllyEventGroup.vue
@@ -0,0 +1,41 @@
+<template>
+    <BattlefieldEventGroup
+        :combat-position-name="combatPositionName"
+        :ally-side="true"
+        :damages="damages"
+        :blocks="blocks"
+    ></BattlefieldEventGroup>
+</template>
+
+<script>
+
+    import {mapGetters} from 'vuex';
+    import BattlefieldEventGroup from "./BattlefieldEventGroup";
+
+    export default {
+        name: "AllyEventGroup",
+        components: {BattlefieldEventGroup},
+        props: {
+            combatPositionName: {
+                type: String,
+                required: true
+            },
+        },
+        computed: {
+            ...mapGetters([
+                '_allyDamages',
+                '_allyBlocks'
+            ]),
+            damages() {
+                return this._allyDamages[this.combatPositionName];
+            },
+            blocks() {
+                return this._allyBlocks[this.combatPositionName];
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
