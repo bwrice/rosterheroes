9f8e3e50130ec50964e479f559956edef5e4f01d
Brian Rice <bwrice83@gmail.com>
2020-11-30 11:51:09 -0500

    add helper methods to CombatGroup

--- resources/js/models/CombatGroup.js
+++ resources/js/models/CombatGroup.js
@@ -6,12 +6,29 @@ export default class CombatGroup {
         this.combatants = combatants.map(combatant => new Combatant(combatant));
     }
 
-    getHealthSum({combatPositionID, healthProperty}) {
-        return this.combatants.filter(combatant => combatant.combatPositionID === combatPositionID)
+    getHealthSum({combatPositionIDs = [], healthProperty}) {
+        return this.combatants.filter(combatant => combatPositionIDs.includes(combatant.combatPositionID))
             .map(combatant => combatant[healthProperty])
             .reduce(function (health, sum) {
                 return health + sum;
             }, 0);
+    }
+
+    getHealthPercent({combatPositionIDs = []}) {
+        let initialHealth = this.getHealthSum({combatPositionIDs, healthProperty: 'initialHealth'});
+        if (initialHealth > 0) {
+            let currentHealth = this.getHealthSum({combatPositionIDs, healthProperty: 'currentHealth'});
+            return (currentHealth/initialHealth) * 100;
+        }
+        return 0;
+    }
+
+    getHealthPercentsObject() {
+        return {
+            'front-line': this.getHealthPercent({combatPositionIDs: [1]}),
+            'back-line': this.getHealthPercent({combatPositionIDs: [2]}),
+            'high-ground': this.getHealthPercent({combatPositionIDs: [3]}),
+        };
     }
 
 }
