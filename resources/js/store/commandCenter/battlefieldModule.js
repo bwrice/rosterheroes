94aacef525163e4ac8430fde4d911d76276d1e2f
Brian Rice <bwrice83@gmail.com>
2020-11-30 11:51:36 -0500

    add more mutations and getters to battlefieldModule

--- resources/js/store/commandCenter/battlefieldModule.js
+++ resources/js/store/commandCenter/battlefieldModule.js
@@ -1,7 +1,20 @@
+import CombatGroup from "../../models/CombatGroup";
 
 export default {
 
     state: {
+        allyTotalInitialHealth: 0,
+        enemyTotalInitialHealth: 0,
+        allyHealthPercents: {
+            'front-line': 0,
+            'back-line': 0,
+            'high-ground': 0
+        },
+        enemyHealthPercents: {
+            'front-line': 0,
+            'back-line': 0,
+            'high-ground': 0
+        },
         allyDamages: {
             'front-line': [],
             'back-line': [],
@@ -21,10 +34,16 @@ export default {
             'front-line': [],
             'back-line': [],
             'high-ground': []
-        }
+        },
     },
 
     getters: {
+        _allyHealthPercents(state) {
+            return state.allyHealthPercents;
+        },
+        _enemyHealthPercents(state) {
+            return state.enemyHealthPercents;
+        },
         _allyDamages(state) {
             return state.allyDamages;
         },
@@ -32,14 +51,37 @@ export default {
             return state.enemyDamages;
         },
         _allyBlocks(state) {
-            return state.allyDamages;
+            return state.allyBlocks;
         },
         _enemyBlocks(state) {
-            return state.enemyDamages;
+            return state.enemyBlocks;
         }
     },
     mutations: {
-
+        SET_ALLY_HEALTH_PERCENTS(state, allyHealthPercents) {
+            state.allyHealthPercents = allyHealthPercents;
+        },
+        SET_ENEMY_HEALTH_PERCENTS(state, enemyHealthPercents) {
+            state.enemyHealthPercents = enemyHealthPercents;
+        },
+        SET_ALLY_TOTAL_INITIAL_HEALTH(state, allyTotalInitialHealth) {
+            state.allyTotalInitialHealth = allyTotalInitialHealth;
+        },
+        SET_ENEMY_TOTAL_INITIAL_HEALTH(state, enemyTotalInitialHealth) {
+            state.enemyTotalInitialHealth = enemyTotalInitialHealth;
+        },
+        SET_ALLY_DAMAGES(state, allyDamages) {
+            state.allyDamages = allyDamages;
+        },
+        SET_ENEMY_DAMAGES(state, enemyDamages) {
+            state.enemyDamages = enemyDamages;
+        },
+        SET_ALLY_BLOCKS(state, allyBlocks) {
+            state.allyBlocks = allyBlocks;
+        },
+        SET_ENEMY_BLOCKS(state, enemyBlocks) {
+            state.enemyBlocks = enemyBlocks;
+        },
     },
 
     actions: {
