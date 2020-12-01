ab11ac604a60e94739460cfd8c12065c3e4433cf
Brian Rice <bwrice83@gmail.com>
2020-11-30 20:24:16 -0500

    cleanup commented-out/unused code

--- resources/js/components/commandCenter/global/battlefield/AllyBattlefieldArc.vue
+++ resources/js/components/commandCenter/global/battlefield/AllyBattlefieldArc.vue
@@ -23,29 +23,8 @@
             ...mapGetters([
                 '_allyHealthPercents'
             ]),
-            // combatPositionID() {
-            //     switch (this.combatPositionName) {
-            //         case 'front-line':
-            //             return 1;
-            //         case 'back-line':
-            //             return 2;
-            //         case 'high-ground':
-            //         default:
-            //             return 3;
-            //     }
-            // },
             healthPercent() {
                 return this._allyHealthPercents[this.combatPositionName];
-                // let combatPositionID = this.combatPositionID();
-                // let initialHealth = this._allyGroup.getHealthSum({
-                //     combatPositionID: combatPositionID,
-                //     healthProperty: 'initialHealth'
-                // });
-                // let currentHealth = this._allyGroup.getHealthSum({
-                //     combatPositionID: combatPositionID,
-                //     healthProperty: 'currentHealth'
-                // });
-                // return initialHealth ? (currentHealth/initialHealth) * 100 : 0;
             }
         }
     }
