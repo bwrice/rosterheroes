22bccab67fd9bfa99d40c10319a5f00028ff2893
Brian Rice <bwrice83@gmail.com>
2020-12-10 21:09:33 -0500

    reduce combat-battlefield size on mobile

--- resources/js/components/commandCenter/views/campaign/SideQuestReplayView.vue
+++ resources/js/components/commandCenter/views/campaign/SideQuestReplayView.vue
@@ -2,7 +2,11 @@
     <v-container>
         <v-row>
             <v-col cols="10" offset="1" offset-sm="2" sm="8" offset-md="0" md="6" lg="5" offset-lg="1" xl="4" offset-xl="2">
-                <CombatBattlefield></CombatBattlefield>
+                <v-row no-gutters class="px-3">
+                    <v-col cols="12">
+                        <CombatBattlefield></CombatBattlefield>
+                    </v-col>
+                </v-row>
             </v-col>
             <v-col cols="12" offset-sm="2" sm="8" offset-md="0" md="6" lg="5" xl="4">
                 <v-card>
