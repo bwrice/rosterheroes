7f0591db0c13b3beae333af116e565597d7f8a95
Brian Rice <bwrice83@gmail.com>
2020-12-17 20:37:39 -0500

    create PlayerSpiritPool component

---
+++ resources/js/components/commandCenter/roster/PlayerSpiritPool.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-card>
+        <v-virtual-scroll
+            :items="playerSpirits"
+            :height="456"
+            :item-height="76"
+            :bench="4"
+        >
+            <template v-slot:default="{ item }">
+                <PlayerSpiritPanel
+                    :player-spirit="item"
+                >
+                    <template v-slot:spirit-actions>
+                        <AddSpiritButton
+                            v-if="hero"
+                            :hero="hero"
+                            :player-spirit="item"
+                        ></AddSpiritButton>
+                    </template>
+                </PlayerSpiritPanel>
+                <v-divider></v-divider>
+            </template>
+        </v-virtual-scroll>
+    </v-card>
+</template>
+
+<script>
+    import PlayerSpiritPanel from "./PlayerSpiritPanel";
+    import Hero from "../../../models/Hero";
+    import AddSpiritButton from "./AddSpiritButton";
+    export default {
+        name: "PlayerSpiritPool",
+        components: {AddSpiritButton, PlayerSpiritPanel},
+        props: {
+            playerSpirits: {
+                type: Array,
+                required: true
+            },
+            hero: {
+                type: Hero
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
