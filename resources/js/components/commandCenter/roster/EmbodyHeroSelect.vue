17aff7db8d25026a84f3d1ac9c2aaaed253ebf2f
Brian Rice <bwrice83@gmail.com>
2020-12-19 20:35:32 -0500

    truncate embody hero button text

--- resources/js/components/commandCenter/roster/EmbodyHeroSelect.vue
+++ resources/js/components/commandCenter/roster/EmbodyHeroSelect.vue
@@ -9,7 +9,7 @@
                 v-bind="attrs"
                 v-on="on"
             >
-                {{ buttonText }}
+                <span class="d-inline-block text-truncate" style="max-width: 80px;">{{ buttonText }}</span>
             </v-btn>
         </template>
         <v-list>
