cddb44b1dc7cf91d519b80d7f810019611c36344
Brian Rice <bwrice83@gmail.com>
2020-12-23 18:54:13 -0500

    use ItemsGroup in LocalStashCard

--- resources/js/components/commandCenter/barracks/LocalStashCard.vue
+++ resources/js/components/commandCenter/barracks/LocalStashCard.vue
@@ -3,51 +3,42 @@
         <v-col cols="12">
             <span class="title font-weight-thin">STASH ({{_currentLocationProvince.name}})</span>
         </v-col>
-        <v-col v-if="stashHasItems" cols="12">
-            <ItemIterator
+        <v-col cols="12">
+            <ItemsGroup
                 :items="_localStash.items"
-                :search-label="searchLabel"
+                :search-label="'Search Stash'"
+                :empty-message="emptyMessage"
+                class="mb-2"
             >
-                <template v-slot:before-expand="props">
-                    <div class="px-2">
-                        <MobileStoreItemButton
-                            :item="props.item"
-                        >
-                        </MobileStoreItemButton>
-                    </div>
+
+                <template v-slot:before-show-icon="{item}">
+                    <MobileStoreItemButton
+                        :item="item"
+                        class="mr-1"
+                    >
+                    </MobileStoreItemButton>
                 </template>
-            </ItemIterator>
-        </v-col>
-        <v-col v-else cols="12">
-            <v-sheet color="rgba(255,255,255, 0.25)" class="my-2">
-                <v-row no-gutters class="pa-2" justify="center" align="center">
-                    <span class="subtitle-1 font-weight-light">Stash in {{_currentLocationProvince.name}} is empty</span>
-                </v-row>
-            </v-sheet>
+            </ItemsGroup>
         </v-col>
     </v-row>
 </template>
 
 <script>
     import { mapGetters } from 'vuex'
-    import PaginationBlock from "../global/PaginationBlock";
-    import ItemIterator from "../global/ItemIterator";
     import MobileStoreItemButton from "./MobileStoreItemButton";
+    import ItemsGroup from "./ItemsGroup";
 
     export default {
         name: "LocalStashCard",
-        components: {MobileStoreItemButton, ItemIterator, PaginationBlock},
+        components: {ItemsGroup, MobileStoreItemButton},
 
         computed: {
             ...mapGetters([
                 '_localStash',
                 '_currentLocationProvince'
             ]),
-            stashHasItems() {
-                return this._localStash.items.length > 0;
-            },
-            searchLabel() {
-                return 'Search Stash';
+            emptyMessage() {
+                return 'Stash in ' + this._currentLocationProvince.name + ' is empty';
             }
         }
     }
