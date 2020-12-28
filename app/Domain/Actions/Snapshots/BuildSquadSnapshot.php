2276fb760f3266d82e525d8b876b149230a1e6c1
Brian Rice <bwrice83@gmail.com>
2020-12-27 19:25:50 -0500

    create SquadSnapshot within DB transaction

--- app/Domain/Actions/Snapshots/BuildSquadSnapshot.php
+++ app/Domain/Actions/Snapshots/BuildSquadSnapshot.php
@@ -8,6 +8,7 @@ use App\Domain\Models\Hero;
 use App\Domain\Models\Squad;
 use App\Domain\Models\SquadSnapshot;
 use App\Facades\CurrentWeek;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class BuildSquadSnapshot extends BuildSnapshot
@@ -42,19 +43,22 @@ class BuildSquadSnapshot extends BuildSnapshot
             }
         }
 
-        /** @var SquadSnapshot $squadSnapshot */
-        $squadSnapshot = SquadSnapshot::query()->create([
-            'uuid' => Str::uuid(),
-            'week_id' => $weekID,
-            'squad_id' => $squad->id,
-            'squad_rank_id' => $squad->squad_rank_id,
-            'experience' => $squad->experience
-        ]);
+        return DB::transaction(function () use ($squad, $weekID) {
 
-        $squad->heroes->each(function (Hero $hero) use ($squadSnapshot) {
-            $this->buildHeroSnapshot->execute($squadSnapshot, $hero);
+            /** @var SquadSnapshot $squadSnapshot */
+            $squadSnapshot = SquadSnapshot::query()->create([
+                'uuid' => Str::uuid(),
+                'week_id' => $weekID,
+                'squad_id' => $squad->id,
+                'squad_rank_id' => $squad->squad_rank_id,
+                'experience' => $squad->experience
+            ]);
+
+            $squad->heroes->each(function (Hero $hero) use ($squadSnapshot) {
+                $this->buildHeroSnapshot->execute($squadSnapshot, $hero);
+            });
+
+            return $squadSnapshot;
         });
-
-        return $squadSnapshot;
     }
 }
