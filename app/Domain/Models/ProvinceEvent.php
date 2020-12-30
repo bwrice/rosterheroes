6cccabc4f2b99edb48e99d47ea5f81ea40d72d99
Brian Rice <bwrice83@gmail.com>
2020-12-29 20:25:57 -0500

    create ProvinceEvent model and migration

---
+++ app/Domain/Models/ProvinceEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Domain\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProvinceEvent extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2020_12_29_235524_create_province_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvinceEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('province_events', function (Blueprint $table) {
+            $table->id();
+            $table->integer('province_id')->unsigned();
+            $table->foreign('province_id')->references('id')->on('provinces');
+            $table->string('event_type');
+            $table->json('data');
+            $table->dateTime('happened_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('province_events');
+    }
+}
