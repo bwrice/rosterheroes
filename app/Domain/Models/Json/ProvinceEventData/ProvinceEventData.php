89edfc2fd8e14f17af3f435c7bffeef929875e07
Brian Rice <bwrice83@gmail.com>
2020-12-29 21:03:13 -0500

    create abstract class ProvinceEventData

---
+++ app/Domain/Models/Json/ProvinceEventData/ProvinceEventData.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Domain\Models\Json\ProvinceEventData;
+
+
+use App\Domain\Models\Province;
+use Carbon\CarbonInterface;
+
+abstract class ProvinceEventData
+{
+    protected Province $province;
+    protected CarbonInterface $happenedAt;
+    protected array $data;
+
+    public function __construct(Province $province, CarbonInterface $happenedAt, array $data)
+    {
+        $this->province = $province;
+        $this->happenedAt = $happenedAt;
+        $this->data = $data;
+    }
+}
