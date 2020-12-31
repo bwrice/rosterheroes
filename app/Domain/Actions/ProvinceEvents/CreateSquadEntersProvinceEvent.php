0f1403568b37fd24a90c57adb2db2537d38895c5
Brian Rice <bwrice83@gmail.com>
2020-12-31 00:34:11 -0500

    dont create new event if recent event of exact data exists

--- app/Domain/Actions/ProvinceEvents/CreateSquadEntersProvinceEvent.php
+++ app/Domain/Actions/ProvinceEvents/CreateSquadEntersProvinceEvent.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Str;
 
 class CreateSquadEntersProvinceEvent
 {
+    public const MINUTES_BETWEEN_EVENTS = 5;
+
     /**
      * @param Province $provinceEntered
      * @param Province $provinceLeft
@@ -23,6 +25,17 @@ class CreateSquadEntersProvinceEvent
      */
     public function execute(Province $provinceEntered, Province $provinceLeft, Squad $squad, CarbonInterface $happenedAt, int $goldCost)
     {
+        /** @var ProvinceEvent $recentEvent */
+        $recentEvent = ProvinceEvent::query()->where('event_type', '=', ProvinceEvent::TYPE_SQUAD_ENTERS_PROVINCE)
+            ->where('province_id', '=', $provinceEntered->id)
+            ->where('squad_id', '=', $squad->id)
+            ->where('happened_at', '>=', now()->subMinutes(self::MINUTES_BETWEEN_EVENTS))
+            ->first();
+
+        if ($recentEvent) {
+            return $recentEvent;
+        }
+
         /** @var ProvinceEvent $event */
         $event = ProvinceEvent::query()->create([
             'uuid' => (string) Str::uuid(),
--- tests/Feature/CreateSquadEntersProvinceEventTest.php
+++ tests/Feature/CreateSquadEntersProvinceEventTest.php
@@ -6,6 +6,7 @@ use App\Domain\Actions\ProvinceEvents\CreateSquadEntersProvinceEvent;
 use App\Domain\Models\Json\ProvinceEventData\SquadEntersProvince;
 use App\Domain\Models\Province;
 use App\Domain\Models\ProvinceEvent;
+use App\Factories\Models\ProvinceEventFactory;
 use App\Factories\Models\SquadFactory;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -26,10 +27,10 @@ class CreateSquadEntersProvinceEventTest extends TestCase
 
     /**
      * @test
+     * @throws \Exception
      */
-    public function it_will_create_a_squad_events_province_event()
+    public function it_will_create_a_squad_enters_province_event()
     {
-
         /** @var Province $provinceEntered */
         $provinceEntered = Province::query()->inRandomOrder()->first();
         /** @var Province $provinceLeft */
@@ -41,10 +42,62 @@ class CreateSquadEntersProvinceEventTest extends TestCase
         $this->assertEquals(ProvinceEvent::TYPE_SQUAD_ENTERS_PROVINCE, $event->event_type);
         $this->assertEquals($provinceEntered->id, $event->province_id);
 
-        /** @var SquadEntersProvince $data */
-        $data = $event->data;
-        $this->assertEquals($squad->uuid, $data->squadUuid());
-        $this->assertEquals($cost, $data->getGoldCost());
-        $this->assertEquals($provinceLeft->uuid, $data->getProvinceLeftUuid());
+        /** @var SquadEntersProvince $eventData */
+        $eventData = $event->getEventData();
+        $this->assertEquals($squad->id, $event->squad->id);
+        $this->assertEquals($cost, $eventData->getGoldCost());
+        $this->assertEquals($provinceLeft->uuid, $eventData->getProvinceLeftUuid());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_not_create_a_squad_enters_province_event_if_a_recent_event_for_squad_and_province_exists()
+    {
+        $minutesAgo = CreateSquadEntersProvinceEvent::MINUTES_BETWEEN_EVENTS - 1;
+        $existingEvent = ProvinceEventFactory::new()->at(now()->subMinutes($minutesAgo))->squadEntersProvince()->create();
+
+        $provinceEntered = $existingEvent->province;
+        $provinceLeft = $provinceEntered->borders()->inRandomOrder()->first();
+        $squad = $existingEvent->squad;
+
+        $eventsCount = ProvinceEvent::query()->where('event_type', '=', ProvinceEvent::TYPE_SQUAD_ENTERS_PROVINCE)
+            ->where('province_id', '=', $provinceEntered->id
+            )->where('squad_id', '=', $squad->id)->count();
+        $this->assertEquals(1, $eventsCount);
+
+        $newEvent = $this->getDomainAction()->execute($provinceEntered, $provinceLeft, $squad, now(), rand(10, 9999));
+        $this->assertEquals($existingEvent->uuid, $newEvent->uuid);
+
+        $eventsCount = ProvinceEvent::query()->where('event_type', '=', ProvinceEvent::TYPE_SQUAD_ENTERS_PROVINCE)
+            ->where('province_id', '=', $provinceEntered->id
+            )->where('squad_id', '=', $squad->id)->count();
+        $this->assertEquals(1, $eventsCount);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_create_a_new_squad_enters_province_event_if_a_NON_recent_event_for_squad_and_province_exists()
+    {
+        $minutesAgo = CreateSquadEntersProvinceEvent::MINUTES_BETWEEN_EVENTS + 5;
+        $existingEvent = ProvinceEventFactory::new()->at(now()->subMinutes($minutesAgo))->squadEntersProvince()->create();
+
+        $provinceEntered = $existingEvent->province;
+        $provinceLeft = $provinceEntered->borders()->inRandomOrder()->first();
+        $squad = $existingEvent->squad;
+
+        $eventsCount = ProvinceEvent::query()->where('event_type', '=', ProvinceEvent::TYPE_SQUAD_ENTERS_PROVINCE)
+            ->where('province_id', '=', $provinceEntered->id
+            )->where('squad_id', '=', $squad->id)->count();
+        $this->assertEquals(1, $eventsCount);
+
+        $newEvent = $this->getDomainAction()->execute($provinceEntered, $provinceLeft, $squad, now(), rand(10, 9999));
+        $this->assertNotEquals($existingEvent->uuid, $newEvent->uuid);
+
+        $eventsCount = ProvinceEvent::query()->where('event_type', '=', ProvinceEvent::TYPE_SQUAD_ENTERS_PROVINCE)
+            ->where('province_id', '=', $provinceEntered->id
+            )->where('squad_id', '=', $squad->id)->count();
+        $this->assertEquals(2, $eventsCount);
     }
 }
