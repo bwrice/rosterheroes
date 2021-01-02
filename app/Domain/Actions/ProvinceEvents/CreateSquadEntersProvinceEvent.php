292272ec70d6bc55ac94b945d85984d207c05b95
Brian Rice <bwrice83@gmail.com>
2021-01-02 12:10:39 -0500

    remove disabling of creating duplicate recent event

--- app/Domain/Actions/ProvinceEvents/CreateSquadEntersProvinceEvent.php
+++ app/Domain/Actions/ProvinceEvents/CreateSquadEntersProvinceEvent.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Str;
 
 class CreateSquadEntersProvinceEvent
 {
-    public const MINUTES_BETWEEN_EVENTS = 5;
 
     /**
      * @param Province $provinceEntered
@@ -26,17 +25,6 @@ class CreateSquadEntersProvinceEvent
      */
     public function execute(Province $provinceEntered, Province $provinceLeft, Squad $squad, CarbonInterface $happenedAt, int $goldCost)
     {
-        /** @var ProvinceEvent $recentEvent */
-        $recentEvent = ProvinceEvent::query()->where('event_type', '=', ProvinceEvent::TYPE_SQUAD_ENTERS_PROVINCE)
-            ->where('province_id', '=', $provinceEntered->id)
-            ->where('squad_id', '=', $squad->id)
-            ->where('happened_at', '>=', now()->subMinutes(self::MINUTES_BETWEEN_EVENTS))
-            ->first();
-
-        if ($recentEvent) {
-            return $recentEvent;
-        }
-
         /** @var ProvinceEvent $provinceEvent */
         $provinceEvent = ProvinceEvent::query()->create([
             'uuid' => (string) Str::uuid(),
