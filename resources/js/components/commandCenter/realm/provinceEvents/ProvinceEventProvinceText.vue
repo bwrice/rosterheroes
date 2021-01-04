2727e6c9a74e46d49a7942c698d4176ea2595ed0
Brian Rice <bwrice83@gmail.com>
2021-01-03 19:58:54 -0500

    create province-event text components

---
+++ resources/js/components/commandCenter/realm/provinceEvents/ProvinceEventProvinceText.vue
@@ -0,0 +1,21 @@
+<template>
+    <span class="accent--text">{{province.name}}</span>
+</template>
+
+<script>
+    import Province from "../../../../models/Province";
+
+    export default {
+        name: "ProvinceEventProvinceText",
+        props: {
+            province: {
+                type: Province,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/commandCenter/realm/provinceEvents/ProvinceEventSquadText.vue
@@ -0,0 +1,19 @@
+<template>
+    <span class="primary--text">{{name}}</span>
+</template>
+
+<script>
+    export default {
+        name: "ProvinceEventSquadText",
+        props: {
+            name: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
