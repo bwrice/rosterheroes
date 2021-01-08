65d765bf5e2e418e471cd9d2d9d14faedd2289f5
Brian Rice <bwrice83@gmail.com>
2021-01-07 19:11:53 -0500

    extract ProvinceEventsScroll from LocalEvents component

--- resources/js/components/commandCenter/realm/LocalEvents.vue
+++ resources/js/components/commandCenter/realm/LocalEvents.vue
@@ -4,27 +4,7 @@
             <span class="title font-weight-thin">Local Events</span>
         </v-col>
         <v-col cols="12">
-            <v-card>
-                <v-virtual-scroll
-                    v-if="_localProvinceEvents.length > 0"
-                    :items="_localProvinceEvents"
-                    :height="340"
-                    :item-height="45"
-                    :bench="4"
-                >
-                    <template v-slot:default="{ item }">
-                        <component :is="eventComponent(item.eventType)" :provinceEvent="item"></component>
-                    </template>
-                </v-virtual-scroll>
-                <v-row
-                    v-else
-                    justify="center"
-                    align="center"
-                    :style="'height: 400px'"
-                    class="no-gutters">
-                    <span class="text-h6 text-lg-h5" style="color: rgba(255, 255, 255, 0.8)">No Recent Events</span>
-                </v-row>
-            </v-card>
+            <ProvinceEventsScroll :province-events="_localProvinceEvents"></ProvinceEventsScroll>
         </v-col>
     </v-row>
 </template>
@@ -32,29 +12,14 @@
 <script>
 
     import {mapGetters} from 'vuex';
-    import SquadEntersProvince from "./provinceEvents/SquadEntersProvince";
-    import SquadLeavesProvince from "./provinceEvents/SquadLeavesProvince";
-    import SquadJoinsQuest from "./provinceEvents/SquadJoinsQuest";
+    import ProvinceEventsScroll from "./provinceEvents/ProvinceEventsScroll";
 
     export default {
         name: "LocalEvents",
-        components: {SquadJoinsQuest, SquadLeavesProvince, SquadEntersProvince},
-        methods: {
-            eventComponent(eventType) {
-                switch (eventType) {
-                    case 'squad-enters-province':
-                        return 'SquadEntersProvince';
-                    case 'squad-leaves-province':
-                        return 'SquadLeavesProvince';
-                    case 'squad-joins-quest':
-                        return 'SquadJoinsQuest';
-                }
-            }
-        },
+        components: {ProvinceEventsScroll},
         computed: {
             ...mapGetters([
-                '_localProvinceEvents',
-                '_provinceByUuid'
+                '_localProvinceEvents'
             ])
         }
     }
---
+++ resources/js/components/commandCenter/realm/provinceEvents/ProvinceEventsScroll.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-card>
+        <v-virtual-scroll
+            v-if="provinceEvents.length > 0"
+            :items="provinceEvents"
+            :height="340"
+            :item-height="45"
+            :bench="4"
+        >
+            <template v-slot:default="{ item }">
+                <component :is="eventComponent(item.eventType)" :provinceEvent="item"></component>
+            </template>
+        </v-virtual-scroll>
+        <v-row
+            v-else
+            justify="center"
+            align="center"
+            :style="'height: 340px'"
+            class="no-gutters">
+            <span class="text-h6 text-lg-h5" style="color: rgba(255, 255, 255, 0.8)">No Recent Events</span>
+        </v-row>
+    </v-card>
+</template>
+
+<script>
+    import SquadEntersProvince from "../provinceEvents/SquadEntersProvince";
+    import SquadLeavesProvince from "../provinceEvents/SquadLeavesProvince";
+    import SquadJoinsQuest from "../provinceEvents/SquadJoinsQuest";
+    export default {
+        name: "ProvinceEventsScroll",
+        props: {
+            provinceEvents: {
+                type: Array,
+                required: true
+            }
+        },
+        components: {SquadJoinsQuest, SquadLeavesProvince, SquadEntersProvince},
+        methods: {
+            eventComponent(eventType) {
+                switch (eventType) {
+                    case 'squad-enters-province':
+                        return 'SquadEntersProvince';
+                    case 'squad-leaves-province':
+                        return 'SquadLeavesProvince';
+                    case 'squad-joins-quest':
+                        return 'SquadJoinsQuest';
+                }
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
