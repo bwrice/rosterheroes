70b55a56d3e4caaccd61b567cd901ba929054504
Brian Rice <bwrice83@gmail.com>
2021-01-15 01:13:56 -0500

    clean-up FindQuestsToJoin

--- app/Domain/Actions/NPC/FindQuestsToJoin.php
+++ app/Domain/Actions/NPC/FindQuestsToJoin.php
@@ -24,43 +24,15 @@ class FindQuestsToJoin
             return collect();
         }
 
-        $questsPerWeek = $npc->getQuestsPerWeek();
-        $availableCampaignStops = $questsPerWeek - $npc->getCurrentCampaign()->campaignStops()->count();
+        $availableCampaignStops = $this->getAvailableCampaignStops($npc);
 
         if (! $availableCampaignStops > 0) {
             return collect();
         }
 
         $npcLevel = $npc->level();
-
-        // Get quests located in province npc can visit
-        $validContinentIDs = Continent::query()->get()->filter(function (Continent $continent) use ($npcLevel) {
-            return $continent->getBehavior()->getMinLevelRequirement() <= $npcLevel;
-        })->pluck('id')->toArray();
-
-        // If there are over 50 quests, we can safely grab 50 randomly and find some ideal quests to join
-        $quests = Quest::query()->with('sideQuests.minions')->whereHas('province', function (Builder $builder) use ($validContinentIDs) {
-            $builder->whereIn('continent_id', $validContinentIDs);
-        })->inRandomOrder()->limit(50)->get();
-
         $idealDifficulty = ($npcLevel * 2) + 5;
-
-        // Sort quests by average difficulty of side-quest relative to ideal difficulty for npc
-        $orderedQuests = $quests->sortBy(function (Quest $quest) use ($npcLevel, $idealDifficulty) {
-            $sideQuestDifficultyAvg = $quest->sideQuests->average(function (SideQuest $sideQuest) {
-                return $sideQuest->difficulty();
-            });
-            return abs($idealDifficulty - $sideQuestDifficultyAvg);
-        });
-
-        $questsCount = $orderedQuests->count();
-
-        /*
-         * Get a rand int between quests-per-week and total quests available
-         * This will make ideal-difficulty quests more likely to be chosen
-         */
-        $amountOfQuestsToTake = $questsCount <= $availableCampaignStops ? $questsCount : rand($availableCampaignStops, $questsCount);
-        $questsToJoin = $orderedQuests->take($amountOfQuestsToTake)->random($availableCampaignStops);
+        $questsToJoin = $this->getQuestsToJoin($npcLevel, $idealDifficulty, $availableCampaignStops);
 
         $sideQuestsPerQuest = $npc->getSideQuestsPerQuest();
 
@@ -81,5 +53,59 @@ class FindQuestsToJoin
                 'side_quests' =>  $sideQuestsToJoin->values()->all()
             ];
         });
+    }
+
+    /**
+     * @param Squad $npc
+     * @return int
+     */
+    protected function getAvailableCampaignStops(Squad $npc): int
+    {
+        $questsPerWeek = $npc->getQuestsPerWeek();
+        $currentCampaign = $npc->getCurrentCampaign();
+        if ($currentCampaign) {
+            $availableCampaignStops = $questsPerWeek - $currentCampaign->campaignStops()->count();
+        } else {
+            $availableCampaignStops = $questsPerWeek;
+        }
+        return $availableCampaignStops;
+    }
+
+    /**
+     * @param int $npcLevel
+     * @param $idealDifficulty
+     * @param int $availableCampaignStops
+     * @return Builder[]|\Illuminate\Database\Eloquent\Collection|mixed
+     */
+    protected function getQuestsToJoin(int $npcLevel, $idealDifficulty, int $availableCampaignStops)
+    {
+        // Get quests located in province npc can visit
+        $validContinentIDs = Continent::query()->get()->filter(function (Continent $continent) use ($npcLevel) {
+            return $continent->getBehavior()->getMinLevelRequirement() <= $npcLevel;
+        })->pluck('id')->toArray();
+
+        // If there are over 50 quests, we can safely grab 50 randomly and find some ideal quests to join
+        $quests = Quest::query()->with('sideQuests.minions')->whereHas('province', function (Builder $builder) use ($validContinentIDs) {
+            $builder->whereIn('continent_id', $validContinentIDs);
+        })->inRandomOrder()->limit(50)->get();
+
+
+        // Sort quests by average difficulty of side-quest relative to ideal difficulty for npc
+        $orderedQuests = $quests->sortBy(function (Quest $quest) use ($npcLevel, $idealDifficulty) {
+            $sideQuestDifficultyAvg = $quest->sideQuests->average(function (SideQuest $sideQuest) {
+                return $sideQuest->difficulty();
+            });
+            return abs($idealDifficulty - $sideQuestDifficultyAvg);
+        });
+
+        $questsCount = $orderedQuests->count();
+
+        /*
+         * Get a rand int between quests-per-week and total quests available
+         * This will make ideal-difficulty quests more likely to be chosen
+         */
+        $amountOfQuestsToTake = $questsCount <= $availableCampaignStops ? $questsCount : rand($availableCampaignStops, $questsCount);
+        $questsToJoin = $orderedQuests->take($amountOfQuestsToTake)->random($availableCampaignStops);
+        return $questsToJoin;
     }
 }
