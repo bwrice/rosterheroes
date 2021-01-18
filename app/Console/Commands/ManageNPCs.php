f48f75a332de2598c1cc409c5e1be13d95bec049
Brian Rice <bwrice83@gmail.com>
2021-01-18 00:16:14 -0500

    use DispatchAutoManageNPCJobs in ManageNPCs command

--- app/Console/Commands/ManageNPCs.php
+++ app/Console/Commands/ManageNPCs.php
@@ -2,9 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Domain\Actions\NPC\AutoManageNPCCampaign;
-use App\Domain\Models\Squad;
-use App\Jobs\AutoManageNPCHeroesJob;
+use App\Domain\Actions\NPC\DispatchAutoManageNPCJobs;
 use Illuminate\Console\Command;
 
 class ManageNPCs extends Command
@@ -24,25 +22,10 @@ class ManageNPCs extends Command
     protected $description = 'Auto Manage NPC squads';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
+     * @param DispatchAutoManageNPCJobs $dispatchAutoManageNPCJobs
      */
-    public function __construct()
+    public function handle(DispatchAutoManageNPCJobs $dispatchAutoManageNPCJobs)
     {
-        parent::__construct();
-    }
-
-    /**
-     * @param AutoManageNPCCampaign $autoManageNPCCampaign
-     */
-    public function handle(AutoManageNPCCampaign $autoManageNPCCampaign)
-    {
-        $squadNPCs = Squad::query()->npc()->get();
-        $squadNPCs->each(function (Squad $npc) use ($autoManageNPCCampaign) {
-            $autoManageNPCCampaign->execute($npc);
-            AutoManageNPCHeroesJob::dispatch($npc);
-        });
-        $this->info("Auto management started for " . $squadNPCs->count() . " NPCs");
+        $dispatchAutoManageNPCJobs->execute();
     }
 }
