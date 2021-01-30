1f201bf7c5c435a35c1eed05d1b98654a7be02f8
Brian Rice <bwrice83@gmail.com>
2021-01-29 20:56:53 -0500

    pass hero-post-type to FindRecruitmentCamp

--- app/Domain/Actions/NPC/FindRecruitmentCamp.php
+++ app/Domain/Actions/NPC/FindRecruitmentCamp.php
@@ -5,6 +5,7 @@ namespace App\Domain\Actions\NPC;
 
 
 use App\Domain\Models\Continent;
+use App\Domain\Models\HeroPostType;
 use App\Domain\Models\RecruitmentCamp;
 use App\Domain\Models\Shop;
 use App\Domain\Models\Squad;
@@ -15,22 +16,21 @@ class FindRecruitmentCamp
 {
     /**
      * @param Squad $npc
+     * @param HeroPostType $heroPostType
      * @return RecruitmentCamp|null
      */
-    public function execute(Squad $npc)
+    public function execute(Squad $npc, HeroPostType $heroPostType)
     {
         $validContinentIDs = Continent::query()->get()->filter(function (Continent $continent) use ($npc) {
             return $continent->getBehavior()->getMinLevelRequirement() <= $npc->level();
         })->pluck('id')->toArray();
 
-        $cheapestHeroPostTypeIDs = HeroPostTypeFacade::cheapestForSquad($npc)->pluck('id')->toArray();
-
         /** @var RecruitmentCamp|null $recruitmentCamp */
         $recruitmentCamp = RecruitmentCamp::query()
             ->whereHas('province', function (Builder $builder) use ($validContinentIDs) {
                 return $builder->whereIn('continent_id', $validContinentIDs);
-            })->whereHas('heroPostTypes', function (Builder $builder) use ($cheapestHeroPostTypeIDs) {
-                return $builder->whereIn('id', $cheapestHeroPostTypeIDs);
+            })->whereHas('heroPostTypes', function (Builder $builder) use ($heroPostType) {
+                return $builder->where('id', '=', $heroPostType->id);
             })->inRandomOrder()
             ->first();
         return $recruitmentCamp;
--- tests/Feature/FindRecruitmentCampTest.php
+++ tests/Feature/FindRecruitmentCampTest.php
@@ -6,7 +6,6 @@ use App\Domain\Actions\NPC\FindRecruitmentCamp;
 use App\Domain\Models\Continent;
 use App\Domain\Models\HeroPostType;
 use App\Domain\Models\Province;
-use App\Facades\HeroPostTypeFacade;
 use App\Factories\Models\RecruitmentCampFactory;
 use App\Factories\Models\SquadFactory;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -31,9 +30,6 @@ class FindRecruitmentCampTest extends TestCase
      */
     public function it_wont_return_recruitment_camp_from_invalid_continent()
     {
-        $heroPostTypes = HeroPostType::all()->shuffle();
-        $campPostTypes = collect();
-
         $invalidContinent = Continent::query()->where('name', '=', Continent::DEMAUXOR)->first();
         $invalidProvinces = Province::query()
             ->where('continent_id', '=', $invalidContinent->id)
@@ -41,26 +37,17 @@ class FindRecruitmentCampTest extends TestCase
             ->take(20)
             ->get();
 
-        /*
-         * We'll pull some random hero-post types for the camps to have
-         * and to mock the return from the HeroPostTypeFacade
-         */
-        for ($i = 1; $i <= rand(2, 3); $i++) {
-            $campPostTypes->push($heroPostTypes->shift());
-        }
-
+        $heroPostType = HeroPostType::query()->inRandomOrder()->first();
         $recruitmentCamp = RecruitmentCampFactory::new()
-            ->withHeroPostTypes($campPostTypes);
+            ->withHeroPostTypes(collect([$heroPostType]));
         $invalidCamps = collect();
         for ($i = 1; $i <= 10; $i++) {
             $invalidCamps->push($recruitmentCamp->withProvinceID($invalidProvinces->random()->id)->create());
         }
 
-        HeroPostTypeFacade::shouldReceive('cheapestForSquad')->andReturn($campPostTypes);
-
         $npc = SquadFactory::new()->atProvince(Province::getStarting()->id)->create();
 
-        $recruitmentCamp = $this->getDomainAction()->execute($npc);
+        $recruitmentCamp = $this->getDomainAction()->execute($npc, $heroPostType);
         if ($recruitmentCamp) {
             $this->assertFalse(in_array($recruitmentCamp->id, $invalidCamps->pluck('id')->toArray()));
         } else {
@@ -80,16 +67,9 @@ class FindRecruitmentCampTest extends TestCase
             ->take(20)
             ->get();
 
-        $cheatPostTypes = HeroPostType::all()->shuffle();
-        $campPostTypes = collect();
-
-        /*
-         * We'll pull some random hero-post types for the camps to have
-         * and to mock the rest as the return from the HeroPostTypeFacade
-         */
-        for ($i = 1; $i <= rand(2, 3); $i++) {
-            $campPostTypes->push($cheatPostTypes->shift());
-        }
+        $campPostTypes = HeroPostType::all()->shuffle();
+        // Remove the hero-post-type we're going to pass into action
+        $heroPostType = $campPostTypes->shift();
 
         $recruitmentCamp = RecruitmentCampFactory::new()
             ->withHeroPostTypes($campPostTypes);
@@ -98,11 +78,9 @@ class FindRecruitmentCampTest extends TestCase
             $invalidCamps->push($recruitmentCamp->withProvinceID($validProvinces->random()->id)->create());
         }
 
-        HeroPostTypeFacade::shouldReceive('cheapestForSquad')->andReturn($campPostTypes);
-
         $npc = SquadFactory::new()->atProvince(Province::getStarting()->id)->create();
 
-        $recruitmentCamp = $this->getDomainAction()->execute($npc);
+        $recruitmentCamp = $this->getDomainAction()->execute($npc, $heroPostType);
         if ($recruitmentCamp) {
             $this->assertFalse(in_array($recruitmentCamp->id, $invalidCamps->pluck('id')->toArray()));
         } else {
