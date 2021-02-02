3471fb1ddef81699a04f7418203f80a99cc75ca0
Brian Rice <bwrice83@gmail.com>
2021-02-01 20:51:06 -0500

    create FindHeroToRecruit domain action

---
+++ app/Domain/Actions/NPC/FindHeroToRecruit.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Domain\Actions\NPC;
+
+
+use App\Domain\Models\HeroPostType;
+use App\Domain\Models\Squad;
+use App\Facades\HeroPostTypeFacade;
+
+class FindHeroToRecruit
+{
+    const NPC_EXTRA_GOLD = 25000;
+
+    protected FindRecruitmentCamp $findRecruitmentCamp;
+
+    public function __construct(FindRecruitmentCamp $findRecruitmentCamp)
+    {
+        $this->findRecruitmentCamp = $findRecruitmentCamp;
+    }
+
+    public function execute(Squad $npc)
+    {
+        /** @var HeroPostType $heroPostType */
+        $heroPostType = HeroPostTypeFacade::cheapestForSquad($npc)->random();
+        $recruitmentCost = $heroPostType->getRecruitmentCost($npc);
+
+        if ($recruitmentCost > ($npc->gold - self::NPC_EXTRA_GOLD)) {
+            return null;
+        }
+
+        $recruitmentCamp = $this->findRecruitmentCamp->execute($npc, $heroPostType);
+
+    }
+}
