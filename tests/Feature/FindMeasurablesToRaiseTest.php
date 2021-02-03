816999264dafd399c9cbdf6f1e21f9bd24918aa4
Brian Rice <bwrice83@gmail.com>
2021-02-03 14:42:57 -0500

    add tests for FindMeasurablesToRaise npc action

---
+++ tests/Feature/FindMeasurablesToRaiseTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Domain\Actions\NPC\FindMeasurablesToRaise;
+use App\Domain\Models\Measurable;
+use App\Domain\Models\MeasurableType;
+use App\Factories\Models\HeroFactory;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FindMeasurablesToRaiseTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @return FindMeasurablesToRaise
+     */
+    protected function getDomainAction()
+    {
+        return app(FindMeasurablesToRaise::class);
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_return_an_empty_collection_if_not_enough_experience_to_raise_any_measurables()
+    {
+        $hero = HeroFactory::new()->withMeasurables()->create();
+        $strength = $hero->getMeasurable(MeasurableType::STRENGTH);
+        $strength->amount_raised = 100;
+        $strength->save();
+
+        $hero = $hero->fresh();
+
+        $this->assertLessThanOrEqual(0, $hero->availableExperience());
+
+        $measurablesToRaise = $this->getDomainAction()->execute($hero);
+        $this->assertTrue($measurablesToRaise->isEmpty());
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_return_every_measurable_with_a_positive_raise_amount_if_enough_experience_available()
+    {
+        $hero = HeroFactory::new()->withMeasurables()->create();
+
+        $hero->squad->experience += 9999;
+        $hero->squad->save();
+
+        $measurablesToRaise = $this->getDomainAction()->execute($hero);
+        MeasurableType::all()->each(function (MeasurableType $measurableType) use ($measurablesToRaise) {
+            $match = $measurablesToRaise->first(function ($raiseArray) use ($measurableType) {
+                /** @var Measurable $measurable */
+                $measurable = $raiseArray['measurable'];
+                return $measurable->measurableType->name === $measurableType->name;
+            });
+            $this->assertNotNull($match, "No raise array found for " . $measurableType->name);
+            $this->assertGreaterThan(0, $match['amount']);
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_not_return_raise_amounts_more_than_the_available_experience_of_the_hero()
+    {
+        $hero = HeroFactory::new()->withMeasurables()->create();
+
+        $hero->squad->experience += rand(500, 9999);
+        $hero->squad->save();
+
+        $availableExperience = $hero->availableExperience();
+
+        $measurablesToRaise = $this->getDomainAction()->execute($hero);
+
+        $sumOfCost = $measurablesToRaise->sum(function ($raiseArray) {
+            /** @var Measurable $measurable */
+            $measurable = $raiseArray['measurable'];
+            return $measurable->getCostToRaise($raiseArray['amount']);
+        });
+
+        $this->assertLessThanOrEqual($availableExperience, $sumOfCost);
+    }
+}
